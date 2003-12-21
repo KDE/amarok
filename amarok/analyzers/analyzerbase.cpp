bce53c472fea0164419b312346d69b058e8d2af9
Mark Kretschmann <kretschmann@kde.org>
2003-12-21 17:59:18 +0000

    Possible fix for the segfault problem in AnalyzerBase::interpolate().
    svn path=/trunk/kdeextragear-1/amarok/; revision=274224

--- amarok/analyzers/analyzerbase.cpp
+++ amarok/analyzers/analyzerbase.cpp
@@ -148,31 +148,34 @@ void AnalyzerBase::mouseReleaseEvent( QMouseEvent * )
 
 void AnalyzerBase::interpolate( std::vector<float> *oldVec, std::vector<float> &newVec ) const
 {
-    uint newSize = newVec.size(); //vector::size() is O(1)
-
-    //necessary? code bloat if not
-    if( newSize == oldVec->size() ) { newVec = *oldVec; return; }
-
-    double pos = 0.0;
-    double step = static_cast<double>( oldVec->size() ) / newSize;
-
-    for ( uint i = 0; i < newSize; ++i, pos += step )
-    {
-        double error = pos - floor( pos );
-        unsigned long offset = static_cast<unsigned long>( pos );
-
-        unsigned long indexLeft = offset + 0;
-
-        if ( indexLeft >= oldVec->size() )
-            indexLeft = oldVec->size() - 1;
-
-        unsigned long indexRight = offset + 1;
-
-        if ( indexRight >= oldVec->size() )
-            indexRight = oldVec->size() - 1;
-
-        newVec[i] = (*oldVec)[indexLeft] * ( 1.0 - error ) +
-                    (*oldVec)[indexRight] * error;
+    if ( oldVec->size() )
+    {    
+        uint newSize = newVec.size(); //vector::size() is O(1)
+    
+        //necessary? code bloat if not
+        if( newSize == oldVec->size() ) { newVec = *oldVec; return; }
+    
+        double pos = 0.0;
+        double step = static_cast<double>( oldVec->size() ) / newSize;
+    
+        for ( uint i = 0; i < newSize; ++i, pos += step )
+        {
+            double error = pos - floor( pos );
+            ulong offset = static_cast<unsigned long>( pos );
+    
+            ulong indexLeft = offset + 0;
+    
+            if ( indexLeft >= oldVec->size() )
+                indexLeft = oldVec->size() - 1;
+    
+            ulong indexRight = offset + 1;
+    
+            if ( indexRight >= oldVec->size() )
+                indexRight = oldVec->size() - 1;
+    
+            newVec[i] = (*oldVec)[indexLeft] * ( 1.0 - error ) +
+                        (*oldVec)[indexRight] * error;
+        }
     }
 }
 
