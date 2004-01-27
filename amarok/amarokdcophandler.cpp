bd409cfe699afaba5a919e679d59be43edd2b155
Max Howell <max.howell@methylblue.com>
2004-01-27 21:03:52 +0000

    Reduce compile dependencies
    svn path=/trunk/kdeextragear-1/amarok/; revision=283075

--- amarok/amarokdcophandler.cpp
+++ amarok/amarokdcophandler.cpp
@@ -1,12 +1,11 @@
+//author Berkus
+//FIXME get berkus to license this under something!
+
 #include "amarokdcophandler.h"
-#include "amarokslider.h"
 #include "engine/enginebase.h"
 #include "playerapp.h"
-#include "playerwidget.h"
 
 #include <dcopclient.h>
-
-#include <kdebug.h>
 
 AmarokDcopHandler::AmarokDcopHandler()
    : DCOPObject( "player" )
@@ -79,8 +78,8 @@ void AmarokDcopHandler::seek(int s)
 
 int AmarokDcopHandler::trackCurrentTime()
 {
-   //FIXME must be a more reliable way to get the currentTime
-   return pApp->m_pPlayerWidget->m_pSlider->value();
+   //return time in seconds
+   return pApp->m_pEngine->position() / 1000;
 }
 
 void AmarokDcopHandler::addMedia(const KURL &url)
