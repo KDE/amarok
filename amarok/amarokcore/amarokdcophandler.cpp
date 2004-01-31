c9431ee1dde9e4303bafe8982b4ee120d0e0f080
Stanislav Karchebny <stanislav.karchebny@kdemail.net>
2004-01-31 23:06:21 +0000

    * Add license as asked
    svn path=/trunk/kdeextragear-1/amarok/; revision=284105

--- amarok/amarokcore/amarokdcophandler.cpp
+++ amarok/amarokcore/amarokdcophandler.cpp
@@ -1,5 +1,19 @@
-//author Berkus
-//FIXME get berkus to license this under something!
+/***************************************************************************
+                          amarokdcophandler.cpp  -  DCOP Implementation
+                             -------------------
+    begin                : Sat Oct 11 2003
+    copyright            : (C) 2003 by Stanislav Karchebny
+    email                : berkus@users.sf.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "amarokdcophandler.h"
 #include "engine/enginebase.h"
--- amarok/amarokcore/amarokdcophandler.h
+++ amarok/amarokcore/amarokdcophandler.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+                          amarokdcophandler.h  -  DCOP Implementation
+                             -------------------
+    begin                : Sat Oct 11 2003
+    copyright            : (C) 2003 by Stanislav Karchebny
+    email                : berkus@users.sf.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef AMAROK_DCOP_HANDLER_H
 #define AMAROK_DCOP_HANDLER_H
 
