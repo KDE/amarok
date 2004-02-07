13b095ee22b06bf8b415280d0c97a228b0555194
Max Howell <max.howell@methylblue.com>
2004-02-07 19:01:01 +0000

    * We moved kconfigXT stuff to amarokcore, but because amarokconfig.h was generated by kconfig_compiler it wasn't removed from the src root when people cvsup'd. Thus this dummy file includes amarokcore/amarokconfig.h so that people don't get confusing errors. * In a few weeks/months we can remove this file and it'll remove it from people's checkouts when they cvsup.
    svn path=/trunk/kdeextragear-1/amarok/; revision=285910

---
+++ amarok/amarokconfig.h
@@ -0,0 +1,7 @@
+//This is a dummy include file
+//remove it in a few months
+//it was required as we used to have amarokconfig.h in this directory
+//but we moved it and since it was a generated file it wasn't removed
+//when people cvsup'ed
+
+#include "amarokcore/amarokconfig.h"
