b2ef8425ede56e87df1e7078e87a788c3864dd7a
Mark Kretschmann <kretschmann@kde.org>
2004-02-19 19:15:09 +0000

    Add files.
    svn path=/trunk/kdeextragear-1/amarok/; revision=289472

---
+++ amarok/loader/loader.cpp
@@ -0,0 +1,58 @@
+/***************************************************************************
+                          loader.cpp  -  description
+                             -------------------
+    begin                : 2004/02/19
+    copyright            : (C) 2004 by Mark Kretschmann
+    email                : markey@web.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <loader.h>
+#include <splash.h>
+
+#include <qprocess.h>
+
+
+Loader::Loader( int& argc, char** argv )
+    : QApplication( argc, argv )
+    , m_pOsd( new OSDWidget )
+{
+    qDebug( "[Loader::Loader()]" );
+
+    m_pOsd->showSplash( "/opt/kde3/share/apps/amarok/images/logo_splash.png" );
+        
+    QProcess* proc = new QProcess( this );
+    proc->addArgument( "amarok" );
+    proc->start();
+    
+    connect( proc, SIGNAL( processExited() ), this, SLOT( loaded() ) );    
+}
+
+
+Loader::~Loader()
+{
+    delete m_pOsd;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE METHODS
+////////////////////////////////////////////////////////////////////////////////
+    
+//SLOT
+void Loader::loaded()
+{
+    qDebug( "[Loader::loaded()]" );
+    quit();
+}
+
+
+#include "loader.moc"
---
+++ amarok/loader/loader.h
@@ -0,0 +1,44 @@
+/***************************************************************************
+                          loader.h  -  description
+                             -------------------
+    begin                : 2004/02/19
+    copyright            : (C) 2004 by Mark Kretschmann
+    email                : markey@web.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef LOADER_H
+#define LOADER_H
+
+#include <qapplication.h>    //baseclass
+
+class OSDWidget;
+
+
+class Loader : public QApplication
+{
+    Q_OBJECT
+    
+    public:
+        Loader( int& argc, char** argv );
+        ~Loader();
+        
+    private slots:
+        void loaded();
+        
+    private:
+        OSDWidget* m_pOsd;
+        
+// ATTRIBUTES ------
+};
+
+
+#endif
