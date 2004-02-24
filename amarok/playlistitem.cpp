74e80ffceb117e65029863838f00d9d2f0d8c947
Mark Kretschmann <kretschmann@kde.org>
2004-02-24 07:31:03 +0000

    ..and activated it again, since it didn't really help that much and had funny side-effects. Quick, get me my prozac.
    svn path=/trunk/kdeextragear-1/amarok/; revision=290827

--- amarok/playlistitem.cpp
+++ amarok/playlistitem.cpp
@@ -68,22 +68,18 @@ PlaylistItem::~PlaylistItem()
 
 MetaBundle PlaylistItem::metaBundle()
 {
-    //<markey> I've currently deactivated Audioproperties::Accurate access here, to see if it helps
-    //with the sound dropout issue on crossfading
-    
     //Do this everytime to save cost of storing int for length/samplerate/bitrate
     //This function isn't called often (on play request), but playlists can contain
     //thousands of items. So favor saving memory over CPU.
-//     TagLib::FileRef f( m_url.path().local8Bit(), true, TagLib::AudioProperties::Accurate );
+    TagLib::FileRef f( m_url.path().local8Bit(), true, TagLib::AudioProperties::Accurate );
 
     //FIXME hold a small cache of metabundles?
     //then return by reference
-    MetaBundle bundle( this, 0 );
-//     MetaBundle bundle( this, f.isNull() ? 0 : f.audioProperties() );
+    MetaBundle bundle( this, f.isNull() ? 0 : f.audioProperties() );
 
     //just set it as we just did an accurate pass
-//     setText(  9, bundle.prettyLength()  );
-//     setText( 10, bundle.prettyBitrate() );
+    setText(  9, bundle.prettyLength()  );
+    setText( 10, bundle.prettyBitrate() );
 
     return bundle;
 }
