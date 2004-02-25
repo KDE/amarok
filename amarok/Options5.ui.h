c5c1b06f31b42c22573ecd77c2082c446402704e
Frederik Holljen <fh@ez.no>
2004-02-25 15:25:30 +0000

    Add header for new file CVS_SILENT
    svn path=/trunk/kdeextragear-1/amarok/; revision=291324

--- amarok/Options5.ui.h
+++ amarok/Options5.ui.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+begin                : 2004/02/25
+copyright            : (C) Frederik Holljen
+email                : fh@ez.no
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 /****************************************************************************
 ** ui.h extension file, included from the uic-generated form implementation.
 **
