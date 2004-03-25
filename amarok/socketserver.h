44b5a5478d6eeee0f5fb62a34111e29ead87fbdc
Max Howell <max.howell@methylblue.com>
2004-03-25 14:02:12 +0000

    Always build the VisServer, wasn't much point to making it optional as Markey said.
    svn path=/trunk/kdeextragear-1/amarok/; revision=298517

--- amarok/socketserver.h
+++ amarok/socketserver.h
@@ -5,10 +5,12 @@
 #ifndef VIS_SOCKETSERVER_H
 #define VIS_SOCKETSERVER_H
 
-//uncomment below to activate this stuff
-//<markey> I don't see why this should be optional at all. It doesn't break anything, so let's remove the #ifdef
 
-//#define AMK_NEW_VIS_SYSTEM
+//TODO move loader server into here too so this file isn't named so badly
+//TODO use only one socket?
+
+
+#define AMK_NEW_VIS_SYSTEM
 
 #include <qserversocket.h>
 
