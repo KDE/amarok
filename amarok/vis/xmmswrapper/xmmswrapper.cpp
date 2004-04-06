36dd798f590c80c02c147f30311a0c5f92584041
Mark Kretschmann <kretschmann@kde.org>
2004-04-06 06:04:46 +0000

    Fix crashbug while listing plugins.
    svn path=/trunk/kdeextragear-1/amarok/; revision=301665

--- amarok/vis/xmmswrapper/xmmswrapper.cpp
+++ amarok/vis/xmmswrapper/xmmswrapper.cpp
@@ -14,12 +14,12 @@
 #include "xmmswrapper.h"
 
 #include <iostream>
-#include <list>
+#include <list> 
 //#include <string> in header
 #include <vector>
 
 #define SHARED_LIB_EXT ".so"
-#define PLUGIN_PATH "/usr/lib/xmms/Visualization/"
+#define PLUGIN_PATH "/usr/X11R6/lib/xmms/Visualization/"
 
 
 //TODO pure c would give a smaller binary
@@ -32,28 +32,26 @@
 
 GtkWidget dummy;
 GtkWidget *equalizerwin = &dummy; //required by msa visplugin
-GtkWidget *playlistwin  = &dummy; //required by msa visplugin
-GtkWidget *mainwin      = &dummy; //required by msa visplugin
+GtkWidget *playlistwin = &dummy; //required by msa visplugin
+GtkWidget *mainwin = &dummy; //required by msa visplugin
 
 
 
-int  tryConnect();
+int tryConnect();
 void vis_disable_plugin( VisPlugin *vp ) {} //seems to be a required function (see loadVis() )
 
 std::string socketpath; //global
 
 
 int
-main( int argc, char** argv )
-{
+main( int argc, char** argv ) {
     //Hi! You can tell I haven't done any c programming in a while can't you! ;-)
 
     //TODO fork after connection?
 
     std::string plugin;
 
-    if( argc == 1 )
-    {
+    if ( argc == 1 ) {
         std::list<std::string> list;
 
         //scan plugins
@@ -63,45 +61,43 @@ main( int argc, char** argv )
         struct stat statbuf;
 
         dir = opendir( dirname );
-        if( !dir ) { std::cerr << "Please edit the PLUGIN_PATH in xmmswrapper.cpp\n"; exit( 1 ); }
+        if ( !dir ) { std::cerr << "Please edit the PLUGIN_PATH in xmmswrapper.cpp\n"; exit( 1 ); }
+        std::cout << "checkpoint 1\n";
 
-        while( (ent = readdir( dir )) != NULL )
-        {
-            std::string filename  = ent->d_name;
-            std::string extension = filename.substr( filename.find_last_of( '.' ) );
-            std::string fullpath  = dirname + filename;
+        while ( ( ent = readdir( dir ) ) != NULL ) {
+            std::string filename = ent->d_name;
+            int index = filename.find_last_of( '.' );
+            if ( index == std::string::npos ) continue;
+            std::string extension = filename.substr( index );
+            std::string fullpath = dirname + filename;
 
-            if( !stat( fullpath.c_str(), &statbuf ) &&
-                S_ISREG( statbuf.st_mode ) &&
-                extension == SHARED_LIB_EXT ) list.push_back( filename );
+            if ( !stat( fullpath.c_str(), &statbuf )
+                    && S_ISREG( statbuf.st_mode )
+                    && extension == SHARED_LIB_EXT )
+                list.push_back( filename );
         }
 
-        closedir(dir);
+        closedir( dir );
 
         std::cout << "Please select a plugin: \n";
 
         std::vector<std::string> v( list.size() );
         std::copy( list.begin(), list.end(), v.begin() );
 
-        for( uint n = 0; n < v.size(); ++n )
-        {
-            std::cout << n+1 << ": " << v[n] << '\n';
+        for ( uint n = 0; n < v.size(); ++n ) {
+            std::cout << n + 1 << ": " << v[ n ] << '\n';
         }
 
-        char c[10];
-
+        char c[ 10 ];
         std::cin >> c;
-
         uint selection = atoi( c );
+        if ( selection > v.size() ) return 1;
 
-        if( selection > v.size() ) return 1;
-
-        plugin = v[selection-1];
-    }
-    else plugin = argv[0];
+        plugin = v[ selection - 1 ];
+    } else plugin = argv[ 0 ];
 
 
-    socketpath  = getenv( "HOME" );
+    socketpath = getenv( "HOME" );
     socketpath += "/.kde/share/apps/amarok"; //TODO get this with "kconfig --type data"
     socketpath += "/visualization_socket";
 
@@ -111,99 +107,94 @@ main( int argc, char** argv )
 
     XmmsWrapper wrap( plugin );
 
-    int    sockfd;
-    int    nbytes = 0;
+    int sockfd;
+    int nbytes = 0;
     const int nch = 1; //no of channels?
 
     sockfd = tryConnect();
 
-    while( (sockfd) != -1 )
-    {
+    while ( ( sockfd ) != -1 ) {
         gtk_main_iteration_do( FALSE );
         usleep( 20 * 1000 );
 
-        if( wrap.renderPCM() )
-        {
-            float  float_data[512];
-            gint16 pcm_data[2][512];
+        if ( wrap.renderPCM() ) {
+            float float_data[ 512 ];
+            gint16 pcm_data[ 2 ][ 512 ];
             memset( pcm_data, 0, 1024 );
 
             send( sockfd, "PCM", 4, 0 );
-            nbytes = recv( sockfd, float_data, 512*sizeof(float), 0 );
+            nbytes = recv( sockfd, float_data, 512 * sizeof( float ), 0 );
 
 
             //NOTE we times by 1<<14 rather than 1<<15 (maximum value of signed 16bit)
             //     this is because values of pcm data tend to range 0-2 (although there
             //     is no theoretical maximum.
 
-            for( uint x = 0; x < 512; ++x )
-            {
-                pcm_data[0][x] = gint16(float_data[x] * (1 << 14));
+            for ( uint x = 0; x < 512; ++x ) {
+                pcm_data[ 0 ][ x ] = gint16( float_data[ x ] * ( 1 << 14 ) );
             }
 
-            wrap.vis()->render_pcm( pcm_data );
-/*
-            if (wrap.vis()->num_pcm_chs_wanted == 1)
-            {
-                gint16 mono_pcm[2][512];
-                calc_mono_pcm(mono_pcm, pcm_data, nch);
-                wrap.vis()->render_pcm( mono_pcm );
-            }
-            else //nch == 2
-            {
-                gint16 stereo_pcm[2][512];
-                calc_stereo_pcm(stereo_pcm, pcm_data, nch);
-                wrap.vis()->render_pcm( stereo_pcm );
-            }
-*/
+            wrap.vis() ->render_pcm( pcm_data );
+            /*
+                        if (wrap.vis()->num_pcm_chs_wanted == 1)
+                        {
+                            gint16 mono_pcm[2][512];
+                            calc_mono_pcm(mono_pcm, pcm_data, nch);
+                            wrap.vis()->render_pcm( mono_pcm );
+                        }
+                        else //nch == 2
+                        {
+                            gint16 stereo_pcm[2][512];
+                            calc_stereo_pcm(stereo_pcm, pcm_data, nch);
+                            wrap.vis()->render_pcm( stereo_pcm );
+                        }
+            */
         }
 
-        if( wrap.renderFFT() ) //NOTE xmms has no else
+        if ( wrap.renderFFT() )  //NOTE xmms has no else
         {
             //TODO check float and gfloat are the same thing!
             //TODO do second channel as mirror for the moment
 
-            float float_data[512];
-            gint16 pcm_data[2][512];
-            gint16 fft_data[2][256];
+            float float_data[ 512 ];
+            gint16 pcm_data[ 2 ][ 512 ];
+            gint16 fft_data[ 2 ][ 256 ];
             memset( fft_data, 0, 512 );
 
             send( sockfd, "PCM", 4, 0 );
-            nbytes = recv( sockfd, float_data, 512*sizeof(float), 0 );
+            nbytes = recv( sockfd, float_data, 512 * sizeof( float ), 0 );
 
-            for( uint x = 0; x < 512; ++x )
-            {
-                pcm_data[0][x] = gint16(float_data[x] * (1 << 14));
+            for ( uint x = 0; x < 512; ++x ) {
+                pcm_data[ 0 ][ x ] = gint16( float_data[ x ] * ( 1 << 14 ) );
             }
 
             static fft_state *state = NULL;
-            gfloat tmp_out[257];
+            gfloat tmp_out[ 257 ];
 
-            if( !state ) state = fft_init();
+            if ( !state ) state = fft_init();
 
-            fft_perform( pcm_data[0], tmp_out, state );
+            fft_perform( pcm_data[ 0 ], tmp_out, state );
 
-            for( uint i = 0; i < 256; i++)
-            {
-                fft_data[0][i] = fft_data[1][i] = ((gint)sqrt(tmp_out[i + 1])) >> 8;
+            for ( uint i = 0; i < 256; i++ ) {
+                fft_data[ 0 ][ i ] = fft_data[ 1 ][ i ] = ( ( gint ) sqrt( tmp_out[ i + 1 ] ) ) >> 8;
             }
 
 
-            wrap.vis()->render_freq( fft_data );
-/*
-            if (wrap.vis()->num_freq_chs_wanted == 1)
-            {
-                gint16 mono_freq[2][256];
-                calc_mono_freq(mono_freq, pcm_data, nch);
-                wrap.vis()->render_freq(mono_freq);
-            }
-            else
-            {
-                gint16 stereo_freq[2][256];
-                calc_stereo_freq(stereo_freq, pcm_data, nch);
-                wrap.vis()->render_freq(stereo_freq);
-            }
-*/
+            wrap.vis() ->render_freq( fft_data );
+            /*
+                        if (wrap.vis()->num_freq_chs_wanted == 1)
+                        {
+                            gint16 mono_freq[2][256];
+                            calc_mono_freq(mono_freq, pcm_data, nch);
+                            wrap.vis()->render_freq(mono_freq);
+                        }
+                        else
+                        {
+                            gint16 stereo_freq[2][256];
+                            calc_stereo_freq(stereo_freq, pcm_data, nch);
+                            wrap.vis()->render_freq(stereo_freq);
+                        }
+            */
         }
     }
 
@@ -211,21 +202,18 @@ main( int argc, char** argv )
 }
 
 int
-tryConnect()
-{
+tryConnect() {
     //try to connect to the LoaderServer
     int fd = socket( AF_UNIX, SOCK_STREAM, 0 );
 
-    if( fd != -1 )
-    {
+    if ( fd != -1 ) {
 
         struct sockaddr_un local;
 
-        strcpy( &local.sun_path[0], socketpath.c_str() );
+        strcpy( &local.sun_path[ 0 ], socketpath.c_str() );
         local.sun_family = AF_UNIX;
 
-        if( connect( fd, (struct sockaddr*) &local, sizeof( local ) ) == -1 )
-        {
+        if ( connect( fd, ( struct sockaddr* ) & local, sizeof( local ) ) == -1 ) {
             close ( fd );
             fd = -1;
         }
@@ -236,47 +224,42 @@ tryConnect()
 
 
 
-XmmsWrapper::XmmsWrapper( const std::string &plugin )
-{
+XmmsWrapper::XmmsWrapper( const std::string &plugin ) {
     std::cout << "[amK] loading xmms plugin: " << plugin << '\n';
 
     std::string
-    path  = PLUGIN_PATH;
+    path = PLUGIN_PATH;
     path += plugin;
 
     { //<load plugin>
 
         void *h;
-        void *(*gpi) (void);
+        void *( *gpi ) ( void );
 
-        if ((h = dlopen( path.c_str(), RTLD_NOW )) == NULL)
-        {
-                std::cout << dlerror() << "\n";
-                return;
+        if ( ( h = dlopen( path.c_str(), RTLD_NOW ) ) == NULL ) {
+            std::cout << dlerror() << "\n";
+            return ;
         }
 
-        if ((gpi = (void*(*)())dlsym(h, "get_vplugin_info")) != NULL)
-        {
-                VisPlugin *p = (VisPlugin*)gpi();
-                p->handle = h;
-                p->filename = strdup( path.c_str() );
-                p->xmms_session = 0;//ctrlsocket_get_session_id(); //FIXME
-                p->disable_plugin = vis_disable_plugin; //FIXME what is this symbol?
+        if ( ( gpi = ( void * ( * ) () ) dlsym( h, "get_vplugin_info" ) ) != NULL ) {
+            VisPlugin * p = ( VisPlugin* ) gpi();
+            p->handle = h;
+            p->filename = strdup( path.c_str() );
+            p->xmms_session = 0; //ctrlsocket_get_session_id(); //FIXME
+            p->disable_plugin = vis_disable_plugin; //FIXME what is this symbol?
 
-                m_vis = p;
-        }
-        else { dlclose(h); return; }
+            m_vis = p;
+        } else { dlclose( h ); return ; }
 
     } //</load plugin>
 
 
-    if(m_vis->init) { std::cout << "[amK] init()\n"; m_vis->init(); }
-    if(m_vis->playback_start)  { std::cout << "[amK] start()\n"; m_vis->playback_start(); }
+    if ( m_vis->init ) { std::cout << "[amK] init()\n"; m_vis->init(); }
+    if ( m_vis->playback_start ) { std::cout << "[amK] start()\n"; m_vis->playback_start(); }
 }
 
 
-XmmsWrapper::~XmmsWrapper()
-{
+XmmsWrapper::~XmmsWrapper() {
     dlclose( m_vis );
 
     std::cout << "[amK] ~\n";
@@ -289,82 +272,75 @@ XmmsWrapper::~XmmsWrapper()
 
 //NOTE as yet, these functions are a little mysterious to me
 
-static void calc_stereo_pcm(gint16 dest[2][512], gint16 src[2][512], gint nch)
-{
-        memcpy(dest[0], src[0], 512 * sizeof(gint16));
-        if(nch == 1)
-                memcpy(dest[1], src[0], 512 * sizeof(gint16));
-        else
-                memcpy(dest[1], src[1], 512 * sizeof(gint16));
+static void calc_stereo_pcm( gint16 dest[ 2 ][ 512 ], gint16 src[ 2 ][ 512 ], gint nch ) {
+    memcpy( dest[ 0 ], src[ 0 ], 512 * sizeof( gint16 ) );
+    if ( nch == 1 )
+        memcpy( dest[ 1 ], src[ 0 ], 512 * sizeof( gint16 ) );
+    else
+        memcpy( dest[ 1 ], src[ 1 ], 512 * sizeof( gint16 ) );
 }
 
-static void calc_mono_pcm(gint16 dest[2][512], gint16 src[2][512], gint nch)
-{
-        gint i;
-        gint16 *d, *sl, *sr;
+static void calc_mono_pcm( gint16 dest[ 2 ][ 512 ], gint16 src[ 2 ][ 512 ], gint nch ) {
+    gint i;
+    gint16 *d, *sl, *sr;
 
-        if(nch == 1)
-                memcpy(dest[0], src[0], 512 * sizeof(gint16));
-        else
-        {
-                d = dest[0];
-                sl = src[0];
-                sr = src[1];
-                for(i = 0; i < 512; i++)
-                {
-                        *(d++) = (*(sl++) + *(sr++)) >> 1;
-                }
+    if ( nch == 1 )
+        memcpy( dest[ 0 ], src[ 0 ], 512 * sizeof( gint16 ) );
+    else {
+        d = dest[ 0 ];
+        sl = src[ 0 ];
+        sr = src[ 1 ];
+        for ( i = 0; i < 512; i++ ) {
+            *( d++ ) = ( *( sl++ ) + *( sr++ ) ) >> 1;
         }
+    }
 }
 
-static void calc_freq(gint16 *dest, gint16 *src)
-{
-/* FIXME
-        static fft_state *state = NULL;
-        gfloat tmp_out[257];
-        gint i;
-
-        if(!state)
-                state = fft_init();
-
-        fft_perform(src,tmp_out,state);
-
-        for(i = 0; i < 256; i++)
-                dest[i] = ((gint)sqrt(tmp_out[i + 1])) >> 8;
-*/
+static void calc_freq( gint16 *dest, gint16 *src ) {
+    /* FIXME
+            static fft_state *state = NULL;
+            gfloat tmp_out[257];
+            gint i;
+     
+            if(!state)
+                    state = fft_init();
+     
+            fft_perform(src,tmp_out,state);
+     
+            for(i = 0; i < 256; i++)
+                    dest[i] = ((gint)sqrt(tmp_out[i + 1])) >> 8;
+    */
 }
 
-static void calc_mono_freq(gint16 dest[2][256], gint16 src[2][512], gint nch)
-{
-/* FIXME
-        gint i;
-        gint16 *d, *sl, *sr, tmp[512];
-
-        if(nch == 1)
-                calc_freq(dest[0], src[0]);
-        else
-        {
-                d = tmp;
-                sl = src[0];
-                sr = src[1];
-                for(i = 0; i < 512; i++)
-                {
-                        *(d++) = (*(sl++) + *(sr++)) >> 1;
-                }
-                calc_freq(dest[0], tmp);
-        }
-*/
+static void calc_mono_freq( gint16 dest[ 2 ][ 256 ], gint16 src[ 2 ][ 512 ], gint nch ) {
+    /* FIXME
+            gint i;
+            gint16 *d, *sl, *sr, tmp[512];
+     
+            if(nch == 1)
+                    calc_freq(dest[0], src[0]);
+            else
+            {
+                    d = tmp;
+                    sl = src[0];
+                    sr = src[1];
+                    for(i = 0; i < 512; i++)
+                    {
+                            *(d++) = (*(sl++) + *(sr++)) >> 1;
+                    }
+                    calc_freq(dest[0], tmp);
+            }
+    */
 }
 
-static void calc_stereo_freq(gint16 dest[2][256], gint16 src[2][512], gint nch)
-{
-/*
-FIXME
-        calc_freq(dest[0], src[0]);
-
-        if(nch == 2)
-                calc_freq(dest[1], src[1]);
-        else
-                memcpy(dest[1], dest[0], 256 * sizeof(gint16));
-*/
+static void calc_stereo_freq( gint16 dest[ 2 ][ 256 ], gint16 src[ 2 ][ 512 ], gint nch ) {
+    /*
+    FIXME
+            calc_freq(dest[0], src[0]);
+     
+            if(nch == 2)
+                    calc_freq(dest[1], src[1]);
+            else
+                    memcpy(dest[1], dest[0], 256 * sizeof(gint16));
+    */
 }
