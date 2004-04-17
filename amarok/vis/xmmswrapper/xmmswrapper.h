9986ecada5493b74939fa4afb7d8da24ff2ff5af
Stephan Kulow <coolo@kde.org>
2004-04-17 19:40:22 +0000

    always return something in non-void functions
    svn path=/trunk/kdeextragear-1/amarok/; revision=304495

--- amarok/vis/xmmswrapper/xmmswrapper.h
+++ amarok/vis/xmmswrapper/xmmswrapper.h
@@ -64,42 +64,42 @@ extern "C"
     gboolean xmms_remote_is_paused(gint session) { return false; }
     gint xmms_remote_get_playlist_pos(gint session) { return 0; }
     void xmms_remote_set_playlist_pos(gint session, gint pos) {}
-    gint xmms_remote_get_playlist_length(gint session) {}
+    gint xmms_remote_get_playlist_length(gint session) { return 0; }
     void xmms_remote_playlist_clear(gint session) {}
-    gint xmms_remote_get_output_time(gint session) {}
+    gint xmms_remote_get_output_time(gint session) { return 0; }
     void xmms_remote_jump_to_time(gint session, gint pos) {}
     void xmms_remote_get_volume(gint session, gint * vl, gint * vr) {}
-    gint xmms_remote_get_main_volume(gint session) {}
-    gint xmms_remote_get_balance(gint session) {}
+    gint xmms_remote_get_main_volume(gint session) { return 0;}
+    gint xmms_remote_get_balance(gint session) { return 0;}
     void xmms_remote_set_volume(gint session, gint vl, gint vr) {}
     void xmms_remote_set_main_volume(gint session, gint v) {}
     void xmms_remote_set_balance(gint session, gint b) {}
-    gchar *xmms_remote_get_skin(gint session) {}
+    gchar *xmms_remote_get_skin(gint session) { return 0; }
     void xmms_remote_set_skin(gint session, gchar * skinfile) {}
-    gchar *xmms_remote_get_playlist_file(gint session, gint pos) {}
-    gchar *xmms_remote_get_playlist_title(gint session, gint pos) {}
-    gint xmms_remote_get_playlist_time(gint session, gint pos) {}
+    gchar *xmms_remote_get_playlist_file(gint session, gint pos) { return 0; }
+    gchar *xmms_remote_get_playlist_title(gint session, gint pos) { return 0; }
+    gint xmms_remote_get_playlist_time(gint session, gint pos) { return 0; }
     void xmms_remote_get_info(gint session, gint * rate, gint * freq, gint * nch) {}
     void xmms_remote_main_win_toggle(gint session, gboolean show) {}
     void xmms_remote_pl_win_toggle(gint session, gboolean show) {}
     void xmms_remote_eq_win_toggle(gint session, gboolean show) {}
-    gboolean xmms_remote_is_main_win(gint session) {}
-    gboolean xmms_remote_is_pl_win(gint session) {}
-    gboolean xmms_remote_is_eq_win(gint session) {}
+    gboolean xmms_remote_is_main_win(gint session) { return false; }
+    gboolean xmms_remote_is_pl_win(gint session) { return false; }
+    gboolean xmms_remote_is_eq_win(gint session) { return false; }
     void xmms_remote_show_prefs_box(gint session) {}
     void xmms_remote_toggle_aot(gint session, gboolean ontop) {}
     void xmms_remote_eject(gint session) {}
     void xmms_remote_playlist_prev(gint session) {}
     void xmms_remote_playlist_next(gint session) {}
     void xmms_remote_playlist_add_url_string(gint session, gchar * string) {}
-    gboolean xmms_remote_is_running(gint session) {}
+    gboolean xmms_remote_is_running(gint session) { return false; }
     void xmms_remote_toggle_repeat(gint session) {}
     void xmms_remote_toggle_shuffle(gint session) {}
-    gboolean xmms_remote_is_repeat(gint session) {}
-    gboolean xmms_remote_is_shuffle(gint session) {}
+    gboolean xmms_remote_is_repeat(gint session) { return false; }
+    gboolean xmms_remote_is_shuffle(gint session) { return false; }
     void xmms_remote_get_eq(gint session, gfloat *preamp, gfloat **bands) {}
-    gfloat xmms_remote_get_eq_preamp(gint session) {}
-    gfloat xmms_remote_get_eq_band(gint session, gint band) {}
+    gfloat xmms_remote_get_eq_preamp(gint session) { return 0;}
+    gfloat xmms_remote_get_eq_band(gint session, gint band) { return 0;}
     void xmms_remote_set_eq(gint session, gfloat preamp, gfloat *bands) {}
     void xmms_remote_set_eq_preamp(gint session, gfloat preamp) {}
     void xmms_remote_set_eq_band(gint session, gint band, gfloat value) {}
@@ -113,7 +113,7 @@ extern "C"
     //these are the actual functions that vis plugins all use
     //xmms/pluginenum.h
     /*
-    ConfigFile *xmms_cfg_new(void) {}
+    ConfigFile *xmms_cfg_new(void) { return 0; }
     ConfigFile *xmms_cfg_open_file(gchar * filename) { return 0; }
     gboolean    xmms_cfg_write_file(ConfigFile * cfg, gchar * filename) { return false; }
     void        xmms_cfg_free(ConfigFile * cfg) {}
@@ -133,7 +133,7 @@ extern "C"
     */
 
     //libxmms/util.h
-    GtkWidget *xmms_show_message(gchar * title, gchar * text, gchar * button_text, gboolean modal, GtkSignalFunc button_action, gpointer action_data) {}
+    GtkWidget *xmms_show_message(gchar * title, gchar * text, gchar * button_text, gboolean modal, GtkSignalFunc button_action, gpointer action_data) { return 0;}
     gboolean   xmms_check_realtime_priority(void) { return false; }
     void       xmms_usleep(gint usec) { usleep( usec ); }
 
@@ -157,21 +157,21 @@ extern "C" {
 
 
     //xmms/util.h
-    gchar *find_file_recursively(const char *dirname, const char *file) {}
+    gchar *find_file_recursively(const char *dirname, const char *file) { return 0;}
     void del_directory(const char *dirname) {}
-    GdkImage *create_dblsize_image(GdkImage * img) {}
-    char *read_ini_string(const char *filename, const char *section, const char *key) {}
-    char *read_ini_string_no_comment(const char *filename, const char *section, const char *key) {}
-    GArray *read_ini_array(const gchar * filename, const gchar * section, const gchar * key) {}
-    GArray *string_to_garray(const gchar * str) {}
+    GdkImage *create_dblsize_image(GdkImage * img) { return 0; }
+    char *read_ini_string(const char *filename, const char *section, const char *key) { return 0; }
+    char *read_ini_string_no_comment(const char *filename, const char *section, const char *key) { return 0; }
+    GArray *read_ini_array(const gchar * filename, const gchar * section, const gchar * key) { return 0; }
+    GArray *string_to_garray(const gchar * str) { return 0; }
     void glist_movedown(GList * list) {}
     void glist_moveup(GList * list) {}
     void util_item_factory_popup(GtkItemFactory * ifactory, guint x, guint y, guint mouse_button, guint32 time) {}
     void util_item_factory_popup_with_data(GtkItemFactory * ifactory, gpointer data, GtkDestroyNotify destroy, guint x, guint y, guint mouse_button, guint32 time) {}
-    GtkWidget *util_create_add_url_window(gchar *caption, GtkSignalFunc ok_func, GtkSignalFunc enqueue_func) {}
-    GtkWidget *util_create_filebrowser(gboolean clear_pl_on_ok) {}
-    gboolean util_filebrowser_is_dir(GtkFileSelection * filesel) {}
-    GdkFont *util_font_load(gchar *name) {}
+    GtkWidget *util_create_add_url_window(gchar *caption, GtkSignalFunc ok_func, GtkSignalFunc enqueue_func) { return 0;}
+    GtkWidget *util_create_filebrowser(gboolean clear_pl_on_ok) { return 0; }
+    gboolean util_filebrowser_is_dir(GtkFileSelection * filesel) { return false; }
+    GdkFont *util_font_load(gchar *name) { return 0; }
     void util_set_cursor(GtkWidget *window) {}
     void util_dump_menu_rc(void) {}
     void util_read_menu_rc(void) {}
