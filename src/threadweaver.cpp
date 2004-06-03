feda57be176517cc4b37183dee3ae97905583e49
Mark Kretschmann <kretschmann@kde.org>
2004-06-03 07:17:36 +0000

    Reformat source with correct indent.
    svn path=/trunk/kdeextragear-1/amarok/; revision=317292

--- src/threadweaver.cpp
+++ src/threadweaver.cpp
@@ -31,25 +31,24 @@ static inline const TagLib::String LocaleAwareTString( const QString &s )
 
 
 ThreadWeaver::ThreadWeaver( QObject *w )
-  : m_parent( w )
-  , m_bool( true )
-  , m_currentJob( 0 )
+        : m_parent( w )
+        , m_bool( true )
+        , m_currentJob( 0 )
 {}
 
 void
 ThreadWeaver::append( Job* const job, bool priorityJob )
 {
-   //intended to be used by GUI thread, but is thread-safe
+    //intended to be used by GUI thread, but is thread-safe
 
     mutex.lock();
-    if( priorityJob )
+    if ( priorityJob )
         m_Q.prepend( job );
     else
         m_Q.append( job );
     mutex.unlock();
 
-    if( !running() )
-    {
+    if ( !running() ) {
         start( QThread::LowestPriority );
     }
 }
@@ -89,15 +88,14 @@ void
 ThreadWeaver::run()
 {
     msleep( 200 ); //this is an attempt to encourage the queue to be filled with more than 1 item before we
-                   //start processing, and thus prevent unecessary stopping and starting of the thread
+    //start processing, and thus prevent unecessary stopping and starting of the thread
 
     kdDebug() << "[weaver] Started..\n";
     QApplication::postEvent( m_parent, new QCustomEvent( ThreadWeaver::Started ) );
 
-    while( m_bool )
-    {
+    while ( m_bool ) {
         mutex.lock();
-        if( m_Q.isEmpty() ) { mutex.unlock(); break; } //we exit the loop here
+        if ( m_Q.isEmpty() ) { mutex.unlock(); break; } //we exit the loop here
         m_currentJob = m_Q.getFirst();
         m_Q.removeFirst();
         mutex.unlock();
@@ -105,10 +103,9 @@ ThreadWeaver::run()
         bool b = m_currentJob->doJob();
 
         mutex.lock();
-        if( m_currentJob )
-        {
-            if( b ) m_currentJob->postJob(); //Qt will delete the job for us
-            else    delete m_currentJob;     //we need to delete the job
+        if ( m_currentJob ) {
+            if ( b ) m_currentJob->postJob(); //Qt will delete the job for us
+            else delete m_currentJob;     //we need to delete the job
         }
         mutex.unlock();
     }
@@ -120,8 +117,8 @@ ThreadWeaver::run()
 
 
 ThreadWeaver::Job::Job( QObject *obj, JobType type )
-  : QCustomEvent( type )
-  , m_target( obj )
+        : QCustomEvent( type )
+        , m_target( obj )
 {}
 
 inline void
@@ -136,10 +133,10 @@ ThreadWeaver::Job::postJob()
 //////////////////////////////////////////////////////////////////////////////////////////
 
 TagReader::TagReader( QObject *o, PlaylistItem *pi )
-   : Job( o, Job::TagReader )
-   , m_item( pi )
-   , m_url( pi->url() )
-   , m_tags( 0 )
+        : Job( o, Job::TagReader )
+        , m_item( pi )
+        , m_url( pi->url() )
+        , m_tags( 0 )
 {}
 
 TagReader::~TagReader()
@@ -150,8 +147,7 @@ TagReader::~TagReader()
 bool
 TagReader::doJob()
 {
-    if( m_url.protocol() == "file" )
-    {
+    if ( m_url.protocol() == "file" ) {
         m_tags = readTags( m_url );
         return true;
     }
@@ -160,29 +156,29 @@ TagReader::doJob()
 }
 
 MetaBundle*
-TagReader::readTags( const KURL &url, bool /*readAudioProps */) //STATIC
+TagReader::readTags( const KURL &url, bool /*readAudioProps */ )  //STATIC
 {
-   //audioproperties are no longer read on demand
-   TagLib::FileRef f( url.path().local8Bit(), true /*readAudioProps*/, TagLib::AudioProperties::Fast ); //this is the slow step
+    //audioproperties are no longer read on demand
+    TagLib::FileRef f( url.path().local8Bit(), true /*readAudioProps*/, TagLib::AudioProperties::Fast ); //this is the slow step
 
-   if ( !f.isNull() )
-      return new MetaBundle( url, f.tag(), f.audioProperties() );
-   else {
-      //TODO is this threadsafe?
-      KFileMetaInfo info ( url );
-      if ( info.isValid() )
-          return new MetaBundle( url, info );         
-   }
-          
-   return 0;
+    if ( !f.isNull() )
+        return new MetaBundle( url, f.tag(), f.audioProperties() );
+    else {
+        //TODO is this threadsafe?
+        KFileMetaInfo info ( url );
+        if ( info.isValid() )
+            return new MetaBundle( url, info );
+    }
+
+    return 0;
 }
 
 void
 TagReader::bindTags()
 {
-   //for GUI access only
-   //we're a friend of PlaylistItem
-   if( m_tags ) m_item->setText( *m_tags );
+    //for GUI access only
+    //we're a friend of PlaylistItem
+    if ( m_tags ) m_item->setText( *m_tags );
 }
 
 
@@ -191,12 +187,12 @@ TagReader::bindTags()
 //////////////////////////////////////////////////////////////////////////////////////////
 
 SearchModule::SearchModule( QObject* parent, QString path, QString token, KListView* resultView, KListViewItem* historyItem )
-   : Job( parent, Job::SearchModule )
-   , m_parent( parent )
-   , m_path( path )
-   , m_token( token )
-   , m_resultView( resultView )
-   , m_historyItem( historyItem )
+        : Job( parent, Job::SearchModule )
+        , m_parent( parent )
+        , m_path( path )
+        , m_token( token )
+        , m_resultView( resultView )
+        , m_historyItem( historyItem )
 {}
 
 SearchModule::~SearchModule()
@@ -215,25 +211,19 @@ bool SearchModule::doJob()
 
 void SearchModule::searchDir( QString path )
 {
-    DIR *d = opendir( QFile::encodeName( path ) );
-    if ( d )
-    {
-        dirent *ent;
-        while ( ( ent = readdir( d ) ) )
-        {
+    DIR * d = opendir( QFile::encodeName( path ) );
+    if ( d ) {
+        dirent * ent;
+        while ( ( ent = readdir( d ) ) ) {
             QString file( ent->d_name );
 
-            if ( file != "." && file != ".." )
-            {
-                DIR *t = opendir( QFile::encodeName( path ) + QFile::encodeName( file ) + "/" );
-                if ( t )
-                {
+            if ( file != "." && file != ".." ) {
+                DIR * t = opendir( QFile::encodeName( path ) + QFile::encodeName( file ) + "/" );
+                if ( t ) {
                     closedir( t );
                     searchDir( path + file + "/" );
-                }
-                else
-                    if ( file.contains( m_token, FALSE ) )
-                    {
+                } else
+                    if ( file.contains( m_token, FALSE ) ) {
                         QApplication::postEvent( m_parent, new ProgressEvent( ProgressEvent::Progress, m_historyItem, m_resultView, ++resultCount, path, file ) );
                         m_resultList.append( path + file );
                     }
@@ -251,12 +241,12 @@ void SearchModule::searchDir( QString path )
 
 CollectionReader::CollectionReader( CollectionDB* parent, QObject* statusBar,
                                     const QStringList& folders, bool recursively, bool incremental )
-   : Job( parent, Job::CollectionReader )
-   , m_parent( parent )
-   , m_statusBar( statusBar )
-   , m_folders( folders )
-   , m_recursively( recursively )
-   , m_incremental( incremental )
+        : Job( parent, Job::CollectionReader )
+        , m_parent( parent )
+        , m_statusBar( statusBar )
+        , m_folders( folders )
+        , m_recursively( recursively )
+        , m_incremental( incremental )
 {}
 
 CollectionReader::~CollectionReader()
@@ -273,10 +263,9 @@ CollectionReader::doJob()
     QStringList entries;
     //iterate over all folders
     for ( uint i = 0; i < m_folders.count(); i++ )
-        readDir( m_folders[i], entries );
+        readDir( m_folders[ i ], entries );
 
-    if ( !entries.empty() )
-    {
+    if ( !entries.empty() ) {
         QApplication::postEvent( m_statusBar, new ProgressEvent( ProgressEvent::Total, entries.count() ) );
         readTags( entries );
     }
@@ -288,17 +277,16 @@ CollectionReader::doJob()
 void
 CollectionReader::readDir( const QString& dir, QStringList& entries )
 {
-    DIR* d = opendir( QFile::encodeName( dir ) );
-    if ( !d ) return;
+    DIR * d = opendir( QFile::encodeName( dir ) );
+    if ( !d ) return ;
     dirent *ent;
     struct stat statBuf;
 
     //update dir statistics for rescanning purposes
     stat( dir.local8Bit(), &statBuf );
-    m_parent->updateDirStats( dir, (long)statBuf.st_mtime );
+    m_parent->updateDirStats( dir, ( long ) statBuf.st_mtime );
 
-    while ( (ent = readdir( d )) )
-    {
+    while ( ( ent = readdir( d ) ) ) {
         QCString entry = ent->d_name;
 
         if ( entry == "." || entry == ".." )
@@ -307,14 +295,12 @@ CollectionReader::readDir( const QString& dir, QStringList& entries )
 
         stat( entry, &statBuf );
 
-        if ( S_ISDIR( statBuf.st_mode ) )
-        {
+        if ( S_ISDIR( statBuf.st_mode ) ) {
             if ( m_recursively )
                 //call ourself recursively for each subdir
                 if ( !m_incremental || !m_parent->isDirInCollection( entry ) )
                     readDir( QFile::decodeName( entry ), entries );
-        }
-        else if ( S_ISREG( statBuf.st_mode ) )
+        } else if ( S_ISREG( statBuf.st_mode ) )
             entries << QString::fromLocal8Bit( entry );
     }
     closedir( d );
@@ -332,18 +318,18 @@ CollectionReader::readTags( const QStringList& entries )
     validExtensions << "jpg" << "png" << "gif" << "jpeg";
 
     for ( uint i = 0; i < entries.count(); i++ ) {
-        if ( !( i % 20 ) ) //don't post events too often since this blocks amaroK
+        if ( !( i % 20 ) )  //don't post events too often since this blocks amaroK
             QApplication::postEvent( m_statusBar, new ProgressEvent( ProgressEvent::Progress, i ) );
 
-        url.setPath( entries[i] );
+        url.setPath( entries[ i ] );
         TagLib::FileRef f( QFile::encodeName( url.path() ), false );  //false == don't read audioprops
 
         if ( !f.isNull() ) {
-            MetaBundle* bundle = new MetaBundle( url, f.tag(), 0 );
+            MetaBundle * bundle = new MetaBundle( url, f.tag(), 0 );
 
             QString command = "INSERT INTO tags_temp "
-                                "( url, dir, createdate, album, artist, genre, title, year, comment, track ) "
-                                "VALUES('";
+                              "( url, dir, createdate, album, artist, genre, title, year, comment, track ) "
+                              "VALUES('";
 
             command += m_parent->escapeString( bundle->url().path() ) + "','";
             command += m_parent->escapeString( bundle->url().directory() ) + "',";
@@ -358,10 +344,9 @@ CollectionReader::readTags( const QStringList& entries )
 
             m_parent->execSql( command );
             delete bundle;
-        } else
-        {
+        } else {
             // Add images to the cover database
-            if ( validExtensions.contains( url.filename().mid( url.filename().findRev('.')+1 ) ) )
+            if ( validExtensions.contains( url.filename().mid( url.filename().findRev( '.' ) + 1 ) ) )
                 m_parent->addImageToPath( url.directory(), url.filename(), true );
         }
     }
@@ -369,15 +354,13 @@ CollectionReader::readTags( const QStringList& entries )
     m_parent->execSql( "BEGIN TRANSACTION;" );
 
     // remove tables and recreate them (quicker than DELETE FROM)
-    if ( !m_incremental )
-    {
+    if ( !m_incremental ) {
         m_parent->dropTables();
         m_parent->createTables();
-    } else
-    {
+    } else {
         // remove old entries from database, only
         for ( uint i = 0; i < m_folders.count(); i++ )
-          m_parent->removeSongsInDir( m_folders[i] );
+            m_parent->removeSongsInDir( m_folders[ i ] );
     }
 
     // rename tables
@@ -396,10 +379,10 @@ CollectionReader::readTags( const QStringList& entries )
 //////////////////////////////////////////////////////////////////////////////////////////
 
 AudioPropertiesReader::AudioPropertiesReader( QObject *o, PlaylistItem *pi )
-   : Job( o )
-   , m_item( pi )
-   , m_listView( static_cast<QListViewItem *>(pi)->listView() )
-   , m_url( pi->url() )
+        : Job( o )
+        , m_item( pi )
+        , m_listView( static_cast<QListViewItem *>( pi ) ->listView() )
+        , m_url( pi->url() )
 {
     //TODO derive this from TagReader?
 }
@@ -411,26 +394,24 @@ AudioPropertiesReader::doJob()
     //TODO it is probably more efficient to do this with less specifity, i.e. when view scrolls put a job
     //     in to read tags for visible items, and then get this function to get the visible items for that moment
 
-    int y  = m_item->itemPos(); //const //FIXME slow function!
-    int h  = m_item->height();  //const
+    int y = m_item->itemPos(); //const //FIXME slow function!
+    int h = m_item->height();  //const
     int y2 = m_listView->contentsY(); //TODO find out the performance of this function
-    if( (y + h) >= y2 && y <= (y2 + m_listView->visibleHeight()) )
-    {
+    if ( ( y + h ) >= y2 && y <= ( y2 + m_listView->visibleHeight() ) ) {
         //This is a quick scan
         //A more accurate scan is done when the track is played, and those properties are recorded with the track
         TagLib::FileRef f( QFile::encodeName( m_url.path() ), true, TagLib::AudioProperties::Fast );
-        int length  = MetaBundle::Unavailable;
+        int length = MetaBundle::Unavailable;
         int bitrate = MetaBundle::Unavailable;
 
-        if( !f.isNull() )
-        {
+        if ( !f.isNull() ) {
             //FIXME do we need to check the ap pointer? Seems to have not crashed so far..
-            TagLib::AudioProperties *ap = f.audioProperties();
-            length  = ap->length();
+            TagLib::AudioProperties * ap = f.audioProperties();
+            length = ap->length();
             bitrate = ap->bitrate();
         }
 
-        m_length  = MetaBundle::prettyLength( length );
+        m_length = MetaBundle::prettyLength( length );
         m_bitrate = MetaBundle::prettyBitrate( bitrate );
 
         return true;
@@ -442,7 +423,7 @@ AudioPropertiesReader::doJob()
 void
 AudioPropertiesReader::completeJob()
 {
-    m_item->setText( PlaylistItem::Length,  m_length  );
+    m_item->setText( PlaylistItem::Length, m_length );
     m_item->setText( PlaylistItem::Bitrate, m_bitrate );
 }
 
@@ -452,10 +433,10 @@ AudioPropertiesReader::completeJob()
 //////////////////////////////////////////////////////////////////////////////////////////
 
 TagWriter::TagWriter( QObject *o, PlaylistItem *pi, const QString &s, const int col )
-  : Job( o )
-  , m_item( pi )
-  , m_tagString( s )
-  , m_tagType( col )
+        : Job( o )
+        , m_item( pi )
+        , m_tagString( s )
+        , m_tagType( col )
 {
     //TODO deepcopy?
     //TODO use a enum for TagType
@@ -468,12 +449,11 @@ TagWriter::doJob()
     const KURL url = m_item->url();
     TagLib::FileRef f( QFile::encodeName( url.path() ), false );
 
-    if( !f.isNull() )
-    {
-        TagLib::Tag *t = f.tag();
+    if ( !f.isNull() ) {
+        TagLib::Tag * t = f.tag();
         const TagLib::String s = LocaleAwareTString( m_tagString );
 
-        switch( m_tagType ) {
+        switch ( m_tagType ) {
         case PlaylistItem::Title:
             t->setTitle( s );
             break;
@@ -523,10 +503,10 @@ TagWriter::completeJob()
 //////////////////////////////////////////////////////////////////////////////////////////
 
 PLStats::PLStats( QObject *o, const KURL &u, const KURL::List &ul )
-  : Job( o, Job::PLStats )
-  , m_url( u )
-  , m_contents( ul )
-  , m_length( 0 )
+        : Job( o, Job::PLStats )
+        , m_url( u )
+        , m_contents( ul )
+        , m_length( 0 )
 {}
 
 bool
@@ -536,11 +516,10 @@ PLStats::doJob()
     //TODO find out if tags are read on creation of the file ref and if so, update them too
     //TODO currently this is horrendously inefficient, tags are read so many times!
 
-    for( KURL::List::const_iterator it = m_contents.begin(); it != m_contents.end(); ++it )
-    {
-        TagLib::FileRef f( QFile::encodeName( (*it).path() ), true, TagLib::AudioProperties::Accurate );
+    for ( KURL::List::const_iterator it = m_contents.begin(); it != m_contents.end(); ++it ) {
+        TagLib::FileRef f( QFile::encodeName( ( *it ).path() ), true, TagLib::AudioProperties::Accurate );
 
-        if( !f.isNull() ) m_length += f.audioProperties()->length();
+        if ( !f.isNull() ) m_length += f.audioProperties() ->length();
     }
 
     return true;
