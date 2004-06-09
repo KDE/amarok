e1de89f971c8cdd8517e6328f93c3422e6e93a5e
Christian Muehlhaeuser <chris@chris.de>
2004-06-09 01:30:23 +0000

    i18n()'d the WelcomeBrowser.
    svn path=/trunk/kdeextragear-1/amarok/; revision=318937

--- src/welcomebrowser.cpp
+++ src/welcomebrowser.cpp
@@ -10,6 +10,7 @@
 #include <khtml_part.h>
 #include <kconfig.h>      //config object
 #include <kdebug.h>
+#include <klocale.h>
 #include <kglobal.h>
 #include <kstandarddirs.h>
 
@@ -73,31 +74,28 @@ void WelcomeBrowser::showPage()
     browser->begin();
     browser->setUserStyleSheet( m_styleSheet );
 
-    QString welcomeText = "<html>"
-                          "<head>"
-                          "</head>"
-                          "<body>"
-                          "<div class='title'>Welcome to amaroK!</div>"
-                          "<div>There are many media players around these days, this is true. But, what's missing from most players is an interface that doesn't get in the way of the user. How many buttons do you have to press when simply adding media to the playlist? amaroK tries to be a little different, providing a simple drag and drop interface that makes playlist handling really easy.</div><br>"
-                          "<div class='title'>Customise your amaroK</div>"
-                          "<div>amaroK has two main modes of operation. It can look and act a little like XMMS and other Winamp clones [SCREENIE], or it can have a single window with a statusbar to keep you up to date with the track progress. You can switch between them using the following links:"
+    QString welcomeText = "<html><body>"
+                          "<div class='title'>" + i18n( "Welcome to amaroK!" ) + "</div>"
+                          "<div>" + i18n( "There are many media players around these days, this is true. But, what's missing from most players is an interface that doesn't get in the way of the user. How many buttons do you have to press when simply adding media to the playlist? amaroK tries to be a little different, providing a simple drag and drop interface that makes playlist handling really easy." ) + "</div><br>"
+                          "<div class='title'>" + i18n( "Customise your amaroK" ) + "</div>"
+                          "<div>" + i18n( "amaroK has two main modes of operation. It can look and act a little like XMMS and other Winamp clones [SCREENIE], or it can have a single window with a statusbar to keep you up to date with the track progress. You can switch between them using the following links:" ) +
                           "<ul>"
-                          "<li><A href='amarok://amaamp_mode'>Switch to XMMS-mode</A>"
-                          "<li><A href='amarok://default_mode'>Switch to amaroK-mode</A>"
+                          "<li><A href='amarok://amaamp_mode'>" + i18n( "Switch to XMMS-mode" ) + "</A>"
+                          "<li><A href='amarok://default_mode'>" + i18n( "Switch to amaroK-mode" ) + "</A>"
                           "</ul>"
                           "</div><br>"
-                          "<div class='title'>Using amaroK</div>"
-                          "<div>amaroK is all about easy playlist building! This welcome text is embedded in a tab in the BrowserBar. Each tab in the bar is a route to your music and media! Open the browsers by clicking the tabs and then drag media from the listings to the playlist! The different browsers are as follows:"
+                          "<div class='title'>" + i18n( "Using amaroK" ) + "</div>"
+                          "<div>" + i18n( "amaroK is all about easy playlist building! This welcome text is embedded in a tab in the BrowserBar. Each tab in the bar is a route to your music and media! Open the browsers by clicking the tabs and then drag media from the listings to the playlist! The different browsers are as follows:" ) +
                           "<ol>"
-                          "<li><span class='subtitle'>File Browser</span><p>The FileBrowser will be the most useful browser to people at first. Use it to navigate your filesystem to where you keep your music and then drag and drop to the playlist. Filtering by <EM>*.mp3 *.ogg</EM> may prove useful to you.</p>"
-                          "<li><span class='subtitle'>Search Browser</span><p>Some people have too much music to remember where it all is. The Search Browser makes it easy to find lost tracks. Specify a search term (eg. 'Chem' to find all your <EM>Chemical Brothers</EM> music), specify a path to start the search in and click <STRONG>Search</STRONG>! You can even drag and drop the files to Konqueror afterwards if you want to move them all to a sensible location so you don't need to search again next time.</p>"
-                          "<li><span class='subtitle'>Collection Browser</span><p>This is amaroK's lightning-fast music database. Assemble the database and enjoy the benefits of fast, categorised music sorting.</p>"
-                          "<li><span class='subtitle'>Context Browser</span><p>Statistics, context, brilliance!</p>"
-                          "<li><span class='subtitle'>Stream Browser</span><p>Internet radio is a fabulous way to find out about new music without upsetting the various record industries of the world. Fetch the streams from the online meta-server and drag and drop something to suit your tastes. You can even submit your own favorite radio stations to the server to increase the choice for everyone else!</p>"
+                          "<li><span class='subtitle'>" + i18n( "File Browser" ) + "</span><p>" + i18n( "The FileBrowser will be the most useful browser to people at first. Use it to navigate your filesystem to where you keep your music and then drag and drop to the playlist. Filtering by <EM>*.mp3 *.ogg</EM> may prove useful to you." ) + "</p>"
+                          "<li><span class='subtitle'>" + i18n( "Search Browser" ) + "</span><p>" + i18n( "Some people have too much music to remember where it all is. The Search Browser makes it easy to find lost tracks. Specify a search term (eg. 'Chem' to find all your <EM>Chemical Brothers</EM> music), specify a path to start the search in and click <STRONG>Search</STRONG>! You can even drag and drop the files to Konqueror afterwards if you want to move them all to a sensible location so you don't need to search again next time." ) + "</p>"
+                          "<li><span class='subtitle'>" + i18n( "Collection Browser" ) + "</span><p>" + i18n( "This is amaroK's lightning-fast music database. Assemble the database and enjoy the benefits of fast, categorised music sorting." ) + "</p>"
+                          "<li><span class='subtitle'>" + i18n( "Context Browser" ) + "</span><p>" + i18n( "Statistics, context, brilliance!" ) + "</p>"
+                          "<li><span class='subtitle'>" + i18n( "Stream Browser" ) + "</span><p>" + i18n( "Internet radio is a fabulous way to find out about new music without upsetting the various record industries of the world. Fetch the streams from the online meta-server and drag and drop something to suit your tastes. You can even submit your own favorite radio stations to the server to increase the choice for everyone else!" ) + "</p>"
                           "</ol>"
                           "</div><br>"
-                          "<div style='border:0;margin-top:2em;text-align:center'>To remove this tab from the BrowserBar, <A href='amarok://remove_tab'>click here</A>.</div>"
-                          "</body>";
+                          "<div style='text-align:center'>" + i18n( "To remove this tab from the BrowserBar," ) + " <A href='amarok://remove_tab'>" + i18n( "click here" ) + "</A>.</div>"
+                          "</body></html>";
 
     browser->write( welcomeText );  
     browser->end();
