8537678c24712cdcff9658d3fd68d89c27fdd4c8
Max Howell <max.howell@methylblue.com>
2004-06-12 04:10:04 +0000

    Don't show rows in the TrackTooltip if the contain empty tags
    svn path=/trunk/kdeextragear-1/amarok/; revision=319862

--- src/tracktooltip.cpp
+++ src/tracktooltip.cpp
@@ -6,7 +6,7 @@
 */
 
 /*
-  tracktooltip.cpp  -  Provides an interface to a plain QWidget, which is independent of KDE (bypassed to X11)
+  tracktooltip.cpp
   begin:     Tue 10 Feb 2004
   copyright: (C) 2004 by Christian Muehlhaeuser
   email:     chris@chris.de
@@ -15,37 +15,34 @@
 #include "tracktooltip.h"
 #include "metabundle.h"
 #include "collectiondb.h"
-#include <dirent.h>
-#include <sys/stat.h>
-#include <kurl.h>
 #include <qtooltip.h>
 
 
 void TrackToolTip::add( QWidget * widget, const MetaBundle & tags )
 {
-    const QString tr = "<tr><td width=70 align=right>%1:</td><td align=left>%2</td></tr>";
-
     CollectionDB db;
     QString tipBuf;
-    QString image;
-    image = db.getImageForPath( tags.url().directory(), "", 80 );
+    const QString image = db.getImageForPath( tags.url().directory(), QString::null, 80 );
+    const QString tableRow = "<tr><td width=70 align=right>%1:</td><td align=left>%2</td></tr>";
+    QStringList left, right;
+    left  << i18n( "Title" ) << i18n( "Artist" ) << i18n( "Album" ) << i18n( "Length" ) << i18n( "Bitrate" ) << i18n( "Samplerate" );
+    right << tags.title() << tags.artist() << tags.album() << tags.prettyLength() << tags.prettyBitrate() << tags.prettySampleRate();
 
     //NOTE it seems to be necessary to <center> each element indivdually
     tipBuf += "<center><b>amaroK</b></center><table cellpadding='2' cellspacing='2' align='center'><tr>";
-    
+
     if ( !image.isEmpty() )
-        tipBuf += QString( "<td><table cellpadding='0' cellspacing='0'><tr><td><img width='80' height='80' src='%1'></td></tr></table></td>" )
-                  .arg( image );
+        tipBuf += QString( "<td><table cellpadding='0' cellspacing='0'><tr><td>"
+                           "<img width='80' height='80' src='%1'>"
+                           "</td></tr></table></td>" ).arg( image );
 
-    tipBuf += "<td><table cellpadding='0' cellspacing='0'>"; //style='font-face: Arial; font-size: 8px;'
-    tipBuf += tr.arg( i18n( "Title" ),  tags.title() );
-    tipBuf += tr.arg( i18n( "Artist" ), tags.artist() );
-    tipBuf += tr.arg( i18n( "Album" ), tags.album() );
-    tipBuf += tr.arg( i18n( "Length" ), tags.prettyLength() );
-    tipBuf += tr.arg( i18n( "Bitrate" ), tags.prettyBitrate() );
-    tipBuf += tr.arg( i18n( "Samplerate" ), tags.prettySampleRate() );
+    tipBuf += "<td><table cellpadding='0' cellspacing='0'>";
+
+    for( uint x = 0; x < left.count(); ++x )
+        if ( !right[x].isEmpty() )
+            tipBuf += tableRow.arg( left[x] ).arg( right[x] );
+
     tipBuf += "</table></td>";
-
     tipBuf += "</tr></table></center>";
 
     QToolTip::add( widget, tipBuf );
