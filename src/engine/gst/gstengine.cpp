c31d38cb86dcbbc4a4ec3beeea899fc1bb8228ce
Mark Kretschmann <kretschmann@kde.org>
2004-07-09 11:25:35 +0000

    Use kill() for deleting TransferJob.
    svn path=/trunk/kdeextragear-1/amarok/; revision=327155

--- src/engine/gst/gstengine.cpp
+++ src/engine/gst/gstengine.cpp
@@ -398,7 +398,7 @@ GstEngine::stop()  //SLOT
     /* stop the thread */
     gst_element_set_state ( m_thread, GST_STATE_NULL );
 
-    delete m_transferJob;
+    m_transferJob->kill();
     m_transferJob = 0;
     
     emit stopped();
@@ -497,7 +497,7 @@ GstEngine::newKioData( KIO::Job*, const QByteArray& array )  //SLOT
     
     if ( m_streamBufIndex >= STREAMBUF_MAX ) {
         kdDebug() << "Gst-Engine: SUSPENDING kio transfer.\n";
-        m_transferJob->suspend();
+        if ( m_transferJob ) m_transferJob->suspend();
     }
             
     if ( m_streamBufIndex + size >= STREAMBUF_SIZE ) {
