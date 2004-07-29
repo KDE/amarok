02002d0eff88c147fab168a17eccb94283ba6e93
Mark Kretschmann <kretschmann@kde.org>
2004-07-29 10:35:19 +0000

    Bump plugin framework version.
    svn path=/trunk/kdeextragear-1/amarok/; revision=333744

--- src/pluginmanager.h
+++ src/pluginmanager.h
@@ -31,7 +31,7 @@ class PluginManager
 {
     public:
         /** Bump this number whenever the plugin framework gets incompatible with older versions */
-        static const int FrameworkVersion = 2;
+        static const int FrameworkVersion = 3;
 
         /**
          * It will return a list of services that match your
