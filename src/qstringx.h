f4bbddc467fb126aca0133ecca10b8906b0b6049
Stefan Bogner <bochi@online.ms>
2004-08-05 08:00:51 +0000

    <cartman> somebody forgot to add qstringx.h ?
    svn path=/trunk/kdeextragear-1/amarok/; revision=336311

---
+++ src/qstringx.h
@@ -0,0 +1,43 @@
+// Copyright (C) 2004 Shintaro Matsuoka <shin@shoegazed.org>
+// See COPYING file for licensing information
+
+#include <qglobal.h>
+#include <qregexp.h>
+#include <qstring.h>
+#include <qstringlist.h>
+
+#ifndef AMAROK_QSTRINGX_H
+#define AMAROK_QSTRINGX_H
+
+namespace amaroK
+{
+
+class QStringx : public QString
+{
+public:
+  QStringx() {};
+  QStringx( QChar ch ) : QString( ch ) {};
+  QStringx( const QString& s ) : QString( s ) {};
+  QStringx( const QByteArray& ba ) : QString( ba ) {};
+  QStringx( const QChar* unicode, uint length ) : QString( unicode, length ) {};
+  QStringx( const char* str ) : QString( str ) {};
+  ~QStringx() {};
+
+  QString args( const QStringList& l );
+};
+
+QString QStringx::args( const QStringList& l )
+{
+  QStringList k = QStringList::split( QRegExp( "%\\d+" ), *this, TRUE );
+  Q_ASSERT( k.count()-1 == l.count() );
+  // let's marge!
+  QString r;
+  for ( unsigned int i=0 ; i < k.count()-1 ; ++i )
+    r += k[i] + l[i];
+  r += k[k.count()-1];
+  return r;
+}
+
+} // namespace amaroK
+
+#endif // AMAROK_QSTRINGX_H
