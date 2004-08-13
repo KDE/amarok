f11bbdcf35087ccecbef1797eeb2f9f7357c8abf
Mark Kretschmann <kretschmann@kde.org>
2004-08-13 10:32:23 +0000

    Script for generating amaroK tarball releases :) Based on experiences from mxcl and /me
    svn path=/trunk/kdeextragear-1/amarok/; revision=337693

---
+++ release_tarball.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+version=`kdialog --inputbox "amaroK version: "` 
+cvsroot=`kdialog --inputbox "CVS root directory: "`
+
+folder=amarok-$version
+mkdir $folder
+pushd $folder
+cvs -d $cvsroot co -l kdeextragear-1
+pushd kdeextragear-1
+
+cvs -z3 co -r KDE_3_2_BRANCH admin 
+cvs -z3 up amarok
+cvs -z3 up doc/amarok
+
+# Remove CVS relevant files
+find -name "CVS" -exec rm -rf {} \;
+find -name ".cvsignore" -exec rm {} \;
+
+pushd amarok
+
+# Move some important files to the root folder
+mv amarok.lsm ..
+mv AUTHORS ..
+mv ChangeLog ..
+mv COPYING ..
+mv INSTALL ..
+mv README ..
+mv TODO ..
+
+pushd src
+cat amarok.h | sed -e "s/APP_VERSION \".*\"/APP_VERSION \"$version"\"/ > amarok.h
+popd
+
+rm -rf debian
+
+popd # kdeextragear-1
+
+# Prevent using unsermake
+oldmake=$unsermake
+export unsermake="" && make -f Makefile.cvs
+export unsermake=$oldmake
+
+find -name "*" -exec touch {} \;
+rm -rf autom4te.cache
+rm stamp-h.in
+
+mv * ..
+popd # amaroK-foo
+rm -rf kdeextragear-1
+popd # root
+tar -cf $folder.tar $folder
+bzip2 $folder.tar
+rm -rf $folder
+
+echo
+md5sum $folder.tar.bz2
+
+echo
+echo "Congratulations :) amaroK $version tarball generated."
+echo "Now drink a few pints, have some fun on #amarok"
+echo
+
