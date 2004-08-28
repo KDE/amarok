447d1c057fd95da144420d7da6d8be85bd7ef2a1
Mark Kretschmann <kretschmann@kde.org>
2004-08-28 21:48:10 +0000

    Make the arts engine provide scope data with the full range.
    svn path=/trunk/kdeextragear-1/amarok/; revision=342292

--- src/engine/arts/artsengine.cpp
+++ src/engine/arts/artsengine.cpp
@@ -321,7 +321,7 @@ const Engine::Scope& ArtsEngine::scope()
     if ( in ) {
         // Convert float to int
         for ( int i = 0; i < m_scopeSize; i++ )
-            out[i] = (int16_t) ( in->at( i ) * (float) ( 1<<15 ) );
+            out[i] = (int16_t) ( in->at( i ) * (float) ( 1<<14 ) );
 
         delete in;
     }
