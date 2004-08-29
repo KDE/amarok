9223c063329e52fbd87073cfb6d591da500b9192
Max Howell <max.howell@methylblue.com>
2004-08-29 14:04:10 +0000

    Line up browser tabs with browsers. Looks pretty ooooo!
    svn path=/trunk/kdeextragear-1/amarok/; revision=342462

--- src/browserbar.cpp
+++ src/browserbar.cpp
@@ -94,7 +94,7 @@ BrowserBar::BrowserBar( QWidget *parent )
     m_tabBar->setFixedWidth( m_pos );
 
 #ifdef BROWSER_BAR_LEFT
-    m_tabBar->move( 0, 20 );
+    m_tabBar->move( 0, 5 );
     m_browserHolder->move( m_pos, 0 );
 #else
     m_playlist->move( 0, 0 );
