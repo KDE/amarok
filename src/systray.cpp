6ca9b718152131bdcaf2d75a26ce522d47cf7710
Enrico Ros <rosenric@dei.unipd.it>
2004-09-02 16:25:04 +0000

    ADD: show progress in system tray.
    svn path=/trunk/kdeextragear-1/amarok/; revision=343537

--- src/systray.cpp
+++ src/systray.cpp
@@ -11,17 +11,21 @@
 #include "systray.h"
 
 #include <qevent.h>
+#include <qimage.h>
 #include <kaction.h>
 #include <kapplication.h>
 #include <kpopupmenu.h>
+#include <kiconeffect.h>
 
 
 amaroK::TrayIcon::TrayIcon( QWidget *playerWidget )
-  : KSystemTray( playerWidget )
+  : KSystemTray( playerWidget ),
+  baseIcon( 0 ), grayedIcon( 0 ), alternateIcon( 0 ),
+  trackLength( 0 ), trackPercent( -1 ), drawnPercent( -1 )
 {
     KActionCollection* const ac = amaroK::actionCollection();
 
-    setPixmap( KSystemTray::loadIcon("amarok") ); // @since 3.2
+    paintProgressIcon();
     setAcceptDrops( true );
 
     ac->action( "prev"  )->plug( contextMenu() );
@@ -44,6 +48,17 @@ amaroK::TrayIcon::TrayIcon( QWidget *playerWidget )
     KAction *quit = actionCollection()->action( "file_quit" );
     quit->disconnect();
     connect( quit, SIGNAL( activated() ), kapp, SLOT( quit() ) );
+
+    // attach to get notified about engine events
+    EngineController::instance()->attach( this );
+}
+
+amaroK::TrayIcon::~TrayIcon( )
+{
+    EngineController::instance()->detach( this );
+    delete baseIcon;
+    delete grayedIcon;
+    delete alternateIcon;
 }
 
 bool
@@ -69,4 +84,89 @@ amaroK::TrayIcon::event( QEvent *e )
     default:
         return KSystemTray::event( e );
     }
+}
+
+void
+amaroK::TrayIcon::engineStateChanged( Engine::State state )
+{
+    // draw the base icon if stopped or idle
+    if ( state != Engine::Playing && state != Engine::Paused )
+        paintProgressIcon();
+}
+
+void
+amaroK::TrayIcon::engineNewMetaData( const MetaBundle &bundle, bool /*trackChanged*/ )
+{
+    trackLength = bundle.length() * 1000;
+}
+
+void
+amaroK::TrayIcon::engineTrackPositionChanged( long position )
+{
+    trackPercent = trackLength ? (100 * position) / trackLength : 100;
+    paintProgressIcon( trackPercent );
+}
+
+void
+amaroK::TrayIcon::paletteChange( const QPalette & op )
+{
+    if ( palette().active().highlight() == op.active().highlight() || !alternateIcon )
+        return;
+        
+    delete alternateIcon;
+    alternateIcon = 0;
+    // force pixmap rebuild and repaint
+    drawnPercent = -1;
+    paintProgressIcon( trackPercent );
+}
+
+void
+amaroK::TrayIcon::paintProgressIcon( int percent )
+{
+    // skip redrawing the same pixmap
+    if ( percent == drawnPercent )
+        return;
+    drawnPercent = percent;
+
+    // load the base trayIcon
+    if ( !baseIcon )
+        baseIcon = new QPixmap( KSystemTray::loadIcon("amarok") );
+    if ( percent > 99  )
+    {
+        setPixmap( *baseIcon ); // @since 3.2
+        return;
+    }
+
+    // make up the grayed icon
+    if ( !grayedIcon )
+    {
+        QImage tmpTrayIcon = baseIcon->convertToImage();
+        KIconEffect::semiTransparent( tmpTrayIcon );
+        grayedIcon = new QPixmap( tmpTrayIcon );
+    }
+    if ( percent < 1  )
+    {
+        setPixmap( *grayedIcon ); // @since 3.2
+        return;
+    }
+    
+    // make up the colored icon
+    if ( !alternateIcon )
+    {
+        QImage tmpTrayIcon = baseIcon->convertToImage();
+        // eros: this looks cool with dark red blue or green but sucks with
+        // other colors (such as kde default's pale pink..). maybe the effect
+        // or the blended color has to be changed..
+        KIconEffect::colorize( tmpTrayIcon, palette().active().highlight().dark(105), 0.9 );
+        alternateIcon = new QPixmap( tmpTrayIcon );
+    }
+
+    // mix [ grayed <-> colored ] icons
+    QPixmap tmpTrayPixmap( *grayedIcon );
+    int height = grayedIcon->height(),
+        sourceH = 1 + (height * percent) / 100, // 1 .. height
+        sourceY = height - sourceH;             // height-1 .. 0
+    copyBlt( &tmpTrayPixmap, 0,sourceY, alternateIcon, 0,sourceY,
+             grayedIcon->width(), sourceH );
+    setPixmap( tmpTrayPixmap );
 }
--- src/systray.h
+++ src/systray.h
@@ -10,16 +10,29 @@
 #define AMAROKSYSTRAY_H
 
 #include <ksystemtray.h>
+#include "engineobserver.h" //baseclass
 
 namespace amaroK {
 
-class TrayIcon : public KSystemTray
+class TrayIcon : public KSystemTray, public EngineObserver
 {
 public:
     TrayIcon( QWidget* );
+    ~TrayIcon( );
+
+protected:
+    // reimpl from engineobserver
+    virtual void engineStateChanged( Engine::State state );
+    virtual void engineNewMetaData( const MetaBundle &bundle, bool trackChanged );
+    virtual void engineTrackPositionChanged( long position );
+    // get notified of 'highlight' color change
+    virtual void paletteChange( const QPalette & oldPalette );
 
 private:
     bool event( QEvent* );
+    void paintProgressIcon( int percent = 100 );
+    QPixmap *baseIcon, *grayedIcon, *alternateIcon;
+    long trackLength, trackPercent, drawnPercent;
 };
 
 }
