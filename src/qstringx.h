07f34c0c61485ed9b986f3637a3d5c2222be4a29
Shintaro Matsuoka <shin@shoegazed.org>
2004-09-17 06:45:08 +0000

    unify coding style
    svn path=/trunk/kdeextragear-1/amarok/; revision=347111

--- src/qstringx.h
+++ src/qstringx.h
@@ -1,13 +1,13 @@
 // Copyright (C) 2004 Shintaro Matsuoka <shin@shoegazed.org>
 // See COPYING file for licensing information
 
+#ifndef AMAROK_QSTRINGX_H
+#define AMAROK_QSTRINGX_H
+
 #include <qglobal.h>
 #include <qregexp.h>
 #include <qstring.h>
 #include <qstringlist.h>
-
-#ifndef AMAROK_QSTRINGX_H
-#define AMAROK_QSTRINGX_H
 
 namespace amaroK
 {
@@ -15,29 +15,26 @@ namespace amaroK
 class QStringx : public QString
 {
 public:
-  QStringx() {};
-  QStringx( QChar ch ) : QString( ch ) {};
-  QStringx( const QString& s ) : QString( s ) {};
-  QStringx( const QByteArray& ba ) : QString( ba ) {};
-  QStringx( const QChar* unicode, uint length ) : QString( unicode, length ) {};
-  QStringx( const char* str ) : QString( str ) {};
-  ~QStringx() {};
-
-  QString args( const QStringList& l );
+    QStringx() {};
+    QStringx( QChar ch ) : QString( ch ) {};
+    QStringx( const QString& s ) : QString( s ) {};
+    QStringx( const QByteArray& ba ) : QString( ba ) {};
+    QStringx( const QChar* unicode, uint length ) : QString( unicode, length ) {};
+    QStringx( const char* str ) : QString( str ) {};
+    ~QStringx() {};
+    
+    QString args( const QStringList& l )
+    {
+        QStringList k = QStringList::split( QRegExp( "%\\d+" ), *this, TRUE );
+        Q_ASSERT( k.count()-1 == l.count() );
+        QString r;
+        for ( unsigned int i=0 ; i < k.count()-1 ; ++i )
+            r += k[i] + l[i];
+        r += k[k.count()-1];
+        return r;
+    }
 };
 
-QString QStringx::args( const QStringList& l )
-{
-  QStringList k = QStringList::split( QRegExp( "%\\d+" ), *this, TRUE );
-  Q_ASSERT( k.count()-1 == l.count() );
-  QString r;
-  for ( unsigned int i=0 ; i < k.count()-1 ; ++i )
-    r += k[i] + l[i];
-  r += k[k.count()-1];
-  return r;
-}
-
 } // namespace amaroK
-
 
 #endif // AMAROK_QSTRINGX_H
