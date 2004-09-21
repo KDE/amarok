d274e2558073f251e00b4b87f72548b8efb2971e
Christian Muehlhaeuser <chris@chris.de>
2004-09-21 06:30:10 +0000

    * Fix: Contextbrowser's current track view.
    svn path=/trunk/kdeextragear-1/amarok/; revision=348080

--- src/contextbrowser.cpp
+++ src/contextbrowser.cpp
@@ -560,9 +560,9 @@ void ContextBrowser::showCurrentTrack() //SLOT
                                     "<img hspace='2' src='%9'></a></td>"
                                     "<td valign='bottom' align='right' width='80%'>" +
                                     i18n( "Track played 1 time", "Track played %n times", values[4].toInt() ) + "<br>" +
-                                    i18n( "Score:" ) + " %11" + "<br>" +
-                                    i18n( "Last play:" ) + " %12" + "<br>" +
-                                    i18n( "First play:" ) + " %13" + "</i></td></tr>" )
+                                    i18n( "Score:" ) + " %10" + "<br>" +
+                                    i18n( "Last play:" ) + " %11" + "<br>" +
+                                    i18n( "First play:" ) + " %12" + "</i></td></tr>" )
                          .args( QStringList()
                                 << escapeHTML( m_currentTrack->artist() )
                                 << escapeHTML( m_currentTrack->title() )
