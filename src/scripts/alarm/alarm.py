3e9ecde731af2f80c15f61413a973618c3f1cafa
Mark Kretschmann <kretschmann@kde.org>
2005-01-11 10:11:15 +0000

    add file
    svn path=/trunk/kdeextragear-1/amarok/; revision=377412

---
+++ src/scripts/alarm/alarm.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+############################################################################
+# Alarm script
+# (c) 2005 Mark Kretschmann <markey@web.de>
+#
+# Depends on: PyQt
+############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+############################################################################
+
+import sys
+import os.path
+from qt import *
+
