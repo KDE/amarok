2d3c1dc0e7fe30254946b6252c55bea02ac6a6cd
Mark Kretschmann <kretschmann@kde.org>
2005-01-30 02:47:55 +0000

    Add template script for ruby/qt, haxx0red by eean.
    svn path=/trunk/kdeextragear-1/amarok/; revision=384006

---
+++ src/scripts/templates/amarok.rb
@@ -0,0 +1,85 @@
+############################################################################
+#    Copyright (C) 2005 by Ian Monroe                                      #
+#    ian@monroe.nu                                                         #
+#                                                                          #
+#    This program is free software; you can redistribute it and#or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation; either version 2 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+############################################################################
+
+class QtIoListener < Qt::Object
+	slots 'dataRecieved()'
+	def initialize(app, iostream, lineHandler)
+		super(app)
+		@lineHandler = lineHandler
+		@iostream = iostream
+		streamListener = Qt::SocketNotifier.new(iostream.fileno, Qt::SocketNotifier.Read, app, 'stdinWatcher')
+		Qt::Object.connect(streamListener, SIGNAL("activated(int)"), self, SLOT("dataRecieved()") )
+
+	end
+	def dataRecieved
+		line = @iostream.gets
+		line.chomp!
+		@lineHandler.call(line)
+	end
+end
+
+class AmaroKLib
+#Either way, AmaroKLib depends of QtRuby by virtue of using the QtIoListener.
+#Making a non-Qt alternative to QtIoListener shouldn't be too difficult,
+#you'll just have to get your hands dirty with threads and then modify the definition of
+#@listen in this class. And send me the class for inclusion in this template. :)
+   def initialize(app, commandSlots)
+		@listener = QtIoListener.new(app, STDIN, Proc.new { |line|
+		#puts "Recieved command #{line}"
+		case line
+			when 'configure' then commandSlots.configure
+			when 'trackChange' then commandSlots.trackChange
+			when 'engineStateChange: empty' then commandSlots.stateChange_empty
+			when 'engineStateChange: idle' then commandSlots.stateChange_idle
+			when 'engineStateChange: paused' then commandSlots.stateChange_paused
+			when 'engineStateChange: playing' then commandSlots.stateChange_playing
+			#not a command given by amaroK, helpful when debugging
+			when 'exit' then commandSlots.exit
+			else $stderr.print "Illegal command: #{line}"
+		end	
+} )
+   end
+end
+
+#make your own child class of AmaroKSlots to send to AmaroKLib.
+class AmaroKSlots
+	def configure
+		nil
+	end
+	def stateChange_empty
+		nil
+	end	
+	def stateChange_idle
+		nil
+	end
+	def stateChange_paused
+		nil
+	end
+	def stateChange_playing
+		nil
+	end
+	def trackChange
+		nil
+	end
+	#not a command given by amaroK, used when debugging
+	def exit
+		nil
+	end
+end
---
+++ src/scripts/templates/ruby_qt_template.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env ruby
+############################################################################
+#    Copyright (C) 2005 by Ian Monroe                                      #
+#    ian@monroe.nu                                                         #
+#                                                                          #
+#    This program is free software; you can redistribute it and#or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation; either version 2 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+############################################################################
+
+require 'Qt'
+require "#{File.dirname( File.expand_path(__FILE__))}/amarok.rb"
+
+#a simple method to call DCOP. If you use Korundum, you can use
+#dR = KDE::DCOPRef.new("amarok", "player"); dR.artist instead
+def dcop (val1, val2 = String.new)
+	str =`dcop amarok player #{val1} #{val2}`
+	str.chomp! if str != nil
+	return str
+end
+
+#you can remove methods you don't need, they're all here just so that you can see
+
+class ExampleActions < AmaroKSlots
+	def initialize(app)
+		@app = app
+	end
+	def configure
+		puts "configure"
+	end
+	def stateChange_empty
+		puts "empty"
+	end	
+	def stateChange_idle
+		puts "idle"
+	end
+	def stateChange_paused
+		puts "paused"
+	end
+	def stateChange_playing
+		puts "playing"
+	end
+	def trackChange
+		puts "track changed"
+		puts "Now playing #{dcop('title')} by #{dcop('artist')}"
+	end
+	def exit
+		@app.exit
+	end
+end
+a = Qt::Application.new(ARGV)
+e = ExampleActions.new(a)
+amaroKCommunication = AmaroKLib.new(a, e)
+a.exec()
