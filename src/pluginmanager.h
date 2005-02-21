4609268acf453dff57cdd9ef6b282cbd6f302655
Stefan Siegel <kde@sdas.de>
2005-02-21 10:42:50 +0000

    Bump framework version because of Engine::Base::length()
    svn path=/trunk/kdeextragear-1/amarok/; revision=391689

--- src/pluginmanager.h
+++ src/pluginmanager.h
@@ -28,7 +28,7 @@ class PluginManager
 {
     public:
         /** Bump this number whenever the plugin framework gets incompatible with older versions */
-        static const int FrameworkVersion = 4;
+        static const int FrameworkVersion = 5;
 
         /**
          * It will return a list of services that match your
