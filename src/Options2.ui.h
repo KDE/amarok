9fb0d92627b89863a024b008d816beacc86e8539
Ian Monroe <ian.monroe@gmail.com>
2005-03-13 06:54:26 +0000

    *now the install new style button works. I basically took code from the slotInstallScript and simplified it. So we're ready to release as far as that is concerned I believe. *I actually remembered to fixuifiles this time. CCMAIL:amarok-devel@lists.sf.net
    svn path=/trunk/kdeextragear-1/amarok/; revision=397135

--- src/Options2.ui.h
+++ src/Options2.ui.h
@@ -11,15 +11,54 @@
 *****************************************************************************/
 #include "amarok.h"
 #include "amarokconfig.h"
+#include "debug.h"
 
 
 #include <kapplication.h>
 #include <kstandarddirs.h>
+#include <kfiledialog.h>
+#include <kmessagebox.h>
+#include <ktar.h>
+class KArchiveDirectory;
+
 
 #include <qdir.h>
 #include <qfileinfo.h>
 
 void Options2::init()
+{
+  updateStyleComboBox();
+}
+
+
+void Options2::installPushButton_clicked()
+{//this code is basically a slotInstallScript
+    KFileDialog dia( QString::null, "*.tar *.tar.bz2 *.tar.gz|" + i18n( "Script Packages (*.tar, *.tar.bz2, *.tar.gz)" ), 0, 0, true );
+    kapp->setTopWidget( &dia );
+    dia.setCaption( kapp->makeStdCaption( i18n( "Select Style Package" ) ) );
+    dia.setMode( KFile::File | KFile::ExistingOnly );
+    if ( !dia.exec() ) return;
+
+    KTar archive( dia.selectedURL().path() );
+
+    if ( !archive.open( IO_ReadOnly ) ) {
+        KMessageBox::sorry( 0, i18n( "Could not read this package." ) );
+        return;
+    }
+
+    QString destination = amaroK::saveLocation( "themes/" );
+    debug() << "copying to " << destination << endl;
+    const KArchiveDirectory* archiveDir = archive.directory();
+
+    archiveDir->copyTo( destination, true );
+   // KIO::NetAccess::move(destination + archiveDir->entries().first()
+    //                ,destination + archiveDir->entries().first().lower());
+    styleComboBox->clear();
+    updateStyleComboBox();
+}
+
+//function assumes emtpy styleComboBox
+void Options2::updateStyleComboBox()
 {
     QStringList styleList = kapp->dirs()->findAllResources("data","amarok/themes/*/stylesheet.css", false);
     QStringList sortedList;
@@ -29,6 +68,6 @@ void Options2::init()
         }
     sortedList.sort();
     foreach(sortedList)
-      styleComboBox->insertItem(*it);    
+      styleComboBox->insertItem(*it);
     styleComboBox->setCurrentItem(AmarokConfig::contextBrowserStyleSheet());
 }
