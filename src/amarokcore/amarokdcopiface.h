9b8f77751947a9342cabc9c2bf907d8804e2af7d
Mark Kretschmann <kretschmann@kde.org>
2005-03-21 21:01:10 +0000

    leftover from last commit.. something went wrong.
    svn path=/trunk/kdeextragear-1/amarok/; revision=399556

--- src/amarokcore/amarokdcopiface.h
+++ src/amarokcore/amarokdcopiface.h
@@ -104,6 +104,7 @@ k_dcop:
    virtual void clearPlaylist() = 0;                        ///< Clears the playlist.
    virtual void shufflePlaylist() = 0;                      ///< Shuffles the playlist.
    virtual QString saveCurrentPlaylist() = 0;               ///< Saves the current playlist to current.xml and returns its path.
+   virtual void removeCurrentTrack() = 0;                   ///< Removes the current-track item from the playlist.
    virtual void playByIndex(int) = 0;                       ///< Starts playing the track at the specified index.
    virtual int  getActiveIndex() = 0;                       ///< Return the index of the currently active track. -1 if none.
    virtual void setStopAfterCurrent( bool ) = 0;            ///< Enables/disables the "Stop After Current Track" feature.
