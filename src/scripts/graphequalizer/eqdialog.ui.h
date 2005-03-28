a52df3949edf3e67fd9b61e345f802420546a818
Ian Monroe <ian.monroe@gmail.com>
2005-03-28 00:03:33 +0000

    Graphequalizer script can now enable and disable the equalizer. Apparently a user thought that graphequalizer was /the/ equalizer and concluded that amaroK's equalizer doesn't work, this should fix that. CCMAIL:amarok-devel@lists.sf.net
    svn path=/trunk/kdeextragear-1/amarok/; revision=401152

--- src/scripts/graphequalizer/eqdialog.ui.h
+++ src/scripts/graphequalizer/eqdialog.ui.h
@@ -9,13 +9,39 @@
 ** These will automatically be called by the form's constructor and
 ** destructor.
 *****************************************************************************/
+#include <dcopclient.h>
+#include <kapplication.h>
+//#include <kdebug.h>
 
 void EqDialog::init()
 {
+ 	//kdDebug() << "start" << endl;
     QCanvas* canvas = new QCanvas();
     canvas->resize(400, 200);
     canvasView->setVScrollBarMode(QScrollView::AlwaysOff);
     canvasView->setHScrollBarMode(QScrollView::AlwaysOff);
     canvasView->setCanvas(canvas);
     canvasView->init();
+    QByteArray send_data, reply_data;
+    QCString reply_type;
+    //kdDebug() << "continue" << endl;
+    if(!KApplication::dcopClient()->call("amarok","player","equalizerEnabled()", send_data, reply_type, reply_data,true,1000));
+    // kdDebug() << "oh crap" << endl;
+    //kdDebug() << "called" << endl;
+    QDataStream answer(reply_data, IO_ReadOnly);
+    //kdDebug() << "answer created" << answer << endl;
+    bool eqEnabled;
+    answer >> eqEnabled;
+    //kdDebug() << "eqEnabled set to " << eqEnabled << endl;
+    eqGroupBox->setChecked(eqEnabled);
+    //kdDebug() << "end" << endl;
+}
+
+
+void EqDialog::eqGroupBox_toggled( bool eqEnabled)
+{
+        QByteArray data;
+        QDataStream arg(data, IO_WriteOnly);
+        arg << eqEnabled;
+        KApplication::dcopClient()->send("amarok", "player", "setEqualizerEnabled(bool)" , data);
 }
