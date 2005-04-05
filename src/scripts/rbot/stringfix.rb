5ffff13c3ff5af77642f0af3f43e08149fec061b
Mark Kretschmann <kretschmann@kde.org>
2005-04-05 16:39:05 +0000

    Tabs, we hates them.
    svn path=/trunk/kdeextragear-1/amarok/; revision=403345

--- src/scripts/rbot/stringfix.rb
+++ src/scripts/rbot/stringfix.rb
@@ -1,18 +1,18 @@
 class String
 
     def shorten(limit)
-	if self.length > limit
-	    self+". " =~ /^(.{#{limit}}[^.!;?]*[.!;?])/mi
-	    return $1
-	end
-	self
+    if self.length > limit
+        self+". " =~ /^(.{#{limit}}[^.!;?]*[.!;?])/mi
+        return $1
     end
-    
+    self
+    end
+
     def riphtml
-	self.gsub(/<[^>]+>/, '').gsub(/&amp;/,'&').gsub(/&quot;/,'"').gsub(/&lt;/,'<').gsub(/&gt;/,'>').gsub(/&ellip;/,'...').gsub(/&apos;/, "'").gsub("\n",'')
+    self.gsub(/<[^>]+>/, '').gsub(/&amp;/,'&').gsub(/&quot;/,'"').gsub(/&lt;/,'<').gsub(/&gt;/,'>').gsub(/&ellip;/,'...').gsub(/&apos;/, "'").gsub("\n",'')
     end
-    
+
     def mysqlize
-	self.gsub(/'/, "''")
+    self.gsub(/'/, "''")
     end
 end
