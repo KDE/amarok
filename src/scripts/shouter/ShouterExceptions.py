82fd4cf148e55d75d72dfe10d71a881462abdb09
James Bellenger <jbellenger@pristine.gm>
2005-04-07 11:38:31 +0000

    Import of amarok shouter script: http://www.kde-apps.org/content/show.php?content=22170
    James Bellenger
    
    
    svn path=/trunk/kdeextragear-1/amarok/; revision=403747

---
+++ src/scripts/shouter/ShouterExceptions.py
@@ -0,0 +1,8 @@
+from exceptions import *
+
+class chunk_not_found_error(Exception):
+	pass
+
+class chunk_exception(Exception):
+	pass
+
---
+++ src/scripts/shouter/test.py
@@ -0,0 +1,3 @@
+from Encoder import *
+e = Encoder( '/mnt/aux/mp3/Cursive/Cursive - The Ugly Organ/03 - Art Is Hard.mp3')
+
