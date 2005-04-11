fd497bd59e79fc73650c47388bcf843b0a1f5f7d
Mark Kretschmann <kretschmann@kde.org>
2005-04-11 14:32:35 +0000

    Add new script.
    svn path=/trunk/kdeextragear-1/amarok/; revision=404805

---
+++ src/scripts/rbot/opinion/opinion.rb
@@ -0,0 +1,33 @@
+# Opinion plugin
+
+class OpinionPlugin < Plugin
+    def help(plugin, topic="")
+        "Ask sanity for its opinion on something"
+    end
+
+    def privmsg(m)
+        opinions = ["makes me happy.",
+                    "makes no sense to me.",
+                    "- I'm loving it!",
+                    "is fantastic!",
+                    "cracks me up, dude.",
+                    "reminds me of GNOME.",
+                    "is like a dream come true.",
+                    "is a sexy hot bitch.",
+                    "has some usability issues.",
+                    "is like a flight with LSD airlines!",
+                    "rocks me hard!",
+                    "should be forbidden.",
+                    "makes me horny.",
+                    "is quite amusing."]
+
+        if m.params
+            @bot.say(m.replyto, "#{m.params} #{opinions.at(rand(opinions.length()))}")
+        end
+   end
+end
+
+plugin = OpinionPlugin.new
+plugin.register("opinion")
+
+
