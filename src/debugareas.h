b11e5e739a6302f90b6f099b45ebbcec86684550
Max Howell <max.howell@methylblue.com>
2005-04-25 01:31:57 +0000

    No longer used
    svn path=/trunk/kdeextragear-1/amarok/; revision=407655

--- src/debugareas.h
+++
@@ -1,7 +0,0 @@
-/* kdDebug() area codes for amaroK */
-#if !defined( DA_COMMON )
-#define DA_COMMON   101000
-#define DA_PLAYER   101001
-#define DA_PLAYLIST 101002
-#define DA_BROWSER  101003
-#endif
