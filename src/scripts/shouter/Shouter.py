754b15084363e1acb837fa4ca1d631ba8fa1be2d
James Bellenger <jbellenger@pristine.gm>
2005-04-27 10:14:49 +0000

    Fixes for broken makefiles, missing headers, and broken paths to env
    svn path=/trunk/kdeextragear-1/amarok/; revision=408133

--- src/scripts/shouter/Shouter.py
+++ src/scripts/shouter/Shouter.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 ############################################################################
 # Main executable. Must be run by amaroK
--- src/scripts/shouter/ShouterExceptions.py
+++ src/scripts/shouter/ShouterExceptions.py
@@ -1,3 +1,16 @@
+############################################################################
+# Binfuncs, taken from eye3D <http://eye3d.nicfit.net>
+#
+# Depends on: Python 2.2
+############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+############################################################################
+
 from exceptions import *
 
 class cfg_not_found_error(Exception):
