e015e11c181496e69c0226683f97e1a8ba3ec04e
Christian Muehlhaeuser <chris@chris.de>
2005-05-10 01:29:53 +0000

    * Make Update button work again (correct enabled-status).
    svn path=/trunk/extragear/multimedia/amarok/; revision=411738

--- src/collectionreader.cpp
+++ src/collectionreader.cpp
@@ -95,8 +95,6 @@ IncrementalCollectionReader::doJob()
 bool
 CollectionReader::doJob()
 {
-    if ( m_folders.isEmpty() )
-        return true;
     if ( !m_db->isConnected() )
         return false;
 
