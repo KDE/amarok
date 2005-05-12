50298f10768b573f63eed470390c916d7854923f
Jakub Stachowski <qbast@go2.pl>
2005-05-12 19:42:07 +0000

    Search harder for Publisher.py Fix my mess of tabs and spaces
    svn path=/trunk/extragear/multimedia/amarok/; revision=412856

--- src/scripts/shouter/StreamPublisher.py
+++ src/scripts/shouter/StreamPublisher.py
@@ -17,6 +17,7 @@ import os
 import sys
 import string
 # find directory containing common Zeroconf files
+sys.path.insert(0,os.path.abspath(os.path.dirname(sys.argv[0])+'/../common'))
 if not os.getenv("KDEDIR") is None: sys.path.insert(0,os.getenv("KDEDIR")+"/share/apps/amarok/scripts/common")
 if not os.getenv("KDEDIRS") is None: sys.path=[p+"/share/apps/amarok/scripts/common" for p in string.split(os.getenv("KDEDIRS"),os.pathsep)]+sys.path
 from Publisher import *
@@ -28,10 +29,10 @@ class StreamPublisher(Publisher):
 
     def services(self):
         sv = []
-    	if not self.cfg_mgr.server_cfg.publish==1: return sv
+        if not self.cfg_mgr.server_cfg.publish==1: return sv
         for i in self.cfg_mgr.stream_cfgs:
-	    sv.append( { "name" : i.name , "port": self.port, "type": "_shoutcast._tcp", "properties": { "path": "/"+i.mount }} )
-	    debug("Will register %s mounted at %s" % (i.name,i.mount))
-	return sv
+            sv.append( { "name" : i.name , "port": self.port, "type": "_shoutcast._tcp", "properties": { "path": "/"+i.mount }} )
+            debug("Will register %s mounted at %s" % (i.name,i.mount))
+        return sv
     
 publisher = StreamPublisher()
