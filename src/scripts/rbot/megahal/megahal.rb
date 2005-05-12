b23b42413534e3434894aa89bdeea0ecfc8f00a4
Mark Kretschmann <kretschmann@kde.org>
2005-05-12 12:36:45 +0000

    more
    svn path=/trunk/extragear/multimedia/amarok/; revision=412730

--- src/scripts/rbot/megahal/megahal.rb
+++ src/scripts/rbot/megahal/megahal.rb
@@ -17,6 +17,7 @@ class MegaHal
     private
     def boundary?(string, pos)
         alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        digit = '0123456789'
 
         if pos == 0
             return false
@@ -26,8 +27,24 @@ class MegaHal
             return true
         end
 
-        if string[pos] == "\" and alpha.include?(string[pos-1]) and alpha.include?(string[pos+1])
+        if string[pos] == ' ' and alpha.include?(string[pos-1]) and alpha.include?(string[pos+1])
             return false
+        end
+
+        if pos > 1 and string[pos-1] == ' ' and alpha.include?(string[pos-2]) and alpha.include?(string[pos])
+            return false
+        end
+
+        if alpha.include?(string[pos]) and not alpha.include?(string[pos-1])
+            return true
+        end
+
+        if not alpha.include?(string[pos]) and alpha.include?(string[pos-1])
+            return true
+        end
+
+        if digit.include?(string[pos] != digit.incldue[pos-1]
+            return true
         end
     end
 
@@ -35,6 +52,16 @@ class MegaHal
     # Breaks a string into an array of words.
     private
     def make_words(input, words)
+        offset = 0
+
+        loop do
+            if boundary?(input, offset)
+
+                offset = 0
+            else
+                offset = offset+1
+            end
+        end
     end
 
 
