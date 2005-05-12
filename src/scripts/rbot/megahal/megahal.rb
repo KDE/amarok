78614e2962ce0186e2c6a89c0e507b7bf5b08430
Mark Kretschmann <kretschmann@kde.org>
2005-05-12 09:54:56 +0000

    Progress.
    svn path=/trunk/extragear/multimedia/amarok/; revision=412662

--- src/scripts/rbot/megahal/megahal.rb
+++ src/scripts/rbot/megahal/megahal.rb
@@ -7,26 +7,47 @@
 # Licens GNU General Public License V2
 
 
-def initialize
+class MegaHal
+
+    def initialize
+    end
+
+
+    # Returns whether or not a word boundary exists in a string at the specified location.
+    private
+    def boundary?(string, pos)
+        alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+        if pos == 0
+            return false
+        end
+
+        if pos == string.length
+            return true
+        end
+
+        if string[pos] == "\" and alpha.include?(string[pos-1]) and alpha.include?(string[pos+1])
+            return false
+        end
+    end
+
+
+    # Breaks a string into an array of words.
+    private
+    def make_words(input, words)
+    end
+
+
+    def do_reply(input)
+        input.upcase!
+        make_words(input, @words)
+
+        learn(@model, @words)
+
+        output = generate_reply(@model, @words)
+        capitalize(output)
+        return output
+    end
 
 end
-
-
-# Breaks a string into an array of words.
-private
-def make_words(input, words)
-end
-
-
-def do_reply(input)
-    input.upcase!
-    make_words(input, @words)
-
-    learn(@model, @words)
-
-    output = generate_reply(@model, @words)
-    capitalize(output)
-    return output
-end
-
 
