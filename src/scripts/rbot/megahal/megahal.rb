e3bdf6fc61d8277e6f6d9aeceee19f13ba46381f
Mark Kretschmann <kretschmann@kde.org>
2005-05-13 17:12:47 +0000

    Ported some code from JMegaHal.
    svn path=/trunk/extragear/multimedia/amarok/; revision=413224

--- src/scripts/rbot/megahal/megahal.rb
+++ src/scripts/rbot/megahal/megahal.rb
@@ -1,16 +1,74 @@
 # This is a port of the MegaHal chatterbot software to Ruby.
 # The original code can be downloaded here: http://megahal.alioth.debian.org/
+# This port is also based on JMegaHal, http://www.jibble.org/jmegahal/
 #
 # (C) 1998 Jason Hutchens
+# (C) 2001-2004 Paul James Mutton
 # (C) 2005 Mark Kretschmann <markey@web.de>
 #
-# Licens GNU General Public License V2
+# License: GNU General Public License V2
 
+
+######################################################################
+# class Quad
+######################################################################
+
+class Quad
+
+    def initialize(s1, s2, s3, s4)
+        @tokens = Array[s1, s2, s3, s4]
+        @canStart = false
+        @canEnd = false
+    end
+
+    def getToken(index)
+        return @tokens[index]
+    end
+
+    def setCanStart(flag)
+        @canStart = flag
+    end
+
+    def setCanEnd(flag)
+        @canEnd = flag
+    end
+
+    def canStart()
+        return @canStart
+    end
+
+    def canEnd()
+        return @canEnd
+    end
+
+    def hashCode()
+        return tokens[0].hash() +
+               tokens[1].hash() +
+               tokens[2].hash() +
+               tokens[3].hash()
+    end
+
+    def ==(other)
+        return other.token[0] == @token[0] and
+               other.token[1] == @token[1] and
+               other.token[2] == @token[2] and
+               other.token[3] == @token[3]
+    end
+
+end
+
+
+######################################################################
+# class MegaHal
+######################################################################
 
 class MegaHal
 
     def initialize
-        @words = Array.new()
+        @words = Hash.new()
+        @quads = Hash.new()
+        @next = Hash.new()
+        @previous = Hash.new()
     end
 
 
@@ -54,13 +112,14 @@ class MegaHal
 
 
     # Breaks a string into an array of words.
-    def make_words(input, words)
+    def make_parts(input)
         offset = 0
+        parts = Array.new()
 
         loop do
             if boundary?(input, offset)
                 # Add word to array
-                words << input.slice(0..offset-1)
+                parts << input.slice(0..offset-1)
 
                 break if offset == input.length()
                 input.slice!(0..offset-1)
@@ -68,6 +127,28 @@ class MegaHal
             else
                 offset = offset+1
             end
+        end
+
+        return parts
+    end
+
+
+    def learn(parts)
+        if parts.size() >= 4
+
+        i = 0
+        while i < parts.size()-3 do
+            quad = Quad.new(parts[0], parts[1], parts[2], parts[3])
+            if quads.containsKey(quad)
+                quad = quads[quad]
+            else
+
+
+            i = i+1
+        end
+
+        else
+            puts "Parts has less than 4 elements, skipping."
         end
     end
 
@@ -78,15 +159,15 @@ class MegaHal
 
     def do_reply(input)
         input.upcase!
-        make_words(input, @words)
+        parts = make_parts(input)
 
-#         learn(@model, @words)
+#         learn(parts)
 #
 #         output = generate_reply(@model, @words)
 #         capitalize(output)
 
         # testing
-        output = @words.join()
+        output = parts.join()
 
         return output
     end
