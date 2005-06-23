9b76dd1c465613194fc3fb02146bd276ff6f2543
Mark Kretschmann <kretschmann@kde.org>
2005-06-23 17:17:34 +0000

    Fix automatic theme download with KNewStuff. As Alex said, it was necessary to overload the install() method of KNewStuff.
    BUG: 107313
    
    
    svn path=/trunk/extragear/multimedia/amarok/; revision=428296

--- src/Options2.ui.h
+++ src/Options2.ui.h
@@ -9,31 +9,78 @@
 ** These will automatically be called by the form's constructor and
 ** destructor.
 *****************************************************************************/
+
 #include "amarok.h"
 #include "amarokconfig.h"
 #include "debug.h"
 
-
 #include <kapplication.h>
-#include <kstandarddirs.h>
 #include <kfiledialog.h>
 #include <kmessagebox.h>
+#include <knewstuff/downloaddialog.h> // knewstuff script fetching
+#include <knewstuff/engine.h>         // "
+#include <knewstuff/knewstuff.h>      // "
+#include <knewstuff/provider.h>       // "
+#include <kstandarddirs.h>
 #include <ktar.h>
-#include <knewstuff/downloaddialog.h>
-class KArchiveDirectory;
-
 
 #include <qdir.h>
 #include <qfileinfo.h>
 
+
+static Options2* s_Options2_instance = 0;
+
+////////////////////////////////////////////////////////////////////////////////
+// class AmarokScriptNewStuff
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * GHNS Customised Download implementation.
+ */
+class AmarokThemeNewStuff : public KNewStuff
+{
+    public:
+    AmarokThemeNewStuff(const QString &type, QWidget *parentWidget=0)
+             : KNewStuff( type, parentWidget )
+    {}
+
+    bool install( const QString& fileName )
+    {
+        KTar archive( fileName );
+
+        if ( !archive.open( IO_ReadOnly ) ) {
+            KMessageBox::sorry( 0, i18n( "Could not read this package." ) );
+            return false;
+        }
+
+        const QString destination = amaroK::saveLocation( "themes/" );
+        debug() << "copying to " << destination << endl;
+        const KArchiveDirectory* archiveDir = archive.directory();
+        archiveDir->copyTo( destination, true );
+
+        s_Options2_instance->styleComboBox->clear();
+        return true;
+    }
+
+    virtual bool createUploadFile( const QString& ) { return false; }
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// class Options2
+////////////////////////////////////////////////////////////////////////////////
+
 void Options2::init()
 {
-  updateStyleComboBox();
+    s_Options2_instance = this;
+
+    updateStyleComboBox();
 }
 
 
+// This method is basically lifted from ScriptManager::slotInstallScript()
 void Options2::installPushButton_clicked()
-{//this code is basically a slotInstallScript
+{
     KFileDialog dia( QString::null, "*.tar *.tar.bz2 *.tar.gz|" + i18n( "Style Packages (*.tar, *.tar.bz2, *.tar.gz)" ), 0, 0, true );
     kapp->setTopWidget( &dia );
     dia.setCaption( kapp->makeStdCaption( i18n( "Select Style Package" ) ) );
@@ -47,36 +94,44 @@ void Options2::installPushButton_clicked()
         return;
     }
 
-    QString destination = amaroK::saveLocation( "themes/" );
+    const QString destination = amaroK::saveLocation( "themes/" );
     debug() << "copying to " << destination << endl;
     const KArchiveDirectory* archiveDir = archive.directory();
-
     archiveDir->copyTo( destination, true );
-   // KIO::NetAccess::move(destination + archiveDir->entries().first()
-    //                ,destination + archiveDir->entries().first().lower());
+
     styleComboBox->clear();
     updateStyleComboBox();
 }
 
+
 //function assumes emtpy styleComboBox
 void Options2::updateStyleComboBox()
 {
-    QStringList styleList = kapp->dirs()->findAllResources("data","amarok/themes/*/stylesheet.css", false);
+    const QStringList styleList = kapp->dirs()->findAllResources("data","amarok/themes/*/stylesheet.css", false);
     QStringList sortedList;
-    foreach (styleList)
-        {
-            sortedList.append(QFileInfo( *it ).dir().dirName());
-        }
+    foreach (styleList) sortedList.append(QFileInfo( *it ).dir().dirName());
     sortedList.append( "Default" );
     sortedList.sort();
-    foreach(sortedList)
-           styleComboBox->insertItem(*it);
+    foreach(sortedList) styleComboBox->insertItem(*it);
+
     styleComboBox->setCurrentItem(AmarokConfig::contextBrowserStyleSheet());
 }
 
+
 void Options2::retrievePushButton_clicked()
 {
-    KNS::DownloadDialog::open("amaroK Themes");
+    // we need this because KNewStuffGeneric's install function isn't clever enough
+    AmarokThemeNewStuff *kns = new AmarokThemeNewStuff( "amarok/themes", this );
+    KNS::Engine *engine = new KNS::Engine( kns, "amarok/theme", this );
+    KNS::DownloadDialog *d = new KNS::DownloadDialog( engine, this );
+    d->setType( "amarok/theme" );
+    // you have to do this by hand when providing your own Engine
+    KNS::ProviderLoader *p = new KNS::ProviderLoader( this );
+    QObject::connect( p, SIGNAL( providersLoaded(Provider::List*) ), d, SLOT( slotProviders (Provider::List *) ) );
+    p->load( "amarok/theme", "http://amarok.kde.org/knewstuff/amarokthemes-providers.xml" );
+
+    d->exec();
+
     styleComboBox->clear();
     updateStyleComboBox();
 }
