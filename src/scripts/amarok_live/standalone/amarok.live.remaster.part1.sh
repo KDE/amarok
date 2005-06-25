940405888d9ddf8698a986f75e68c0d6f4dd610d
Leo Franchi <lfranchi@kde.org>
2005-06-25 18:50:58 +0000

    More feedback on what the script is doing Added standalone versions in standalone/. They will not be installed, just live in svn.
    svn path=/trunk/extragear/multimedia/amarok/; revision=428901

--- src/scripts/amarok_live/standalone/amarok.live.remaster.part1.sh
+++ src/scripts/amarok_live/standalone/amarok.live.remaster.part1.sh
@@ -1,37 +1,21 @@
 #!/usr/bin/env bash
 #set -x 
-
-############################################################################
-# Disassemble an amaroK LiveCD iso file into a specified directory
-# for the purpose of adding a users own music and putting it back together
+# Disassemble a mklivecd iso file into the /tmp/mklivecd directory
+# Author: Ivan Kerekes <ikerekes@gmail.com>
 #
 # Based on a script authored by Ivan Kerekes <ikerekes@gmail.com>
-# and modified by Leo Franchi <lfranchi at gmaildotcom>, Mike Diehl 
-# <madpenguin8 at yahoodotcom> and Greg Meyer <greg at gkmwebdotcom> 
-#
-# See the file called README for more information
-############################################################################
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-############################################################################
+# and modified by Leo Franchi, Mike Diehl and Greg Meyer 
 
 # We need to be root to mount the loopback
+
 # check for root and exit if not
 if [ `/usr/bin/whoami` = 'root' ]; then
 
-dcop --all-users amarok playlist popupMessage "Welcome to the amaroK live cd remaster utility. The first step is to select the iso image"
+kdialog --title "amaroK livecd remaster" --yesno "Welcome to the amaroK live cd remaster utility. The first step is to select the iso image, would you like to continue."
+
+if [ $? = 0 ] ; then
 
 iso=`kdialog --getopenfilename /home "*.iso"`
-
-if [ $iso = 0 ] ; then
-    exit;
-fi
-
-dcop --all-users amarok playlist popupMessage "Now please select a working directory. You will need 1.5 GB of free space in this dir."
 
 WORK=`kdialog --title "Choose working directory" --getexistingdirectory .`
 
@@ -49,13 +33,13 @@ while [ "$redo" = "0" ]; do
         if [[ -n  `df | grep $tmp` ]] ; then # we got it in df, find the space left
             anotmp=`df | grep $tmp |  sed "s~^\([^ ]*\) *\([^ ]*\) *\([^ ]*\) *\([^ ]*\) *\([^ ]*\) *\([^ ]*\)$~\4~"`  # now we have a string, first item is free space in /
             free=`echo $anotmp  | sed "s~^\([^ ]*\) \(.*\)~\1~"` # get first space-delimited item
-#			echo "comparing" $free "to 1572864"
+			echo "comparing" $free "to 1572864"
             if [[ $free -gt 1572864 ]] ; then
                 enough=1
                 redo=1
                 break
             else
-                dcop --all-users amarok playlist popupMessage "Not enough free space. Please select another folder."
+                kdialog --sorry "Not enough free space. Please select another folder."
                 break
             fi
         else
@@ -65,7 +49,7 @@ while [ "$redo" = "0" ]; do
             else #normal, removes one dir from path
                 tmp=$res
             fi
-#            echo "new tmp: " $tmp
+            echo "new tmp: " $tmp
         fi
     done
     if [[ "$redo" = "0" ]]; then
@@ -80,8 +64,6 @@ while [ "$redo" = "0" ]; do
     fi
 done
 
-echo $WORK > /tmp/amarok.script
-
 # Mount the iso if not already mounted
 if [ ! -d "$DATADIR" ]; then
    DATADIR=$WORK/livecd_data$$
@@ -90,8 +72,6 @@ if [ ! -d "$DATADIR" ]; then
 fi
 
 # Make the working directories and blow out the initrd.gz into a separate directory 
-
-dcop --all-users amarok playlist popupMessage "Unpacking started. Please be patient."
  
 mkdir -p $WORK/mklivecd/livecd
 cp -a --preserve "$DATADIR"/* $WORK/mklivecd/livecd/
@@ -115,7 +95,7 @@ if [[  `cat /proc/filesystems | grep squash | wc -l` = 0 ]]; then
 	modprobe squashfs
 	if [[  `cat /proc/filesystems | grep squash | wc -l` = 0 ]]; then
 
-    	 dcop --all-users amarok playlist popupMessage "You do not have squashfs support enabled. You need to have a patched kernel with squashfs. You can find more info about squashfs, and how to patch your kernel, here: http://tldp.org/HOWTO/SquashFS-HOWTO/"
+    	kdialog --title "amaroK livecd remaster" --error "You do not have squashfs support enabled. You need to have a patched kernel with squashfs. You can find more info about squashfs, and how to patch your kernel, here: http://tldp.org/HOWTO/SquashFS-HOWTO/"
 	
 		rm -rf $WORK/mklivecd
 	fi
@@ -127,12 +107,17 @@ mount -o loop -t squashfs $WORK/mklivecd/livecd/livecd.sqfs $WORK/amarok.livecd/
 # gotta copy it locally so the user can add files to it
 
 mkdir $WORK/amarok.live/
-dcop --all-users amarok playlist shortStatusMessage "Copying files now. Please be patient, this step takes a long time."
+kdialog --title "amaroK livecd remaster" --msgbox "Copying files now. Please be patient, this step takes a long time."
+echo
+echo "Please wait, copying in progress."
+echo
 cp -a $WORK/amarok.livecd/* $WORK/amarok.live/
 umount $WORK/amarok.livecd/
 rmdir $WORK/amarok.livecd
 
-dcop --all-users amarok playlist popupMessage "Copying done. To add music to the amaroK livecd, select the tracks you wish to add in the playlist, and select \"Add to livecd\" from the right click menu. Please do not add more than about 380 mb, as then the resulting ISO will be too large to fit on a CD-ROM. Once you are done, select Create Remastered CD. Enjoy!"
+kdialog --title "amaroK livecd remaster" --msgbox "Copying done. To add music to the amaroK livecd, place additional music in /tmp/amarok.live/music/ Please do not add more than about 380 mb, as then the resulting ISO will be too large to fit on a CD-ROM. Once you are done, run the amarok.live.remaster.part2.sh script and you are finished!."
+
+fi
 
 else 
 
---
+++ src/scripts/amarok_live/standalone/amarok.live.remaster.part2.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#first, resquash the users' new fs
+
+if [ `/usr/bin/whoami` = 'root' ]; then
+
+	WORK=`kdialog --title "Choose working directory" --getexistingdirectory .`
+
+	kdialog --title "amaroK livecd remaster" --msgbox "The music that you added is now being squashed and added to the livecd. Please be VERY patient as this step can take a LONG time."
+	
+	mksquashfs $WORK/amarok.live/ $WORK/mklivecd/livecd/livecd.sqfs -noappend
+
+
+	olddir=`pwd`
+	cd $WORK/mklivecd
+	dd if=/dev/zero of=livecd/isolinux/initrd bs=1k count=4646
+	mke2fs -q -m 0 -F -N 1250 -s 1 livecd/isolinux/initrd
+	mount -o loop -t ext2 livecd/isolinux/initrd initrd.mnt
+	rm -rf initrd.mnt/lost+found
+	(cd initrd.dir ; tar cf - .) | (cd initrd.mnt ; tar xf -)
+	umount initrd.mnt
+	cd livecd/isolinux
+	gzip -f -9 initrd
+	cd ..
+	ll
+	rm -f livecd.iso
+	mkisofs -J -R -V "Livecd Test" -o $WORK/amaroK.live.custom.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table .
+	cd $olddir
+	mv $WORK/amaroK.live.custom.iso .
+	
+	kdialog --title "amaroK livecd remaster" --yesno "Livecd creation is done. The new amaroK live image is in `pwd`, called amaroK.live.custom.iso. You can burn that with any standard burner and enjoy.
+	
+	Do you want to make more cds later? If so, please click yes, and you can simply re-run part2 of this script. If you are done, click no and the temporary files will be erased. You will need to rerun part1 to make another cd."
+	
+	if [[ $? = 1 ]]; then
+		rm -rf $WORK/mklivecd/
+		rm -rf $WORK/amarok.live/
+	fi
+
+	
+else
+	kdialog --title "amaroK livecd remaster" --sorry "You must run this script as root. Try running 'kdesu sh amarok.live.remaster.part1.sh' instead."
+
+fi
