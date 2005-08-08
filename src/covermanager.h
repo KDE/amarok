cb847e5ab01f3f8ee2f1a77d9e0540e8cc47177a
Seb Ruiz <ruiz@kde.org>
2005-08-08 00:23:07 +0000

    * Partially fix mem leak for cover manager - overloading the destructor means that ~QIconViewItem is not called and hence does not delete the items. I still have 10Mb lying around somewhere * I tried to use the cached images for loading the images, but no decrease in memory usage was noticed. CCBUG:95042
    svn path=/trunk/extragear/multimedia/amarok/; revision=443943

--- src/covermanager.h
+++ src/covermanager.h
@@ -111,7 +111,6 @@ class CoverView : public KIconView
 
     public:
         CoverView( QWidget *parent = 0, const char *name = 0, WFlags f = 0 );
-        ~CoverView() { }
 
     protected:
         QDragObject *dragObject();
