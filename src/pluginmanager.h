4be52b0fe4372975172930503b774b77bc7d293c
Mark Kretschmann <kretschmann@kde.org>
2005-08-09 07:13:53 +0000

    Executed bump_plugin_version.rb
    svn path=/trunk/extragear/multimedia/amarok/; revision=444200

--- src/pluginmanager.h
+++ src/pluginmanager.h
@@ -28,7 +28,7 @@ class PluginManager
 {
     public:
         /** Bump this number whenever the plugin framework gets incompatible with older versions */
-        static const int FrameworkVersion = 8;
+        static const int FrameworkVersion = 9;
 
         /**
          * It will return a list of services that match your
