8eed95a637fcd89175d797f80a8c8805c58c68ca
Mark Kretschmann <kretschmann@kde.org>
2005-08-26 07:46:59 +0000

    Actually make my last commit work O.o
    I'm wondering if we could save&restore the window settings, like "Keep above others"? That might be comfy.
    
    
    svn path=/trunk/extragear/multimedia/amarok/; revision=453435

--- src/playerwindow.cpp
+++ src/playerwindow.cpp
@@ -84,7 +84,7 @@ createWidget( const QRect &r, QWidget *parent, const char *name = 0, Qt::WFlags 
 
 
 PlayerWidget::PlayerWidget( QWidget *parent, const char *name, bool enablePlaylist )
-    : QWidget( parent, name, Qt::WType_TopLevel|WStyle_Customize|WStyle_DialogBorder )
+    : QWidget( parent, name, Qt::WType_TopLevel )
     , EngineObserver( EngineController::instance() )
     , m_minimalView( false )
     , m_pAnimTimer( new QTimer( this ) )
