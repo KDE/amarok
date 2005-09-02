a5c637bbc4f44d1e3edf3337cd937abaf3c38d58
Mark Kretschmann <kretschmann@kde.org>
2005-09-02 16:38:55 +0000

    Beginnings of pretty popup menu.
    svn path=/trunk/extragear/multimedia/amarok/; revision=456337

---
+++ src/prettypopupmenu.cpp
@@ -0,0 +1,101 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Mark Kretschmann <markey@web.de>                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
+ ***************************************************************************/
+
+#include "prettypopupmenu.h"
+
+
+#include <kstandarddirs.h>
+
+
+////////////////////////////////////////////////////////////////////////////////
+// public
+////////////////////////////////////////////////////////////////////////////////
+
+PrettyPopupMenu::PrettyPopupMenu( QWidget* parent, const char* name )
+    : KPopupMenu( parent, name )
+    , m_sidePixmap( locate( "data","amarok/data/menu_sidepixmap.png" ) )
+{}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// private
+////////////////////////////////////////////////////////////////////////////////
+
+void
+PrettyPopupMenu::setMinimumSize(const QSize & s)
+{
+    KPopupMenu::setMinimumSize(s.width() + m_sidePixmap.width(), s.height());
+}
+
+void
+PrettyPopupMenu::setMaximumSize(const QSize & s)
+{
+    KPopupMenu::setMaximumSize(s.width() + m_sidePixmap.width(), s.height());
+}
+
+void
+PrettyPopupMenu::setMinimumSize(int w, int h)
+{
+    KPopupMenu::setMinimumSize(w + m_sidePixmap.width(), h);
+}
+
+void
+PrettyPopupMenu::setMaximumSize(int w, int h)
+{
+  KPopupMenu::setMaximumSize(w + m_sidePixmap.width(), h);
+}
+
+void
+PrettyPopupMenu::paintEvent( QPaintEvent* e )
+{
+    if (sidePixmap.isNull()) {
+        PanelServiceMenu::paintEvent(e);
+        return;
+    }
+
+    QPainter p(this);
+    p.setClipRegion(e->region());
+
+    style().drawPrimitive( QStyle::PE_PanelPopup, &p,
+                           QRect( 0, 0, width(), height() ),
+                           colorGroup(), QStyle::Style_Default,
+                           QStyleOption( frameWidth(), 0 ) );
+
+    QRect r = sideImageRect();
+    r.setBottom( r.bottom() - sidePixmap.height() );
+    if ( r.intersects( e->rect() ) )
+    {
+        p.drawTiledPixmap( r, sideTilePixmap );
+    }
+
+    r = sideImageRect();
+    r.setTop( r.bottom() - sidePixmap.height() );
+    if ( r.intersects( e->rect() ) )
+    {
+        QRect drawRect = r.intersect( e->rect() );
+        QRect pixRect = drawRect;
+        pixRect.moveBy( -r.left(), -r.top() );
+        p.drawPixmap( drawRect.topLeft(), sidePixmap, pixRect );
+    }
+
+    drawContents( &p );
+}
+
+
+#include "prettypopupmenu.moc"
---
+++ src/prettypopupmenu.h
@@ -0,0 +1,42 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Mark Kretschmann <markey@web.de>                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
+ ***************************************************************************/
+
+#ifndef AMAROK_PRETTYPOPUPMENU_H
+#define AMAROK_PRETTYPOPUPMENU_H
+
+#include <kpopupmenu.h>
+
+#include <qpixmap.h>
+
+
+class PrettyPopupMenu : public KPopupMenu
+{
+    Q_OBJECT
+
+public:
+    PrettyPopupMenu( QWidget *parent = 0, const char *name = 0 );
+
+private:
+    void paintEvent( QPaintEvent* e );
+
+    QPixmap m_sidePixmap;
+};
+
+
+#endif /*AMAROK_PRETTYPOPUPMENU_H*/
