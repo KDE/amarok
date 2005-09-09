30533628886c0d7723a95380022d10cc28307f33
Mark Kretschmann <kretschmann@kde.org>
2005-09-09 08:13:17 +0000

    bump_plugin_version
    svn path=/trunk/extragear/multimedia/amarok/; revision=458890

--- src/pluginmanager.h
+++ src/pluginmanager.h
@@ -28,7 +28,7 @@ class PluginManager
 {
     public:
         /** Bump this number whenever the plugin framework gets incompatible with older versions */
-        static const int FrameworkVersion = 10;
+        static const int FrameworkVersion = 11;
 
         /**
          * It will return a list of services that match your
