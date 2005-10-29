2b807a0952a867e8f481f64324d5ecc305508aa1
Mark Kretschmann <kretschmann@kde.org>
2005-10-29 07:12:03 +0000

    SVN_SILENT fix copyright header
    svn path=/trunk/extragear/multimedia/amarok/; revision=475409

--- src/scriptmanager.h
+++ src/scriptmanager.h
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2004-2005 by Mark Kretschmann <markey@web.de>           *
+ *                      2005 by Seb Ruiz <me@sebruiz.net>                  *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -14,7 +15,7 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.             *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
  ***************************************************************************/
 
 #ifndef AMAROK_SCRIPTMANAGER_H
