77b05e6e18491c79487deb4d417addc6f2a37c83
Mark Kretschmann <kretschmann@kde.org>
2005-11-12 11:03:23 +0000

    add script
    svn path=/trunk/extragear/multimedia/amarok/; revision=479925

---
+++ src/scripts/rbot/grouphug/grouphug.rb
@@ -0,0 +1,16 @@
+# (c) 2005 Mark Kretschmann <markey@web.de>
+# License: GPL V2
+
+
+require "net/http"
+
+h = Net::HTTP.new( "grouphug.us", 80 )
+resp, data = h.get( "/random" )
+
+reg = Regexp.new( '(<td class="conf-text")(.*?)(<p>)(.*?)(</p>)', Regexp::MULTILINE )
+confession = reg.match( data )[4]
+confession.gsub!( /<.*?>/, "" )
+
+puts confession
+
+
