4c82d9e170d4c5bb6622f943ec3e5ba7e8cfaf3c
Mark Kretschmann <kretschmann@kde.org>
2005-11-12 11:08:21 +0000

    remove tabs
    svn path=/trunk/extragear/multimedia/amarok/; revision=479927

--- src/scripts/rbot/grouphug/grouphug.rb
+++ src/scripts/rbot/grouphug/grouphug.rb
@@ -9,8 +9,8 @@ resp, data = h.get( "/random" )
 
 reg = Regexp.new( '(<td class="conf-text")(.*?)(<p>)(.*?)(</p>)', Regexp::MULTILINE )
 confession = reg.match( data )[4]
-confession.gsub!( /<.*?>/, "" )
+confession.gsub!( /<.*?>/, "" ) # Remove html tags
+confession.gsub!( "\t", "" ) # Remove tab characters
 
 puts confession
-
 
