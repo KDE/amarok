c298fdbee73328c800b6a01d76153e4196ceea3c
Mark Kretschmann <kretschmann@kde.org>
2005-11-28 18:31:48 +0000

    Here we go.. go.. here we go again! Sweet new script for embedding cover images in MP3 ID3-V2 tags. Powered by Ruby!
    svn path=/trunk/extragear/multimedia/amarok/; revision=483914

---
+++ src/scripts/embedcover/addimage2mp3.rb
@@ -0,0 +1,179 @@
+#!/usr/bin/env ruby
+#
+# Script for embedding album cover images in MP3 files. This requires an existing
+# ID3-V2 tag in the file.
+#
+# (c) 2005 Mark Kretschmann <markey@web.de>
+# License: GNU General Public License V2
+
+
+#
+# Returns the size of the ID3-V2 tag. In other words, the offset from
+# where the real mp3 data starts.
+# @see http://id3lib.sourceforge.net/id3/id3v2com-00.html#sec3.1
+#
+def getId3v2Size( data )
+    return data[6]*2**21 + data[7]*2**14 + data[8]*2**7 + data[9]
+end
+
+#
+# Sets the size of the ID3-V2 tag.
+# @see http://id3lib.sourceforge.net/id3/id3v2com-00.html#sec3.1
+#
+def setId3v2Size( data, size )
+    data[6] = size >> 21 & 0x7f
+    data[7] = size >> 14 & 0x7f
+    data[8] = size >> 7  & 0x7f
+    data[9] = size >> 0  & 0x7f
+end
+
+
+############################################################################
+# MAIN
+############################################################################
+
+path = ""
+destination = ""
+
+if $*.length() < 2 or $*[0] == "--help"
+    puts( "Usage: addimage2mp3.rb image source [destination]" )
+    puts()
+    puts( "Adds an image to the ID3-V2 tag of a MP3 file. Requires an existing ID3-V2 tag in" )
+    puts( "the file." )
+    puts()
+    exit( 1 )
+end
+
+imagepath = $*[0]
+path = $*[1]
+destination = path
+
+if $*.length() == 3
+    destination = $*[2]
+end
+
+
+unless FileTest::readable_real?( imagepath )
+    puts( "Error: Image not found or not readable." )
+    exit( 1 )
+end
+
+unless FileTest::readable_real?( path )
+    puts( "Error: Source not found or not readable." )
+    exit( 1 )
+end
+
+unless File.extname( path ) == ".mp3" or File.extname( path ) == ".MP3"
+    puts( "Error: File is not mp3." )
+    exit( 1 )
+end
+
+# unless FileTest.writable_real?( destination )
+#     puts( "Error: Destination file not writable." )
+#     exit( 1 )
+# end
+
+
+file = File.new( path, "r" )
+data = file.read()
+id3length = 0
+
+
+if data[0,3] == "ID3"
+    id3length = getId3v2Size( data )
+    puts( "ID3-V2 detected. Tag size: #{id3length}" )
+else
+    puts( "ID3-V1 detected." )
+    puts( "Error: File does not have a ID3-V2 tag." )
+    exit( 1 )
+end
+
+
+file = File.new( imagepath, "r" )
+imagesrc = file.read()
+image = String.new()
+
+
+# Unsynchronize image data (replace 0xfff* with 0xff00f*)
+index = 0
+while index < imagesrc.length()
+    if imagesrc[index] == 0xff and index < imagesrc.length() - 2
+        if imagesrc[index+1] & 0b11100000 == 0b11100000
+            image << imagesrc[index]
+            image << 0x00
+            image << imagesrc[index+1]
+            index += 2
+            next
+        end
+    end
+
+    image << imagesrc[index]
+    index += 1
+end
+
+
+apicframe = String.new()
+apicframe << 0x00  # text encoding
+apicframe << "image/png"  #FIXME set according to image format
+apicframe << 0x00  # mimetype end
+apicframe << 0x03  # Picture type (Cover front)
+apicframe << 0x00  # Description (empty)
+apicframe << image
+
+apicheader = String.new()
+apicheader << "APIC"
+apicheader << ( ( apicframe.length() >> 21 ) & 0x7f )
+apicheader << ( ( apicframe.length() >> 14 ) & 0x7f )
+apicheader << ( ( apicframe.length() >> 7  ) & 0x7f )
+apicheader << ( ( apicframe.length() >> 0  ) & 0x7f )
+apicheader << 0x00  # Flags
+apicheader << 0x02  # Flags (Set: Unsynchronized)
+
+
+# Find end of last ID3 frame, before padding or footer (if present)
+
+puts()
+puts( "Locating end of last ID3 frame.." )
+
+index = 10  # Skip ID3 Header
+until data[index] == 0 or data[index..index+2] == "3DI"
+    frametype = data[index..index+3]
+    puts( "Frame Type : #{frametype}" )
+    if frametype == "APIC"
+        puts( "Error: File does already contain an image." )
+        exit( 1 )
+    end
+    framesize = data[index+4]*2**21 + data[index+5]*2**14 + data[index+6]*2**7 + data[index+7]
+    index += 10 + framesize
+end
+
+index += 10 if data[index..index+2] == "3DI"  # Footer
+
+
+# Insert APIC frame into string, after the last ID3 frame
+data[index, 0] = apicheader + apicframe
+
+
+# Adjust ID3V2 tag size
+id3length += apicheader.length() + apicframe.length()
+setId3v2Size( data, id3length )
+puts()
+puts( "Adjusting new ID3 size: #{id3length + 10}" )
+
+
+puts()
+print( "Writing file..  " )
+destfile = File::open( destination, File::CREAT|File::TRUNC|File::WRONLY )
+
+if destfile == nil
+    puts( "Error: Destination file is not writable." )
+    exit( 1 )
+end
+
+destfile << data
+puts( "done." )
+
+
+puts()
+puts( "done." )
+
---
+++ src/scripts/embedcover/embedcover.rb
@@ -0,0 +1,87 @@
+#!/usr/bin/env ruby
+#
+# (c) 2005 Mark Kretschmann <markey@web.de>
+# License: GNU General Public License V2
+
+
+require 'md5'
+require "uri"
+
+
+MenuItemName = "EmbedCover DoIt!"
+
+
+def cleanup()
+    `dcop amarok script removeCustomMenuItem #{MenuItemName}`
+end
+
+
+trap( "SIGTERM" ) { cleanup() }
+
+`dcop amarok script addCustomMenuItem #{MenuItemName}`
+
+
+loop do
+    message = gets().chomp()
+    command = /[A-Za-z]*/.match( message ).to_s()
+
+    case command
+        when "configure"
+            msg  = '"EmbedCover does not have configuration options. Simply select a track in the '
+            msg += 'playlist, then start EmbedCover from the context-menu (right mouse click)."'
+
+            `dcop amarok playlist popupMessage "#{msg}"`
+
+        when "customMenuClicked"
+            if message.include?( MenuItemName )
+                args = message.split()
+                # Remove the command args
+                3.times() { args.delete_at( 0 ) }
+
+                # Iterate over all selected files
+                args.each() do |arg|
+                    uri = URI.parse( arg )
+                    file = URI.unescape( uri.path() )
+                    puts file
+
+                    backend = File.dirname( File.expand_path( __FILE__ ) ) + "/addimage2mp3.rb"
+
+                    # Query is two parts, first ID, then name
+                    artist_id = `dcop amarok collection query "SELECT DISTINCT artist FROM tags WHERE url = '#{file}'"`.chomp()
+                    artist    = `dcop amarok collection query "SELECT DISTINCT artist.name FROM artist WHERE id = '#{artist_id}'"`.chomp()
+
+                    album_id  = `dcop amarok collection query "SELECT DISTINCT album FROM tags WHERE url = '#{file}'"`.chomp()
+                    album     = `dcop amarok collection query "SELECT DISTINCT album.name FROM album WHERE id = '#{album_id}'"`.chomp()
+
+                    puts( "ArtistId : #{artist_id}" )
+                    puts( "Artist   : #{artist}" )
+                    puts( "AlbumId  : #{album_id}" )
+                    puts( "Album    : #{album}" )
+
+
+                    md5sum = MD5.hexdigest( "#{artist.downcase()}#{album.downcase()}" )
+                    imagefolder = "#{ENV['HOME']}/.kde/share/apps/amarok/albumcovers/large/"
+                    image = "#{imagefolder}#{md5sum}"
+
+                    puts( "Imagepath: #{image}" )
+
+                    unless FileTest.exist?( image )
+                        `dcop amarok playlist popupMessage "EmbedCover: No image found for this track."`
+                        next
+                    end
+
+                    output = `ruby #{backend} "#{image}" "#{file}"`
+
+                    if $?.success?()
+                        `dcop amarok playlist popupMessage "EmbedCover has successfully embedded the image."`
+                    else
+                        reg = Regexp.new( "Error:.*", Regexp::MULTILINE )
+                        errormsg = reg.match( output )
+
+                        `dcop amarok playlist popupMessage "EmbedCover #{errormsg}"`
+                    end
+                end
+            end
+    end
+end
+
