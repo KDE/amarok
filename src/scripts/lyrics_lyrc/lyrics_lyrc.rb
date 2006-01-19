15acffe858fe93da9a24c1321fbace92973f8acc
Mark Kretschmann <kretschmann@kde.org>
2006-01-19 21:49:30 +0000

    Added glue code for script manager.
    svn path=/trunk/extragear/multimedia/amarok/; revision=500279

--- src/scripts/lyrics_lyrc/lyrics_lyrc.rb
+++ src/scripts/lyrics_lyrc/lyrics_lyrc.rb
@@ -9,42 +9,74 @@
 require "net/http"
 require "uri"
 
-
-artist = "John Lennon"
-title  = "Imagine"
-
-h = Net::HTTP.new( "lyrc.com.ar", 80 )
-response = h.get( "/en/tema1en.php?artist=#{URI.escape( artist )}&songname=#{URI.escape( title )}" )
-
-unless response.code == "200"
-    puts( "HTTP Error: #{response.message}" )
-    exit( 1 )
+def cleanup()
+    `dcop amarok script enableExternalLyrics false`
 end
 
-lyrics = response.body()
+def fetchLyrics( artist, title )
+    # artist = "John Lennon"
+    # title  = "Imagine"
 
-# Remove images, links, scripts, and styles
-lyrics.gsub!( /<[iI][mM][gG][^>]*>/, "" );
-lyrics.gsub!( /<[aA][^>]*>[^<]*<\/[aA]>/, "" )
-lyrics.gsub!( /<[sS][cC][rR][iI][pP][tT][^>]*>[^<]*(<!--[^>]*>)*[^<]*<\/[sS][cC][rR][iI][pP][tT]>/, "" )
-lyrics.gsub!( /<[sS][tT][yY][lL][eE][^>]*>[^<]*(<!--[^>]*>)*[^<]*<\/[sS][tT][yY][lL][eE]>/, "" )
+    h = Net::HTTP.new( "lyrc.com.ar", 80 )
+    response = h.get( "/en/tema1en.php?artist=#{artist}&songname=#{title}" )
 
-lyricsPos = lyrics.index( /<[fF][oO][nN][tT][ ]*[sS][iI][zZ][eE][ ]*='2'[ ]*>/ )
+    unless response.code == "200"
+        puts( "HTTP Error: #{response.message}" )
+        exit( 1 )
+    end
 
-if lyricsPos == nil
-    puts( "Could not locate lyrics in the body." )
-    exit( 1 )
+    lyrics = response.body()
+
+    # Remove images, links, scripts, and styles
+    lyrics.gsub!( /<[iI][mM][gG][^>]*>/, "" );
+    lyrics.gsub!( /<[aA][^>]*>[^<]*<\/[aA]>/, "" )
+    lyrics.gsub!( /<[sS][cC][rR][iI][pP][tT][^>]*>[^<]*(<!--[^>]*>)*[^<]*<\/[sS][cC][rR][iI][pP][tT]>/, "" )
+    lyrics.gsub!( /<[sS][tT][yY][lL][eE][^>]*>[^<]*(<!--[^>]*>)*[^<]*<\/[sS][tT][yY][lL][eE]>/, "" )
+
+    lyricsPos = lyrics.index( /<[fF][oO][nN][tT][ ]*[sS][iI][zZ][eE][ ]*='2'[ ]*>/ )
+
+    if lyricsPos == nil
+        puts( "Could not locate lyrics in the body." )
+        exit( 1 )
+    end
+
+    lyrics = lyrics[lyricsPos..lyrics.length()]
+
+    if lyrics.include?( "<p><hr" )
+        lyrics = lyrics[0, lyrics.index( "<p><hr" )]
+    else
+        lyrics = lyrics[0, lyrics.index( "<br><br>" )]
+    end
+
+
+    return lyrics
 end
 
-lyrics = lyrics[lyricsPos..lyrics.length()]
 
-if lyrics.include?( "<p><hr" )
-    lyrics = lyrics[0, lyrics.index( "<p><hr" )]
-else
-    lyrics = lyrics[0, lyrics.index( "<br><br>" )]
+##################################################################
+# MAIN
+##################################################################
+
+trap( "SIGTERM" ) { cleanup() }
+
+`dcop amarok script enableExternalLyrics true`
+
+
+loop do
+    message = gets().chomp()
+    args = message.split()[1, args.length()]
+    command = /[A-Za-z]*/.match( message ).to_s()
+
+    case command
+        when "configure"
+            msg  = '"This script does not have configuration options."'
+            `dcop amarok playlist popupMessage "#{msg}"`
+
+        when "fetchLyrics"
+            artist = args[0]
+            title  = args[1]
+
+            result = fetchLyrics( artist, title )
+    end
 end
-
-
-
-puts lyrics
 
