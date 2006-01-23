184dc4cb06f8f455b6473a4e334785900bcbf7e0
Mark Kretschmann <kretschmann@kde.org>
2006-01-23 10:48:36 +0000

    Beginnings of astraweb.com lyrics script.
    svn path=/trunk/extragear/multimedia/amarok/; revision=501544

---
+++ src/scripts/lyrics_astraweb/lyrics_astraweb.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+#
+# amaroK Script for fetching song lyrics from http://lyrics.astraweb.com.
+#
+# (c) 2006 Mark Kretschmann <markey@web.de>
+#
+# License: GNU General Public License V2
+
+
+require "net/http"
+require "uri"
+
+def showLyrics( lyrics )
+    # Important, otherwise we might execute arbitrary nonsense in the DCOP call
+    lyrics.gsub!( '"', "'" )
+    lyrics.gsub!( '`', "'" )
+
+    `dcop amarok contextbrowser showLyrics "#{lyrics}"`
+end
+
+def fetchLyrics( artist, title )
+    h = Net::HTTP.new( "search.lyrics.astraweb.com", 80 )
+    response = h.get( "/?word=#{artist}+#{title}" )
+
+    unless response.code == "200"
+        lyrics = "HTTP Error: #{response.message}"
+        `dcop amarok contextbrowser showLyrics "#{lyrics}"`
+        return
+    end
+
+    lyrics = response.body()
+
+    showLyrics( lyrics )
+end
+
+
+##################################################################
+# MAIN
+##################################################################
+
+loop do
+    message = gets().chomp()
+    command = /[A-Za-z]*/.match( message ).to_s()
+
+    case command
+        when "configure"
+            msg  = '"This script does not have configuration options."'
+            `dcop amarok playlist popupMessage "#{msg}"`
+
+        when "fetchLyrics"
+            args = message.split()
+
+            artist = args[1]
+            title  = args[2]
+
+            fetchLyrics( artist, title )
+
+        when "fetchLyricsByUrl"
+            url = message.split()[1]
+
+            fetchLyrics( url )
+    end
+end
+
