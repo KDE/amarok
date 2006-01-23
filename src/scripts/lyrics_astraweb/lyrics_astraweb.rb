c1ed24813ffc76c41d34e34dd0e888b7c517c6eb
Mark Kretschmann <kretschmann@kde.org>
2006-01-23 18:30:19 +0000

    Right, suggestions parsing/transformation works now.
    svn path=/trunk/extragear/multimedia/amarok/; revision=501712

--- src/scripts/lyrics_astraweb/lyrics_astraweb.rb
+++ src/scripts/lyrics_astraweb/lyrics_astraweb.rb
@@ -30,15 +30,18 @@ def fetchLyrics( artist, title )
 
     body = response.body()
     body = body[body.index( '<tr><td bgcolor="#BBBBBB"' )..body.index( "More Songs &gt;" ) - 1]
+    body.gsub!( "\n", "" ) # No need for \n, just complicates our RegExps
 
     entries = body.split( '<tr><td bgcolor="#BBBBBB"' )
     entries.delete_at( 0 )
 
     lyrics = "Suggestions : <br>"
     entries.each do |entry|
-        url = /http:\/\/display.[^"]*/.match( entry ).to_s()
-        title = /(http:\/\/display.*">)([^<]*)/.match( entry )[2].to_s()
-        lyrics += title + "\n"
+        url = /display.[^"]*/.match( entry ).to_s()
+        title = /(http:\/\/display\.lyrics.*">)([^<]*)/.match( entry )[2].to_s()
+        artist = /(Artist:.*html">)([^<]*)/.match( entry )[2].to_s()
+
+        lyrics += "<a href='#{url}'>#{artist} - #{title}</a><br>"
     end
 
     puts( lyrics )
