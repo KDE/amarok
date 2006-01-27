ccf37a414f1dc57238478fc073734d692a5048fe
Mark Kretschmann <kretschmann@kde.org>
2006-01-27 07:50:38 +0000

    * Port some more handy debug functions. * Add some API docs.
    svn path=/trunk/extragear/multimedia/amarok/; revision=502733

--- src/scripts/debug.rb
+++ src/scripts/debug.rb
@@ -1,13 +1,37 @@
 #!/usr/bin/env ruby
 #
 # Debug functions, emulating the ones we have in amaroK
+#
+# (c) 2006   Mark Kretschmann <markey@web.de>
+# (c) 2003-5 Max Howell <max.howell@methylblue.com>
 
 
+DEBUG_PREFIX = "TestClass"
+
+ #
+ # @short Use this to label sections of your code
+ #
+ # Usage:
+ #
+ #     def function()
+ #         Debug_Block
+ #
+ #         debug "output1"
+ #         debug "output2"
+ #     end
+ #
+ # Will output:
+ #
+ #     BEGIN: function()
+ #       [prefix] output1
+ #       [prefix] output2
+ #     END: function() - Took 0.1s
+ #
 def debug_block()
     d = Debug_Block.new()
     funcname = /(`)(.*)(')/.match( caller( 1 )[0] )[2].to_s()
     t1 = Time.new
-    indent = " " * ( caller( 2 ).size - 1 ) * 2
+    indent = " " * ( caller( 1 ).size - 1 ) * 2
 
     ObjectSpace.define_finalizer( d, Proc.new {
         puts( "#{indent}END__: #{funcname}() - Took #{Time.new - t1}s" )
@@ -17,10 +41,28 @@ def debug_block()
     return d
 end
 
-
 class Debug_Block
 end
 
+def debug( str )
+    indent = " " * ( caller( 1 ).size ) * 2
+    puts( "#{indent}[#{DEBUG_PREFIX}] #{str}" )
+end
+
+def warning( str )
+    indent = " " * ( caller( 1 ).size ) * 2
+    puts( "#{indent}WARNING: [#{DEBUG_PREFIX}] #{str}" )
+end
+
+def error( str )
+    indent = " " * ( caller( 1 ).size ) * 2
+    puts( "#{indent}ERROR: [#{DEBUG_PREFIX}] #{str}" )
+end
+
+
+####################################################################
+# MAIN
+####################################################################
 
 def funA()
     debug_block
@@ -31,11 +73,15 @@ end
 def funB()
     debug_block
 
+    debug( "This is a test" )
+
     funC()
 end
 
 def funC()
     debug_block
+
+    warning( "What the hell is going on here" )
 end
 
 
