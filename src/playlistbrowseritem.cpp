b1d424e2cd118eb07b1a52536380fe127307be70
Bart Cerneels <bart.cerneels@kde.org>
2006-02-09 11:35:51 +0000

    nope, that doesn't fix anything, revert. Sorry markey
    svn path=/trunk/extragear/multimedia/amarok/; revision=507490

--- src/playlistbrowseritem.cpp
+++ src/playlistbrowseritem.cpp
@@ -1321,7 +1321,7 @@ PodcastChannel::setXml( const QDomNode &xml, const int feedType )
     m_title = xml.namedItem( "title" ).toElement().text();
     setText( 0, m_title );
 
-    if( m_settings == 0 ) // podcastchannel is created, therefore m_settings doesn't exist yet
+    if( !m_updating ) // podcastchannel is created, therefore m_settings doesn't exist yet
         if( m_channelSettings.isNull() ) //no channelsettings found, create new PodcastSettings based on settings from parent
         {
             m_settings = new PodcastSettings(
