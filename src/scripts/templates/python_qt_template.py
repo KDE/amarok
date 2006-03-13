a6ac0f61d7a1f935c9b20c0c717333ef9c38a61b
Seb Ruiz <ruiz@kde.org>
2006-03-13 12:14:26 +0000

    Fix bug in python template script BUG: 120286
    svn path=/trunk/extragear/multimedia/amarok/; revision=518204

--- src/scripts/templates/python_qt_template.py
+++ src/scripts/templates/python_qt_template.py
@@ -24,7 +24,7 @@ from time import sleep
 try:
     from qt import *
 except:
-    popen( "kdialog --sorry 'PyQt (Qt bindings for Python) is required for this script.'" )
+    os.popen( "kdialog --sorry 'PyQt (Qt bindings for Python) is required for this script.'" )
     raise
 
 
