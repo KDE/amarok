71e28a2c72b6bd3edc5ace5065adb80d34e9b9b7
Gábor Lehel <illissius@gmail.com>
2006-03-24 17:00:57 +0000

    backport playlist menu glitch fix
    svn path=/branches/stable/extragear/multimedia/amarok/; revision=522184

--- src/prettypopupmenu.cpp
+++ src/prettypopupmenu.cpp
@@ -157,7 +157,7 @@ PrettyPopupMenu::paintEvent( QPaintEvent* e )
     QPainter p( this );
 
     QRect r = sideImageRect();
-    r.setTop( r.bottom() - s_sidePixmap.height() + 1 ); //TODO Find reason for the off-by-one bug
+    r.setTop( r.bottom() - s_sidePixmap.height() );
     if ( r.intersects( e->rect() ) )
     {
         QRect drawRect = r.intersect( e->rect() ).intersect( sideImageRect() );
@@ -168,12 +168,16 @@ PrettyPopupMenu::paintEvent( QPaintEvent* e )
 
     p.setClipRegion( e->region() );
 
+
+    //NOTE The order is important here. drawContents() must be called before drawPrimitive(),
+    //     otherwise we get rendering glitches.
+
+    drawContents( &p );
+
     style().drawPrimitive( QStyle::PE_PanelPopup, &p,
                            QRect( 0, 0, width(), height() ),
                            colorGroup(), QStyle::Style_Default,
                            QStyleOption( frameWidth(), 0 ) );
-
-    drawContents( &p );
 }
 
 
