63367957d90365ea2b64a34d4f4780963ebac188
Mark Kretschmann <kretschmann@kde.org>
2006-04-07 09:13:43 +0000

    This script is a lifesaver, let's keep it around.
    svn path=/trunk/extragear/multimedia/amarok/; revision=527198

---
+++ src/scripts/databasescripts/redoPodcasts.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+query = `dcop amarok collection query "SELECT url FROM podcastchannels;"`
+print "Grabbing podcast feeds from database...\n"
+
+podcasts = query.split("\n")
+
+print "Delete all the podcasts from the playlist browser.  Once done, press something. Don't screw this up."
+message = gets()
+print "Sure you have removed them?"
+message = gets()
+
+podcasts.each do |channel|
+    print "Adding podcast: #{channel}\n"
+    `dcop amarok playlistbrowser addPodcast #{channel}`
+end
+print "Done.\n"
