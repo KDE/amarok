734a7c71c8de1b136fed928a70d1f5efbd64414a
Mark Kretschmann <kretschmann@kde.org>
2006-04-13 13:50:53 +0000

    Improve plugin, now using hashes
    svn path=/trunk/extragear/multimedia/amarok/; revision=529451

--- src/scripts/rbot/opinion/opinion.rb
+++ src/scripts/rbot/opinion/opinion.rb
@@ -1,31 +1,37 @@
 # Plugin for the Ruby IRC bot (http://linuxbrit.co.uk/rbot/)
-# (c) 2005 Mark Kretschmann <markey@web.de>
+# (c) 2005-2006 Mark Kretschmann <markey@web.de>
 # Licensed under GPL V2.
 
 
 class OpinionPlugin < Plugin
     def help(plugin, topic="")
-        "Ask sanity for its opinion on something"
+        "Ask the bot for its opinion on something."
     end
 
     def privmsg(m)
-        opinions = ["makes me happy.",
-                    "makes no sense to me.",
-                    "- I'm loving it!",
-                    "is fantastic!",
-                    "cracks me up, dude.",
-                    "reminds me of GNOME.",
-                    "is like a dream come true.",
-                    "is a sexy hot bitch.",
-                    "has some usability issues.",
-                    "is like a flight with LSD airlines!",
-                    "rocks me hard!",
-                    "should be forbidden.",
-                    "makes me horny.",
-                    "is quite amusing."]
+        opinions = [" makes me happy.",
+                    " makes no sense to me.",
+                    " - I'm loving it!",
+                    " is like a dream come true.",
+                    " is a sexy hot bitch.",
+                    " is like a flight with LSD airlines!",
+                    " rocks me hard!",
+                    " makes me horny.",
+                    " is quite amusing.",
+                    " - by eean's beard, I'm into it!",
+                    ", waaay into it.",
+
+                    " cracks me up, dude.",
+                    " reminds me of GNOME.",
+                    " has some usability issues.",
+                    " should be forbidden.",
+                    ", hell, I'd rather use Winamp.",
+                    "!! OMG!! PONIES!!!\nLOL!!"
+                    ]
 
         if m.params
-            @bot.say(m.replyto, "#{m.params} #{opinions.at(rand(opinions.length()))}")
+            index = m.params.hash % opinions.length
+            @bot.say(m.replyto, "#{m.params}#{opinions.at( index ) }")
         end
    end
 end
