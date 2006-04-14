03fbc62b24bbbaee8664037dc61e6b09c37dabd4
Mark Kretschmann <kretschmann@kde.org>
2006-04-14 22:11:31 +0000

    * Don't repeat questions, until all are used up. * Some code improvements.
    svn path=/trunk/extragear/multimedia/amarok/; revision=529957

--- src/scripts/rbot/quiz/quiz.rb
+++ src/scripts/rbot/quiz/quiz.rb
@@ -3,11 +3,22 @@
 # Licensed under GPL V2.
 
 
+class Bundle
+    attr_reader :question, :answer
+
+    def initialize( question, answer )
+        @question = question
+        @answer = answer
+    end
+end
+
+
 class QuizPlugin < Plugin
     def initialize()
         super
 
         @quest = Array.new
+        @quest_orig = Array.new
 
         @current_question = nil
         @current_answer  = nil
@@ -25,7 +36,7 @@ class QuizPlugin < Plugin
             return
         end
 
-        @quest = []
+        @quest_orig = []
         data = data.split( "QUIZ DATA START" )[1]
         data = data.split( "QUIZ DATA END" )[0]
 
@@ -33,8 +44,19 @@ class QuizPlugin < Plugin
 
         entries.each do |e|
             p = e.split( "</p><p>" )
-            @quest << p[0].chomp
-            @quest << p[1].chomp
+            b = Bundle.new( p[0].chomp, p[1].chomp )
+            @quest_orig << b
+        end
+    end
+
+    def shuffle()
+        @quest.clear
+        temp = @quest_orig.dup
+
+        temp.length.times do
+            i = rand( temp.length )
+            @quest << temp[i]
+            temp.delete_at( i )
         end
     end
 
@@ -45,11 +67,13 @@ class QuizPlugin < Plugin
     def privmsg( m )
         case m.message
             when "ask" then
-                if @quest.empty? then fetch_data( m ) end
+                if @quest_orig.empty? then fetch_data( m ) end
+                if @quest.empty? then shuffle end
 
-                i = rand( @quest.length / 2 ) * 2
-                @current_question = @quest[ i ]
-                @current_answer   = @quest[ i + 1 ]
+                i = rand( @quest.length )
+                @current_question = @quest[ i ].question
+                @current_answer   = @quest[ i ].answer
+                @quest.delete_at( i )
 
                 @bot.say( m.replyto, @current_question )
 
@@ -60,6 +84,7 @@ class QuizPlugin < Plugin
 
             when "quiz_fetch" then
                 fetch_data( m )
+                shuffle
         end
     end
 
