865ecce845355c9c19692dbbd637279f1f6971f5
Mark Kretschmann <kretschmann@kde.org>
2006-04-15 17:20:01 +0000

    Plugin now handles per-channel data, so that it no longer confuses questions when used on multiple channels.
    svn path=/trunk/extragear/multimedia/amarok/; revision=530183

--- src/scripts/rbot/quiz/quiz.rb
+++ src/scripts/rbot/quiz/quiz.rb
@@ -1,8 +1,14 @@
 # Plugin for the Ruby IRC bot (http://linuxbrit.co.uk/rbot/)
+#
+# A quiz game.
+#
 # (c) 2006 Mark Kretschmann <markey@web.de>
 # Licensed under GPL V2.
 
 
+#######################################################################
+# CLASS Bundle
+#######################################################################
 class Bundle
     attr_reader :question, :answer
 
@@ -13,15 +19,105 @@ class Bundle
 end
 
 
-class QuizPlugin < Plugin
-    def initialize()
-        super
+#######################################################################
+# CLASS Quiz
+# One Quiz instance per channel, handles all channel specific stuff
+#######################################################################
+class Quiz
+    def initialize( channel, plugin )
+        @channel = channel
+        @plugin = plugin
 
         @quest = Array.new
-        @quest_orig = Array.new
 
         @current_question = nil
         @current_answer  = nil
+    end
+
+    def shuffle()
+        @quest.clear
+        temp = @plugin.quest_orig.dup
+
+        temp.length.times do
+            i = rand( temp.length )
+            @quest << temp[i]
+            temp.delete_at( i )
+        end
+    end
+
+    def privmsg( m )
+        case m.message
+            when "ask"
+                shuffle if @quest.empty?
+
+                i = rand( @quest.length )
+                @current_question = @quest[ i ].question
+                @current_answer   = @quest[ i ].answer
+                @quest.delete_at( i )
+
+                @plugin.bot.say( m.replyto, @current_question )
+
+            when "answer"
+                @plugin.bot.say( m.replyto, "The correct answer was: #{@current_answer}" )
+
+                @current_question = nil
+
+            when "quiz_fetch"
+                @plugin.fetch_data( m )
+                shuffle
+
+            when "hint"
+                if @current_question == nil
+                    @plugin.bot.say( m.replyto, "Get a question first!" )
+                else
+                    s = ""
+                    @current_answer.length.times do
+                        s << "."
+                    end
+
+                    index = rand( s.length )
+                    s[index] = @current_answer[index]
+
+                    @plugin.bot.say( m.replyto, "Hint: #{s}" )
+                end
+
+            when "quiz_stats"
+                fetch_data( m ) if @plugin.quest_orig.empty?
+
+                @plugin.bot.say( m.replyto, "Questions in database: #{@plugin.quest_orig.length}" )
+        end
+    end
+
+    def listen( m )
+        if m.message.downcase == @current_answer.downcase
+            replies = []
+            replies << "BINGO!! #{m.sourcenick} got it right. The answer was: #{@current_answer}"
+            replies << "OMG!! PONIES!! #{m.sourcenick} is the cutest. The answer was: #{@current_answer}"
+            replies << "HUZZAAAH! #{m.sourcenick} did it again. The answer was: #{@current_answer}"
+            replies << "YEEEHA! Cowboy #{m.sourcenick} scored again. The answer was: #{@current_answer}"
+            replies << "STRIKE! #{m.sourcenick} pwned you all. The answer was: #{@current_answer}"
+            replies << "YAY :)) #{m.sourcenick} is totally invited to my next sleepover. The answer was: #{@current_answer}"
+
+            @plugin.bot.say( m.replyto, replies[rand( replies.length )] )
+
+            @current_question = nil
+        end
+    end
+end
+
+
+#######################################################################
+# CLASS QuizPlugin
+#######################################################################
+class QuizPlugin < Plugin
+    attr_accessor :bot
+    attr_reader   :quest_orig
+
+    def initialize()
+        super
+
+        @quest_orig = Array.new
+        @quizzes = Hash.new
     end
 
     def fetch_data( m )
@@ -49,79 +145,23 @@ class QuizPlugin < Plugin
         end
     end
 
-    def shuffle()
-        @quest.clear
-        temp = @quest_orig.dup
-
-        temp.length.times do
-            i = rand( temp.length )
-            @quest << temp[i]
-            temp.delete_at( i )
-        end
-    end
-
     def help( plugin, topic="" )
-        "Quiz game. Tell me 'ask' to start. You can add new questions at http://amarok.kde.org/amarokwiki/index.php/Rbot_Quiz"
+        "Quiz game. Tell me 'ask' to start. 'hint' for getting a hint and 'answer' for quick solving.\nYou can add new questions at http://amarok.kde.org/amarokwiki/index.php/Rbot_Quiz"
     end
 
     def privmsg( m )
-        case m.message
-            when "ask"
-                fetch_data( m ) if @quest_orig.empty?
-                shuffle if @quest.empty?
+        fetch_data( m ) if @quest_orig.empty?
 
-                i = rand( @quest.length )
-                @current_question = @quest[ i ].question
-                @current_answer   = @quest[ i ].answer
-                @quest.delete_at( i )
-
-                @bot.say( m.replyto, @current_question )
-
-            when "answer"
-                @bot.say( m.replyto, "The correct answer was: #{@current_answer}" )
-
-                @current_question = nil
-
-            when "quiz_fetch"
-                fetch_data( m )
-                shuffle
-
-            when "hint"
-                if @current_question == nil
-                    @bot.say( m.replyto, "Get a question first!" )
-                else
-                    s = ""
-                    @current_answer.length.times do
-                        s << "."
-                    end
-
-                    index = rand( s.length )
-                    s[index] = @current_answer[index]
-
-                    @bot.say( m.replyto, "Hint: #{s}" )
-                end
-
-            when "quiz_stats"
-                fetch_data( m ) if @quest_orig.empty?
-
-                @bot.say( m.replyto, "Questions in database: #{@quest_orig.length}" )
+        unless @quizzes.has_key?( m.target )
+            @quizzes[m.target] = Quiz.new( m.target, self )
         end
+
+        @quizzes[m.target].privmsg( m )
     end
 
     def listen( m )
-        return if @current_question == nil
-
-        if m.message.downcase == @current_answer.downcase
-            replies = []
-            replies << "BINGO!! #{m.sourcenick} got it right. The answer was: #{@current_answer}"
-            replies << "OMG!! PONIES!!! #{m.sourcenick} is the cutest. The answer was: #{@current_answer}"
-            replies << "HUZZAAAH! #{m.sourcenick} did it again. The answer was: #{@current_answer}"
-            replies << "YEEEHA! Cowboy #{m.sourcenick} scored again. The answer was: #{@current_answer}"
-            replies << "STRIKE! #{m.sourcenick} pwned you all. The answer was: #{@current_answer}"
-
-            @bot.say( m.replyto, replies[rand( replies.length )] )
-
-            @current_question = nil
+        if @quizzes.has_key?( m.target )
+            @quizzes[m.target].listen( m )
         end
     end
 end
