33df27359902d68d925ec6f6b8e50aa7d674ed4e
Mark Kretschmann <kretschmann@kde.org>
2006-04-15 07:44:06 +0000

    Add handy script for transforming the icons from the artists's tarballs to the final filenames.
    svn path=/trunk/extragear/multimedia/amarok/; revision=530037

---
+++ src/scripts/copy_icons.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+#
+# Transforms icon filenames from an icon tarball to correct format for committing
+#
+# (c) 2006 Mark Kretschmann <markey@web.de>
+# Licensed under GPL V2.
+
+
+require 'fileutils'
+
+
+def copy_icons( res )
+    folder = "#{res}x#{res}"
+
+    Dir.foreach( folder ) do |file|
+        next if file[0, 1] == "."
+        name = File.basename( file, ".png" )
+        FileUtils.cp( "#{folder}/#{file}", "hi#{res}-action-amarok_#{name}.png" )
+    end
+end
+
+
+
+copy_icons( "16" )
+copy_icons( "22" )
+copy_icons( "32" )
+copy_icons( "48" )
+copy_icons( "64" )
