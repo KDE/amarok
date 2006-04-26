b4b38e032f5b627dca84e9bb14c17cfce345255c
Peter C. Ndikuwera <pndiku@gmail.com>
2006-04-26 13:50:04 +0000

    SVN_SILENT Add myself to the credits
    svn path=/trunk/extragear/multimedia/amarok/; revision=534147

--- src/main.cpp
+++ src/main.cpp
@@ -83,6 +83,7 @@ int main( int argc, char *argv[] )
     aboutData.addCredit( "Nenad Grujicic", I18N_NOOP( "Splash screen" ), "mchitman@neobee.net" );
     aboutData.addCredit( "Olivier Bédard", I18N_NOOP( "Website hosting" ), "paleo@pwsp.net" );
     aboutData.addCredit( "Paul Cifarelli", I18N_NOOP( "HelixPlayer engine" ), "paul@cifarelli.net" );
+    aboutData.addCredit( "Peter C. Ndikuwera", I18N_NOOP( "Bugfixes, PostgreSQL support" ), "pndiku@gmail.com" );
     aboutData.addCredit( "Reigo Reinmets", I18N_NOOP( "Wikipedia support, patches" ), "xatax@hot.ee" );
     aboutData.addCredit( "Roland Gigler", I18N_NOOP( "MAS engine" ), "rolandg@web.de" );
     aboutData.addCredit( "Sami Nieminen", I18N_NOOP( "Audioscrobbler support" ), "sami.nieminen@iki.fi" );
