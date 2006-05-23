57b7bacf222eef6b857205b11c810206291db8d3
Nimrod Gutman <nimrod.gutman@gmail.com>
2006-05-23 08:39:37 +0000

    I've modified track.h to remove the NjbTrack:: lines. They seem to be causing problems with some versions of gcc. (from Shash)
    svn path=/trunk/extragear/multimedia/amarok/; revision=543949

--- src/mediadevice/njb/track.h
+++ src/mediadevice/njb/track.h
@@ -37,31 +37,31 @@ class NjbTrack {
         bool operator==( const NjbTrack& second ) const { return id == second.id; }
 
     public:
-        unsigned NjbTrack::getId() const { return id; }
-        unsigned NjbTrack::getSize() const { return size; }
-        unsigned NjbTrack::getDuration() const { return duration; }
-        unsigned NjbTrack::getTrackNum() const { return tracknum; }
-        QString NjbTrack::getYear() const { return year; }
-        QString NjbTrack::getGenre() const { return genre; }
-        QString NjbTrack::getArtist() const { return artist; }
-        QString NjbTrack::getAlbum() const { return album; }
-        QString NjbTrack::getTitle() const { return title; }
-        QString NjbTrack::getCodec() const { return codec; }
-        QString NjbTrack::getFilename() const { return filename; }
+        unsigned getId() const { return id; }
+        unsigned getSize() const { return size; }
+        unsigned getDuration() const { return duration; }
+        unsigned getTrackNum() const { return tracknum; }
+        QString getYear() const { return year; }
+        QString getGenre() const { return genre; }
+        QString getArtist() const { return artist; }
+        QString getAlbum() const { return album; }
+        QString getTitle() const { return title; }
+        QString getCodec() const { return codec; }
+        QString getFilename() const { return filename; }
 
-        void NjbTrack::setId( unsigned newId) { id = newId; }
-        void NjbTrack::setSize( unsigned newSize) { size = newSize; }
-        void NjbTrack::setDuration( unsigned newDuration) { duration = newDuration; }
-        void NjbTrack::setTrackNum( unsigned newTrackNum) { tracknum = newTrackNum; }
-        void NjbTrack::setYear( QString newYear) { year = newYear; }
-        void NjbTrack::setGenre( QString newGenre) { genre = newGenre; }
-        void NjbTrack::setArtist( QString newArtist) { artist = newArtist; }
-        void NjbTrack::setAlbum( QString newAlbum) { album = newAlbum; }
-        void NjbTrack::setTitle( QString newTitle) { title = newTitle; }
-        void NjbTrack::setCodec( QString newCodec) { codec = newCodec; }
-        void NjbTrack::setFilename( QString newFilename) { filename = newFilename; }
+        void setId( unsigned newId) { id = newId; }
+        void setSize( unsigned newSize) { size = newSize; }
+        void setDuration( unsigned newDuration) { duration = newDuration; }
+        void setTrackNum( unsigned newTrackNum) { tracknum = newTrackNum; }
+        void setYear( QString newYear) { year = newYear; }
+        void setGenre( QString newGenre) { genre = newGenre; }
+        void setArtist( QString newArtist) { artist = newArtist; }
+        void setAlbum( QString newAlbum) { album = newAlbum; }
+        void setTitle( QString newTitle) { title = newTitle; }
+        void setCodec( QString newCodec) { codec = newCodec; }
+        void setFilename( QString newFilename) { filename = newFilename; }
 
-        MetaBundle* NjbTrack::getMetaBundle();
+        MetaBundle* getMetaBundle();
 
     private:
         unsigned id;
