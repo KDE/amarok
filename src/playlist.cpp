2bd1bc1a75790fa99f64de927fabcb49b34cf4ae
Jocke Andersson <ajocke@gmail.com>
2006-06-26 14:12:43 +0000

    OK, markey. Removed code instead of commenting.
    svn path=/trunk/extragear/multimedia/amarok/; revision=555158

--- src/playlist.cpp
+++ src/playlist.cpp
@@ -4158,20 +4158,6 @@ void Playlist::contentsWheelEvent( QWheelEvent *e )
                 changed << p;
             if( changed.findRef( m_nextTracks.at( dest - s ) ) == -1 )
                 changed << m_nextTracks.at( dest - s );
-/*
-            if ( dynamicMode() ) // move tracks in the dynamic list.
-            {
-                PlaylistItem* after;
-                if ( dest == 0 ) // new position is first in queue, after the current track
-                    after = m_currentTrack;
-                else if ( s < 0 ) // move down, below dest
-                    after = m_nextTracks.at( dest + s );
-                else // move up, above dest
-                    after = m_nextTracks.at( dest );
-                if ( after )
-                    moveItem( item, 0, after ); // move the item
-            }
-*/
             m_nextTracks.replace( dest, m_nextTracks.at( dest - s ) );
             m_nextTracks.replace( dest - s, p );
         }
