dbae0171a6771d2c66e3b18b1561dd3a8f5c54b9
Stephan Binner <binner@kde.org>
2006-06-26 09:09:06 +0000

    don't confuse Intel's C compiler
    svn path=/trunk/extragear/multimedia/amarok/; revision=555092

--- src/playerwindow.h
+++ src/playerwindow.h
@@ -106,7 +106,7 @@ class PlayerWidget : public QWidget, public EngineObserver
 
     protected:
         /** Observer reimpls **/
-        void engineStateChanged( Engine::State state, Engine::State state = Engine::Empty );
+        void engineStateChanged( Engine::State state, Engine::State oldstate = Engine::Empty );
         void engineVolumeChanged( int percent );
         void engineNewMetaData( const MetaBundle &/*bundle*/, bool /*trackChanged*/ );
         void engineTrackPositionChanged( long /*position*/, bool /*userSeek*/ );
