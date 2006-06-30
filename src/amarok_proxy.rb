6c84330f051f6433a5916accc649dcb28a585564
Mark Kretschmann <kretschmann@kde.org>
2006-06-30 14:02:11 +0000

    Refactored and improved version by Michael Fellinger: * Much nicer code * Reliable SYNC detection.
    svn path=/trunk/extragear/multimedia/amarok/; revision=556510

--- src/amarok_proxy.rb
+++ src/amarok_proxy.rb
@@ -4,6 +4,7 @@
 #
 # (c) 2006 Paul Cifarelli <paul@cifarelli.net>
 # (c) 2006 Mark Kretschmann <markey@web.de>
+# (c) 2006 Michael Fellinger <manveru@weez-int.com>
 #
 # License: GNU General Public License V2
 
@@ -13,90 +14,106 @@ require "uri"
 
 include Socket::Constants
 
-def puts( string )
-    $stderr.puts( "AMAROK_PROXY: " + string )
-end
+class LastFM
+  def initialize port, remote_url
 
+    puts( "startup" )
 
-def cp_to_empty( s, o )
-    useragent = ""
-    s.each_line do |data|
-        puts( data )
-        if data.include?( "User-Agent:" )
-           label,useragent = data.split(/\s*\:\s*/)
-        end
-        o.write( data )
-        return useragent if data.chomp == ""
+    #
+    # amarok is, well, Amarok
+    #
+    amarok  = TCPServer.new( port )
+    amaroks = amarok.accept
+
+    #
+    # serv is the lastfm stream server
+    #
+    uri = URI.parse( remote_url )
+    serv = TCPSocket.new( uri.host, uri.port )
+    puts "running with port: #{uri.port} and host: #{uri.host}"
+
+    # here we substitute the proxy GET
+    data = amaroks.readline
+    get = "GET #{uri.path || '/'}?#{uri.query} HTTP/1.1\r\n"
+
+    puts data.inspect
+    puts get.inspect
+    puts "#{data} but sending #{get}"
+    serv.puts get
+
+    # the client initiates everything - so copy it to the server
+    puts "COPY from amarok -> serv"
+    useragent = cp_to_empty amaroks, serv
+
+    puts "COPY from serv -> amarok"
+    cp_to_empty serv, amaroks
+
+    # now copy from the server to amarok
+    puts "Before cp_all()"
+    cp_all serv, amaroks
+
+    puts "useragent is #{useragent}"
+    if !useragent.include?( "xine" ) && amaroks.eof
+      puts "EOF Detected, reconnecting"
+      amaroks = amarok.accept
+      cp_all( serv, amaroks )
     end
-end
+  end
 
+  def puts string
+    $stderr.puts "AMAROK_PROXY: #{string}"
+  end
 
-def cp_all( s, o )
+  def safe_write(output, data)
+    begin
+      output.write data
+    rescue
+      puts "error from output.write, #{$!}"
+      puts $!.backtrace.inspect
+      break
+    end
+  end
+
+  def cp_to_empty( income, output )
+    puts "cp_to_empty( income => #{income.inspect}, output => #{output.inspect}"
+    income.each_line do |data|
+      puts (data = data.chomp)
+      if data.empty?
+        label, useragent = data.split(/\s*\:\s*/) if data =~ /User-Agent:/
+        puts [label, useragent].inspect
+        return "\n#{useragent}"
+      end
+      safe_write output, data
+    end
+  end
+
+  def cp_all( income, output )
+    puts "cp_all( income => #{income.inspect}, output => #{output.inspect}"
     loop do
-        data = s.read( 1000 )
-        break if data == nil
-
-        # intercept SYNCs
-        if data.include?( "SYNC" ) # FIXME won't detect the SYNC if it spreads over fragment boundary
-            data.gsub!( "SYNC", "" )
-            puts( "SYNC" )
+      data = income.read( 1000 )
+      unless data.empty?
+        # takes double memory (2000), but makes safe that no sync comes through
+        # i think that's a good trade-off ;)
+        if (curr = (last ||= '') + data) =~ /SYNC/
+          data = curr.split('SYNC').last
+          puts 'SYNC'
         end
-
-        begin
-            o.write( data )
-        rescue
-            puts( "error from o.write, #{$!}" )
-            break
-        end
+        safe_write output, data
+      end
+      last = data
     end
+  end
 end
 
+begin
+  puts 'startup'
+  port, remote_url = ARGV
+  puts "running with port: #{port} and url: #{remote_url}"
 
-puts( "startup" )
-
-port = $*[0].to_i
-remote_url = $*[1]
-
-#
-# amarok is, well, Amarok
-#
-amarok  = TCPServer.new( port )
-amaroks = amarok.accept
-
-#
-# serv is the lastfm stream server
-#
-uri = URI.parse( remote_url )
-serv = TCPSocket.new( uri.host, uri.port )
-
-# here we substitute the proxy GET
-data = amaroks.readline
-g = "GET " << uri.path
-g << "/" if uri.path == ""
-g << "?" << uri.query if uri.query && uri.query != ""
-g << " HTTP/1.1\r\n"
-
-puts( data << "but sending " << g )
-serv.puts( g )
-
-# the client initiates everything - so copy it to the server
-puts "COPY from amarok -> serv"
-useragent = cp_to_empty( amaroks, serv )
-
-puts "COPY from serv -> amarok"
-cp_to_empty( serv, amaroks )
-
-# now copy from the server to amarok
-puts( "Before cp_all()" )
-cp_all( serv, amaroks )
-
-puts "useragent is " << useragent
-if !useragent.include?( "xine" ) && amaroks.eof
-   puts( "EOF Detected, reconnecting" )
-   amaroks = amarok.accept
-   cp_all( serv, amaroks )
+  LastFM.new port, remote_url
+rescue
+  $stderr.puts $!.to_s
+  $stderr.puts $!.backtrace.inspect
 end
-
-
 
 puts( "exiting" )
