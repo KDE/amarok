b2552a614c240579bdeb32f992dc4441e1d4a02c
Seb Ruiz <ruiz@kde.org>
2006-07-04 09:25:56 +0000

    Disable the lastfm options if there is no username, to comply with upcoming last.fm regulations
    svn path=/trunk/extragear/multimedia/amarok/; revision=557881

---
+++ src/Options8.ui.h
@@ -0,0 +1,20 @@
+// (c) 2006 Seb Ruiz <me@sebruiz.net>
+
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you want to add, delete, or rename functions or slots, use
+** Qt Designer to update this file, preserving your code.
+**
+** You should not define a constructor or destructor in this file.
+** Instead, write your code in functions called init() and destroy().
+** These will automatically be called by the form's constructor and
+** destructor.
+*****************************************************************************/
+
+
+void Options8::updateServices( const QString &s )
+{
+    groupBox2->setEnabled( !s.isEmpty() );
+}
+
