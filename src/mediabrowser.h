ec956392d2b22eeaf33edf7e8bf954e8072a836d
Diego Pettenò <flameeyes@gentoo.org>
2006-07-05 22:10:05 +0000

    Include metabundle.h file as there is usage of an instance. Patch from Michael Landin Hostbaek.
    svn path=/trunk/extragear/multimedia/amarok/; revision=558727

--- src/mediabrowser.h
+++ src/mediabrowser.h
@@ -19,11 +19,11 @@
 #include <kurl.h>            //stack allocated
 #include <kio/global.h>      //filesize_t
 #include "scrobbler.h"       //SubmitItem
+#include "metabundle.h"
 
 class MediaBrowser;
 class MediaDevice;
 class MediaView;
-class MetaBundle;
 class SpaceLabel;
 class TransferDialog;
 
