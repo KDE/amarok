104cdadd693ca0e06359220ac826b6e66f623514
Mark Kretschmann <kretschmann@kde.org>
2006-07-20 22:07:47 +0000

    Make last.fm aware :)
    svn path=/trunk/extragear/multimedia/amarok/; revision=564689

--- src/scripts/nowplaying/amaroknowplaying.rb
+++ src/scripts/nowplaying/amaroknowplaying.rb
@@ -2,38 +2,43 @@
 #
 # Now playing script for IRC. Use with the "/exec -o" command of your client.
 #
-# (c) 2005 Mark Kretschmann <markey@web.de>
+# (c) 2005-2006 Mark Kretschmann <markey@web.de>
 # License: GNU General Public License V2
 
 
-title  = `dcop amarok player title 2> /dev/null`.chomp()
-exit( 1 ) unless $?.success?() # Abort if amaroK isn't running
-artist = `dcop amarok player artist`.chomp()
-album  = `dcop amarok player album`.chomp()
-year   = `dcop amarok player year`.chomp()
+title  = `dcop amarok player title 2> /dev/null`.chomp
+exit( 1 ) unless $?.success? # Abort if amaroK isn't running
+artist = `dcop amarok player artist`.chomp
+album  = `dcop amarok player album`.chomp
+year   = `dcop amarok player year`.chomp
+lastfm = `dcop amarok player lastfmStation`.chomp
 
 output = ""
 
 
-if title.empty?()
-    output += `dcop amarok player nowPlaying`.chomp()
+if title.empty?
+    output += `dcop amarok player nowPlaying`.chomp
 else
     # Strip file extension
     extensions = ".ogg", ".mp3", ".wav"
-    if extensions.include?( File.extname( title ).downcase() )
-        title = title[0, title.length()-4]
+    if extensions.include?( File.extname( title ).downcase )
+        title = title[0, title.length-4]
     end
 
     output += "#{title}"
-    output += " - #{artist}" unless artist.empty?()
+    output += " - #{artist}" unless artist.empty?
 
-    unless album.empty?()
+    unless album.empty?
         output += " [#{album}"
         output += ", #{year}" unless year == "0"
         output += "]"
     end
+
+    unless lastfm.empty?
+        output += " (Last.fm #{lastfm})"
+    end
 end
 
 
-puts( "np: #{output}" ) unless output.empty?()
+puts( "np: #{output}" ) unless output.empty?
 
