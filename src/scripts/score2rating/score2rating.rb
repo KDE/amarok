b34bcc2db09723f57349629605ab4ebc25e7fb7f
Jocke Andersson <ajocke@gmail.com>
2006-07-26 19:01:37 +0000

    New version of Score2Rating! (will hit kde-apps after packaging...) * Better error handling, prints a list of failed tracks (together with my mail address) to STDERR when unsuccessful (It prepares its own "backtrace", which ends up in Amarok's error dialog). * Escapes all (?) bad characters which confuses the dcop query commands (now also including "double quotes").
    svn path=/trunk/extragear/multimedia/amarok/; revision=566699

--- src/scripts/score2rating/score2rating.rb
+++ src/scripts/score2rating/score2rating.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Score2Rating 2.2 (for Amarok 1.4)
+# Score2Rating 2.5 (for Amarok 1.4)
 # ---------------------------------
 #
 # First of all:
@@ -27,10 +27,11 @@ loop do
 
   case command
   when "configure"
-    result = `dcop amarok collection query "SELECT url FROM statistics WHERE percentage > 0"`.chomp() #List tracks that have a score
+    result = `dcop amarok collection query "SELECT url FROM statistics WHERE percentage > 0 ORDER BY url"`.chomp() #List tracks that have a score
     list = result.split( "\n" )
 
     success = true #Assume the procedure is successful
+    failList = Array.new()
 
     tracknum = 0
 
@@ -41,49 +42,57 @@ loop do
       tracknum += 1
       system( "dcop #{dialog} setProgress #{tracknum}" ) if dialog.length > 0
 
-      sqlurl = url.gsub( /(['`])/, '\\\\\\1' ) #\Escape 'single quotes' and `backticks`
+      sqlurl = url.gsub( /(['"`])/, '\\\\\\1' ) #\Escape 'single' and "double" quotes and `backticks`
 
-      percentage = Float( `dcop amarok collection query "SELECT percentage FROM statistics WHERE url='#{sqlurl}'"`.chomp() )
+      begin
+        percentage = Float( `dcop amarok collection query "SELECT percentage FROM statistics WHERE url='#{sqlurl}'"`.chomp() )
 
-      rating = Integer( `dcop amarok collection query "SELECT rating FROM statistics WHERE url='#{sqlurl}'"`.chomp() )
-      oldrating = rating
+        rating = Integer( `dcop amarok collection query "SELECT rating FROM statistics WHERE url='#{sqlurl}'"`.chomp() )
+        oldrating = rating
 
-      case percentage #The intervals are wrapped so that no scores will be left out. No existing ratings will be downgraded either.
+        case percentage #The intervals are wrapped so that no scores will be left out. No existing ratings will be downgraded either.
         when 0..30
-          rating = [ rating, 2 ].max #Awful
+            rating = [ rating, 2 ].max #Awful
         when 30..45
-          rating = [ rating, 3 ].max #Barely Tolerable
+            rating = [ rating, 3 ].max #Barely Tolerable
         when 45..60
-          rating = [ rating, 4 ].max #Tolerable
+            rating = [ rating, 4 ].max #Tolerable
         when 60..70
-          rating = [ rating, 5 ].max #Okay
+            rating = [ rating, 5 ].max #Okay
         when 70..80
-          rating = [ rating, 6 ].max #Good (Tracks played full length once (75) will be here)
+            rating = [ rating, 6 ].max #Good (Tracks played full length once (75) will be here)
         when 80..85
-          rating = [ rating, 7 ].max #Very good
+            rating = [ rating, 7 ].max #Very good
         when 85..90
-          rating = [ rating, 8 ].max #Excellent (Tracks played full length twice (87) will be here)
+            rating = [ rating, 8 ].max #Excellent (Tracks played full length twice (87) will be here)
         when 90..95
-          rating = [ rating, 9 ].max #Amazing (Tracks played full length three times (91) will be here)
+            rating = [ rating, 9 ].max #Amazing (Tracks played full length three times (91) will be here)
         when 95..100
-          rating = [ rating, 10 ].max #Favourite
-      end
-
-      if rating != oldrating then #If a new rating was calculated
-        if !system( "dcop amarok collection query \"UPDATE statistics SET rating='#{rating}' WHERE url='#{sqlurl}'\" > /dev/null 2>&1" ) then #If the command fails:
-          success = false #The procedure wasn't successful
+            rating = [ rating, 10 ].max #Favourite
         end
+
+        if rating != oldrating then #If a new rating was calculated
+          if !system( "dcop amarok collection query \"UPDATE statistics SET rating='#{rating}' WHERE url='#{sqlurl}'\" > /dev/null 2>&1" ) then #If the command fails:
+            success = false #The procedure wasn't successful
+            failList << "DCOP: " + url
+          end
+        end
+      rescue
+        success = false
+        failList << "SEVERE: " + url
       end
     end
 
     system( "dcop #{dialog} close" ) if dialog.length > 0
 
     if success == true then #If the procedure was sucessful
-      system( "dcop amarok playlist popupMessage \"Done! All your tracks (that have a score) should now have ratings. You will have to reload your playlist to see them, though.\" > /dev/null" )
-      exit(0) #Exit without error
+      system( "kdialog --msgbox \"Done! All your tracks (that have a score) should now have ratings. You will have to reload your playlist to see them, though.\"" )
     else
-      system( "dcop amarok playlist popupMessage \"Score2Rating has finished its conversion, but the process failed for at least one track. You might want to try running it again. (To see the new ratings, you will have to reload your playlist.)\" > /dev/null" )
-      exit(1) #Exit with error
+      failCount = failList.length
+      STDERR.puts( "FAILED URL LIST START\n" + failList.join("\n") + "\nFAILED URL LIST END\nPlease e-mail the entire contents of this error report to ajocke (at) gmail (dot) com to help further development of Score2Rating.")
+
+      system( "kdialog --sorry \"Score2Rating has finished its conversion, but the process failed for #{failCount} track(s).\nA list of the failed URL(s) will be given to you if you click Details in Amarok's error dialog which will appear after you dismiss this dialog.\n(To see the new ratings, you will have to reload your playlist.)\"" )
+      exit(38) #Exit with error to give the user Amarok's error box
     end
   end
 end
