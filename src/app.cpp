50de4d3911a80f9f521373c2f47d6361d72ba127
Martin Aumüller <aumuell@reserv.at>
2006-08-20 12:49:59 +0000

    re-enable fixHyperThreading for 1.4.2
    svn path=/trunk/extragear/multimedia/amarok/; revision=574864

--- src/app.cpp
+++ src/app.cpp
@@ -114,7 +114,7 @@ App::App()
     new amaroK::DcopScriptHandler();
     new amaroK::DcopDevicesHandler();
 
-    //fixHyperThreading();
+    fixHyperThreading();
 
     QTimer::singleShot( 0, this, SLOT( continueInit() ) );
 
