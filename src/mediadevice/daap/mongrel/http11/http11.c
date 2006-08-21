09f6589a97d97b7332017f11d7c69ea9d58353a9
Ian Monroe <ian.monroe@gmail.com>
2006-08-21 17:29:46 +0000

    r6420@wasabi:  ian | 2006-08-21 12:29:11 -0500  *fixed the Makefile.am problems, so the Makefiles are created fine  *Mongrel doesn't compile. Weird issues - it doesn't like the 'static inline int'   of ruby.h.  *Due to a unrelated client segfault on internet disconnects, I have disabled DAAP  for the 1.4.2 release.
    svn path=/trunk/extragear/multimedia/amarok/; revision=575564

--- src/mediadevice/daap/mongrel/http11/http11.c
+++ src/mediadevice/daap/mongrel/http11/http11.c
@@ -2,7 +2,11 @@
  * Copyright (c) 2005 Zed A. Shaw
  * You can redistribute it and/or modify it under the same terms as Ruby.
  */
-#include "ruby.h"
+
+
+extern "C" {
+	#include <ruby.h>
+}
 #include "ext_help.h"
 #include <assert.h>
 #include <string.h>
@@ -143,7 +147,7 @@ void header_done(void *data, const char *at, size_t length)
 
   rb_hash_aset(req, global_gateway_interface, global_gateway_interface_value);
   if((temp = rb_hash_aref(req, global_http_host)) != Qnil) {
-    // ruby better close strings off with a '\0' dammit
+    /* ruby better close strings off with a '\0' dammit */
     colon = strchr(RSTRING(temp)->ptr, ':');
     if(colon != NULL) {
       rb_hash_aset(req, global_server_name, rb_str_substr(temp, 0, colon - RSTRING(temp)->ptr));
@@ -375,7 +379,7 @@ VALUE URIClassifier_init(VALUE self)
 {
   VALUE hash;
 
-  // we create an internal hash to protect stuff from the GC
+/* we create an internal hash to protect stuff from the GC */
   hash = rb_hash_new();
   rb_ivar_set(self, id_handler_map, hash);
 
@@ -491,23 +495,23 @@ VALUE URIClassifier_resolve(VALUE self, VALUE uri)
 
   handler = tst_search(uri_str, tst, &pref_len);
 
-  // setup for multiple return values
+  /* setup for multiple return values */
   result = rb_ary_new();
 
   if(handler) {
     rb_ary_push(result, rb_str_substr (uri, 0, pref_len));
-    // compensate for a script_name="/" where we need to add the "/" to path_info to keep it consistent
+    /* compensate for a script_name="/" where we need to add the "/" to path_info to keep it consistent */
     if(pref_len == 1 && uri_str[0] == '/') {
-      // matches the root URI so we have to use the whole URI as the path_info
+      /* matches the root URI so we have to use the whole URI as the path_info */
       rb_ary_push(result, uri);
     } else {
-      // matches a script so process like normal
+      /* matches a script so process like normal */
       rb_ary_push(result, rb_str_substr(uri, pref_len, RSTRING(uri)->len));
     }
       
     rb_ary_push(result, (VALUE)handler);
   } else {
-    // not found so push back nothing
+    /* not found so push back nothing */
     rb_ary_push(result, Qnil);
     rb_ary_push(result, Qnil);
     rb_ary_push(result, Qnil);
