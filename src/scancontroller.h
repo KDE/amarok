ee90d8b849ec2a2213732a1fdf37cdf46414c636
Andrew Turner <andrewturner512+kdebugs@googlemail.com>
2006-08-30 15:36:07 +0000

    Revert accidentally changed scan controller retries constant.
    svn path=/trunk/extragear/multimedia/amarok/; revision=578922

--- src/scancontroller.h
+++ src/scancontroller.h
@@ -99,7 +99,7 @@ class ScanController : public ThreadWeaver::DependentJob, public QXmlDefaultHand
         void customEvent( QCustomEvent* );
 
         // Member variables:
-        static const uint MAX_RESTARTS = 2;
+        static const uint MAX_RESTARTS = 20;
 
         KProcIO* m_scanner;
         QStringList m_folders;
