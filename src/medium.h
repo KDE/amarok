692693c290615442e92335df919ca9d9053af04c
Seb Ruiz <ruiz@kde.org>
2006-09-22 01:10:53 +0000

    tabs to spaces
    svn path=/trunk/extragear/multimedia/amarok/; revision=587237

--- src/medium.h
+++ src/medium.h
@@ -28,71 +28,71 @@
 class LIBAMAROK_EXPORT Medium
 {
 public:
-	typedef QValueList<const Medium> List;
+    typedef QValueList<const Medium> List;
 
     static const uint AUTODETECTED = 0;
-	static const uint ID = 1;
-	static const uint NAME = 2;
-	static const uint LABEL = 3;
-	static const uint USER_LABEL = 4;
-	static const uint MOUNTABLE = 5;
-	static const uint DEVICE_NODE = 6;
-	static const uint MOUNT_POINT = 7;
-	static const uint FS_TYPE = 8;
-	static const uint MOUNTED = 9;
-	static const uint BASE_URL = 10;
-	static const uint MIME_TYPE = 11;
-	static const uint ICON_NAME = 12;
-	static const uint PROPERTIES_COUNT = 13;
-	static const QString SEPARATOR;
+    static const uint ID = 1;
+    static const uint NAME = 2;
+    static const uint LABEL = 3;
+    static const uint USER_LABEL = 4;
+    static const uint MOUNTABLE = 5;
+    static const uint DEVICE_NODE = 6;
+    static const uint MOUNT_POINT = 7;
+    static const uint FS_TYPE = 8;
+    static const uint MOUNTED = 9;
+    static const uint BASE_URL = 10;
+    static const uint MIME_TYPE = 11;
+    static const uint ICON_NAME = 12;
+    static const uint PROPERTIES_COUNT = 13;
+    static const QString SEPARATOR;
 
-	Medium();
+    Medium();
     Medium(const Medium *medium);
-	Medium(const QString &id, const QString &name);
-	static const Medium create(const QStringList &properties);
-	static List createList(const QStringList &properties);
+    Medium(const QString &id, const QString &name);
+    static const Medium create(const QStringList &properties);
+    static List createList(const QStringList &properties);
 
-	const QStringList &properties() const { return m_properties; };
+    const QStringList &properties() const { return m_properties; };
 
     bool isAutodetected() const { return m_properties[AUTODETECTED]=="true"; };
-	QString id() const { return m_properties[ID]; };
-	QString name() const { return m_properties[NAME]; };
-	QString label() const { return m_properties[LABEL]; };
-	QString userLabel() const { return m_properties[USER_LABEL]; };
-	bool isMountable() const { return m_properties[MOUNTABLE]=="true"; };
-	QString deviceNode() const { return m_properties[DEVICE_NODE]; };
-	QString mountPoint() const { return m_properties[MOUNT_POINT]; };
-	QString fsType() const { return m_properties[FS_TYPE]; };
-	bool isMounted() const { return m_properties[MOUNTED]=="true"; };
-	QString baseURL() const { return m_properties[BASE_URL]; };
-	QString mimeType() const { return m_properties[MIME_TYPE]; };
-	QString iconName() const { return m_properties[ICON_NAME]; };
+    QString id() const { return m_properties[ID]; };
+    QString name() const { return m_properties[NAME]; };
+    QString label() const { return m_properties[LABEL]; };
+    QString userLabel() const { return m_properties[USER_LABEL]; };
+    bool isMountable() const { return m_properties[MOUNTABLE]=="true"; };
+    QString deviceNode() const { return m_properties[DEVICE_NODE]; };
+    QString mountPoint() const { return m_properties[MOUNT_POINT]; };
+    QString fsType() const { return m_properties[FS_TYPE]; };
+    bool isMounted() const { return m_properties[MOUNTED]=="true"; };
+    QString baseURL() const { return m_properties[BASE_URL]; };
+    QString mimeType() const { return m_properties[MIME_TYPE]; };
+    QString iconName() const { return m_properties[ICON_NAME]; };
 
-	bool needMounting() const;
-	KURL prettyBaseURL() const;
-	QString prettyLabel() const;
+    bool needMounting() const;
+    KURL prettyBaseURL() const;
+    QString prettyLabel() const;
 
     void setAutodetected(bool autodetected);
     void setId(const QString &id);
     void setMountPoint(const QString &mountPoint);
-	void setName(const QString &name);
-	void setLabel(const QString &label);
-	void setUserLabel(const QString &label);
+    void setName(const QString &name);
+    void setLabel(const QString &label);
+    void setUserLabel(const QString &label);
     void setFsType(const QString &type);
 
-	bool mountableState(bool mounted);
-	void mountableState(const QString &deviceNode,
-	                    const QString &mountPoint,
-	                    const QString &fsType, bool mounted);
-	void unmountableState(const QString &baseURL = QString::null);
+    bool mountableState(bool mounted);
+    void mountableState(const QString &deviceNode,
+                        const QString &mountPoint,
+                        const QString &fsType, bool mounted);
+    void unmountableState(const QString &baseURL = QString::null);
 
-	void setMimeType(const QString &mimeType);
-	void setIconName(const QString &iconName);
+    void setMimeType(const QString &mimeType);
+    void setIconName(const QString &iconName);
 
 private:
-	void loadUserLabel();
+    void loadUserLabel();
 
-	QStringList m_properties;
+    QStringList m_properties;
 
 friend class QValueListNode<const Medium>;
 };
