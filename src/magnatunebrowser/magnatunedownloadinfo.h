9ef3ab31c5a7008a2a0ace1cca3374f1e7075ab2
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2006-11-05 17:18:31 +0000

    And yet more license headers. I might actually learn to remember this somtime. maybe...
    svn path=/trunk/extragear/multimedia/amarok/; revision=602302

--- src/magnatunebrowser/magnatunedownloadinfo.h
+++ src/magnatunebrowser/magnatunedownloadinfo.h
@@ -1,14 +1,23 @@
-//
-// C++ Interface: magnatune_download_info
-//
-// Description: 
-//
-//
-// Author: Mark Kretschmann <markey@web.de>, (C) 2006
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/*
+  Copyright (c) 2006  Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
+*/
+
+
 #ifndef MAGNATUNE_DOWNLOAD_INFO_H
 #define MAGNATUNE_DOWNLOAD_INFO_H
 
