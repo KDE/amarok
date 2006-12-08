aa7ba3c9abd8b8b78ea3e4b976d4b1b2d64c56bd
Mark Kretschmann <kretschmann@kde.org>
2006-12-08 08:19:50 +0000

    Don't pass things through the shell. Thanks to Dirk Mueller for the patch.
    svn path=/trunk/extragear/multimedia/amarok/; revision=611452

--- src/scripts/mp3fix/mp3fixer.rb
+++ src/scripts/mp3fix/mp3fixer.rb
@@ -15,15 +15,20 @@ MenuItemName = "MP3Fixer FixIt!"
 
 
 def cleanup()
-    `dcop amarok script removeCustomMenuItem #{MenuItemName}`
-    `rm #{Dir.getwd()}/mp3fixer_playlist.m3u`
+    system("dcop", "amarok", "script", "removeCustomMenuItem", MenuItemName)
+    # use unlink ??!
+    system("rm",  Dir.getwd() + "/mp3fixer_playlist.m3u")
 end
 
+class String
+    def shellquote
+        return "'" + self.gsub("'", "'\\\\''") + "'"
+    end
+end
 
 trap( "SIGTERM" ) { cleanup() }
 
-`dcop amarok script addCustomMenuItem #{MenuItemName}`
-
+system("dcop", "amarok", "script", "addCustomMenuItem", MenuItemName)
 
 loop do
     message = gets().chomp()
@@ -34,7 +39,7 @@ loop do
             msg  = 'Mp3Fixer does not have configuration options. Simply select a track in the '
             msg += 'playlist, then start Mp3Fixer from the context-menu (right mouse click).'
 
-            `dcop amarok playlist popupMessage "#{msg}"`
+            system("dcop", "amarok", "playlist", "popupMessage", msg)
 
         when "customMenuClicked"
             if message.include?( MenuItemName )
@@ -54,34 +59,34 @@ loop do
 
                     mp3fix = File.dirname( File.expand_path( __FILE__ ) ) + "/mp3fix.rb"
 
-                    `dcop amarok playlist shortStatusMessage "Mp3Fixer is analyzing the file '#{filename}'..."`
-                    output = `ruby #{mp3fix} "#{path}"`
+                    system("dcop", "amarok", "playlist", "shortStatusMessage", "Mp3Fixer is analyzing the file '#{filename}'...")
+                    output = `env ruby #{mp3fix.shellquote} #{path.shellquote}`
 
                     if $?.success?()
                         reg = Regexp.new( "MP3FIX REPAIR SUMMARY:.*", Regexp::MULTILINE )
                         report = reg.match( output ).to_s()
                         report.gsub!( "\n", "<BR/>" )
-                        `dcop amarok playlist popupMessage "#{report}"`
+                        system("dcop", "amarok", "playlist", "popupMessage", report)
 
                         folders << File.dirname( path ) unless folders.include?( File.dirname( path ) )
                     else
                         reg = Regexp.new( "Error:.*", Regexp::MULTILINE )
                         errormsg = reg.match( output )
 
-                        `dcop amarok playlist popupMessage "Mp3Fixer #{errormsg}"`
+                        system("dcop", "amarok", "playlist", "popupMessage", "Mp3Fixer #{errormsg}")
                     end
                 end
 
                 # Touch all folders of the modified files, so that the scanner picks then up
                 folders.each do |folder|
-                    `touch #{folder}`
+                    system("touch", folder)
                 end
-                `dcop amarok collection scanCollectionChanges`
+                system("dcop", "amarok", "collection", "scanCollectionChanges")
 
                 # Refresh the playlist
-                `dcop amarok playlist saveM3u #{Dir.getwd()}/mp3fixer_playlist.m3u false`
-                `dcop amarok playlist clearPlaylist`
-                `dcop amarok playlist addMedia #{Dir.getwd()}/mp3fixer_playlist.m3u`
+                system("dcop", "amarok", "playlist", "saveM3u", Dir.getwd() + "/mp3fixer_playlist.m3u", "false")
+                system("dcop", "amarok", "playlist", "clearPlaylist")
+                system("dcop", "amarok", "playlist", "addMedia", Dir.getwd() + "/mp3fixer_playlist.m3u")
             end
     end
 end
