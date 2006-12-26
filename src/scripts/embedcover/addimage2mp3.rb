9f79c8e82819caa64f25c36d7e3c256a49ea44d0
Mark Kretschmann <kretschmann@kde.org>
2006-12-26 15:17:36 +0000

    change tab size
    svn path=/trunk/extragear/multimedia/amarok/; revision=616720

--- src/scripts/embedcover/addimage2mp3.rb
+++ src/scripts/embedcover/addimage2mp3.rb
@@ -13,7 +13,7 @@
 # @see http://id3lib.sourceforge.net/id3/id3v2com-00.html#sec3.1
 #
 def getId3v2Size( data )
-    return data[6]*2**21 + data[7]*2**14 + data[8]*2**7 + data[9]
+  return data[6]*2**21 + data[7]*2**14 + data[8]*2**7 + data[9]
 end
 
 #
@@ -21,52 +21,52 @@ end
 # @see http://id3lib.sourceforge.net/id3/id3v2com-00.html#sec3.1
 #
 def setId3v2Size( data, size )
-    data[6] = size >> 21 & 0x7f
-    data[7] = size >> 14 & 0x7f
-    data[8] = size >> 7  & 0x7f
-    data[9] = size >> 0  & 0x7f
+  data[6] = size >> 21 & 0x7f
+  data[7] = size >> 14 & 0x7f
+  data[8] = size >> 7  & 0x7f
+  data[9] = size >> 0  & 0x7f
 end
 
 #
 # Unsynchronize the entire ID3-V2 tag, frame by frame (replace 0xfff* with 0xff00f*)
 #
 def unsynchronize( data )
-    data[5] |= 0b10000000  # Set Unsychronization global tag flag
-    index = 10  # Skip ID3 Header
+  data[5] |= 0b10000000  # Set Unsychronization global tag flag
+  index = 10  # Skip ID3 Header
 
-    until data[index] == 0 or data[index..index+2] == "3DI"
-        frametype = data[index..index+3]
-        framesize = data[index+4]*2**21 + data[index+5]*2**14 + data[index+6]*2**7 + data[index+7]
+  until data[index] == 0 or data[index..index+2] == "3DI"
+    frametype = data[index..index+3]
+    framesize = data[index+4]*2**21 + data[index+5]*2**14 + data[index+6]*2**7 + data[index+7]
 
-        # Check if frame is already unsynced
-        if data[index+9] & 0x02 == 0
-            puts( "#{frametype}  unsynced " )
-            data[index+9] |= 0x02
-            index += 10
+    # Check if frame is already unsynced
+    if data[index+9] & 0x02 == 0
+      puts( "#{frametype}  unsynced " )
+      data[index+9] |= 0x02
+      index += 10
 
-            framesize.times() do
-                sync = data[index] == 0xff
-                sync1 = data[index+1] & 0b11100000 == 0b11100000
-                sync2 = data[index+1] & 0b11111111 == 0b00000000
+      framesize.times() do
+        sync = data[index] == 0xff
+        sync1 = data[index+1] & 0b11100000 == 0b11100000
+        sync2 = data[index+1] & 0b11111111 == 0b00000000
 
-                if sync and ( sync1 or sync2 ) and index < framesize - 2
-                    print( "." ) if sync1
-                    print( "O" ) if sync2
-                    data[index+1, 0] = "\0"
-                    framesize += 1
-                    index += 1
-                end
-
-                index += 1
-            end
-        else
-            puts( "#{frametype}  synced" )
-            index += framesize + 10
+        if sync and ( sync1 or sync2 ) and index < framesize - 2
+          print( "." ) if sync1
+          print( "O" ) if sync2
+          data[index+1, 0] = "\0"
+          framesize += 1
+          index += 1
         end
-    end
 
-    # Index == new tag length
-    return index - 10
+        index += 1
+      end
+    else
+      puts( "#{frametype}  synced" )
+      index += framesize + 10
+    end
+  end
+
+  # Index == new tag length
+  return index - 10
 end
 
 
@@ -78,12 +78,12 @@ path = ""
 destination = ""
 
 if $*.length() < 2 or $*[0] == "--help"
-    puts( "Usage: addimage2mp3.rb image source [destination]" )
-    puts()
-    puts( "Adds an image to the ID3-V2 tag of a MP3 file. Requires an existing ID3-V2 tag in" )
-    puts( "the file." )
-    puts()
-    exit( 1 )
+  puts( "Usage: addimage2mp3.rb image source [destination]" )
+  puts()
+  puts( "Adds an image to the ID3-V2 tag of a MP3 file. Requires an existing ID3-V2 tag in" )
+  puts( "the file." )
+  puts()
+  exit( 1 )
 end
 
 imagepath = $*[0]
@@ -92,37 +92,37 @@ destination = path
 
 
 if $*.length() == 3
-    destination = $*[2]
+  destination = $*[2]
 end
 
 
 unless FileTest::readable_real?( imagepath )
-    puts( "Error: Image not found or not readable." )
-    exit( 1 )
+  puts( "Error: Image not found or not readable." )
+  exit( 1 )
 end
 
 unless FileTest::readable_real?( path )
-    puts( "Error: Source not found or not readable." )
-    exit( 1 )
+  puts( "Error: Source not found or not readable." )
+  exit( 1 )
 end
 
 unless File.extname( path ).downcase() == ".mp3"
-    puts( "Error: File is not mp3." )
-    exit( 1 )
+  puts( "Error: File is not mp3." )
+  exit( 1 )
 end
 
 
 mimetype = case File.extname( imagepath ).downcase()
-    when ".bmp" then "image/bmp"
-    when ".gif" then "image/gif"
-    when ".jpg" then "image/jpeg"
-    when ".jpeg" then "image/jpeg"
-    when ".png" then "image/png"
-    when "" then "image/png"  # amaroK's cover images are always PNG
+when ".bmp" then "image/bmp"
+when ".gif" then "image/gif"
+when ".jpg" then "image/jpeg"
+when ".jpeg" then "image/jpeg"
+when ".png" then "image/png"
+when "" then "image/png"  # amaroK's cover images are always PNG
 
-    else
-        puts( "Error: Image type invalid." )
-        exit( 1 )
+else
+  puts( "Error: Image type invalid." )
+  exit( 1 )
 end
 
 
@@ -132,12 +132,12 @@ id3length = 0
 
 
 if data[0,3] == "ID3"
-    id3length = getId3v2Size( data )
-    puts( "ID3-V2 detected. Tag size: #{id3length}" )
+  id3length = getId3v2Size( data )
+  puts( "ID3-V2 detected. Tag size: #{id3length}" )
 else
-    puts( "ID3-V1 detected." )
-    puts( "Error: File does not have a ID3-V2 tag." )
-    exit( 1 )
+  puts( "ID3-V1 detected." )
+  puts( "Error: File does not have a ID3-V2 tag." )
+  exit( 1 )
 end
 
 
@@ -170,14 +170,14 @@ puts( "Locating end of last ID3 frame.." )
 
 index = 10  # Skip ID3 Header
 until data[index] == 0 or data[index..index+2] == "3DI"
-    frametype = data[index..index+3]
-    puts( "Frame Type : #{frametype}" )
-    if frametype == "APIC"
-        puts( "Error: File does already contain an image." )
-        exit( 1 )
-    end
-    framesize = data[index+4]*2**21 + data[index+5]*2**14 + data[index+6]*2**7 + data[index+7]
-    index += 10 + framesize
+  frametype = data[index..index+3]
+  puts( "Frame Type : #{frametype}" )
+  if frametype == "APIC"
+    puts( "Error: File does already contain an image." )
+    exit( 1 )
+  end
+  framesize = data[index+4]*2**21 + data[index+5]*2**14 + data[index+6]*2**7 + data[index+7]
+  index += 10 + framesize
 end
 
 index += 10 if data[index..index+2] == "3DI"  # Footer
@@ -207,8 +207,8 @@ print( "Writing file..  " )
 destfile = File::open( destination, File::CREAT|File::TRUNC|File::WRONLY )
 
 if destfile == nil
-    puts( "Error: Destination file is not writable." )
-    exit( 1 )
+  puts( "Error: Destination file is not writable." )
+  exit( 1 )
 end
 
 destfile << data
@@ -217,4 +217,3 @@ puts( "done." )
 
 puts()
 puts( "done." )
-
--- src/scripts/embedcover/embedcover.rb
+++ src/scripts/embedcover/embedcover.rb
@@ -14,12 +14,12 @@ MenuItemName = "EmbedCover DoIt!"
 
 
 def cleanup()
-    `dcop amarok script removeCustomMenuItem #{MenuItemName}`
+  `dcop amarok script removeCustomMenuItem #{MenuItemName}`
 end
 
 
 def sql_escape( string )
-    return string.gsub( /[']/, "''" )
+  return string.gsub( /[']/, "''" )
 end
 
 
@@ -29,82 +29,81 @@ trap( "SIGTERM" ) { cleanup() }
 
 
 loop do
-    message = gets().chomp()
-    command = /[A-Za-z]*/.match( message ).to_s()
+  message = gets().chomp()
+  command = /[A-Za-z]*/.match( message ).to_s()
 
-    case command
-        when "configure"
-            msg  = 'EmbedCover does not have configuration options. Simply select a track in the '
-            msg += 'playlist, then start EmbedCover from the context-menu (right mouse click).'
+  case command
+  when "configure"
+    msg  = 'EmbedCover does not have configuration options. Simply select a track in the '
+    msg += 'playlist, then start EmbedCover from the context-menu (right mouse click).'
 
-            `dcop amarok playlist popupMessage "#{msg}"`
+    `dcop amarok playlist popupMessage "#{msg}"`
 
-        when "customMenuClicked"
-            if message.include?( MenuItemName )
-                args = message.split()
-                # Remove the command args
-                3.times() { args.delete_at( 0 ) }
+  when "customMenuClicked"
+    if message.include?( MenuItemName )
+      args = message.split()
+      # Remove the command args
+      3.times() { args.delete_at( 0 ) }
 
-                # Iterate over all selected files
-                args.each() do |arg|
-                    uri = URI.parse( arg )
-                    file = URI.unescape( uri.path() )
+      # Iterate over all selected files
+      args.each() do |arg|
+        uri = URI.parse( arg )
+        file = URI.unescape( uri.path() )
 
-                    puts( "Path: #{file}" )
+        puts( "Path: #{file}" )
 
-                    backend = File.dirname( File.expand_path( __FILE__ ) ) + "/addimage2mp3.rb"
+        backend = File.dirname( File.expand_path( __FILE__ ) ) + "/addimage2mp3.rb"
 
-                    # Query is two parts, first ID, then name
-                    artist_id = `dcop amarok collection query "SELECT DISTINCT artist FROM tags WHERE url = '#{sql_escape( file )}'"`.chomp()
-                    artist    = `dcop amarok collection query "SELECT DISTINCT artist.name FROM artist WHERE id = '#{artist_id}'"`.chomp()
+        # Query is two parts, first ID, then name
+        artist_id = `dcop amarok collection query "SELECT DISTINCT artist FROM tags WHERE url = '#{sql_escape( file )}'"`.chomp()
+        artist    = `dcop amarok collection query "SELECT DISTINCT artist.name FROM artist WHERE id = '#{artist_id}'"`.chomp()
 
-                    album_id  = `dcop amarok collection query "SELECT DISTINCT album FROM tags WHERE url = '#{sql_escape( file )}'"`.chomp()
-                    album     = `dcop amarok collection query "SELECT DISTINCT album.name FROM album WHERE id = '#{album_id}'"`.chomp()
+        album_id  = `dcop amarok collection query "SELECT DISTINCT album FROM tags WHERE url = '#{sql_escape( file )}'"`.chomp()
+        album     = `dcop amarok collection query "SELECT DISTINCT album.name FROM album WHERE id = '#{album_id}'"`.chomp()
 
-                    puts( "ArtistId : #{artist_id}" )
-                    puts( "Artist   : #{artist}" )
-                    puts( "AlbumId  : #{album_id}" )
-                    puts( "Album    : #{album}" )
+        puts( "ArtistId : #{artist_id}" )
+        puts( "Artist   : #{artist}" )
+        puts( "AlbumId  : #{album_id}" )
+        puts( "Album    : #{album}" )
 
-                    if artist_id.empty?() and album_id.empty?()
-                        `dcop amarok playlist popupMessage "EmbedCover Error: This track is not in your Collection."`
-                        next
-                    end
+        if artist_id.empty?() and album_id.empty?()
+          `dcop amarok playlist popupMessage "EmbedCover Error: This track is not in your Collection."`
+          next
+        end
 
-                    md5sum = MD5.hexdigest( "#{artist.downcase()}#{album.downcase()}" )
-                    imagefolder = "#{ENV['HOME']}/.kde/share/apps/amarok/albumcovers/large/"
-                    image = "#{imagefolder}#{md5sum}"
+        md5sum = MD5.hexdigest( "#{artist.downcase()}#{album.downcase()}" )
+        imagefolder = "#{ENV['HOME']}/.kde/share/apps/amarok/albumcovers/large/"
+        image = "#{imagefolder}#{md5sum}"
 
-                    puts( "Imagepath: #{image}" )
+        puts( "Imagepath: #{image}" )
 
-                    unless FileTest.exist?( image )
-                        # If there is no imported image, check if there is an image associated
-                        # in the music folder
+        unless FileTest.exist?( image )
+          # If there is no imported image, check if there is an image associated
+          # in the music folder
 
-                        sql = "SELECT path FROM images WHERE artist LIKE #{sql_escape( artist )} AND album LIKE #{sql_escape( album )} ORDER BY path;"
-                        images = `dcop amarok collection query #{sql}`.split( "\n" )
+          sql = "SELECT path FROM images WHERE artist LIKE #{sql_escape( artist )} AND album LIKE #{sql_escape( album )} ORDER BY path;"
+          images = `dcop amarok collection query #{sql}`.split( "\n" )
 
-                        # FIXME select best image from array, like CollectionDB does
-                        image = images.first()
+          # FIXME select best image from array, like CollectionDB does
+          image = images.first()
 
-                        if image == nil or not FileTest.exist?( image )
-                            `dcop amarok playlist popupMessage "EmbedCover: No image found for this track."`
-                            next
-                        end
-                    end
+          if image == nil or not FileTest.exist?( image )
+            `dcop amarok playlist popupMessage "EmbedCover: No image found for this track."`
+            next
+          end
+        end
 
-                    output = `ruby #{backend} "#{image}" "#{file}"`
+        output = `ruby #{backend} "#{image}" "#{file}"`
 
-                    if $?.success?()
-                        `dcop amarok playlist popupMessage "EmbedCover has successfully embedded the image."`
-                    else
-                        reg = Regexp.new( "Error:.*", Regexp::MULTILINE )
-                        errormsg = reg.match( output )
+        if $?.success?()
+          `dcop amarok playlist popupMessage "EmbedCover has successfully embedded the image."`
+        else
+          reg = Regexp.new( "Error:.*", Regexp::MULTILINE )
+          errormsg = reg.match( output )
 
-                        `dcop amarok playlist popupMessage "EmbedCover #{errormsg}"`
-                    end
-                end
-            end
+          `dcop amarok playlist popupMessage "EmbedCover #{errormsg}"`
+        end
+      end
     end
+  end
 end
-
