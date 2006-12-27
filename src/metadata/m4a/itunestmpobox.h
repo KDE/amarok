dc6317d1e78103e05bbda0eda4249c921c243e94
Martin Aumüller <aumuell@reserv.at>
2006-12-27 09:28:16 +0000

    reduce krazy amount of complaints by adding license CCMAIL: jochen.issing@isign-softart.de
    svn path=/trunk/extragear/multimedia/amarok/; revision=616893

--- src/metadata/m4a/itunestmpobox.h
+++ src/metadata/m4a/itunestmpobox.h
@@ -1,3 +1,24 @@
+/***************************************************************************
+    copyright            : (C) 2002, 2003, 2006 by Jochen Issing
+    email                : jochen.issing@isign-softart.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *   This library is free software; you can redistribute it and/or modify  *
+ *   it  under the terms of the GNU Lesser General Public License version  *
+ *   2.1 as published by the Free Software Foundation.                     *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful, but   *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA  02110-1301  USA                                                   *
+ ***************************************************************************/
+
 #ifndef ITUNESTMPOBOX_H
 #define ITUNESTMPOBOX_H
 
