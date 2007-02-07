d5db7a84379a7b37d70f7b12e16af73eec383cbf
Laurent Montel <montel@kde.org>
2007-02-07 09:17:58 +0000

    Stupid me: bad copy/paste
    svn path=/trunk/extragear/multimedia/amarok/; revision=631159

--- src/amarok_export.h
+++ src/amarok_export.h
@@ -26,7 +26,7 @@
 #else // not windows
 
 #define AMAROK_EXPORT KDE_EXPORT
-#define AMAROK_TAGLIB_EXPORT KDE_IMPORT
+#define AMAROK_TAGLIB_EXPORT KDE_EXPORT
 #endif /* not windows */
 
 #endif /* _AMAROK_EXPORT_H */
