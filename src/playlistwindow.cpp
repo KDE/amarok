049a06118aa7b34ae8a27d2c2a1eafcb2a5e2222
Seb Ruiz <ruiz@kde.org>
2007-02-08 12:15:46 +0000

    Don't show text in top playlistwindow toolbar.
    svn path=/trunk/extragear/multimedia/amarok/; revision=631548

--- src/playlistwindow.cpp
+++ src/playlistwindow.cpp
@@ -364,6 +364,7 @@ void PlaylistWindow::init()
 
     { //<Search LineEdit>
         KToolBar *bar = new KToolBar( m_browsers->contentsWidget(), "NotMainToolBar" );
+        bar->setToolButtonStyle( Qt::ToolButtonIconOnly );
         bar->setFixedHeight( 30 ); //FIXME This is just a hack
         bar->setIconDimensions( 22 ); //looks more sensible
         bar->setMovable( false ); //removes the ugly frame
