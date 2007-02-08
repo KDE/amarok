96848f0a1b88b163459976d2808d987c497b1749
Alexandre Pereira de Oliveira <aoliveira@kdemail.net>
2007-02-08 22:44:32 +0000

    less reasons for crashing on exit: KActions belonging to the window, not to the Application
    svn path=/trunk/extragear/multimedia/amarok/; revision=631747

--- src/app.cpp
+++ src/app.cpp
@@ -421,58 +421,58 @@ void App::initGlobalShortcuts()
     KAction* action;
 
 //    m_pGlobalAccel->insert( "play", i18n( "Play" ), 0, KKey("WIN+x"), 0, ec, SLOT( play() ), true, true );
-    action = new KAction( i18n( "Play" ), this );
+    action = new KAction( i18n( "Play" ), m_pPlaylistWindow );
     action->setGlobalShortcut( KShortcut( "WIN+x" ) );
     connect( action, SIGNAL( triggered() ), ec, SLOT( play() ) );
 
 //    m_pGlobalAccel->insert( "pause", i18n( "Pause" ), 0, 0, 0, ec, SLOT( pause() ), true, true );
-    action = new KAction( i18n( "Pause" ), this );
+    action = new KAction( i18n( "Pause" ), m_pPlaylistWindow );
     connect( action, SIGNAL( triggered() ), ec, SLOT( pause() ) );
 
 //    m_pGlobalAccel->insert( "play_pause", i18n( "Play/Pause" ), 0, KKey("WIN+c"), 0, ec, SLOT( playPause() ), true, true );
-    action = new KAction( i18n( "Play/Pause" ), this );
+    action = new KAction( i18n( "Play/Pause" ), m_pPlaylistWindow );
     action->setGlobalShortcut( KShortcut( "WIN+c" ) );
     connect( action, SIGNAL( triggered() ), ec, SLOT( playPause() ) );
 
 //    m_pGlobalAccel->insert( "stop", i18n( "Stop" ), 0, KKey("WIN+v"), 0, ec, SLOT( stop() ), true, true );
-    action = new KAction( i18n( "Stop" ), this );
+    action = new KAction( i18n( "Stop" ), m_pPlaylistWindow );
     action->setGlobalShortcut( KShortcut( "WIN+v" ) );
     connect( action, SIGNAL( triggered() ), ec, SLOT( stop() ) );
 
 //    m_pGlobalAccel->insert( "stop_after_global", i18n( "Stop Playing After Current Track" ), 0, KKey("WIN+CTRL+v"), 0, Playlist::instance()->qscrollview(), SLOT( toggleStopAfterCurrentTrack() ), true, true );
-    action = new KAction( i18n( "Stop Playing After Current Track" ), this );
+    action = new KAction( i18n( "Stop Playing After Current Track" ), m_pPlaylistWindow );
     action->setGlobalShortcut( KShortcut( "WIN+CTRL+v" ) );
     connect( action, SIGNAL( triggered() ), Playlist::instance()->qscrollview(), SLOT( toggleStopAfterCurrentTrack() ) );
 
 //    m_pGlobalAccel->insert( "next", i18n( "Next Track" ), 0, KKey("WIN+b"), 0, ec, SLOT( next() ), true, true );
-    action = new KAction( i18n( "Next Track" ), this );
+    action = new KAction( i18n( "Next Track" ), m_pPlaylistWindow );
     action->setGlobalShortcut( KShortcut( "WIN+b" ) );
     connect( action, SIGNAL( triggered() ), ec, SLOT( next() ) );
 
 //    m_pGlobalAccel->insert( "prev", i18n( "Previous Track" ), 0, KKey("WIN+z"), 0, ec, SLOT( previous() ), true, true );
-    action = new KAction( i18n( "Previous Track" ), this );
+    action = new KAction( i18n( "Previous Track" ), m_pPlaylistWindow );
     action->setGlobalShortcut( KShortcut( "WIN+z" ) );
     connect( action, SIGNAL( triggered() ), ec, SLOT( previous() ) );
 
 //    m_pGlobalAccel->insert( "volup", i18n( "Increase Volume" ), 0, KKey("WIN+KP_Add"), 0, ec, SLOT( increaseVolume() ), true, true );
-    action = new KAction( i18n( "Increase Volume" ), this );
+    action = new KAction( i18n( "Increase Volume" ), m_pPlaylistWindow );
     action->setGlobalShortcut( KShortcut( "WIN+KP_Add" ) );
     connect( action, SIGNAL( triggered() ), ec, SLOT( increaseVolume() ) );
 
 //    m_pGlobalAccel->insert( "voldn", i18n( "Decrease Volume" ), 0, KKey("WIN+KP_Subtract"), 0, ec, SLOT( decreaseVolume() ), true, true );
-    action = new KAction( i18n( "Decrease Volume" ), this );
+    action = new KAction( i18n( "Decrease Volume" ), m_pPlaylistWindow );
     action->setGlobalShortcut( KShortcut( "WIN+KP_Subtract" ) );
     connect( action, SIGNAL( triggered() ), ec, SLOT( decreaseVolume() ) );
 
 
 //    m_pGlobalAccel->insert( "seekforward", i18n( "Seek Forward" ), 0, KKey("WIN+Shift+KP_Add"), 0, ec, SLOT( seekForward() ), true, true );
-    action = new KAction( i18n( "Seek Forward" ), this );
+    action = new KAction( i18n( "Seek Forward" ), m_pPlaylistWindow );
     action->setGlobalShortcut( KShortcut( "WIN+Shift+KP_Add" ) );
     connect( action, SIGNAL( triggered() ), ec, SLOT( seekForward() ) );
 
 
 //    m_pGlobalAccel->insert( "seekbackward", i18n( "Seek Backward" ), 0, KKey("WIN+Shift+KP_Subtract"), 0, ec, SLOT( seekBackward() ), true, true );
-    action = new KAction( i18n( "Seek Backward" ), this );
+    action = new KAction( i18n( "Seek Backward" ), m_pPlaylistWindow );
     action->setGlobalShortcut( KShortcut( "WIN+Shift+KP_Subtract" ) );
     connect( action, SIGNAL( triggered() ), ec, SLOT( seekbackward() ) );
 
