7958e9b821ff1af019ed82e5dbc39183892eab8e
Jeff Mitchell <mitchell@kde.org>
2007-02-21 00:52:56 +0000

    Put star-color-changing options in config (not working yet).  I wanted to use QLabels, as they look a bit nicer, but there's no clickable property for them. And for some reason there's a space below the QFrame holding the stars that doesn't show up in Designer...don't know what's causing it.  I'll look later, unless someone more experienced knows what's wrong and wants to fix it  :-)
    svn path=/branches/stable/extragear/multimedia/amarok/; revision=635815

--- src/Options1.ui.h
+++ src/Options1.ui.h
@@ -15,13 +15,15 @@
 
 #include "amarokconfig.h"
 #include "moodbar.h"
+#include "starmanager.h"
 #include <kstandarddirs.h>
 
 
 void Options1::init()
 {
     slotUpdateMoodFrame();
-    
+    slotUpdateRatingsFrame();
+
     QStringList browsers;
     browsers << "konqueror" << "firefox" << "opera" << "galeon" << "epiphany"
              << "safari" << "mozilla";
@@ -67,19 +69,66 @@ void Options1::init()
 void Options1::slotUpdateMoodFrame()
 {
     if( Moodbar::executableExists() )
-      {
+    {
         moodbarHelpLabel->hide();
         moodFrame->setEnabled(true);
 
         kcfg_MakeMoodier->setEnabled(kcfg_ShowMoodbar->isChecked());
         kcfg_AlterMood->setEnabled(kcfg_ShowMoodbar->isChecked() && kcfg_MakeMoodier->isChecked());
         kcfg_MoodsWithMusic->setEnabled(kcfg_ShowMoodbar->isChecked());
-      }
+    }
 
     else
-      {
+    {
         moodbarHelpLabel->show();
         kcfg_ShowMoodbar->setChecked(false);
         moodFrame->setEnabled(false);
-      }
+    }
+}
+
+void Options1::slotUpdateRatingsFrame()
+{
+    kcfg_CustomRatingsColors->setEnabled( kcfg_UseRatings->isChecked() );
+    bool enableStars = kcfg_UseRatings->isChecked() && kcfg_CustomRatingsColors->isChecked();
+    {
+        ratingsFrame->setEnabled( enableStars );
+
+        fivestar_1->setPixmap( *StarManager::instance()->getStar( 5 ) );
+        fivestar_2->setPixmap( *StarManager::instance()->getStar( 5 ) );
+        fivestar_3->setPixmap( *StarManager::instance()->getStar( 5 ) );
+        fivestar_4->setPixmap( *StarManager::instance()->getStar( 5 ) );
+        fivestar_5->setPixmap( *StarManager::instance()->getStar( 5 ) );
+        fivestar_1->setEnabled( enableStars );
+        fivestar_2->setEnabled( enableStars );
+        fivestar_3->setEnabled( enableStars );
+        fivestar_4->setEnabled( enableStars );
+        fivestar_5->setEnabled( enableStars );
+
+        fourstar_1->setPixmap( *StarManager::instance()->getStar( 4 ) );
+        fourstar_2->setPixmap( *StarManager::instance()->getStar( 4 ) );
+        fourstar_3->setPixmap( *StarManager::instance()->getStar( 4 ) );
+        fourstar_4->setPixmap( *StarManager::instance()->getStar( 4 ) );
+        fourstar_1->setEnabled( enableStars );
+        fourstar_2->setEnabled( enableStars );
+        fourstar_3->setEnabled( enableStars );
+        fourstar_4->setEnabled( enableStars );
+
+        threestar_1->setPixmap( *StarManager::instance()->getStar( 3 ) );
+        threestar_2->setPixmap( *StarManager::instance()->getStar( 3 ) );
+        threestar_3->setPixmap( *StarManager::instance()->getStar( 3 ) );
+        threestar_1->setEnabled( enableStars );
+        threestar_2->setEnabled( enableStars );
+        threestar_3->setEnabled( enableStars );
+
+        twostar_1->setPixmap( *StarManager::instance()->getStar( 2 ) );
+        twostar_2->setPixmap( *StarManager::instance()->getStar( 2 ) );
+        twostar_1->setEnabled( enableStars );
+        twostar_2->setEnabled( enableStars );
+
+        onestar_1->setPixmap( *StarManager::instance()->getStar( 1 ) );
+        onestar_1->setEnabled( enableStars );
+
+        halfstar->setPixmap( *StarManager::instance()->getHalfStar() );
+        halfstar->setEnabled( enableStars );
+    }
 }
