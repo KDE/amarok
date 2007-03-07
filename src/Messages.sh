e4cc0f7cce302fb10fd28f452bf82b15eb91ae6b
Stephan Kulow <coolo@kde.org>
2007-03-07 13:50:18 +0000

    fixing bash syntax
    svn path=/trunk/extragear/multimedia/amarok/; revision=640273

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 $EXTRACTRC `find . -name "*.rc" -o -name "*.ui" -o -name "*.kcfg"` > rc.cpp
-LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -
-o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
+LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
 if test -n "$LIST"; then
 	$XGETTEXT $LIST -o $podir/amarok.pot
-fi	
+fi
