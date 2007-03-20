dd49a60a230bec53b0be5635b5f309ccb3dda461
Seb Ruiz <ruiz@kde.org>
2007-03-20 12:44:45 +0000

    skeleton for a graphics item to contain a bunch of album (eg, newest of favourite albums box)
    svn path=/trunk/extragear/multimedia/amarok/; revision=644571

---
+++ src/contextview/albumbox.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+ * copyright            : (C) 2007 Seb Ruiz <ruiz@kde.org>                 *
+ **************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef AMAROK_ALBUMBOX_H
+#define AMAROK_ALBUMBOX_H
+
+#include "contextbox.h"
+
+class QGraphicsItem;
+class QGraphicsPixmapItem;
+class QGraphicsRectItem;
+class QGraphicsScene;
+class QSize;
+
+namespace Context
+{
+    /*
+     * A group of items for displaying a "row" in the box, consisting of an image
+     * and some sort of descriptive text
+     */
+    class AlbumItem : public QGraphicsItemGroup
+    {
+        public:
+            AlbumItem( QGraphicsItem *parent = 0, QGraphicsScene *scene = 0 );
+            ~AlbumItem() { }
+
+            void setCover( const QString &location );
+            void setCover( const QPixmap &image );
+
+        private:
+            QGraphicsPixmapItem *m_coverItem;
+    };
+
+    /*
+     * Contains a bunch of those AlbumItem groups in a table like format, one on
+     * each row
+     */
+    class AlbumBox : public ContextBox
+    {
+        public:
+            AlbumBox( QGraphicsItem *parent = 0, QGraphicsScene *scene = 0 );
+            ~AlbumBox() { /* delete, disconnect and disembark */ }
+        
+            void setTitle( const QString &title );
+
+        private:
+            void createBox();
+            void setBoundingRectSize( const QSize &sz );
+
+            QGraphicsRectItem *m_boundingBox;
+            QGraphicsTextItem *m_titleItem;
+    };
+
+}
+
+#endif
+
