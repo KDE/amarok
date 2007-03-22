2d47fcfebca6f841851c11d5c8f2c913499acc15
Dan Meltzer <parallelgrapefruit@gmail.com>
2007-03-22 02:07:20 +0000

    Fix overeager cleanup of config.h.cmake
    svn path=/trunk/extragear/multimedia/amarok/; revision=645254

--- config.h.cmake
+++ config.h.cmake
@@ -22,6 +22,24 @@
 /* have libnjb */
 #cmakedefine HAVE_LIBNJB 1
 
+/* The size of a `char *', as computed by sizeof. */
+#define SIZEOF_CHAR_P 4
+
+/* The size of a `int', as computed by sizeof. */
+#define SIZEOF_INT 4
+
+/* The size of a `long', as computed by sizeof. */
+#define SIZEOF_LONG 4
+
+/* The size of a `short', as computed by sizeof. */
+#define SIZEOF_SHORT 2
+
+/* The size of a `size_t', as computed by sizeof. */
+#define SIZEOF_SIZE_T 4
+
+/* The size of a `unsigned long', as computed by sizeof. */
+#define SIZEOF_UNSIGNED_LONG 4
+
 /* have LIBVISUAL */
 #cmakedefine HAVE_LIBVISUAL 1
 
