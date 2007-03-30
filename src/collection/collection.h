8384ea453be65c9e27e6ffca78afeff8f31fa1ff
Maximilian Kossick <maximilian.kossick@googlemail.com>
2007-03-30 13:50:32 +0000

    fixed copyright
    svn path=/trunk/extragear/multimedia/amarok/; revision=648089

--- src/collection/collection.h
+++ src/collection/collection.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2006-2007 Maximilian Kossick <maximilian.kossick@googlemail.com>
+ *  Copyright (c) 2007 Maximilian Kossick <maximilian.kossick@googlemail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
