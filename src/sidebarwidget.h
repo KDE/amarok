a8ecca5da0628d36f72a90bda112f63b6d157a4b
Mark Kretschmann <kretschmann@kde.org>
2007-04-17 20:09:00 +0000

    update copyright
    svn path=/trunk/extragear/multimedia/amarok/; revision=655140

--- src/sidebarwidget.h
+++ src/sidebarwidget.h
@@ -1,5 +1,6 @@
 /*
   Copyright (c) 2006 Gábor Lehel <illissius@gmail.com>
+  Copyright (c) 2007 Mark Kretschmann <markey@web.de> 
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Library General Public
