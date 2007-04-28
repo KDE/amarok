ed89ff8152e8de254010848cc6f704323d6a51dc
Maximilian Kossick <maximilian.kossick@googlemail.com>
2007-04-28 08:54:58 +0000

    use CollectionManager instead of CollectionDB
    svn path=/trunk/extragear/multimedia/amarok/; revision=658709

--- src/servicebrowser/databasehandlerbase.cpp
+++ src/servicebrowser/databasehandlerbase.cpp
@@ -14,12 +14,12 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301, USA.          *
  ***************************************************************************/
 
 #include "databasehandlerbase.h"
 
-#include "collectiondb.h"
+#include "collectionmanager.h"
 
 /*DatabaseHandlerBase* 
 DatabaseHandlerBase::instance()
@@ -37,17 +37,17 @@ void
 DatabaseHandlerBase::begin( )
 {
 
-    CollectionDB *db = CollectionDB::instance();
+    CollectionManager *mgr = CollectionManager::instance();
     QString queryString = "BEGIN;";
-    db->query( queryString );
+    mgr->sqlQuery( queryString );
 }
 
 void 
 DatabaseHandlerBase::commit( )
 {
-    CollectionDB *db = CollectionDB::instance();
+    CollectionManager *mgr = CollectionManager::instance();
     QString queryString = "COMMIT;";
-    db->query( queryString );
+    mgr->sqlQuery( queryString );
 }
 
 
