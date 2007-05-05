8c8ac8e3d532a023fe604d7753a78d8b2778994a
Mark Kretschmann <kretschmann@kde.org>
2007-05-05 10:49:00 +0000

    Increase frequency of the main timer (300ms -> 150ms), so that the progress slider handler moves more smoothly. (needs a higher resolution because it's so wide now)
    svn path=/trunk/extragear/multimedia/amarok/; revision=661365

--- src/enginecontroller.h
+++ src/enginecontroller.h
@@ -63,7 +63,7 @@ public:
     void updateBundleRating( const int rating ) { m_bundle.setRating(rating); } //Can't update metabundle rating from bundle(), d'oh
 
     //xx000, xx100, xx200, so at most will be 200ms delay before time displays are updated
-    static const int MAIN_TIMER = 300;
+    static const int MAIN_TIMER = 150;
 
     /*enum Filetype { MP3 };*/ //assuming MP3 for time being
     AMAROK_EXPORT static bool installDistroCodec(const QString& engine /*Filetype type*/);
