21376a92f2260a9f40b56184f6ac8fe7e6a27bed
Mark Kretschmann <kretschmann@kde.org>
2007-05-12 09:05:10 +0000

    Make ctor protected.
    svn path=/trunk/extragear/multimedia/amarok/; revision=663760

--- src/configdialog/ConfigDialogBase.h
+++ src/configdialog/ConfigDialogBase.h
@@ -1,3 +1,22 @@
+/***************************************************************************
+ *   Copyright (C) 2004-2007 by Mark Kretschmann <markey@web.de>           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301, USA.          *
+ ***************************************************************************/
+
 #ifndef CONFIGDIALOGBASE_H
 #define CONFIGDIALOGBASE_H
 
@@ -7,12 +26,13 @@
 class ConfigDialogBase : public QWidget
 {
     public:
-        ConfigDialogBase( QWidget* parent ) : QWidget( parent ) {}
-
         virtual void updateSettings() = 0;
 
         virtual bool hasChanged() = 0;
         virtual bool isDefault() = 0;
+
+    protected:
+        ConfigDialogBase( QWidget* parent ) : QWidget( parent ) {}
 };
 
 
