32d3e20c87e55322bdbd38aa2309e1525b58bfa7
Mark Kretschmann <kretschmann@kde.org>
2007-05-12 16:13:11 +0000

    Add copyright
    svn path=/trunk/extragear/multimedia/amarok/; revision=663905

--- src/configdialog/GeneralConfig.cpp
+++ src/configdialog/GeneralConfig.cpp
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2004-2007 by Mark Kretschmann <markey@web.de>           *
+ *   Copyright (C) 2005      by Ian Monroe <ian@monroe.nu>                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
