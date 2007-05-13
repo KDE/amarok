e0b0131ccb63b7347a32ae9ae0283bef238c051f
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2007-05-13 18:50:27 +0000

    Add correct copyright headers to the 2 new files
    svn path=/trunk/extragear/multimedia/amarok/; revision=664353

--- src/collectionbrowser/collectionsortfilterproxymodel.h
+++ src/collectionbrowser/collectionsortfilterproxymodel.h
@@ -1,14 +1,22 @@
-//
-// C++ Interface: collectionsortfilterproxymodel
-//
-// Description: 
-//
-//
-// Author:  <>, (C) 2007
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+ *   Copyright (c) 2007  Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
+ ***************************************************************************/
+
 #ifndef COLLECTIONSORTFILTERPROXYMODEL_H
 #define COLLECTIONSORTFILTERPROXYMODEL_H
 
@@ -17,7 +25,7 @@
 /**
 This is a custom QSortFilterProxyModel that solves some issues with sorting a model that usses lazy loading
 
-	@author 
+	@author Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
 */
 class CollectionSortFilterProxyModel : public QSortFilterProxyModel
 {
