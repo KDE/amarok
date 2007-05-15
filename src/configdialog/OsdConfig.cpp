b177140e790d93e66479cedbae6a5336bc7852fa
Mark Kretschmann <kretschmann@kde.org>
2007-05-15 08:19:44 +0000

    Add copyright.
    svn path=/trunk/extragear/multimedia/amarok/; revision=664934

--- src/configdialog/OsdConfig.cpp
+++ src/configdialog/OsdConfig.cpp
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2004-2007 by Mark Kretschmann <markey@web.de>           *
+ *   Copyright (C) 2004      by Frederik Holljen <fh@ez.no>                *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
