4580ffbb367f7b2411c0b9bb860b3bfb9a0eff97
Ian Monroe <ian.monroe@gmail.com>
2007-05-19 21:41:41 +0000

    r9225@gomashio:  ian | 2007-05-19 16:41:30 -0500  replacing LGPL headers with GPLv2, the LGPL was a mistake
    svn path=/trunk/extragear/multimedia/amarok/; revision=666435

--- src/collectionbrowser/singlecollectiontreeitemmodel.h
+++ src/collectionbrowser/singlecollectiontreeitemmodel.h
@@ -1,23 +1,11 @@
- /*
-  Copyright (c) 2007  Alexandre Pereira de Oliveira <aleprj@gmail.com>
-  Copyright (c) 2007 Maximilian Kossick <maximilian.kossick@googlemail.com>
-  Copyright (c) 2007 Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Library General Public
-  License as published by the Free Software Foundation; either
-  version 2 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Library General Public License for more details.
-
-  You should have received a copy of the GNU Library General Public License
-  along with this library; see the file COPYING.LIB.  If not, write to
-  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-  Boston, MA 02110-1301, USA.
-*/
+/******************************************************************************
+ * copyright: (c) 2007 Alexandre Pereira de Oliveira <aleprj@gmail.com>       *
+ *            (c) 2007 Maximilian Kossick <maximilian.kossick@googlemail.com> *
+ *            (c) 2007 Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>         *
+ *   This program is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU General Public License version 2           *
+ *   as published by the Free Software Foundation.                            *
+ ******************************************************************************/
 
 #ifndef SINGLECOLLECTIONTREEITEMMODEL_H
 #define SINGLECOLLECTIONTREEITEMMODEL_H
