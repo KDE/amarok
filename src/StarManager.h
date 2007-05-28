14ba11cf72b8681238edc488664487e97dcaee9a
Maximilian Kossick <maximilian.kossick@googlemail.com>
2007-05-28 08:45:07 +0000

    EBN warnings--
    svn path=/trunk/extragear/multimedia/amarok/; revision=668967

--- src/StarManager.h
+++ src/StarManager.h
@@ -1,14 +1,20 @@
-//
-// C++ Interface: starmanager
-//
-// Description: Small little manager to return the color stars we want
-//
-//
-// Author: Jeff Mitchell <kde-dev@emailgoeshere.com>, (C) 2007
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/* 
+   Copyright (C) 2007 Jeff Mitchell <kde-dev@emailgoeshere.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+*/
 
 
 #ifndef AMAROK_STAR_MANAGER_H
