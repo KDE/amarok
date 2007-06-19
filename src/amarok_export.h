35e465c9b901aad9d5690eba061927c54c7d6ea0
Laurent Montel <montel@kde.org>
2007-06-19 20:05:48 +0000

    Regenate export file
    svn path=/trunk/extragear/multimedia/amarok/; revision=677741

--- src/amarok_export.h
+++ src/amarok_export.h
@@ -1,27 +1,40 @@
-// Copyright:  See COPYING file that comes with this distribution
+/*  This file is part of the KDE project
+    Copyright (C) 2007 David Faure <faure@kde.org>
 
-// Do not add new exports to this file if you can help it.
-// Use amarok_libs_export.h or amarok_engines_export.h as touching
-// this file forces a largeish rebuild.
-#ifndef _AMAROK_EXPORT_H_
-#define _AMAROK_EXPORT_H_
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef AMAROK_EXPORT_H
+#define AMAROK_EXPORT_H
+
+/* needed for KDE_EXPORT and KDE_IMPORT macros */
 #include <kdemacros.h>
 
-#ifdef Q_WS_WIN
-
 #ifndef AMAROK_EXPORT
-# ifdef MAKE_AMAROK_LIB
+# if defined(MAKE_AMAROK_LIB)
+   /* We are building this library */ 
 #  define AMAROK_EXPORT KDE_EXPORT
 # else
+   /* We are using this library */ 
 #  define AMAROK_EXPORT KDE_IMPORT
 # endif
 #endif
 
-#else // not windows
+# ifndef AMAROK_EXPORT_DEPRECATED
+#  define AMAROK_EXPORT_DEPRECATED KDE_DEPRECATED AMAROK_EXPORT
+# endif
 
-#define AMAROK_EXPORT KDE_EXPORT
-
-#endif /* not windows */
-
-#endif /* _AMAROK_EXPORT_H */
+#endif
--- src/amarok_libs_export.h
+++ src/amarok_libs_export.h
@@ -1,25 +1,40 @@
-//Copyright: see the COPYING file that comes with this distribution
+/*  This file is part of the KDE project
+    Copyright (C) 2007 David Faure <faure@kde.org>
 
-#ifndef _AMAROK_LIBS_EXPORT_H
-#define _AMAROK_LIBS_EXPORT_H
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef AMAROK_LIBS_EXPORT_H
+#define AMAROK_LIBS_EXPORT_H
+
+/* needed for KDE_EXPORT and KDE_IMPORT macros */
 #include <kdemacros.h>
 
-#ifdef Q_WS_WIN
-
 #ifndef AMAROK_TAGLIB_EXPORT
-# ifdef MAKE_AMAROK_TAGLIB_LIB
+# if defined(MAKE_AMAROK_TAGLIB_LIB)
+   /* We are building this library */ 
 #  define AMAROK_TAGLIB_EXPORT KDE_EXPORT
 # else
+   /* We are using this library */ 
 #  define AMAROK_TAGLIB_EXPORT KDE_IMPORT
 # endif
 #endif
 
+# ifndef AMAROK_TAGLIB_EXPORT_DEPRECATED
+#  define AMAROK_TAGLIB_EXPORT_DEPRECATED KDE_DEPRECATED AMAROK_TAGLIB_EXPORT
+# endif
 
-#else //not windows
-
-#define AMAROK_TAGLIB_EXPORT KDE_EXPORT
-
-#endif //not windows
-
-#endif // _AMAROK_LIBS_EXPORT_H
+#endif
