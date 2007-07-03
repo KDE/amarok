259a29fe0aeede057b617d423267a571194f3eb0
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2007-07-03 15:46:46 +0000

    commiting these two files so Bart has something to play with
    svn path=/trunk/extragear/multimedia/amarok/; revision=682877

---
+++ src/servicebrowser/scriptableservice/ScriptableServiceCollection.cpp
@@ -0,0 +1,58 @@
+//
+// C++ Implementation: scriptableservicecollection
+//
+// Description: 
+//
+//
+// Author:  <>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "ScriptableServiceCollection.h"
+#include "support/memoryquerymaker.h"
+
+
+ScriptableServiceCollection::ScriptableServiceCollection(const QString & name)
+    : Collection()
+    , MemoryCollection()
+{ 
+    m_name = name;
+}
+
+
+ScriptableServiceCollection::~ScriptableServiceCollection()
+{
+}
+
+QueryMaker*
+ScriptableServiceCollection::queryMaker()
+{
+    return new MemoryQueryMaker( this, collectionId() );
+}
+
+QString ScriptableServiceCollection::prettyName() const
+{
+    return m_name;
+}
+
+QString ScriptableServiceCollection::collectionId() const
+{
+    return m_name;
+}
+
+
+bool ScriptableServiceCollection::possiblyContainsTrack(const KUrl & url) const
+{
+    return false;
+}
+
+Meta::TrackPtr ScriptableServiceCollection::ScriptableServiceCollection::trackForUrl(const KUrl & url)
+{
+    return TrackPtr();
+}
+
+CollectionLocation * ScriptableServiceCollection::ScriptableServiceCollection::location() const
+{
+    return 0;
+}
---
+++ src/servicebrowser/scriptableservice/ScriptableServiceCollection.h
@@ -0,0 +1,47 @@
+//
+// C++ Interface: scriptableservicecollection
+//
+// Description: 
+//
+//
+// Author:  <>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef SCRIPTABLESERVICECOLLECTION_H
+#define SCRIPTABLESERVICECOLLECTION_H
+
+#include "collection.h"
+#include "support/memorycollection.h"
+
+/**
+A collection for use by the scriptable service. Stores everything in memory
+
+	@author 
+*/
+class ScriptableServiceCollection : public Collection, public MemoryCollection
+{
+public:
+    ScriptableServiceCollection( const QString &name );
+
+    virtual ~ScriptableServiceCollection();
+
+    virtual QueryMaker * queryMaker();
+    virtual void startFullScan() { }
+
+    virtual QString collectionId() const;
+    virtual QString prettyName() const;
+  
+    virtual bool possiblyContainsTrack( const KUrl &url ) const;
+    virtual Meta::TrackPtr trackForUrl( const KUrl &url );
+
+    virtual CollectionLocation* location() const;
+
+private:
+
+    QString m_name;
+
+};
+
+#endif
