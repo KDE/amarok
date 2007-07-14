f4ace3a88ba2362670a21d5417a1209317271336
Maximilian Kossick <maximilian.kossick@googlemail.com>
2007-07-14 13:30:56 +0000

    ...and less debug output here
    svn path=/trunk/extragear/multimedia/amarok/; revision=687831

--- src/collection/sqlcollection/sqlquerybuilder.cpp
+++ src/collection/sqlcollection/sqlquerybuilder.cpp
@@ -148,7 +148,7 @@ SqlQueryBuilder::run()
     }
     else
     {
-        debug() << "Query is " << query() << endl;
+        //debug() << "Query is " << query() << endl;
         d->worker = new SqlWorkerThread( this );
         connect( d->worker, SIGNAL( done( ThreadWeaver::Job* ) ), SLOT( done( ThreadWeaver::Job* ) ) );
         ThreadWeaver::Weaver::instance()->enqueue( d->worker );
