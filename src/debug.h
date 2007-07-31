358fda7bbcf282e3a5e372b000b19bf5e705fb41
Jeff Mitchell <mitchell@kde.org>
2007-07-31 16:55:01 +0000

    Attempt to fix breakage from new kdelibs
    svn path=/trunk/extragear/multimedia/amarok/; revision=694773

--- src/debug.h
+++ src/debug.h
@@ -104,10 +104,10 @@ namespace Debug
             KDEBUG_FATAL = 3
         };
 
-        static inline kdbgstream debug()   { mutex.lock(); QByteArray ind = indent(); mutex.unlock(); return kdbgstream( ind.data(), 0, KDEBUG_INFO  ) << AMK_PREFIX; }
-        static inline kdbgstream warning() { mutex.lock(); QByteArray ind = indent(); mutex.unlock(); return kdbgstream( ind.data(), 0, KDEBUG_WARN  ) << AMK_PREFIX << "[WARNING!] "; }
-        static inline kdbgstream error()   { mutex.lock(); QByteArray ind = indent(); mutex.unlock(); return kdbgstream( ind.data(), 0, KDEBUG_ERROR ) << AMK_PREFIX << "[ERROR!] "; }
-        static inline kdbgstream fatal()   { mutex.lock(); QByteArray ind = indent(); mutex.unlock(); return kdbgstream( ind.data(), 0, KDEBUG_FATAL ) << AMK_PREFIX; }
+        static inline kdbgstream debug()   { mutex.lock(); QByteArray ind = indent(); mutex.unlock(); return kdbgstream( new QString( ind.data() + KDEBUG_INFO ) ) << AMK_PREFIX; }
+        static inline kdbgstream warning() { mutex.lock(); QByteArray ind = indent(); mutex.unlock(); return kdbgstream( new QString( ind.data() + KDEBUG_WARN ) ) << AMK_PREFIX << "[WARNING!] "; }
+        static inline kdbgstream error()   { mutex.lock(); QByteArray ind = indent(); mutex.unlock(); return kdbgstream( new QString( ind.data() + KDEBUG_ERROR ) ) << AMK_PREFIX << "[ERROR!] "; }
+        static inline kdbgstream fatal()   { mutex.lock(); QByteArray ind = indent(); mutex.unlock(); return kdbgstream( new QString( ind.data() + KDEBUG_FATAL ) ) << AMK_PREFIX; }
 
         typedef kdbgstream DebugStream;
 
