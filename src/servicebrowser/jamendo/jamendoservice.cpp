7d2dd057c0950e0fe782d11a874b75933d315fe6
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2007-09-11 14:18:39 +0000

    add leeo's new jamendo service icon.... this one is fuuuun
    svn path=/trunk/extragear/multimedia/amarok/; revision=711130

--- src/servicebrowser/jamendo/jamendoservice.cpp
+++ src/servicebrowser/jamendo/jamendoservice.cpp
@@ -33,7 +33,7 @@ JamendoService::JamendoService(const QString & name)
 {
 
     setShortDescription("A site where artists can showcase their creations to the world");
-    setIcon( KIcon( Amarok::icon( "download" ) ) );
+    setIcon( KIcon( Amarok::icon( "amarok_jamendo" ) ) );
 
 }
 
