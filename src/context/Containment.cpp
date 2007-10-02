c62051f18850523683dc83b2a889e9b821438e7e
Leo Franchi <lfranchi@kde.org>
2007-10-02 03:32:34 +0000

    engines work better when all the parts are included
    svn path=/trunk/extragear/multimedia/amarok/; revision=719909

---
+++ src/context/Containment.cpp
@@ -0,0 +1,123 @@
+/***************************************************************************
+* copyright            : (C) 2007 Leo Franchi <lfranchi@gmail.com>        *
+**************************************************************************/
+
+/***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#include "Containment.h"
+
+#include "plasma/corona.h"
+
+#include <QGraphicsSceneContextMenuEvent>
+#include <QGraphicsScene>
+#include <KMenu>
+
+namespace Context
+{
+
+Containment::Containment( QGraphicsItem* parent, const QString& serviceId, uint containmentId ) 
+        : Plasma::Containment( parent, serviceId, containmentId )
+        , m_appletBrowserAction( 0 )
+{}
+
+Containment::Containment( QObject* parent, const QVariantList& args )
+        : Plasma::Containment( parent, args )
+        , m_appletBrowserAction( 0 )
+{}
+
+Containment::~Containment()
+{
+}
+
+QList<QAction*> Containment::contextActions()
+{
+    
+    if (!m_appletBrowserAction) {
+        m_appletBrowserAction = new QAction(i18n("Add applet"), this);
+        connect(m_appletBrowserAction, SIGNAL(triggered(bool)), this, SLOT(launchAppletBrowser( "amarok" )));
+    }
+    
+    QList<QAction*> actions;
+    actions.append(m_appletBrowserAction);
+    return actions;
+}
+
+// reimplemented and modified from Plasma::Containment
+void Containment::contextMenuEvent(QGraphicsSceneContextMenuEvent* event)
+{
+
+    QPointF point = event->screenPos();
+
+    QGraphicsItem* item = scene()->itemAt(point);
+    if (item == this) {
+        item = 0;
+    }
+    
+    Applet* applet = 0;
+    
+    while (item) {
+        applet = qgraphicsitem_cast<Applet*>(item);
+        if (applet) {
+            break;
+        }
+        
+        item = item->parentItem();
+    }
+    
+    KMenu desktopMenu;
+    //kDebug() << "context menu event " << immutable;
+    if (!applet) {
+        if (!scene() || static_cast<Plasma::Corona*>(scene())->isImmutable()) {
+            QGraphicsItem::contextMenuEvent(event);
+            return;
+        }
+        
+        desktopMenu.addAction( m_appletBrowserAction );
+    } else if (applet->isImmutable()) {
+        QGraphicsItem::contextMenuEvent(event);
+        return;
+    } else {
+        bool hasEntries = false;
+        if (applet->hasConfigurationInterface()) {
+            QAction* configureApplet = new QAction(i18n("%1 Settings...", applet->name()), &desktopMenu);
+            connect(configureApplet, SIGNAL(triggered(bool)),
+                    applet, SLOT(showConfigurationInterface()));
+            desktopMenu.addAction(configureApplet);
+            hasEntries = true;
+        }
+        
+        if (scene() && !static_cast<Plasma::Corona*>(scene())->isImmutable()) {
+            QAction* closeApplet = new QAction(i18n("Remove this %1", applet->name()), &desktopMenu);
+            connect(closeApplet, SIGNAL(triggered(bool)),
+                    applet, SLOT(destroy()));
+            desktopMenu.addAction(closeApplet);
+            hasEntries = true;
+        }
+        
+        QList<QAction*> actions = applet->contextActions();
+        if (!actions.isEmpty()) {
+            desktopMenu.addSeparator();
+            desktopMenu.addAction( m_appletBrowserAction );
+            hasEntries = true;
+        }
+        
+        if (!hasEntries) {
+            QGraphicsItem::contextMenuEvent(event);
+            return;
+        }
+    }
+    
+    event->accept();
+    desktopMenu.exec(event->screenPos());
+}
+
+
+} // namespace Context
+#include "Containment.moc"
---
+++ src/context/Containment.h
@@ -0,0 +1,56 @@
+/***************************************************************************
+* copyright            : (C) 2007 Leo Franchi <lfranchi@gmail.com>        *
+**************************************************************************/
+
+/***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef AMAROK_CONTAINMENT_H
+#define AMAROK_CONTAINMENT_H
+
+#include "amarok_export.h"
+
+#include "plasma/containment.h"
+
+#include <QAction>
+
+namespace Context
+{
+
+class AMAROK_EXPORT Containment : public Plasma::Containment
+{
+    Q_OBJECT
+public:
+    Containment(QGraphicsItem* parent = 0,
+                const QString& serviceId = QString(),
+                uint containmentId = 0);
+    
+    Containment(QObject* parent, const QVariantList& args);
+    
+    ~Containment();
+    
+    virtual void saveToConfig( KConfig& conf ) = 0;
+    virtual void loadConfig( KConfig& conf ) = 0;
+    
+    virtual void update() = 0;
+    
+    QList<QAction*> contextActions();
+    
+public slots:
+    virtual void appletRemoved( QObject* object ) = 0;
+    
+protected:
+    void contextMenuEvent(QGraphicsSceneContextMenuEvent * event);
+    
+private:
+    QAction* m_appletBrowserAction;
+};
+
+} // Context namespace
+#endif
