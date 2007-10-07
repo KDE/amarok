e45a2ffeb1ab0f6322d2abba27eb080ea2b416d3
Maximilian Kossick <maximilian.kossick@googlemail.com>
2007-10-07 18:09:47 +0000

    added capability interfaces
    svn path=/trunk/extragear/multimedia/amarok/; revision=722615

---
+++ src/meta/Capability.h
@@ -0,0 +1,46 @@
+/* This file is part of the KDE project
+   Copyright (C) 2007 Maximilian Kossick <maximilian.kossick@googlemail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+*/
+
+#ifndef AMAROK_CAPABILITY_H
+#define AMAROK_CAPABILITY_H
+
+#include "amarok_export.h"
+
+#include <QObject>
+
+namespace Meta
+{
+    class AMAROK_EXPORT Capability : public QObject
+    {
+        Q_OBJECT
+        Q_ENUMS( Type )
+
+        public:
+            //add additinal capabilities here
+            enum Type { Unknown = 0
+                        , Editable = 1
+                        , LastFm = 2
+                        , Buyable = 3 };
+
+            virtual ~Capability() {};
+
+    };
+}
+
+
+#endif
---
+++ src/meta/LastFmCapability.h
@@ -0,0 +1,41 @@
+/* This file is part of the KDE project
+   Copyright (C) 2007 Maximilian Kossick <maximilian.kossick@googlemail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+*/
+
+#ifndef AMAROK_LASTFMCAPABILITY_H
+#define AMAROK_LASTFMCAPABILITY_H
+
+#include "amarok_export.h"
+#include "meta/Capability.h"
+
+namespace Meta
+{
+    class AMAROK_EXPORT LastFmCapability : public Meta::Capability
+    {
+        Q_OBJECT
+        public:
+            virtual ~LastFmCapability() {};
+
+            virtual void love() = 0;
+            virtual void ban() = 0;
+            virtual void skip() = 0;
+
+            static Type capabiltyInterfaceType() { return Meta::Capability::LastFm; }
+    };
+}
+
+#endif
