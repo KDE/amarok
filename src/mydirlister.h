95c940e8fc76c2420b791b442a860f4f7e08d26f
Dan Meltzer <parallelgrapefruit@gmail.com>
2007-10-09 16:52:36 +0000

    Forgotten leftovers.  Don't build mydiroperator any more and port the 'is a playlist' check in mydirlister
    svn path=/trunk/extragear/multimedia/amarok/; revision=723433

--- src/mydirlister.h
+++ src/mydirlister.h
@@ -5,29 +5,31 @@
 //     media files in that directory show a longMessage (preferably one that disappears when given a signal)
 
 #include "enginecontroller.h"
+#include "PlaylistHandler.h"
 
 #include <kdirlister.h>
 #include <kfileitem.h>
 
 class MyDirLister : public KDirLister {
 public:
-    MyDirLister( bool delayedMimeTypes ) : KDirLister() { setDelayedMimeTypes( delayedMimeTypes ); }
+    MyDirLister( bool delayedMimeTypes ) : KDirLister() { setDelayedMimeTypes( delayedMimeTypes ); setAutoUpdate( true ); }
 
 protected:
-    virtual bool matchesMimeFilter( const KFileItem *item ) const {
+    virtual bool matchesFilter( const KFileItem &item ) const {
+        PlaylistHandler ph;
         return
-            item->isDir() ||
-            EngineController::canDecode( item->url() ) ||
-            item->url().protocol() == "audiocd" ||
-// Port 2.0            PlaylistFile::isPlaylistFile( item->name() ) ||
-            item->name().endsWith( ".mp3", Qt::CaseInsensitive ) || //for now this is less confusing for the user
-            item->name().endsWith( ".aa", Qt::CaseInsensitive ) || //for adding to iPod
-            item->name().endsWith( ".mp4", Qt::CaseInsensitive ) || //for adding to iPod
-            item->name().endsWith( ".m4v", Qt::CaseInsensitive ) || //for adding to iPod
-            item->name().endsWith( ".m4b", Qt::CaseInsensitive ) || //for adding to iPod
-            item->name().endsWith( ".ogg", Qt::CaseInsensitive ) ||
-            item->name().endsWith( ".flac", Qt::CaseInsensitive ) ||
-            item->name().endsWith( ".wma", Qt::CaseInsensitive );
+            ( item.isDir() && !item.isHidden() ) ||
+            EngineController::canDecode( item.url() ) ||
+            item.url().protocol() == "audiocd" ||
+            ph.isPlaylist( item.url() ) ||
+            item.name().endsWith( ".mp3", Qt::CaseInsensitive ) || //for now this is less confusing for the user
+            item.name().endsWith( ".aa", Qt::CaseInsensitive ) || //for adding to iPod
+            item.name().endsWith( ".mp4", Qt::CaseInsensitive ) || //for adding to iPod
+            item.name().endsWith( ".m4v", Qt::CaseInsensitive ) || //for adding to iPod
+            item.name().endsWith( ".m4b", Qt::CaseInsensitive ) || //for adding to iPod
+            item.name().endsWith( ".ogg", Qt::CaseInsensitive ) ||
+            item.name().endsWith( ".flac", Qt::CaseInsensitive ) ||
+            item.name().endsWith( ".wma", Qt::CaseInsensitive );
     }
 };
 
