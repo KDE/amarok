6ad25e7ad1049be3972b15dab3b06c8fdaac4128
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2007-11-12 12:23:51 +0000

    commit a few missing deleted from last commit
    svn path=/trunk/extragear/multimedia/amarok/; revision=735681

--- src/servicebrowser/ServiceDynamicCollection.cpp
+++ src/servicebrowser/ServiceDynamicCollection.cpp
@@ -9,10 +9,12 @@
 // Copyright: See COPYING file that comes with this distribution
 //
 //
-#include "servicedynamiccollection.h"
+#include "ServiceDynamicCollection.h"
 
-ServiceDynamicCollection::ServiceDynamicCollection()
- : Collection()
+ServiceDynamicCollection::ServiceDynamicCollection( const QString &id, const QString &prettyName )
+    : ServiceCollection()
+    , m_collectionId( id )
+    , m_prettyName( prettyName )
 {
 }
 
@@ -22,3 +24,70 @@ ServiceDynamicCollection::~ServiceDynamicCollection()
 }
 
 
+
+
+void ServiceDynamicCollection::addTrack(QString key, Meta::TrackPtr trackPtr)
+{
+    MemoryCollection::addTrack( key, trackPtr );
+    const Meta::ServiceTrack * track = static_cast< const Meta::ServiceTrack * >( trackPtr.data() );
+    
+    if ( track->id() != 0 )
+        m_trackIdMap.insert( track->id(), trackPtr );
+}
+
+void ServiceDynamicCollection::addArtist(QString key, Meta::ArtistPtr artistPtr)
+{
+    MemoryCollection::addArtist( key, artistPtr );
+    const Meta::ServiceArtist * artist = static_cast< const Meta::ServiceArtist* >( artistPtr.data() );
+    
+    if ( artist->id() != 0 )
+        m_artistIdMap.insert( artist->id(), artistPtr );
+}
+
+void ServiceDynamicCollection::addAlbum(QString key, Meta::AlbumPtr albumPtr)
+{
+    MemoryCollection::addAlbum( key, albumPtr );
+    const Meta::ServiceAlbum * album = static_cast< const Meta::ServiceAlbum* >( albumPtr.data() );
+    
+    if ( album->id() != 0 )
+        m_albumIdMap.insert( album->id(), albumPtr );
+}
+
+void ServiceDynamicCollection::addGenre(QString key, Meta::GenrePtr genrePtr)
+{
+    MemoryCollection::addGenre( key, genrePtr );
+    const Meta::ServiceGenre * genre = static_cast< const Meta::ServiceGenre * >( genrePtr.data() );
+    
+    if ( genre->id() != 0 )
+        m_genreIdMap.insert( genre->id(), genrePtr );
+}
+
+Meta::TrackPtr ServiceDynamicCollection::trackById(int id)
+{
+    return m_trackIdMap.value( id );
+}
+
+Meta::AlbumPtr ServiceDynamicCollection::albumById(int id)
+{
+    return m_albumIdMap.value( id );
+}
+
+Meta::ArtistPtr ServiceDynamicCollection::artistById(int id)
+{
+    return m_artistIdMap.value( id );
+}
+
+Meta::GenrePtr ServiceDynamicCollection::genreById(int id)
+{
+    return m_genreIdMap.value( id );
+}
+
+QString ServiceDynamicCollection::collectionId() const
+{
+    return m_collectionId;
+}
+
+QString ServiceDynamicCollection::prettyName() const
+{
+    return m_prettyName;
+}
--- src/servicebrowser/ServiceDynamicCollection.h
+++ src/servicebrowser/ServiceDynamicCollection.h
@@ -12,14 +12,20 @@
 #ifndef SERVICEDYNAMICCOLLECTION_H
 #define SERVICEDYNAMICCOLLECTION_H
 
-#include <collection.h>
+#include <servicecollection.h>
+
+typedef QMap<int, Meta::TrackPtr> TrackIdMap;
+typedef QMap<int, Meta::ArtistPtr> ArtistIdMap;
+typedef QMap<int, Meta::AlbumPtr> AlbumIdMap;
+typedef QMap<int, Meta::GenrePtr> GenreIdMap;
+
 
 /**
 A specialised collection used for services that dynamically fetch their data from somewhere ( a web service, an external program, etc....)
 
 	@author 
 */
-class ServiceDynamicCollection : public Collection
+class ServiceDynamicCollection : public ServiceCollection
 {
 public:
 
@@ -29,22 +35,47 @@ public:
         virtual ~ServiceDynamicCollection();
 
         virtual void startFullScan() {} //TODO
-        virtual QueryMaker* queryMaker();
+        virtual QueryMaker* queryMaker() = 0;
 
-        virtual QString collectionId() const;
+        virtual QString collectionId()  const;
         virtual QString prettyName() const;
 
-        virtual QStringList query( const QString &query );
-        virtual int insert( const QString &statement, const QString &table );
+        virtual QStringList query( const QString &query ) { Q_UNUSED( query ); return QStringList(); }
+        virtual int insert( const QString &statement, const QString &table ) { Q_UNUSED( statement ); Q_UNUSED( table ); return 0; }
 
-        virtual QString escape( QString text ) const;
+        virtual QString escape( QString text ) const { Q_UNUSED( text ); return QString(); }
 
+
+        Meta::TrackPtr trackById( int id );
+        Meta::AlbumPtr albumById( int id );
+        Meta::ArtistPtr artistById( int id );
+        Meta::GenrePtr genreById( int id );
+
+
+        //Override some stuff to be able to hande id mappings
+        
+
+        void addTrack( QString key, Meta::TrackPtr trackPtr );
+        void addArtist( QString key, Meta::ArtistPtr artistPtr);
+        void addAlbum ( QString key, Meta::AlbumPtr albumPtr );
+        void addGenre( QString key, Meta::GenrePtr genrePtr);
+
+        //TODO:
+        //void setTrackMap( TrackMap map ) { m_trackMap = map; }
+        //void setArtistMap( ArtistMap map ) { m_artistMap = map; }
+        //void setAlbumMap( AlbumMap map ) { m_albumMap = map; }
+        //void setGenreMap( GenreMap map ) { m_genreMap = map; }
 
     private:
         ServiceMetaFactory * m_metaFactory;
 
         QString m_collectionId;
         QString m_prettyName;
+
+        TrackIdMap m_trackIdMap;
+        ArtistIdMap m_artistIdMap;
+        AlbumIdMap m_albumIdMap;
+        GenreIdMap m_genreIdMap;
 
 };
 
