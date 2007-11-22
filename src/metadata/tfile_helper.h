6a13ad72e740df8f45fcfe78b4b4660392937d7c
Shane King <kde@dontletsstart.com>
2007-11-22 06:58:01 +0000

    Added license to tfile_helper.h
    svn path=/trunk/extragear/multimedia/amarok/; revision=739881

--- src/metadata/tfile_helper.h
+++ src/metadata/tfile_helper.h
@@ -1,3 +1,24 @@
+/***************************************************************************
+    copyright            : (C) 2007 by Shane King
+    email                : kde@dontletsstart.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *   This library is free software; you can redistribute it and/or modify  *
+ *   it  under the terms of the GNU Lesser General Public License version  *
+ *   2.1 as published by the Free Software Foundation.                     *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful, but   *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA  02110-1301  USA                                                   *
+ ***************************************************************************/
+
 #ifndef TFILE_HELPER_H
 #define TFILE_HELPER_H
 
