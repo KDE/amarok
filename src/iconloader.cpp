fba3b0cdf79aefb1ffc76062176f741f4121c0f5
Jakob Petsovits <jpetso@gmx.at>
2007-11-22 21:07:49 +0000

    Fix/adapt most icon names in Amarok. Most of them should now be correct (and in Oxygen) or covered by sensible fallbacks. One still needs to be drawn by the artists (media-playlist-repeat), one has its rename pending (history -> view-history) and some are still private to Amarok or unclear.
    svn path=/trunk/extragear/multimedia/amarok/; revision=740245

--- src/iconloader.cpp
+++ src/iconloader.cpp
@@ -21,11 +21,14 @@
 #include "amarokconfig.h"
 
 #include <qmap.h>
+#include <qapplication.h>
 
 
 QString
 Amarok::icon( const QString& name ) //declared in amarok.h
 {
+    bool isRTL = QApplication::isRightToLeft();
+
     // We map our Amarok icon theme names to system icons, instead of using the same
     // naming scheme. This has two advantages:
     // 1. Our icons can have simpler and more meaningful names
@@ -33,32 +36,32 @@ Amarok::icon( const QString& name ) //declared in amarok.h
     static QMap<QString, QString> iconMap;
 
     if( iconMap.empty() ) {
-        iconMap["add_lyrics"]           = "edit_add";
-        iconMap["add_playlist"]         = "arrow_down";
-        iconMap["album"]                = "drive-optical";
-        iconMap["artist"]               = "user-female";
+        iconMap["add_lyrics"]           = "list-add";
+        iconMap["add_playlist"]         = "list-add";
+        iconMap["album"]                = "media-album";
+        iconMap["artist"]               = "view-media-artist";
         iconMap["audioscrobbler"]       = "audioscrobbler";
         iconMap["love"]                 = "love";
         iconMap["back"]                 = "media-skip-backward";
-        iconMap["burn"]                 = "cdburn";
-        iconMap["change_language"]      = "configure";
-        iconMap["clock"]                = "history";
+        iconMap["burn"]                 = "media-optical-recordable-burn";
+        iconMap["change_language"]      = "preferences-desktop-locale";
+        iconMap["clock"]                = "view-history";
         iconMap["collection"]           = "collection";
         iconMap["configure"]            = "configure";
         iconMap["covermanager"]         = "covermanager";
-        iconMap["device"]               = "ipod-unmount";
+        iconMap["device"]               = "multimedia-player-apple-ipod";
         iconMap["download"]             = "get-hot-new-stuff";
         iconMap["dynamic"]              = "dynamic";
-        iconMap["edit"]                 = "edit";
-        iconMap["editcopy"]             = "editcopy";
-        iconMap["equalizer"]            = "media-equalizer";
+        iconMap["edit"]                 = "object-edit";
+        iconMap["editcopy"]             = "edit-copy";
+        iconMap["equalizer"]            = "view-media-equalizer";
         iconMap["external"]             = "system-run";
-        iconMap["fastforward"]          = "2rightarrow";
-        iconMap["favourite_genres"]     = "kfm";
+        iconMap["fastforward"]          = "media-seek-forward";
+        iconMap["favourite_genres"]     = "system-file-manager";
         iconMap["files"]                = "folder";
-        iconMap["files2"]               = "folder_red";
-        iconMap["info"]                 = "dialog-information";
-        iconMap["lyrics"]               = "text-center";
+        iconMap["files2"]               = "folder-red";
+        iconMap["info"]                 = "document-properties";
+        iconMap["lyrics"]               = "view-media-lyrics";
         iconMap["magnatune"]            = "services";
         iconMap["mostplayed"]           = "favorites";
         iconMap["music"]                = "media-podcast";
@@ -67,39 +70,39 @@ Amarok::icon( const QString& name ) //declared in amarok.h
         iconMap["play"]                 = "media-playback-start";
         iconMap["playlist"]             = "media-playlist";
         iconMap["playlist_clear"]       = "edit-clear-list";
-        iconMap["playlist_refresh"]     = "rebuild";
-        iconMap["queue"]                = "arrow-down-double";
-        iconMap["queue_track"]          = "2rightarrow";
-        iconMap["dequeue_track"]        = "2leftarrow";
-        iconMap["random"]               = "roll";
-        iconMap["random_album"]         = "drive-optical";
-        iconMap["random_no"]            = "arrow-right";
-        iconMap["random_track"]         = "roll";
+        iconMap["playlist_refresh"]     = "view-refresh";
+        iconMap["queue"]                = "go-bottom";
+        iconMap["queue_track"]          = isRTL ? "go-previous" : "go-next";
+        iconMap["dequeue_track"]        = isRTL ? "go-next" : "go-previous";
+        iconMap["random"]               = "media-playlist-shuffle";
+        iconMap["random_album"]         = "media-album-shuffle";
+        iconMap["random_no"]            = "go-next";
+        iconMap["random_track"]         = "media-playlist-shuffle";
         iconMap["redo"]                 = "edit-redo";
         iconMap["refresh"]              = "view-refresh";
         iconMap["remove"]               = "edit-delete";
-        iconMap["remove_from_playlist"] = "remove";
-        iconMap["repeat_album"]         = "drive-optical";
-        iconMap["repeat_no"]            = "arrow-down";
-        iconMap["repeat_playlist"]      = "repeat_playlist";
-        iconMap["repeat_track"]         = "repeat_track";
-        iconMap["rescan"]               = "edit-refresh";
+        iconMap["remove_from_playlist"] = "list-remove";
+        iconMap["repeat_album"]         = "media-album-repeat";
+        iconMap["repeat_no"]            = "go-down";
+        iconMap["repeat_playlist"]      = "media-playlist-repeat";
+        iconMap["repeat_track"]         = "media-track-repeat";
+        iconMap["rescan"]               = "view-refresh";
         iconMap["rewind"]               = "media-seek-backward";
         iconMap["save"]                 = "document-save";
         iconMap["scripts"]              = "signature";
-        iconMap["search"]               = "find";
+        iconMap["search"]               = "edit-find";
         iconMap["settings_engine"]      = "amarok";
-        iconMap["settings_general"]     = "configure";
-        iconMap["settings_indicator"]   = "tv";
-        iconMap["settings_playback"]    = "speaker";
+        iconMap["settings_general"]     = "preferences-other";
+        iconMap["settings_indicator"]   = "preferences-desktop-display";
+        iconMap["settings_playback"]    = "preferences-desktop-sound";
         iconMap["settings_view"]        = "edit-find";
         iconMap["stop"]                 = "media-playback-stop";
         iconMap["podcast"]              = "media-podcast";
-        iconMap["podcast2"]             = "podcast_new";
-        iconMap["track"]                = "sound";
+        iconMap["podcast2"]             = "media-podcast-new";
+        iconMap["track"]                = "audio-x-generic";
         iconMap["undo"]                 = "edit-undo";
-        iconMap["visualizations"]       = "color-fill";
-        iconMap["zoom"]                 = "edit-find";
+        iconMap["visualizations"]       = "view-media-visualization";
+        iconMap["zoom"]                 = "zoom-in";
     }
 
     static QMap<QString, QString> amarokMap;
