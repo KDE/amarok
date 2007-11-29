98f83681c09bca252b40d9523f9142212dc7acfb
Albert Astals Cid <tsdgeos@terra.es>
2007-11-29 20:05:55 +0000

    Use >> to get translators placeholders
    svn path=/trunk/extragear/multimedia/amarok/; revision=743080

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-$EXTRACTRC `find . -name "*.rc" -o -name "*.ui" -o -name "*.kcfg"` > rc.cpp
+$EXTRACTRC `find . -name "*.rc" -o -name "*.ui" -o -name "*.kcfg"` >> rc.cpp
 LIST=`find . -name \*.h -o -name \*.cpp`
 if test -n "$LIST"; then
 	$XGETTEXT $LIST -o $podir/amarok.pot
