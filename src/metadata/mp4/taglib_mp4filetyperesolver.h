ac0cdd6ed3ad04832db6cd3a61a84967e413f39e
Jeff Mitchell <mitchell@kde.org>
2007-12-12 17:14:00 +0000

    Whoops, fix compile...
    svn path=/branches/stable/extragear/multimedia/amarok/; revision=747655

--- src/metadata/mp4/taglib_mp4filetyperesolver.h
+++ src/metadata/mp4/taglib_mp4filetyperesolver.h
@@ -31,7 +31,7 @@
 
 class MP4FileTypeResolver : public TagLib::FileRef::FileTypeResolver
 {
-    TagLib::File *createFile(TagLibFileName fileName,
+    TagLib::File *createFile(const char *fileName,
             bool readAudioProperties,
             TagLib::AudioProperties::ReadStyle audioPropertiesStyle) const;
 
