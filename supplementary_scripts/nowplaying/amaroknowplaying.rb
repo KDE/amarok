6503f2b5865a7dc37c51dcd6f3a8992f43e815f4
Dan Meltzer <parallelgrapefruit@gmail.com>
2008-01-16 04:18:22 +0000

    Silly typo
    svn path=/trunk/extragear/multimedia/amarok/; revision=762085

--- supplementary_scripts/nowplaying/amaroknowplaying.rb
+++ supplementary_scripts/nowplaying/amaroknowplaying.rb
@@ -23,7 +23,7 @@ if title.empty?
     output += `dcop amarok player nowPlaying`.chomp
 else
     # Strip file extension
-    extensions = ".ogg", ".mp3", ".wav", ".flac", ".fla", ".wma", ".mpc", ".ogg"
+    extensions = ".ogg", ".mp3", ".wav", ".flac", ".fla", ".wma", ".mpc", ".oga"
     ext = File.extname( title ).downcase
 
     if extensions.include?( ext )
