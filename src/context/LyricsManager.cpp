e55d8effc2514b5e9d40902f97aaf9f333166b5d
Dan Meltzer <parallelgrapefruit@gmail.com>
2008-01-24 18:45:17 +0000

    Remove misleading comment that is no longer applicable
    svn path=/trunk/extragear/multimedia/amarok/; revision=765816

--- src/context/LyricsManager.cpp
+++ src/context/LyricsManager.cpp
@@ -129,8 +129,6 @@ void LyricsManager::lyricsResult( QByteArray cXmlDoc, bool cached ) //SLOT
     } else
     {
         lyrics = el.text();
-        // TODO this crashes amarok.
-        // BAD META!
         EngineController::instance()->currentTrack()->setCachedLyrics( xmldoc); // TODO: setLyricsByPath?
 
         const QString title      = el.attribute( "title" );
