d45583bd555cfcbbc556087e7d542f6505cc061e
Bart Cerneels <bart.cerneels@kde.org>
2008-02-04 09:15:26 +0000

    Compile fix.
    SVN_SILENT
    
    svn path=/trunk/extragear/multimedia/amarok/; revision=770632

--- src/podcasts/PodcastReader.cpp
+++ src/podcasts/PodcastReader.cpp
@@ -77,7 +77,6 @@ bool
 PodcastReader::update( PodcastChannelPtr channel )
 {
     DEBUG_BLOCK
-    debug() << "channel = " << (unsigned int)(channel.data());
     m_channel = channel;
     m_current = static_cast<PodcastMetaCommon *>(channel.data());
 
