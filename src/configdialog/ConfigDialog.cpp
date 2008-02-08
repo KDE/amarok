fe4178d4215b6058ed5a7f273e700966ce7b9f26
Harald Sitter <sitter.harald@gmail.com>
2008-02-08 16:44:25 +0000

    Icon updates by Lee Olson. Lee, please try complying to the icon naming spec, it's taking me quite some time to fix the names.
    CCMAIL: leetolson@gmail.com
    
    svn path=/trunk/extragear/multimedia/amarok/; revision=772400

--- src/configdialog/ConfigDialog.cpp
+++ src/configdialog/ConfigDialog.cpp
@@ -60,8 +60,8 @@ Amarok2ConfigDialog::Amarok2ConfigDialog( QWidget *parent, const char* name, KCo
     addPage( engine,      i18n( "Engine" ), "preferences-media-engine-amarok", i18n( "Configure Engine" ) );
     addPage( osd,         i18n( "OSD" ), "preferences-indicator-amarok", i18n( "Configure On-Screen-Display" ) );
     addPage( collection,  i18n( "Collection" ), "collection-amarok", i18n( "Configure Collection" ) );
-    addPage( mediadevice, i18n( "Media Devices" ), "multimedia-player-amarok", i18n( "Configure Portable Player Support" ) );
-    addPage( services,    i18n( "Services" ), "get-hot-new-stuff-amarok", i18n( "Configure Services" ) );
+    addPage( mediadevice, i18n( "Media Devices" ), "preferences-multimedia-player-amarok", i18n( "Configure Portable Player Support" ) );
+    addPage( services,    i18n( "Services" ), "services-amarok", i18n( "Configure Services" ) );
     
 
     setButtons( Help | Ok | Apply | Cancel );
