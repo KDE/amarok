6a76f550380df0ad4ac8001ce0e514356b38047b
Harald Sitter <sitter.harald@gmail.com>
2008-03-01 23:01:56 +0000

    welcome taglib to neon
    svn path=/trunk/extragear/multimedia/amarok/; revision=781122

--- supplementary_scripts/neon/distros/ubuntu.rb
+++ supplementary_scripts/neon/distros/ubuntu.rb
@@ -18,7 +18,7 @@
 DEBPATH     = NEONPATH + "/distros/ubuntu"
 DEBBASEPATH = ROOTPATH + "/#{DATE}-ubuntu"
 LPPATH      = "http://ppa.launchpad.net/amarok-nightly/ubuntu/pool/main/a/amarok-nightly"
-PACKAGES    = ["qt","strigi","kdelibs","kdebase-runtime","amarok"]
+PACKAGES    = ["qt","strigi","kdelibs","kdebase-runtime","taglib","amarok"]
 
 debemail    = ENV['DEBEMAIL']
 debfullname = ENV['DEBFULLNAME']
