0324676f58f33a5ea11f1630061cb919d47909ed
Dan Meltzer <parallelgrapefruit@gmail.com>
2008-03-18 15:39:31 +0000

    SVN_SILENT: I'm not really 1800 years old :/
    svn path=/trunk/extragear/multimedia/amarok/; revision=787063

--- src/SimilarArtistsAction.h
+++ src/SimilarArtistsAction.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (c) 200  Dan Meltzer <hydrogen@notyetimplemented.com        *
+ *   Copyright (c) 2008  Dan Meltzer <hydrogen@notyetimplemented.com        *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
