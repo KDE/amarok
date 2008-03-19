4cf0d04f8b91bf69a95c0c4cea04f4f7606c3bde
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2008-03-19 14:27:27 +0000

    whoops, forgot these two!
    svn path=/trunk/extragear/multimedia/amarok/; revision=787554

---
+++ src/servicebrowser/ShowInServiceAction.h
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *   Copyright (c) 2008  Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+#ifndef SHOWINSERVICEACTION_H
+#define SHOWINSERVICEACTION_H
+
+#include "servicebase.h"
+#include "servicemetabase.h"
+
+#include <QAction>
+
+/**
+An action to let the user locate and show the artist or album of a track in the correct service
+
+	@author 
+*/
+AMAROK_EXPORT class ShowInServiceAction : public QAction
+{
+    Q_OBJECT
+public:
+    AMAROK_EXPORT ShowInServiceAction( ServiceBase * service, Meta::ServiceTrack *track );
+
+    ~ShowInServiceAction();
+
+private slots:
+    void slotTriggered();
+
+private:
+    Meta::ServiceTrack * m_track;
+    ServiceBase * m_service;
+};
+
+#endif
