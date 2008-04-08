ffdd04885a2c698fc6801526d43fb7aba6af936f
Erik Hovland <erik@hovland.org>
2008-04-08 15:42:39 +0000

    Use the C++ idiom for calling the constructor.
    svn path=/branches/stable/extragear/multimedia/amarok/; revision=794773

--- src/playlistbrowser.cpp
+++ src/playlistbrowser.cpp
@@ -454,7 +454,7 @@ void PlaylistBrowser::loadCoolStreams()
         QDomElement e = n.toElement();
         QString name = e.attribute( "name" );
         e = n.namedItem( "url" ).toElement();
-        KURL url  = KURL( e.text() );
+        KURL url( e.text() );
         last = new StreamEntry( m_coolStreams, last, url, name );
         last->setKept( false );
     }
