8f86ba62720b733122efbde7b5687acbcf16eb97
Dan Meltzer <parallelgrapefruit@gmail.com>
2008-04-09 17:59:47 +0000

    debug--
    svn path=/trunk/extragear/multimedia/amarok/; revision=795288

--- src/CoverFetcher.cpp
+++ src/CoverFetcher.cpp
@@ -336,7 +336,6 @@ CoverFetcher::finishedXmlFetch( KJob *job ) //SLOT
         else if( list.item( i ).nodeName() == "Item" )
         {
             const QDomNode node = list.item( i );
-            debug() << "I Has an itemnode, parsing it!" << endl;
             parseItemNode( node );
         }
     }
@@ -356,7 +355,6 @@ void CoverFetcher::parseItemNode( const QDomNode &node )
         default: size = "Large";  break;
     }
     size += "Image";
-    debug() << "Fetching size: " << size << endl;
 
     while ( !it.isNull() ) {
         if ( it.isElement() ) {
@@ -364,19 +362,16 @@ void CoverFetcher::parseItemNode( const QDomNode &node )
             if(e.tagName()=="ASIN")
             {
                 m_asin = e.text();
-                debug() << "setting the ASIN as" << m_asin << endl;
                 m_coverAsins += m_asin;
             }
             else if(e.tagName() == "DetailPageURL" )
             {
                 m_amazonURL = e.text();
-                debug() << "Setting the details url to: " << m_amazonURL << endl;
                 m_coverAmazonUrls += m_amazonURL;
             }
             else if( e.tagName() == size )
             {
                 QDomNode subIt = e.firstChild();
-                debug() << "NAME: "  << subIt.nodeName() << "VALUE: " << subIt.nodeValue() << endl;
                 while( !subIt.isNull() )
                 {
                     if( subIt.isElement() )
@@ -386,7 +381,6 @@ void CoverFetcher::parseItemNode( const QDomNode &node )
                         {
                             const QString coverUrl = subE.text();
                             m_coverUrls += coverUrl;
-                            debug() << "Setting Cover URL to: " << coverUrl << endl;
                             break;
                         }
                     }
@@ -408,12 +402,10 @@ void CoverFetcher::parseItemNode( const QDomNode &node )
                         if( iter.nodeName() == "Artist" )
                         {
                             artist = iter.toElement().text();
-                            debug() << "Set Artist to: " << artist << endl;
                         }
                         else if( iter.nodeName() == "Title" )
                         {
                             album = iter.toElement().text();
-                            debug() << "Set Album to: " << album << endl;
                         }
                     }
                 }
