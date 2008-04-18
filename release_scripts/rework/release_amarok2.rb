f9cf0efce924620056e447dbaffa6b43ebe3558d
Harald Sitter <sitter.harald@gmail.com>
2008-04-18 19:16:01 +0000

    * cleanups * use KDE e.V. approved license header
    svn path=/trunk/extragear/multimedia/amarok/; revision=798651

--- release_scripts/rework/release_amarok2.rb
+++ release_scripts/rework/release_amarok2.rb
@@ -1,19 +1,22 @@
 #!/usr/bin/env ruby
 #
-# Generates an Amarok release tarball from KDE SVN
+# Generates a release tarball from KDE SVN
 #
 # Copyright (C) 2007-2008 Harald Sitter <harald@getamarok.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License or (at your option) version 3 or any later version
+# accepted by the membership of KDE e.V. (or its successor approved
+# by the membership of KDE e.V.), which shall act as a proxy 
+# defined in Section 14 of version 3 of the license.
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -23,10 +26,10 @@ SECTION   = "multimedia"
 BASEPATH  = Dir.getwd()
 
 require 'fileutils'
-require './lib/libbase.rb'
-require './lib/librelease.rb'
-require './lib/libl10n.rb'
-require './lib/libtag.rb'
+require 'lib/libbase.rb'
+require 'lib/librelease.rb'
+require 'lib/libl10n.rb'
+require 'lib/libtag.rb'
 
 def Amarok()
   # Change version
@@ -41,7 +44,7 @@ def Amarok()
   file.close()
   Dir.chdir("..") #amarok
 
-  # Remove crap
+  # Remove unnecessary stuff
   toberemoved = ["release_scripts","supplementary_scripts","src/history"]
   for object in toberemoved
     FileUtils.rm_rf(object)
