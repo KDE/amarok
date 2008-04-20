d1c03effe1044e5533e88892b4accf165e084c62
Seb Ruiz <ruiz@kde.org>
2008-04-20 14:09:45 +0000

    typo
    svn path=/trunk/extragear/multimedia/amarok/; revision=799128

--- src/covermanager/CoverFetchingActions.cpp
+++ src/covermanager/CoverFetchingActions.cpp
@@ -93,7 +93,7 @@ void SetCustomCoverAction::init()
 {
     setText( i18n("Set Custom Cover") );
     setIcon( KIcon("list-remove") );
-    setToolTip( i18np("Set custom artwork for this album", "Set custom artowkr for these %1 albums", m_albums.count()) );
+    setToolTip( i18np("Set custom artwork for this album", "Set custom artwork for these %1 albums", m_albums.count()) );
     BaseCoverAction::init();
 }
 
