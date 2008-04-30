9f7cabb7a32ad97fa9dff3dee3295fa0f775fffd
Dan Meltzer <parallelgrapefruit@gmail.com>
2008-04-30 16:20:01 +0000

    Move amarokcore files to src/ and clean up a lot of cmakelists.  This will probably require deleting your build directory. CCMAIL: amarok-devel@kde.org
    svn path=/trunk/extragear/multimedia/amarok/; revision=802788

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@
 
 //#define AMAROK_USE_DRKONQI
 
-extern AMAROK_EXPORT class KAboutData aboutData; //defined in amarokcore/app.cpp
+extern AMAROK_EXPORT class KAboutData aboutData; //defined in app.cpp
 
 
 int main( int argc, char *argv[] )
