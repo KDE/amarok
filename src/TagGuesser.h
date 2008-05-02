28a11bcaf1889a980b3514c94d1ab1d8459c8cd0
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2008-05-02 17:52:29 +0000

    Yet another license fix by Lydia
    svn path=/trunk/extragear/multimedia/amarok/; revision=803366

--- src/TagGuesser.h
+++ src/TagGuesser.h
@@ -1,13 +1,20 @@
-// (c) 2005 Alexandre Pereira de Oliveira <aleprj@gmail.com>
-// (c) 2003 Frerich Raabe <raabe@kde.org>
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/******************************************************************************
+ * Copyright (c) 2003 Frerich Raabe <raabe@kde.org>                           *
+ *           (c) 2005 Alexandre Pereira de Oliveira <aleprj@gmail.com>        *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or              *
+ * modify it under the terms of the GNU General Public License as             *
+ * published by the Free Software Foundation; either version 2 of             *
+ * the License, or (at your option) any later version.                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.      *
+ ******************************************************************************/
  
 #ifndef TAGGUESSER_H
 #define TAGGUESSER_H
