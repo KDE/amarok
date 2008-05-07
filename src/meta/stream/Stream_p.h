c289b8541e793193e2cb5cf2b19dda521786a3d3
Mark Kretschmann <kretschmann@kde.org>
2008-05-07 15:23:52 +0000

    copyright++
    svn path=/trunk/extragear/multimedia/amarok/; revision=805100

--- src/meta/stream/Stream_p.h
+++ src/meta/stream/Stream_p.h
@@ -1,5 +1,6 @@
 /*
    Copyright (C) 2007-2008 Maximilian Kossick <maximilian.kossick@googlemail.com>
+   Copyright (C) 2008 Mark Kretschmann <kretschmann@kde.org>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
