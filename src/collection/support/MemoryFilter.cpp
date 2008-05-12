b84ada562b4a4376a51653ba141a97ce444375ac
Maximilian Kossick <maximilian.kossick@googlemail.com>
2008-05-12 13:11:32 +0000

    added missing files
    svn path=/trunk/extragear/multimedia/amarok/; revision=806803

---
+++ src/collection/support/MemoryFilter.cpp
@@ -0,0 +1,549 @@
+/*
+   Copyright (C) 2008 Maximilian Kossick <maximilian.kossick@googlemail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+*/
+
+#include "MemoryFilter.h"
+
+#include <QtAlgorithms>
+
+namespace FilterFactory
+{
+    MemoryFilter* filter( qint64 value, const QString &filter, bool matchBegin, bool matchEnd )
+    {
+        MemoryFilter *result = 0;
+        switch( value )
+        {
+            case QueryMaker::valTitle:
+            {
+                result = new TitleMemoryFilter( filter, matchBegin, matchEnd );
+                break;
+            }
+            case QueryMaker::valAlbum:
+            {
+                result = new AlbumMemoryFilter( filter, matchBegin, matchEnd );
+                break;
+            }
+            case QueryMaker::valArtist:
+            {
+                result = new ArtistMemoryFilter( filter, matchBegin, matchEnd );
+                break;
+            }
+            case QueryMaker::valYear:
+            {
+                result = new YearMemoryFilter( filter, matchBegin, matchEnd );
+                break;
+            }
+            case QueryMaker::valComposer:
+            {
+                result = new ComposerMemoryFilter( filter, matchBegin, matchEnd );
+                break;
+            }
+            case QueryMaker::valComment:
+            {
+                result = new CommentMemoryFilter( filter, matchBegin, matchEnd );
+            }
+        }
+        return result;
+    }
+
+    MemoryFilter* numberFilter( qint64 value, qint64 filter, QueryMaker::NumberComparison compare )
+    {
+        NumberMemoryFilter *result = 0;
+        switch( value )
+        {
+            case QueryMaker::valTrackNr:
+                result = new TrackNumberFilter();
+                break;
+            case QueryMaker::valDiscNr:
+                result = new DiscNumberFilter();
+                break;
+            case QueryMaker::valRating:
+                result = new RatingFilter();
+                break;
+            case QueryMaker::valScore:
+                result = new ScoreFilter();
+                break;
+            case QueryMaker::valPlaycount:
+                result = new PlaycountFilter();
+                break;
+            case QueryMaker::valFirstPlayed:
+                result = new FirstPlayedFilter();
+                break;
+            case QueryMaker::valLastPlayed:
+                result = new LastPlayedFilter();
+                break;
+            case QueryMaker::valLength:
+                result = new LengthFilter();
+                break;
+            case QueryMaker::valFilesize:
+                result = new FilesizeFilter();
+                break;
+            case QueryMaker::valSamplerate:
+                result = new SampleRateFilter();
+                break;
+            case QueryMaker::valBitrate:
+                result = new BitrateFilter();
+                break;
+        }
+        Q_ASSERT_X( result, "FilterFactory::numberFilter", "called numberFilter with an illegal value, value was " + value );
+        result->setFilter( filter, compare );
+        return result;
+    }
+}
+
+MemoryFilter::MemoryFilter()
+{
+}
+
+MemoryFilter::~MemoryFilter()
+{
+}
+
+ContainerMemoryFilter::ContainerMemoryFilter()
+    : MemoryFilter()
+{
+}
+
+ContainerMemoryFilter::~ContainerMemoryFilter()
+{
+    qDeleteAll( m_filters );
+}
+
+void
+ContainerMemoryFilter::addFilter( MemoryFilter *filter )
+{
+    m_filters.append( filter );
+}
+
+AndContainerMemoryFilter::AndContainerMemoryFilter()
+    : ContainerMemoryFilter()
+{
+}
+
+AndContainerMemoryFilter::~AndContainerMemoryFilter()
+{
+}
+
+bool
+AndContainerMemoryFilter::filterMatches( const Meta::TrackPtr &track ) const
+{
+    if( m_filters.isEmpty() )
+        return false;
+
+    foreach( MemoryFilter *filter, m_filters )
+    {
+        if( !filter->filterMatches( track ) )
+            return false;
+    }
+    return true;
+}
+
+OrContainerMemoryFilter::OrContainerMemoryFilter()
+    : ContainerMemoryFilter()
+{
+}
+
+OrContainerMemoryFilter::~OrContainerMemoryFilter()
+{
+}
+
+bool
+OrContainerMemoryFilter::filterMatches( const Meta::TrackPtr &track ) const
+{
+    if( m_filters.isEmpty() )
+        return false;
+
+    foreach( MemoryFilter *filter, m_filters )
+    {
+        if( filter->filterMatches( track ) )
+            return true;
+    }
+    return false;
+}
+
+NegateMemoryFilter::NegateMemoryFilter( MemoryFilter *filter )
+    :MemoryFilter()
+    , m_filter( filter )
+{
+}
+
+NegateMemoryFilter::~NegateMemoryFilter()
+{
+    delete m_filter;
+}
+
+bool
+NegateMemoryFilter::filterMatches( const Meta::TrackPtr &track ) const
+{
+    return !m_filter->filterMatches( track );
+}
+
+StringMemoryFilter::StringMemoryFilter()
+    : MemoryFilter()
+    , m_matchBegin( false )
+    , m_matchEnd( false )
+{
+}
+
+StringMemoryFilter::~StringMemoryFilter()
+{
+}
+
+void
+StringMemoryFilter::setFilter( const QString &filter, bool matchBegin, bool matchEnd )
+{
+    m_filter = filter;
+    m_matchBegin = matchBegin;
+    m_matchEnd = matchEnd;
+}
+
+bool
+StringMemoryFilter::filterMatches( const Meta::TrackPtr &track ) const
+{
+    QString str = value( track );
+    if( m_matchBegin && m_matchEnd )
+    {
+        return QString::compare( str, m_filter, Qt::CaseInsensitive ) == 0;
+    }
+    else if( m_matchBegin )
+    {
+        return str.startsWith( m_filter, Qt::CaseInsensitive );
+    }
+    else if( m_matchEnd )
+    {
+        return str.endsWith( m_filter, Qt::CaseInsensitive );
+    }
+    else
+    {
+        return str.contains( m_filter, Qt::CaseInsensitive );
+    }
+}
+
+TitleMemoryFilter::TitleMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd )
+    : StringMemoryFilter()
+{
+    setFilter( filter, matchBegin, matchEnd );
+}
+
+TitleMemoryFilter::~TitleMemoryFilter()
+{
+}
+
+QString
+TitleMemoryFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->name();
+}
+
+
+ArtistMemoryFilter::ArtistMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd )
+    : StringMemoryFilter()
+{
+    setFilter( filter, matchBegin, matchEnd );
+}
+
+ArtistMemoryFilter::~ArtistMemoryFilter()
+{
+}
+
+QString
+ArtistMemoryFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->artist()->name();
+}
+
+AlbumMemoryFilter::AlbumMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd )
+    : StringMemoryFilter()
+{
+    setFilter( filter, matchBegin, matchEnd );
+}
+
+AlbumMemoryFilter::~AlbumMemoryFilter()
+{
+}
+
+QString
+AlbumMemoryFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->album()->name();
+}
+
+GenreMemoryFilter::GenreMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd )
+    : StringMemoryFilter()
+{
+    setFilter( filter, matchBegin, matchEnd );
+}
+
+GenreMemoryFilter::~GenreMemoryFilter()
+{
+}
+
+QString
+GenreMemoryFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->genre()->name();
+}
+
+ComposerMemoryFilter::ComposerMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd )
+    : StringMemoryFilter()
+{
+    setFilter( filter, matchBegin, matchEnd );
+}
+
+ComposerMemoryFilter::~ComposerMemoryFilter()
+{
+}
+
+QString
+ComposerMemoryFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->composer()->name();
+}
+
+YearMemoryFilter::YearMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd )
+    : StringMemoryFilter()
+{
+    setFilter( filter, matchBegin, matchEnd );
+}
+
+YearMemoryFilter::~YearMemoryFilter()
+{
+}
+
+QString
+YearMemoryFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->year()->name();
+}
+
+CommentMemoryFilter::CommentMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd )
+    : StringMemoryFilter()
+{
+    setFilter( filter, matchBegin, matchEnd );
+}
+
+CommentMemoryFilter::~CommentMemoryFilter()
+{
+}
+
+QString
+CommentMemoryFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->comment();
+}
+
+NumberMemoryFilter::NumberMemoryFilter()
+    : MemoryFilter()
+    , m_filter( 0 )
+    , m_compare( QueryMaker::Equals )
+{
+}
+
+NumberMemoryFilter::~NumberMemoryFilter()
+{
+}
+
+void
+NumberMemoryFilter::setFilter( qint64 filter, QueryMaker::NumberComparison compare )
+{
+    m_filter = filter;
+    m_compare = compare;
+}
+
+bool
+NumberMemoryFilter::filterMatches( const Meta::TrackPtr &track ) const
+{
+    qint64 currentValue = value( track );
+    switch( m_compare )
+    {
+        case QueryMaker::Equals:
+            return currentValue == m_filter;
+        case QueryMaker::GreaterThan:
+            return currentValue > m_filter;
+        case QueryMaker::LessThan:
+            return currentValue < m_filter;
+    }
+    return false;
+}
+
+TrackNumberFilter::TrackNumberFilter()
+    : NumberMemoryFilter()
+{
+}
+
+TrackNumberFilter::~TrackNumberFilter()
+{
+}
+
+qint64
+TrackNumberFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->trackNumber();
+}
+
+DiscNumberFilter::DiscNumberFilter()
+    : NumberMemoryFilter()
+{
+}
+
+DiscNumberFilter::~DiscNumberFilter()
+{
+}
+
+qint64
+DiscNumberFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->discNumber();
+}
+
+RatingFilter::RatingFilter()
+    : NumberMemoryFilter()
+{
+}
+
+RatingFilter::~RatingFilter()
+{
+}
+
+qint64
+RatingFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->rating();
+}
+
+ScoreFilter::ScoreFilter()
+    : NumberMemoryFilter()
+{
+}
+
+ScoreFilter::~ScoreFilter()
+{
+}
+
+qint64
+ScoreFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->score();
+}
+
+PlaycountFilter::PlaycountFilter()
+    : NumberMemoryFilter()
+{
+}
+
+PlaycountFilter::~PlaycountFilter()
+{
+}
+
+qint64
+PlaycountFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->playCount();
+}
+
+FirstPlayedFilter::FirstPlayedFilter()
+    : NumberMemoryFilter()
+{
+}
+
+FirstPlayedFilter::~FirstPlayedFilter()
+{
+}
+
+qint64
+FirstPlayedFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->firstPlayed();
+}
+
+LastPlayedFilter::LastPlayedFilter()
+    : NumberMemoryFilter()
+{
+}
+
+LastPlayedFilter::~LastPlayedFilter()
+{
+}
+
+qint64
+LastPlayedFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->lastPlayed();
+}
+
+LengthFilter::LengthFilter()
+    : NumberMemoryFilter()
+{
+}
+
+LengthFilter::~LengthFilter()
+{
+}
+
+qint64
+LengthFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->length();
+}
+
+FilesizeFilter::FilesizeFilter()
+    : NumberMemoryFilter()
+{
+}
+
+FilesizeFilter::~FilesizeFilter()
+{
+}
+
+qint64
+FilesizeFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->filesize();
+}
+
+SampleRateFilter::SampleRateFilter()
+    : NumberMemoryFilter()
+{
+}
+
+SampleRateFilter::~SampleRateFilter()
+{
+}
+
+qint64
+SampleRateFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->sampleRate();
+}
+
+BitrateFilter::BitrateFilter()
+    : NumberMemoryFilter()
+{
+}
+
+BitrateFilter::~BitrateFilter()
+{
+}
+
+qint64
+BitrateFilter::value( const Meta::TrackPtr &track ) const
+{
+    return track->bitrate();
+}
+
+
+
---
+++ src/collection/support/MemoryFilter.h
@@ -0,0 +1,281 @@
+/*
+   Copyright (C) 2008 Maximilian Kossick <maximilian.kossick@googlemail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+*/
+
+#ifndef MEMORYFILTER_H
+#define MEMORYFILTER_H
+
+#include "amarok_export.h"
+#include "Meta.h"
+#include "QueryMaker.h"
+
+#include <QList>
+#include <QString>
+
+class MemoryFilter;
+
+namespace FilterFactory
+{
+    MemoryFilter* filter( qint64 value, const QString &filter, bool matchBegin, bool matchEnd );
+    MemoryFilter* numberFilter( qint64 value, qint64 filter, QueryMaker::NumberComparison compare );
+}
+
+class AMAROK_EXPORT MemoryFilter
+{
+    public:
+        MemoryFilter();
+        virtual ~MemoryFilter();
+        virtual bool filterMatches( const Meta::TrackPtr &track ) const = 0;
+};
+
+class AMAROK_EXPORT ContainerMemoryFilter : public MemoryFilter
+{
+    public:
+        ContainerMemoryFilter();
+        virtual ~ContainerMemoryFilter();
+        void addFilter( MemoryFilter *filter );
+    protected:
+        QList<MemoryFilter*> m_filters;
+};
+
+class AMAROK_EXPORT AndContainerMemoryFilter : public ContainerMemoryFilter
+{
+    public:
+        AndContainerMemoryFilter();
+        virtual ~AndContainerMemoryFilter();
+        virtual bool filterMatches( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT OrContainerMemoryFilter : public ContainerMemoryFilter
+{
+    public:
+        OrContainerMemoryFilter();
+        virtual ~OrContainerMemoryFilter();
+        virtual bool filterMatches( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT NegateMemoryFilter : public MemoryFilter
+{
+    public:
+        NegateMemoryFilter( MemoryFilter *filter );
+        virtual ~NegateMemoryFilter();
+        virtual bool filterMatches( const Meta::TrackPtr &track ) const;
+    private:
+        MemoryFilter *m_filter;
+};
+
+class AMAROK_EXPORT StringMemoryFilter : public MemoryFilter
+{
+    public:
+        StringMemoryFilter();
+        virtual ~StringMemoryFilter();
+        virtual bool filterMatches( const Meta::TrackPtr &track ) const;
+
+        void setFilter( const QString &filter, bool matchBegin, bool matchEnd );
+    protected:
+        virtual QString value( const Meta::TrackPtr &track ) const = 0;
+
+    private:
+        QString m_filter;
+        bool m_matchBegin;
+        bool m_matchEnd;
+};
+
+class AMAROK_EXPORT TitleMemoryFilter : public StringMemoryFilter
+{
+    public:
+        TitleMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd );
+        virtual ~TitleMemoryFilter();
+
+    protected:
+        virtual QString value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT ArtistMemoryFilter : public StringMemoryFilter
+{
+    public:
+        ArtistMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd );
+        virtual ~ArtistMemoryFilter();
+
+    protected:
+        virtual QString value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT AlbumMemoryFilter : public StringMemoryFilter
+{
+    public:
+        AlbumMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd );
+        virtual ~AlbumMemoryFilter();
+
+    protected:
+        virtual QString value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT GenreMemoryFilter : public StringMemoryFilter
+{
+    public:
+        GenreMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd );
+        virtual ~GenreMemoryFilter();
+
+    protected:
+        virtual QString value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT ComposerMemoryFilter : public StringMemoryFilter
+{
+    public:
+        ComposerMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd );
+        virtual ~ComposerMemoryFilter();
+
+    protected:
+        virtual QString value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT YearMemoryFilter : public StringMemoryFilter
+{
+    public:
+        YearMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd );
+        virtual ~YearMemoryFilter();
+
+    protected:
+        virtual QString value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT CommentMemoryFilter : public StringMemoryFilter
+{
+    public:
+        CommentMemoryFilter( const QString &filter, bool matchBegin, bool matchEnd );
+        virtual ~CommentMemoryFilter();
+
+    protected:
+        virtual QString value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT NumberMemoryFilter : public MemoryFilter
+{
+    public:
+        NumberMemoryFilter();
+        virtual ~NumberMemoryFilter();
+        void setFilter( qint64 filter, QueryMaker::NumberComparison compare );
+        bool filterMatches( const Meta::TrackPtr &track ) const;
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const = 0;
+    private:
+        qint64 m_filter;
+        QueryMaker::NumberComparison m_compare;
+};
+
+class AMAROK_EXPORT TrackNumberFilter : public NumberMemoryFilter
+{
+    public:
+        TrackNumberFilter();
+        virtual ~TrackNumberFilter();
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT DiscNumberFilter : public NumberMemoryFilter
+{
+    public:
+        DiscNumberFilter();
+        virtual ~DiscNumberFilter();
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT RatingFilter : public NumberMemoryFilter
+{
+    public:
+        RatingFilter();
+        virtual ~RatingFilter();
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT ScoreFilter : public NumberMemoryFilter
+{
+    public:
+        ScoreFilter();
+        virtual ~ScoreFilter();
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT PlaycountFilter : public NumberMemoryFilter
+{
+    public:
+        PlaycountFilter();
+        virtual ~PlaycountFilter();
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT FirstPlayedFilter : public NumberMemoryFilter
+{
+    public:
+        FirstPlayedFilter();
+        virtual ~FirstPlayedFilter();
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT LastPlayedFilter : public NumberMemoryFilter
+{
+    public:
+        LastPlayedFilter();
+        virtual ~LastPlayedFilter();
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT LengthFilter : public NumberMemoryFilter
+{
+    public:
+        LengthFilter();
+        virtual ~LengthFilter();
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT FilesizeFilter : public NumberMemoryFilter
+{
+    public:
+        FilesizeFilter();
+        virtual ~FilesizeFilter();
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT SampleRateFilter : public NumberMemoryFilter
+{
+    public:
+        SampleRateFilter();
+        virtual ~SampleRateFilter();
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const;
+};
+
+class AMAROK_EXPORT BitrateFilter : public NumberMemoryFilter
+{
+    public:
+        BitrateFilter();
+        virtual ~BitrateFilter();
+    protected:
+        virtual qint64 value( const Meta::TrackPtr &track ) const;
+};
+
+#endif
