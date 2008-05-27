4939d14e17de5821574413a124c71f79b7e7dea7
Casey Link <unnamedrambler@gmail.com>
2008-05-27 18:52:26 +0000

    The Beginning of ServiceCollectionLocation
    svn path=/trunk/extragear/multimedia/amarok/; revision=813449

---
+++ src/servicebrowser/ServiceCollectionLocation.cpp
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c) 2008 Casey Link <unnamedrambler@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+#include "ServiceCollectionLocation.h"
+
+using namespace Meta;
+
+ServiceCollectionLocation::ServiceCollectionLocation( ServiceCollection const *parentCollection )
+    : CollectionLocation()
+    , m_collection( const_cast<ServiceCollection*>( parentCollection ) )
+    , m_removeSources( false )
+    , m_overwriteFiles( false )
+{}
+
+ServiceCollectionLocation::~ServiceCollectionLocation()
+{
+    DEBUG_BLOCK
+}
+
+QString ServiceCollectionLocation::prettyLocation() const
+{
+    return return QString();
+}
+
+bool ServiceCollectionLocation::isWriteable() const
+{
+    return false;
+}
+
+bool ServiceCollectionLocation::isOrganizable() const
+{
+    return false;
+}
