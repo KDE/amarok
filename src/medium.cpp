f25e6a1e9d6bb39a30883de013c6a123d5f4894b
Allen Winter <winter@kde.org>
2008-06-05 14:26:00 +0000

    fix Kevin's email address
    svn path=/trunk/extragear/multimedia/amarok/; revision=817208

--- src/medium.cpp
+++ src/medium.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE Project
-   Copyright (c) 2004 KÃÂ©vin Ottens <ervin ipsquad net>
+   Copyright (c) 2004 KÃÂ©vin Ottens <ervin@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
