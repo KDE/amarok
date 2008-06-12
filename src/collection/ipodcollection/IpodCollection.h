00753b8d96622159b638fab46e48eed7ed380cd4
Alejandro Daniel Wainzinger <aikawarazuni@gmail.com>
2008-06-12 16:52:20 +0000

    durka durka!
    svn path=/trunk/extragear/multimedia/amarok/; revision=819931

--- src/collection/ipodcollection/IpodCollection.h
+++ src/collection/ipodcollection/IpodCollection.h
@@ -9,6 +9,8 @@
    as published by the Free Software Foundation; either version 2
    of the License, or (at your option) any later version.
 
+zomg random stuff
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
