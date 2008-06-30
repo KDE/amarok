d6d48f03823682d8eb565b41bc178d3bc358f909
Laurent Montel <montel@kde.org>
2008-06-30 06:52:05 +0000

    #warning works with gcc
    svn path=/trunk/extragear/multimedia/amarok/; revision=826232

--- src/main.cpp
+++ src/main.cpp
@@ -108,9 +108,10 @@ int main( int argc, char *argv[] )
 
     App::initCliArgs();
     KUniqueApplication::addCmdLineOptions();
-
+#ifdef __GNUC__
     #warning "REMOVE NonUniqueInstance FLAG BEFORE RELEASE!"
     #warning "It's a convenience hack for developing, so that we can start multiple Amarok instances."
+#endif
     //if (!KUniqueApplication::start()) {
     if ( !KUniqueApplication::start( KUniqueApplication::NonUniqueInstance ) ) {
         fprintf(stderr, "Amarok is already running!\n");
