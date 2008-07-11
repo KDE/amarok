177c8a2e2dd950b994e326d007030336b2ef94ad
Dan Meltzer <parallelgrapefruit@gmail.com>
2008-07-11 22:55:20 +0000

    Rebuild the database if one downgrades as well.  We still need to considder statistics tables, but thats probably a future issue if that changes. BUG: 142712
    svn path=/trunk/extragear/multimedia/amarok/; revision=831145

--- src/collection/sqlcollection/DatabaseUpdater.cpp
+++ src/collection/sqlcollection/DatabaseUpdater.cpp
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
+*/
 
 #include "DatabaseUpdater.h"
 
@@ -49,6 +49,12 @@ DatabaseUpdater::update()
     {
         createTables();
         m_collection->query( "INSERT INTO admin(key, version) VALUES ('DB_VERSION', 1);" );
+    }
+    else if( dbVersion > DB_VERSION )
+    {
+        cleanPermanentTables();
+        createTables();
+        m_collection->query( "INSERT INTO admin(key, version) VALUES( 'DB_VERSION', 1);" );
     }
 }
 
