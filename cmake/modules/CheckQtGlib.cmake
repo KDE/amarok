ee8dac4614165b28a32def0b2e9f0f50f0889d91
Ian Monroe <ian.monroe@gmail.com>
2008-07-16 20:47:47 +0000

    fix for the Qt glib test
    svn path=/trunk/extragear/multimedia/amarok/; revision=833462

--- cmake/modules/CheckQtGlib.cmake
+++ cmake/modules/CheckQtGlib.cmake
@@ -1,5 +1,6 @@
+set (CMAKE_REQUIRED_INCLUDES ${QT_QTCORE_INCLUDE_DIR})
 CHECK_CXX_SOURCE_COMPILES("
-#include <QtCore/QtGlobal>
+#include <QtGlobal>
 #ifdef QT_NO_GLIB
 #error \"Qt was compiled with Glib disabled\"
 #endif", QT4_GLIB_SUPPORT)
