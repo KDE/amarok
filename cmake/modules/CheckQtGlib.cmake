84c46b68371500359d152659673a5670d741c79c
Ian Monroe <ian.monroe@gmail.com>
2008-07-17 03:17:14 +0000

    now the check Qt glib test works
    svn path=/trunk/extragear/multimedia/amarok/; revision=833575

--- cmake/modules/CheckQtGlib.cmake
+++ cmake/modules/CheckQtGlib.cmake
@@ -1,6 +1,14 @@
-set (CMAKE_REQUIRED_INCLUDES ${QT_INCLUDES})
+set(CMAKE_REQUIRED_INCLUDES ${QT_INCLUDES})
+set(CMAKE_REQUIRED_DEFINITIONS "")
+set(CMAKE_REQUIRED_FLAGS "")
 CHECK_CXX_SOURCE_COMPILES("
 #include <QtCore/QtGlobal>
-#ifdef QT_NO_GLIB
+int main()
+{
+#if defined(QT_NO_GLIBASDF)
 #error \"Qt was compiled with Glib disabled\"
-#endif", QT4_GLIB_SUPPORT)
+#endif
+return 0;
+}" 
+QT4_GLIB_SUPPORT)
+
