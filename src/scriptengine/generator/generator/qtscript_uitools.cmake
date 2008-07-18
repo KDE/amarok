b02f32c75ea133bd5a4298bb3c7f7254f5d7b963
Ian Monroe <ian.monroe@gmail.com>
2008-07-18 17:45:12 +0000

    Fix UiTools module
    svn path=/trunk/extragear/multimedia/amarok/; revision=834352

--- src/scriptengine/generator/generator/qtscript_uitools.cmake
+++ src/scriptengine/generator/generator/qtscript_uitools.cmake
@@ -1,6 +1,7 @@
 set( Generated_QtUiTools_SRCS
            ${CMAKE_CURRENT_BINARY_DIR}/generated_cpp/com_trolltech_qt_uitools/qtscript_QUiLoader.cpp
            ${CMAKE_CURRENT_BINARY_DIR}/generated_cpp/com_trolltech_qt_uitools/qtscriptshell_QUiLoader.cpp
+           ${CMAKE_CURRENT_BINARY_DIR}/generated_cpp/com_trolltech_qt_uitools/main.cpp
 )
 set_source_files_properties( ${Generated_QtUiTools_SRCS} PROPERTIES GENERATED true )
 add_library( qtscript_uitools MODULE ${Generated_QtUiTools_SRCS} )
