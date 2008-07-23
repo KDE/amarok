466936485fe51cd71460afdb4513cd33b8c52d95
Ian Monroe <ian.monroe@gmail.com>
2008-07-23 16:46:02 +0000

    a much improved Amarok Script Console *derived a prototype from QMainWindow, mainly as a way to keep the eval's namespace clean *some keybindings
    svn path=/trunk/extragear/multimedia/amarok/; revision=837039

--- src/scripts/script_console/main.js
+++ src/scripts/script_console/main.js
@@ -2,40 +2,113 @@ Importer.loadQtBinding( "qt.core" );
 Importer.loadQtBinding( "qt.gui" );
 Importer.loadQtBinding( "qt.uitools" );
 
-function executeLine() 
+function globalScopeEval( inputCode )
 {
-  try {
-    command = new QListWidgetItem( inputLine.plainText + ":");
-    boldFont = new QFont();
-    boldFont.setBold( true );
-    command.setFont( boldFont );
-
-    results = new QListWidgetItem(  eval( inputLine.plainText ) + " " );
-    historyList.addItem( command  );
-    historyList.addItem( results );
+  try
+  {
+      result = eval( inputCode ) + " ";
   }
-  catch( error ) {
-    print( error );
+  catch( error )
+  {
+      result = error + " ";
   }
+  return result;
 }
 
-function onConfigure()
+function ScriptConsoleMainWindow()
+{
+  QMainWindow.call( this, null );
+
+  Amarok.configured.connect( this, this.onConfigure );
+
+  var mainWidget = new QWidget( this );
+  this.historyList = new QListWidget( mainWidget );
+  this.historyList.sizeHint = new QSize(900, 600);
+  this.historyList.size = new QSize(900, 600);
+  this.inputLine = new QTextEdit( mainWidget );
+  this.executeButton = new QPushButton( mainWidget );
+  this.commandArray = [];
+  this.commandPos = -1;
+  this.executeButton.clicked.connect( this, this.executeLine );
+  this.executeButton.text = "Execute Code";
+  this.windowTitle = "Amarok Script Console";
+  //the following line doesn't work for some reason:
+  //executeButton.shortcut = new QKeySequence( "CTRL+Return" );
+
+  var executeShortcut = new QShortcut( this );
+  executeShortcut.key =  new QKeySequence( "CTRL+Return" );
+  executeShortcut.activated.connect( this, this.executeLine );
+
+  var backHistoryShortcut = new QShortcut( this );
+  backHistoryShortcut.key = new QKeySequence( QKeySequence.StandardKey( QKeySequence.MoveToPreviousPage ) );
+  backHistoryShortcut.activated.connect( this, this.backHistory );
+
+  var forwardHistoryShortcut = new QShortcut( this );
+  forwardHistoryShortcut.key = new QKeySequence( QKeySequence.StandardKey( QKeySequence.MoveToNextPage ) );
+  forwardHistoryShortcut.activated.connect( this, this.forwardHistory );
+
+  var explanationItem = new QListWidgetItem("The Amarok Script Console allows you to easily execute JavaScript with access to all functions\nand methods you would have in an Amarok script.\nInformation on scripting for Amarok is available at:\nhttp://amarok.kde.org/scripting\nExecute code: CTRL-Enter\nBack in code history: Page Up\nForward in code history: Page Down");
+  //explanationItem.setForeground( new QBrush( new QColor(Qt.darkGray) ) );
+  explanationItem.setFlags( !Qt.ItemIsSelectable );
+  this.historyList.addItem( explanationItem );
+
+  var layout = new QGridLayout( mainWidget );
+  layout.addWidget( this.historyList, 0, 0, 1, 2 );
+  layout.addWidget( this.inputLine, 1, 0, 3, 2 );
+  layout.addWidget( this.executeButton, 4, 1 );
+  mainWidget.setLayout( layout );
+  this.setCentralWidget( mainWidget );
+  this.resize(750, 400);
+  this.show();
+}
+
+ScriptConsoleMainWindow.prototype = new QMainWindow();
+
+ScriptConsoleMainWindow.prototype.onConfigure = function()
 {
     Amarok.alert( "This script does not require any configuration." );
 }
 
-var mainWindow = new QMainWindow();
-var mainWidget = new QWidget( mainWindow );
-var historyList = new QListWidget( mainWidget );
-var inputLine = new QTextEdit( mainWidget );
-var executeButton = new QPushButton( mainWidget );
-executeButton.clicked.connect( this, this.executeLine );
-Amarok.configured.connect( onConfigure );
+ScriptConsoleMainWindow.prototype.executeLine = function()
+{
+    command = new QListWidgetItem( this.inputLine.plainText );
+    this.commandArray.unshift( this.inputLine.plainText );
+    boldFont = new QFont();
+    boldFont.setBold( true );
+    command.setFont( boldFont );
+    result = globalScopeEval.call( null, this.inputLine.plainText );
+    resultsRow = new QListWidgetItem( result );
+    this.historyList.addItem( command  );
+    this.historyList.addItem( resultsRow );
+    this.historyList.setCurrentItem( resultsRow );
+    this.inputLine.plainText = "";
+    this.commandPos = -1;
+}
 
-var layout = new QGridLayout( mainWidget );
-layout.addWidget( this.historyList, 0, 0, 1, 2 );
-layout.addWidget( this.inputLine, 1, 0, 3, 2 );
-layout.addWidget( this.executeButton, 4, 1 );
-mainWidget.setLayout( layout );
-mainWindow.setCentralWidget( mainWidget );
-mainWindow.show();
+ScriptConsoleMainWindow.prototype.backHistory = function()
+{
+  if( this.commandArray.length > ( this.commandPos + 1 ) )
+  {
+      this.commandPos++;
+  }
+  if( this.commandArray.length > 0 )
+  {
+    this.inputLine.plainText = this.commandArray[ this.commandPos ];
+  }
+  print( "back, " + this.commandArray[ this.commandPos ] );
+}
+
+ScriptConsoleMainWindow.prototype.forwardHistory = function()
+{
+  if( this.commandArray.length > 0 )
+  {
+    if( this.commandPos > 0 )
+    {
+      this.commandPos--;
+    }
+    this.inputLine.plainText = this.commandArray[ this.commandPos ];
+  }
+  print( "forward, " + this.commandPos + ": " + this.commandArray[ this.commandPos ] );
+}
+
+scriptConsoleMainWindow = new ScriptConsoleMainWindow();
--- src/scripts/script_console/script.spec
+++ src/scripts/script_console/script.spec
@@ -10,5 +10,5 @@ X-KDE-PluginInfo-Name=Amarok Script Console
 X-KDE-PluginInfo-Version=1.0
 X-KDE-PluginInfo-Category=Generic
 X-KDE-PluginInfo-Depends=Amarok2.0
-X-KDE-PluginInfo-License=LGPL
+X-KDE-PluginInfo-License=GPL
 X-KDE-PluginInfo-EnabledByDefault=false
