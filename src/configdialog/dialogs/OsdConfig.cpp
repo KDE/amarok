0fc9048a8714470071776af76ace5037f82bf7e0
Mark Kretschmann <kretschmann@kde.org>
2008-08-07 07:24:54 +0000

    Code formatting is a lost art. Or people need better glasses. Or both!
    svn path=/trunk/extragear/multimedia/amarok/; revision=843403

--- src/configdialog/dialogs/OsdConfig.cpp
+++ src/configdialog/dialogs/OsdConfig.cpp
@@ -41,24 +41,24 @@ OsdConfig::OsdConfig( QWidget* parent )
     for( int i = 0; i < numScreens; i++ )
         kcfg_OsdScreen->addItem( QString::number( i ) );
 
-    connect( kcfg_OsdDrawShadow,      SIGNAL( toggled(bool) ),
-             m_osdPreview,           SLOT( setDrawShadow(bool) ) );
-    connect( kcfg_OsdTextColor,       SIGNAL( changed(const QColor&) ),
-             m_osdPreview,           SLOT( setTextColor(const QColor&) ) );
-    connect( kcfg_OsdUseCustomColors, SIGNAL( toggled(bool) ),
-             this,                    SLOT( useCustomColorsToggled(bool) ) );
-    connect( kcfg_OsdBackgroundColor, SIGNAL( changed(const QColor&) ),
-             m_osdPreview,           SLOT( setBackgroundColor(const QColor&) ) );
+    connect( kcfg_OsdDrawShadow,      SIGNAL( toggled( bool ) ),
+             m_osdPreview,            SLOT( setDrawShadow( bool ) ) );
+    connect( kcfg_OsdTextColor,       SIGNAL( changed( const QColor& ) ),
+             m_osdPreview,            SLOT( setTextColor(const QColor& ) ) );
+    connect( kcfg_OsdUseCustomColors, SIGNAL( toggled( bool ) ),
+             this,                    SLOT( useCustomColorsToggled( bool ) ) );
+    connect( kcfg_OsdBackgroundColor, SIGNAL( changed( const QColor& ) ),
+             m_osdPreview,            SLOT( setBackgroundColor( const QColor& ) ) );
 #if 0
     connect( kcfg_OsdFont,            SIGNAL( fontSelected(const QFont&) ),
-             m_osdPreview,           SLOT( setFont(const QFont&) ) );
+             m_osdPreview,            SLOT( setFont(const QFont&) ) );
 #endif
-    connect( kcfg_OsdScreen,          SIGNAL( activated(int) ),
-             m_osdPreview,           SLOT( setScreen(int) ) );
-    connect( kcfg_OsdEnabled,         SIGNAL( toggled(bool) ),
-             m_osdPreview,           SLOT( setShown(bool) ) );
-    connect( kcfg_OsdUseTranslucency, SIGNAL( toggled(bool) ),
-             m_osdPreview,           SLOT( setTranslucent(bool) ));
+    connect( kcfg_OsdScreen,          SIGNAL( activated( int ) ),
+             m_osdPreview,            SLOT( setScreen( int ) ) );
+    connect( kcfg_OsdEnabled,         SIGNAL( toggled( bool ) ),
+             m_osdPreview,            SLOT( setShown( bool ) ) );
+    connect( kcfg_OsdUseTranslucency, SIGNAL( toggled( bool ) ),
+             m_osdPreview,            SLOT( setTranslucent( bool ) ) );
 
     Amarok::QStringx text = i18n(
             "<h3>Tags Displayed in OSD</h3>"
