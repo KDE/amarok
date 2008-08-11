9dabd3dfc50848ae4df451c99ada7c143725744b
William Viana Soares <vianasw@gmail.com>
2008-08-11 20:38:48 +0000

    License++
    svn path=/trunk/extragear/multimedia/amarok/; revision=845496

--- src/context/applets/albums/AlbumEntry.h
+++ src/context/applets/albums/AlbumEntry.h
@@ -1,3 +1,17 @@
+/*******************************************************************************
+* copyright              : (C) 2008 William Viana Soares <vianasw@gmail.com>   *
+*                                                                              *
+********************************************************************************/
+
+/*******************************************************************************
+*                                                                              *
+*   This program is free software; you can redistribute it and/or modify       *
+*   it under the terms of the GNU General Public License as published by       *
+*   the Free Software Foundation; either version 2 of the License, or          *
+*   (at your option) any later version.                                        *
+*                                                                              *
+********************************************************************************/
+
 #ifndef ALBUM_ENTRY_H
 #define ALBUM_ENTRY_H
 
