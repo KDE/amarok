KDE/amarok

commit 17d832805213dc3d3bd0f9461fc5e11d50646cdc

Use AmarokMimeData to do the work when dropping items from the Collection Browser onto the Append or Load symbols in the central pane. This reduces code duplication and fixes a bug where if multiple items were selected and dropped, the tracks would not be sorted properly when added to the playlist.
svn path=/trunk/extragear/multimedia/amarok/; revision=847552

src/browsers/collectionbrowser/CollectionTreeView.cpp
@@ -22,6 +22,7 @@
 #include "CollectionTreeView.h"
 
 #include "Amarok.h"
+#include "AmarokMimeData.h"
 #include "Debug.h"
 #include "CollectionLocation.h"
 #include "CollectionManager.h"
@@ -453,37 +454,19 @@ CollectionTreeView::slotCollapsed( const QModelIndex &index )
 }
 
 void
-CollectionTreeView::playChildTracks( CollectionTreeItem *item, Playlist::AddOptions insertMode) const
+CollectionTreeView::playChildTracks( CollectionTreeItem *item, Playlist::AddOptions insertMode)
 {
-    DEBUG_BLOCK
-    if( !item->allDescendentTracksLoaded() )
-    {
-        QueryMaker *qm = item->queryMaker();
-        CollectionTreeItem *tmp = item;
-        while( tmp->isDataItem() )
-        {
-            if ( tmp->data() )
-                qm->addMatch( tmp->data() );
-            else
-                qm->setAlbumQueryMode( QueryMaker::OnlyCompilations );
-            tmp = tmp->parent();
-        }
-        m_treeModel->addFilters( qm );
-        The::playlistModel()->insertOptioned( qm, insertMode );
-    }
-    else
-    {
-        Meta::TrackList tracks = item->descendentTracks();
-        qStableSort( tracks.begin(), tracks.end(), Amarok::trackNumberLessThan);
-        The::playlistModel()->insertOptioned( tracks, insertMode );
-    }
+    QSet<CollectionTreeItem*> items;
+    items.insert( item );
+
+    playChildTracks( items, insertMode );
 }
 
 void
-CollectionTreeView::playChildTracks( const QSet<CollectionTreeItem*> &items, Playlist::AddOptions insertMode ) const
+CollectionTreeView::playChildTracks( const QSet<CollectionTreeItem*> &items, Playlist::AddOptions insertMode )
 {
-    //find all selected parents in the list and ignore the rest
-    QSet<CollectionTreeItem*> parents;
+    //Ensure that if a parent and child are both selected we ignore the child
+    QList<CollectionTreeItem*> parents;
     foreach( CollectionTreeItem *item, items )
     {
         CollectionTreeItem *tmpItem = item;
@@ -495,18 +478,31 @@ CollectionTreeView::playChildTracks( const QSet<CollectionTreeItem*> &items, Pla
             }
             else
             {
-                parents.insert( tmpItem );
+                parents.append( tmpItem );
                 break;
             }
         }
     }
-    bool first = true;
-    foreach( CollectionTreeItem *item, parents )
-    {
-        //FIXME:we are ignoring the order of the sleected items
-        playChildTracks( item, first ? insertMode : Playlist::Append );
-        first = false;
-    }
+
+    //Store the type of playlist insert to be done and cause a slot to be invoked when the tracklist has been generated.
+    AmarokMimeData *mime = dynamic_cast<AmarokMimeData*>( m_treeModel->mimeData( parents ) );
+    m_playChildTracksMode.insert( mime, insertMode );
+    connect( mime, SIGNAL( trackListSignal( Meta::TrackList ) ), this, SLOT( playChildTracksSlot( Meta::TrackList) ) );
+    mime->getTrackListSignal();
+
+}
+
+void
+CollectionTreeView::playChildTracksSlot( Meta::TrackList list ) //slot
+{
+    AmarokMimeData *mime = dynamic_cast<AmarokMimeData*>( sender() );
+
+    Playlist::AddOptions insertMode = m_playChildTracksMode.take( mime );
+
+    qStableSort( list.begin(), list.end(), Amarok::trackNumberLessThan );
+    The::playlistModel()->insertOptioned( list, insertMode );
+
+    mime->deleteLater();
 }
 
 

src/browsers/collectionbrowser/CollectionTreeItemModelBase.cpp
@@ -153,36 +153,52 @@ CollectionTreeItemModelBase::mimeTypes() const
     return types;
 }
 
-QMimeData * CollectionTreeItemModelBase::mimeData(const QModelIndexList & indices) const
+QMimeData*
+CollectionTreeItemModelBase::mimeData(const QModelIndexList & indices) const
 {
     if ( indices.isEmpty() )
+        return 0;
+
+    QList<CollectionTreeItem*> items;
+
+    foreach( const QModelIndex &index, indices ) 
+    {
+        if ( index.isValid() )
+        {
+            items << static_cast<CollectionTreeItem*>( index.internalPointer() );
+        }
+    }
+
+    return mimeData( items );
+}
+
+QMimeData*
+CollectionTreeItemModelBase::mimeData(const QList<CollectionTreeItem*> & items) const
+{
+    if ( items.isEmpty() )
         return 0;
 
     Meta::TrackList tracks;
     QList<QueryMaker*> queries;
 
-    foreach( const QModelIndex &index, indices )
+    foreach( CollectionTreeItem *item, items )
     {
-        if( index.isValid() )
+        if( item->allDescendentTracksLoaded() )
+            tracks << item->descendentTracks();
+        else
         {
-            CollectionTreeItem *item = static_cast<CollectionTreeItem*>(index.internalPointer());
-            if( item->allDescendentTracksLoaded() )
-                tracks << item->descendentTracks();
-            else
+            QueryMaker *qm = item->queryMaker();
+            CollectionTreeItem *tmpItem = item;
+            while( tmpItem->isDataItem() )
             {
-                QueryMaker *qm = item->queryMaker();
-                CollectionTreeItem *tmpItem = item;
-                while( tmpItem->isDataItem() )
-                {
-                    if( tmpItem->data() )
-                        qm->addMatch( tmpItem->data() );
-                    else
-                        qm->setAlbumQueryMode( QueryMaker::OnlyCompilations );
-                    tmpItem = tmpItem->parent();
-                }
-                addFilters( qm );
-                queries.append( qm );
+                if( tmpItem->data() )
+                    qm->addMatch( tmpItem->data() );
+                else
+                    qm->setAlbumQueryMode( QueryMaker::OnlyCompilations );
+                tmpItem = tmpItem->parent();
             }
+            addFilters( qm );
+            queries.append( qm );
         }
     }
 

src/browsers/collectionbrowser/CollectionTreeItemModelBase.h
@@ -74,6 +74,7 @@ class AMAROK_EXPORT CollectionTreeItemModelBase : public QAbstractItemModel
         virtual int columnCount(const QModelIndex &parent = QModelIndex()) const;
 
         virtual QStringList mimeTypes() const;
+        virtual QMimeData* mimeData( const QList<CollectionTreeItem*> &items ) const;
         virtual QMimeData* mimeData( const QModelIndexList &indices ) const;
 
         virtual QPixmap iconForLevel( int level ) const;

src/browsers/collectionbrowser/CollectionTreeView.h
@@ -21,6 +21,7 @@
 #include "CollectionSortFilterProxyModel.h"
 #include "CollectionTreeItem.h"
 #include "playlist/PlaylistModel.h"
+#include "Meta.h"
 
 #include <QSet>
 #include <QSortFilterProxyModel>
@@ -32,6 +33,8 @@ class CollectionSortFilterProxyModel;
 class CollectionTreeItemModelBase;
 class PopupDropper;
 class PopupDropperAction;
+class AmarokMimeData;
+
 
 
 typedef QList<PopupDropperAction *> PopupDropperActionList;
@@ -61,6 +64,8 @@ class CollectionTreeView: public QTreeView
     public slots:
         void slotSetFilterTimeout();
 
+        void playChildTracksSlot( Meta::TrackList list );
+
         /**
          * Bypass the filter timeout if we really need to start filtering *now*
          */
@@ -89,8 +94,8 @@ class CollectionTreeView: public QTreeView
     private:
         // Utility function to play all items
         // that have this as a parent..
-        void playChildTracks( CollectionTreeItem *item, Playlist::AddOptions insertMode ) const;
-        void playChildTracks( const QSet<CollectionTreeItem*> &items, Playlist::AddOptions insertMode ) const;
+        void playChildTracks( CollectionTreeItem *item, Playlist::AddOptions insertMode );
+        void playChildTracks( const QSet<CollectionTreeItem*> &items, Playlist::AddOptions insertMode );
         void editTracks( const QSet<CollectionTreeItem*> &items ) const;
         void organizeTracks( const QSet<CollectionTreeItem*> &items ) const;
         void copyTracks( const QSet<CollectionTreeItem*> &items, Collection *destination, bool removeSources ) const;
@@ -119,6 +124,8 @@ class CollectionTreeView: public QTreeView
 
         QHash<PopupDropperAction*, Collection*> m_currentCopyDestination;
         QHash<PopupDropperAction*, Collection*> m_currentMoveDestination;
+
+        QMap<AmarokMimeData*, Playlist::AddOptions> m_playChildTracksMode;
 
         QSet<CollectionTreeItem*> m_currentItems;
 

commit 7d5801337961043505599eda5d8143832fd57fa1

Fix bug introduced in r847552: dragging an album and a track from it from the Collection Browser to the central pane Append/Load icons no longer appends the album twice.
svn path=/trunk/extragear/multimedia/amarok/; revision=847641

src/browsers/collectionbrowser/CollectionTreeView.cpp
@@ -466,7 +466,7 @@ void
 CollectionTreeView::playChildTracks( const QSet<CollectionTreeItem*> &items, Playlist::AddOptions insertMode )
 {
     //Ensure that if a parent and child are both selected we ignore the child
-    QList<CollectionTreeItem*> parents;
+    QSet<CollectionTreeItem*> parents;
     foreach( CollectionTreeItem *item, items )
     {
         CollectionTreeItem *tmpItem = item;
@@ -478,14 +478,14 @@ CollectionTreeView::playChildTracks( const QSet<CollectionTreeItem*> &items, Pla
             }
             else
             {
-                parents.append( tmpItem );
+                parents.insert( tmpItem );
                 break;
             }
         }
     }
 
     //Store the type of playlist insert to be done and cause a slot to be invoked when the tracklist has been generated.
-    AmarokMimeData *mime = dynamic_cast<AmarokMimeData*>( m_treeModel->mimeData( parents ) );
+    AmarokMimeData *mime = dynamic_cast<AmarokMimeData*>( m_treeModel->mimeData( QList<CollectionTreeItem*>::fromSet( parents ) ) );
     m_playChildTracksMode.insert( mime, insertMode );
     connect( mime, SIGNAL( trackListSignal( Meta::TrackList ) ), this, SLOT( playChildTracksSlot( Meta::TrackList) ) );
     mime->getTrackListSignal();