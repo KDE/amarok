c8d3267c01ce7357a53b4bdb829cc348a2b205bf
Casey Link <unnamedrambler@gmail.com>
2008-09-01 03:32:17 +0000

    Updated the rbot bug script to work with the new version of bugs.kde.org. Also, the script will now respond to channel messages that include bug numbers/urls.
    svn path=/trunk/extragear/multimedia/amarok/; revision=855537

--- supplementary_scripts/rbot/bug.rb
+++ supplementary_scripts/rbot/bug.rb
@@ -1,52 +1,64 @@
-#(C) Berkus Madfire
-#Needs xmlsimple from http://www.maik-schmidt.de/xml-simple.html
-
-require 'net/http'
-require 'xmlsimple'
-
+#-- vim:sw=2:et
+#++
+# Author:: Berkus Madfire
+# Author:: Casey Link <unnamedrambler@gmail.com>
+#
+# Copyright:: (C) Berkus Madfire
+# Copyright:: (C) 2008 Casey Link
+#
+# License:: GPL v2
+require 'rexml/document'
 
 class KDEBugPlugin < Plugin
-    def riphtml s
-        s.gsub(/<[^>]+>/, '').gsub(/&amp;/,'&').gsub(/&quot;/,'"').gsub(/&lt;/,'<').gsub(/&gt;/,'>').gsub(/&ellip;/,'...').gsub(/&apos;/, "'").gsub("\n",'')
-        return s
-    end
+   include REXML
+   def help(plugin,topic="")
+	  "bug #<number> - displays bugs.kde.org information for given bug number"
+   end
 
-    def help(plugin,topic="")
-	"bug #<number> - displays bugs.kde.org information for given bug number"
+   def message(m)
+      a = m.message.to_s.scan(/(?:bug|#|show_bug.cgi\?id=)+([0-9]{5,8})/)
+      m.reply bug(a[0]) if a.length > 0
+   end
+
+   def bug(a)
+     if a.length == 0
+         return "You called the wrong number..."
+     end
+     begin
+         uri = "http://bugs.kde.org/xml.cgi?id=#{a}"
+         xml = @bot.httputil.get_response(uri)
+         doc = Document.new xml.body
+         bug = doc.root.elements["bug"]
+       if xml.message == "OK"
+         if bug.attributes["error"] == "NotFound"
+           return "Bug #{a} was not found in Bugzilla."
+         else
+           str = "PRODUCT: #{bug.elements["product"].text}"
+           if res = XPath.first( doc, "/bugzilla/bug/component" ) then
+             str += " / #{bug.elements["component"].text}"
+           end
+           str += " | STATUS: #{bug.elements["bug_status"].text}"
+           if res = XPath.first( doc, "/bugzilla/bug/resolution" ) then
+             str += " | RESOLUTION: #{bug.elements["resolution"].text}"
+           end
+           str += " | URL: http://bugs.kde.org/show_bug.cgi?id=#{a} | DESCRIPTION: #{bug.elements["short_desc"].text.ircify_html}"
+           return str
+         end
+       else
+         return "Request to bugs.kde.org failed. Message not OK."
+       end
+     rescue
+       return "Request to bugs.kde.org failed"
+     end
     end
 
     def privmsg(m)
-	unless m.params
-	    m.reply("incorrect usage: "+help(m.plugin))
-	    return
-	end
-	a = m.params.scan(/^#?([0-9]+)/)
-	if a.length == 0
-	    m.reply("You called the wrong number...")
-	    return
-	end
-#	m.reply("Please wait, querying...")
-	begin
-	    h = Net::HTTP.new("bugs.kde.org", 80)
-	    resp = h.get("/xml.cgi?id=#{a}", nil)
-	    if resp.message == "OK"
-		bug = XmlSimple.xml_in(resp.body)
-		if bug['bug'][0]['error'] == 'NotFound'
-		    m.reply("Bug #{a} was not found in Bugzilla.")
-		else
-		    str = "PRODUCT: #{bug['bug'][0]['product']}"
-		    str += " / #{bug['bug'][0]['component']}" if bug['bug'][0]['component']
-		    str += " | STATUS: #{bug['bug'][0]['bug_status']}"
-		    str += " | RESOLUTION: #{bug['bug'][0]['resolution']}" if bug['bug'][0]['resolution']
-		    str += " | URL: http://bugs.kde.org/show_bug.cgi?id=#{a} | DESCRIPTION: #{riphtml(bug['bug'][0]['short_desc'].to_s)}"
-		    m.reply(str)
-		end
-	    else
-		m.reply("Request to bugs.kde.org failed. Message not OK.")
-	    end
-	rescue
-	    m.reply("Request to bugs.kde.org failed")
-	end
+      unless m.params
+          m.reply("incorrect usage: "+help(m.plugin))
+          return
+      end
+      a = m.params.scan(/^#?([0-9]+)/)
+      m.reply bug(a)
     end
 end
 
