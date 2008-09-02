03835c9eb4eed94fd97ea1e6e48f37826cb67a92
Leo Franchi <lfranchi@kde.org>
2008-09-02 17:50:19 +0000

    add lyricwiki script from Aaron Reichman. talk about fast turnaround!
    svn path=/trunk/extragear/multimedia/amarok/; revision=856352

---
+++ src/scripts/lyrics_lyricwiki/main.js
@@ -0,0 +1,73 @@
+/**************************************************************************
+*   Amarok 2 lyrics script to fetch lyrics from lyricwiki.org             *
+*                                                                         *
+*   Copyright                                                             *
+*   (C) 2008 Aaron Reichman <reldruh@gmail.com>                           *
+*   (C) 2008 Leo Franchi <lfranchi@kde.org>                               *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+**************************************************************************/
+
+
+Importer.loadQtBinding("qt.core");
+Importer.loadQtBinding("qt.gui");
+Importer.loadQtBinding("qt.network");
+
+function onFinished( reply )
+{
+    try
+    {
+        //Amarok.alert("reply.finished was emitted!");
+        response = reply.readAll();
+        ts = new QTextStream( response, QIODevice.ReadOnly );
+        dat = ts.readAll();
+        dat = "<lyric artist=\"artist name\" title=\"song title\" page_url=\"http://lyricwiki.org\">" + dat + "</lyric>"
+        print( "got result: " + dat );
+        Amarok.Lyrics.showLyrics(dat);
+    } catch( err )
+    {
+        print( "got error: " + err );
+    }
+}
+
+function openconnection(artist, title)
+{
+    try
+    {
+        var connection = new QNetworkAccessManager();
+        var url = new QUrl("http://lyricwiki.org/api.php?func=getSong&artist=" + artist +"&song=" + title +"&fmt=txt");
+
+        connection.finished.connect( onFinished );
+
+        //Amarok.alert( "fetching" );
+        connection.get( new QNetworkRequest( new QUrl( url ) ) );
+
+    } catch( err )
+    {
+        print( "error!: " + err );
+    }
+}
+
+
+function getLyrics(artist, title, url)
+{
+	//Amarok.alert("test");
+	var currentartist = artist;
+	var currenttitle = title;
+	openconnection(currentartist, currenttitle);
+}
+
+Amarok.Lyrics.fetchLyrics.connect( getLyrics );
---
+++ src/scripts/lyrics_lyricwiki/script.spec
@@ -0,0 +1,16 @@
+[Desktop Entry]
+Icon=amarok
+Type=script
+ServiceTypes=KPluginInfo
+
+Name=Lyricwiki
+Comment=A script to fetch lyrics from lyricwiki.org
+
+X-KDE-PluginInfo-Author=Aaron Reichman
+X-KDE-PluginInfo-Email=reldruh@gmail.com
+X-KDE-PluginInfo-Name=LyricWiki
+X-KDE-PluginInfo-Version=.2
+X-KDE-PluginInfo-Category=Lyrics
+X-KDE-PluginInfo-Depends=Amarok2.0
+X-KDE-PluginInfo-License=LGPL
+X-KDE-PluginInfo-EnabledByDefault=false
