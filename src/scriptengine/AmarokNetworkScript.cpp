ed341c374f288ef1fc48d2d34b8a2301eb03c6c2
Leo Franchi <lfranchi@kde.org>
2008-09-08 21:06:18 +0000

    add copyright and changelog
    svn path=/trunk/extragear/multimedia/amarok/; revision=858871

--- src/scriptengine/AmarokNetworkScript.cpp
+++ src/scriptengine/AmarokNetworkScript.cpp
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2008 Peter ZHOU <peterzhoulei@gmail.com>                     *
+* Copyright (C) 2008 Leo Franchi <lfranchi@kde.org>                          *
 *                                                                            *
 * This program is free software; you can redistribute it and/or              *
 * modify it under the terms of the GNU General Public License as             *
