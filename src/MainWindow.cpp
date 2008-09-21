1684226ac5ad56f665bc57a0c246c087923998bc
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2008-09-21 12:52:31 +0000

    Make main toolbar full width ( minus a 3 pixel margin to make it lign up with the rest of the app )
    svn path=/trunk/extragear/multimedia/amarok/; revision=863209

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -161,9 +161,9 @@ MainWindow::init()
     m_controlBar->layout()->setContentsMargins( 0, 0, 0, 0 );
     m_controlBar->layout()->setSpacing( 0 );
 
-    toolbarSpacer->addStretch( 1 );
+    toolbarSpacer->addSpacing( 3 );
     toolbarSpacer->addWidget( m_controlBar, 20);
-    toolbarSpacer->addStretch( 1 );
+    toolbarSpacer->addSpacing( 3 );
 
     PERF_LOG( "Create sidebar" )
     m_browsers = new SideBar( this, new KVBox );
