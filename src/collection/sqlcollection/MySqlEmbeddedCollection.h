e470fc13590ecfc70dd757203d2cd90c5e364750
Edward Hades Toroshchin <edward.hades@gmail.com>
2008-10-03 10:47:40 +0000

    Added copyright year.
    svn path=/trunk/extragear/multimedia/amarok/; revision=867349

--- src/collection/sqlcollection/MySqlEmbeddedCollection.h
+++ src/collection/sqlcollection/MySqlEmbeddedCollection.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) Edward Toroshchin <edward.hades@gmail.com>
+ *  Copyright (c) 2008 Edward Toroshchin <edward.hades@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
