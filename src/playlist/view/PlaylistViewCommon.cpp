e1ce74644ee4828990b9bd60ce8d075b9e0d261d
Seb Ruiz <ruiz@kde.org>
2008-10-25 02:40:18 +0000

    Enable context menu entry "jump to current track". Although this was removed due to the string freeze, Soren requested that if with such a huge break with the importer if this could be re-added. I didn't see any problem with it. Translators: string allows us to expose important functionality in the UI. CCMAIL: kde-i18n-doc@kde.org
    svn path=/trunk/extragear/multimedia/amarok/; revision=875653

--- src/playlist/view/PlaylistViewCommon.cpp
+++ src/playlist/view/PlaylistViewCommon.cpp
@@ -66,8 +66,8 @@ Playlist::ViewCommon::trackMenu( QWidget *parent, const QModelIndex *index, cons
         menu->addAction( playAction );
     }
     
-    //menu->addSeparator();
-    //menu->addAction( i18n( "Show active track" ), parent, SLOT( scrollToActiveTrack() ) ); // FIXME: enable this after string freeze
+    menu->addSeparator();
+    menu->addAction( i18n( "Show active track" ), parent, SLOT( scrollToActiveTrack() ) );
     //( menu->addAction( KIcon( "media-track-queue-amarok" ), i18n( "Queue Track" ), parent, SLOT( queueItem() ) ) )->setEnabled( false );
     //( menu->addAction( KIcon( "media-playback-stop-amarok" ), i18n( "Stop Playing After Track" ), parent, SLOT( stopAfterTrack() ) ) )->setEnabled( false );
     
