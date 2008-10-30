71e92c2acb4e634f7c3e2413bfe5dd28294c0ce8
Patrick Spendrin <ps_ml@gmx.de>
2008-10-30 22:01:06 +0000

    fix export macro
    svn path=/trunk/extragear/multimedia/amarok/; revision=877989

--- src/services/lastfm/lastfm/DllExportMacro.h
+++ src/services/lastfm/lastfm/DllExportMacro.h
@@ -14,41 +14,24 @@
 #ifndef LASTFM_DLL_EXPORT_MACRO_H
 #define LASTFM_DLL_EXPORT_MACRO_H
 
-#include "amarok_export.h"
+#include <kdemacros.h>
 
-#define LASTFM_RADIO_DLLEXPORT AMAROK_EXPORT
-#define LASTFM_FINGERPRINT_DLLEXPORT AMAROK_EXPORT
-#define LASTFM_WS_DLLEXPORT AMAROK_EXPORT
-#define LASTFM_CORE_DLLEXPORT AMAROK_EXPORT
-#define LASTFM_TYPES_DLLEXPORT AMAROK_EXPORT
-#define LASTFM_SCROBBLE_DLLEXPORT AMAROK_EXPORT
-
-
+#ifndef LASTFM_EXPORT
+# if defined(MAKE_AMAROK_SERVICE_LIBLASTFM_LIB)
+   /* We are building this library */ 
+#  define LASTFM_EXPORT KDE_EXPORT
+# else
+   /* We are using this library */ 
+#  define LASTFM_EXPORT KDE_IMPORT
+# endif
 #endif
-/***************************************************************************
-* copyright            : (C) 2008 Leo Franchi <lfranchi@kde.org>          *
- **************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
-#ifndef LASTFM_DLL_EXPORT_MACRO_H
-#define LASTFM_DLL_EXPORT_MACRO_H
-
-#include "amarok_export.h"
-
-#define LASTFM_RADIO_DLLEXPORT AMAROK_EXPORT
-#define LASTFM_FINGERPRINT_DLLEXPORT AMAROK_EXPORT
-#define LASTFM_WS_DLLEXPORT AMAROK_EXPORT
-#define LASTFM_CORE_DLLEXPORT AMAROK_EXPORT
-#define LASTFM_TYPES_DLLEXPORT AMAROK_EXPORT
-#define LASTFM_SCROBBLE_DLLEXPORT AMAROK_EXPORT
+#define LASTFM_RADIO_DLLEXPORT LASTFM_EXPORT
+#define LASTFM_FINGERPRINT_DLLEXPORT LASTFM_EXPORT
+#define LASTFM_WS_DLLEXPORT LASTFM_EXPORT
+#define LASTFM_CORE_DLLEXPORT LASTFM_EXPORT
+#define LASTFM_TYPES_DLLEXPORT LASTFM_EXPORT
+#define LASTFM_SCROBBLE_DLLEXPORT LASTFM_EXPORT
 
 
 #endif
