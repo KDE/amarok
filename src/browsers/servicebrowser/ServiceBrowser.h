403f7ab3e6b298770fb403a986cae9cdf3575aea
Seb Ruiz <ruiz@kde.org>
2008-11-08 13:05:39 +0000

    Celebrate nhn's latest fix by applying a good dose of style to his code :)
    svn path=/trunk/extragear/multimedia/amarok/; revision=881540

--- src/browsers/servicebrowser/ServiceBrowser.h
+++ src/browsers/servicebrowser/ServiceBrowser.h
@@ -33,108 +33,102 @@
 class ServiceListDelegate;
 
 /**
-A browser for selecting and displaying a service in the style of the first imbedded Magnatune store from a list of available services. Allows many services to be shown as a single tab.
-Implemented as a singleton.
-
-@author Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
-*/
+ *  A browser for selecting and displaying a service in the style of the first imbedded Magnatune store from a list of available services. Allows many services to be shown as a single tab.
+ *  Implemented as a singleton.
+ *
+ *  @author Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
+ */
 class ServiceBrowser : public KVBox
 {
     Q_OBJECT
 
-public:
+    public:
+        /**
+         * Get the ServiceBrowser instance. Create it if it does not exist yet. ( Singleton pattern ).
+         * @return The ServiceBrowser instance.
+         */
+        static ServiceBrowser *instance();
 
-    /**
-     * Get the ServiceBrowser instance. Create it if it does not exist yet. ( Singleton pattern ).
-     * @return The ServiceBrowser instance.
-     */
-    static ServiceBrowser * instance();
+        /**
+         * Destructor.
+         */
+        ~ServiceBrowser();
 
-    /**
-     * Destructor.
-     */
-    ~ServiceBrowser();
+        /**
+         * Get a map of the loaded services.
+         * @return the map of services.
+         */
+        QMap<QString,ServiceBase*> services();
 
-    /**
-     * Get a map of the loaded services.
-     * @return the map of services.
-     */
-    QMap<QString, ServiceBase *> services();
+        /**
+         * Remove a named service from the service browser.
+         * @param name The name of the service to remove.
+         */
+        void removeService( const QString &name );
 
-    /**
-     * Remove a named service from the service browser.
-     * @param name The name of the service to remove.
-     */
-    void removeService( const QString &name );
+        /**
+         * Reset a service and make it reload configuration. Not fully implemented..
+         * @param name The name of the service to reset.
+         */
+        void resetService( const QString &name );
+    
+        /**
+         * Make a service show. Hide any other active service if needed.
+         * @param name the service to show.
+         */
+        void showService( const QString &name );
 
-    /**
-     * Reset a service and make it reload configuration. Not fully implemented..
-     * @param name The name of the service to reset.
-     */
-    void resetService( const QString &name );
+    public slots:
+        /**
+         * Add a service.
+         * @param service The service to add.
+         */
+        void addService( ServiceBase *service );
 
-    /**
-     * Make a service show. Hide any other active service if needed.
-     * @param name the service to show.
-     */
-    void showService( const QString &name );
+        /**
+         * Set a scriptable service manager to handle scripted services.
+         * @param scriptableServiceManager The scriptable service manager to set.
+         */
+        void setScriptableServiceManager( ScriptableServiceManager *scriptableServiceManager );
 
-public slots:
+    protected:
+        /**
+         * Hanlde palette changes to ensure the service list is always tinted correctly.
+         * @param oldPalette The previously used palette.
+         */
+        virtual void paletteChange( const QPalette & oldPalette );
 
-    /**
-     * Add a service.
-     * @param service The service to add.
-     */
-    void addService( ServiceBase * service );
+    private:
+        /**
+         * Private constructor ( Singleton pattern )
+         * @param parent The parent widget.
+         * @param name The name of this widget.
+         */
+        ServiceBrowser( QWidget *parent, const QString& name );
 
-    /**
-     * Set a scriptable service manager to handle scripted services.
-     * @param scriptableServiceManager The scriptable service manager to set.
-     */
-    void setScriptableServiceManager( ScriptableServiceManager * scriptableServiceManager );
+        static ServiceBrowser    *s_instance;
 
-protected:
+        QTreeView                *m_serviceListView;
 
-    /**
-     * Hanlde palette changes to ensure the service list is always tinted correctly.
-     * @param oldPalette The previously used palette.
-     */
-    virtual void paletteChange( const QPalette & oldPalette );
+        QMap<QString,ServiceBase*> m_services;
+        ServiceBase              *m_currentService;
 
-private:
+        ScriptableServiceManager *m_scriptableServiceManager;
+        bool                      m_usingContextView;
+        ServiceListModel         *m_serviceListModel;
+        ServiceListDelegate      *m_delegate;
 
-    /**
-     * Private constructor ( Singleton pattern )
-     * @param parent The parent widget.
-     * @param name The name of this widget.
-     */
-    ServiceBrowser(QWidget * parent, const QString& name );
+    private slots:
+        /**
+         * Slot called when an item in the service list has been activated and the corrosponding service should be shown.
+         * @param index The index that was activated
+         */
+        void serviceActivated( const QModelIndex &index );
 
-    static ServiceBrowser * s_instance;
-
-    QTreeView * m_serviceListView;
-
-    QMap<QString, ServiceBase *> m_services;
-    ServiceBase * m_currentService;
-
-    ScriptableServiceManager * m_scriptableServiceManager;
-    bool m_usingContextView;
-    ServiceListModel * m_serviceListModel;
-    ServiceListDelegate * m_delegate;
-
-
-private slots:
-
-    /**
-     * Slot called when an item in the service list has been activated and the corrosponding service should be shown.
-     * @param index The index that was activated
-     */
-    void serviceActivated( const QModelIndex & index );
-
-    /**
-     * Slot called when the active service should be hidden the service selection list shown again.
-     */
-    void home();
+        /**
+         * Slot called when the active service should be hidden the service selection list shown again.
+         */
+        void home();
 };
 
 
