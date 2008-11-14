b2b9a4a5041a9736c39f658e9e17c4b7fe730175
Jeff Mitchell <mitchell@kde.org>
2008-11-14 22:25:56 +0000

    svn cp over popupdropper
    svn path=/trunk/extragear/multimedia/amarok/; revision=884426

---
+++ src/context/popupdropper/PopupDropperItem_p.h
@@ -0,0 +1,76 @@
+/***************************************************************************
+ *   Copyright (c) 2008  Jeff Mitchell <mitchell@kde.org>                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef POPUPDROPPER_ITEM_P_H
+#define POPUPDROPPER_ITEM_P_H
+
+#include <QtDebug>
+#include <QColor>
+#include <QFont>
+#include <QTimeLine>
+
+#include "PopupDropperItem.h"
+
+class PopupDropperItemPrivate
+{
+
+public:
+
+    PopupDropperItemPrivate( PopupDropperItem* parent );
+    
+    ~PopupDropperItemPrivate();
+
+    PopupDropperAction* action;
+    QString text;
+    QTimeLine hoverTimer;
+    QString elementId;
+    QGraphicsTextItem* textItem;
+    QGraphicsRectItem* borderRectItem;
+    QGraphicsSvgItem* svgItem;
+    QGraphicsRectItem* hoverIndicatorRectItem;
+    QGraphicsRectItem* hoverIndicatorRectFillItem;
+    int borderWidth;
+    int hoverIndicatorRectWidth;
+    QFont font;
+    bool submenuTrigger;
+    QColor baseTextColor;
+    QColor hoveredTextColor;
+    QPen hoveredBorderPen;
+    QBrush hoveredFillBrush;
+    QBrush hoverIndicatorRectFillBrush;
+    bool hoveredOver;
+    bool customBaseTextColor;
+    bool customHoveredTextColor;
+    bool customHoveredBorderPen;
+    bool customHoveredFillBrush;
+    bool separator;
+    QString file;
+    QRect blankElementRect;
+    int horizontalOffset;
+    int textOffset;
+    PopupDropperItem::HoverIndicatorShowStyle hoverIndicatorShowStyle;
+    PopupDropperItem::Orientation orientation;
+    PopupDropper* pd;
+
+private:
+    PopupDropperItem* q;
+};
+
+#endif
+
---
+++ src/context/popupdropper/PopupDropperView.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+ *   Copyright (c) 2008  Jeff Mitchell <mitchell@kde.org>                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef POPUPDROPPER_VIEW_H
+#define POPUPDROPPER_VIEW_H
+
+#include <QGraphicsView>
+
+class PopupDropper;
+class PopupDropperViewPrivate;
+
+class PopupDropperView : public QGraphicsView
+{
+    Q_OBJECT
+
+public:
+    PopupDropperView( PopupDropper *pd, QGraphicsScene *scene, QWidget *parent );
+    ~PopupDropperView();
+
+    void dropEvent( QDropEvent *event );
+    void dragEnterEvent( QDragEnterEvent *event );
+    void dragMoveEvent( QDragMoveEvent *event );
+    void dragLeaveEvent( QDragLeaveEvent *event );
+
+    void resetView();
+
+    void deactivateHover();
+
+    bool entered() const;
+    void setEntered( bool entered );
+    
+private:
+    friend class PopupDropperViewPrivate;
+    PopupDropperViewPrivate* const d;
+};
+
+#endif /* AMAROK_POPUPDROPPER_VIEW_H */
+
