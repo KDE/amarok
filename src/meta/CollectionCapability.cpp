31658e59416638e840cc3c106cfe964f37219e95
Alejandro Daniel Wainzinger <aikawarazuni@gmail.com>
2008-11-16 12:31:17 +0000

    Copyright 2008.  2007 was last year... ~lart xevix
    svn path=/trunk/extragear/multimedia/amarok/; revision=884960

--- src/meta/CollectionCapability.cpp
+++ src/meta/CollectionCapability.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2007 Alejandro Wainzinger <aikawarazuni@gmail.com>
+   Copyright (C) 2008 Alejandro Wainzinger <aikawarazuni@gmail.com>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
