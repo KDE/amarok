df9e2ea3b29aace5292308252581a6d21d7000a0
Andrey Esin <gmlastik@gmail.com>
2008-11-30 20:01:17 +0000

    Fix becuase after SVN commit 890698 displaying artist name and album name doesn't work in TagDialog.
    svn path=/trunk/extragear/multimedia/amarok/; revision=890984

--- src/dialogs/TagDialog.cpp
+++ src/dialogs/TagDialog.cpp
@@ -767,15 +767,16 @@ void TagDialog::readTags()
 
     const QFontMetrics fnt =  ui->trackArtistAlbumLabel->fontMetrics();
     const int len = ui->trackArtistAlbumLabel->width();
-    QString curTrackAlbName, curArtistName;
+    QString curTrackAlbName
+    QString curArtistName;
     
     QString curTrackName = fnt.elidedText( Qt::escape( m_currentTrack->name() ), Qt::ElideRight, len );
     QString curTrackPretName = fnt.elidedText( Qt::escape( m_currentTrack->prettyName() ), Qt::ElideRight, len );
     
     if( m_currentTrack->album() )
-        QString curTrackAlbName = fnt.elidedText( Qt::escape( m_currentTrack->album()->name() ), Qt::ElideRight, len );
+        curTrackAlbName = fnt.elidedText( Qt::escape( m_currentTrack->album()->name() ), Qt::ElideRight, len );
     if( m_currentTrack->artist() )
-        QString curArtistName = fnt.elidedText( Qt::escape( m_currentTrack->artist()->name() ), Qt::ElideRight, len );
+        curArtistName = fnt.elidedText( Qt::escape( m_currentTrack->artist()->name() ), Qt::ElideRight, len );
     
     
     if( m_currentTrack->album() && m_currentTrack->album()->name().isEmpty() )
