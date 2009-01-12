fc904191821be048aa9934c33808a0307dedaff0
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2009-01-12 20:06:10 +0000

    Add a pile of lessThanFoo sorting functions to Playlist::Item
    svn path=/trunk/extragear/multimedia/amarok/; revision=910177

--- src/playlist/PlaylistItem.h
+++ src/playlist/PlaylistItem.h
@@ -39,19 +39,45 @@ namespace Playlist
                 Queued
             };
 
-            Item() : m_track( 0 ), m_state( Invalid ), m_id( 0 ) { }
-            Item( Meta::TrackPtr track );
+            Item() : m_track ( 0 ), m_state ( Invalid ), m_id ( 0 ) { }
+            Item ( Meta::TrackPtr track );
             ~Item();
             Meta::TrackPtr track() const { return m_track; }
 
             State state() const { return m_state; }
-            void setState( State s ) { m_state = s; }
+            void setState ( State s ) { m_state = s; }
 
             /**
              * @return a random number which uniquely identifies this particular
              * instance of a track in the playlist
              */
             quint64 id() const { return m_id; }
+
+            //sorting functions galore...
+            static bool lessThanAlbum ( Item* left, Item* right );
+            static bool lessThanAlbumArtist ( Item* left, Item* right );
+            static bool lessThanArtist ( Item* left, Item* right );
+            static bool lessThanBitrate ( Item* left, Item* right );
+            static bool lessThanBpm ( Item* left, Item* right );
+            static bool lessThanComment ( Item* left, Item* right );
+            static bool lessThanComposer ( Item* left, Item* right );
+            static bool lessThanDirectory ( Item* left, Item* right );
+            static bool lessThanDiscNumber ( Item* left, Item* right );
+            static bool lessThanFilename ( Item* left, Item* right );
+            static bool lessThanFilesize ( Item* left, Item* right );
+            static bool lessThanGenre ( Item* left, Item* right );
+            static bool lessThanLastPlayed ( Item* left, Item* right );
+            static bool lessThanLength ( Item* left, Item* right );
+            static bool lessThanMood ( Item* left, Item* right );
+            static bool lessThanPlayCount ( Item* left, Item* right );
+            static bool lessThanRating ( Item* left, Item* right );
+            static bool lessThanSampleRate ( Item* left, Item* right );
+            static bool lessThanScore ( Item* left, Item* right );
+            static bool lessThanSource ( Item* left, Item* right );
+            static bool lessThanTitle ( Item* left, Item* right );
+            static bool lessThanTrackNumber ( Item* left, Item* right );
+            static bool lessThanType ( Item* left, Item* right );
+            static bool lessThanYear ( Item* left, Item* right );
 
         private:
             Meta::TrackPtr m_track;
@@ -60,6 +86,6 @@ namespace Playlist
     };
 }
 
-Q_DECLARE_METATYPE( Playlist::Item* )
+Q_DECLARE_METATYPE ( Playlist::Item* )
 
 #endif
