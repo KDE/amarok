001b10cbf82c7ef95443ccb17c22799411bb4739
Casey Link <unnamedrambler@gmail.com>
2009-01-19 06:15:09 +0000

    Created a Worker class that collections can use to thread trackForUrl.
    svn path=/trunk/extragear/multimedia/amarok/; revision=913321

---
+++ src/collection/TrackForUrlWorker.cpp
@@ -0,0 +1,39 @@
+/***************************************************************************
+*   Copyright (c) 2009  Casey Link <unnamedrambler@gmail.com>             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+***************************************************************************/
+#include "TrackForUrlWorker.h"
+
+Amarok::TrackForUrlWorker::TrackForUrlWorker ( const KUrl &url ) : ThreadWeaver::Job(), mUrl ( url )
+{
+    connect ( this, SIGNAL ( done ( ThreadWeaver::Job* ) ), SLOT ( completeJob() ) );
+}
+
+Amarok::TrackForUrlWorker::TrackForUrlWorker( const QString &url ) : ThreadWeaver::Job(), mUrl ( KUrl ( url ) )
+{
+    connect ( this, SIGNAL ( done ( ThreadWeaver::Job* ) ), SLOT ( completeJob() ) );
+}
+
+Amarok::TrackForUrlWorker::~TrackForUrlWorker()
+{}
+
+void
+Amarok::TrackForUrlWorker::completeJob()
+{
+    emit ( finishedLookup ( mTrack ) );
+    deleteLater();
+}
