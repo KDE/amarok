62775a27996852a859d63af077b13f7048ac2421
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2009-02-01 21:10:52 +0000

    Add album token (DOH!)
    svn path=/trunk/extragear/multimedia/amarok/; revision=919921

--- src/dialogs/PlaylistLayoutEditDialog.cpp
+++ src/dialogs/PlaylistLayoutEditDialog.cpp
@@ -29,6 +29,7 @@ PlaylistLayoutEditDialog::PlaylistLayoutEditDialog( QWidget *parent )
 {
     setupUi( this );
 
+    tokenPool->addToken( new Token( columnNames[Album], iconNames[Album], Album )) ;
     tokenPool->addToken( new Token( columnNames[Artist], iconNames[Artist], Artist )) ;
     tokenPool->addToken( new Token( columnNames[Comment], iconNames[Comment], Comment ) );
     tokenPool->addToken( new Token( columnNames[Composer], iconNames[Composer], Composer ) );
