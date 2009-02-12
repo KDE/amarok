4bcc0fae2220564217560bb6c6d5d3e0379f9572
Gary David Steinert <fatlardo@gmail.com>
2009-02-12 13:27:48 +0000

    Fix problems for Mamarok where the sidebar's minimum width is far too large on a large resolution display.
    svn path=/trunk/extragear/multimedia/amarok/; revision=925104

--- src/Sidebar.h
+++ src/Sidebar.h
@@ -78,6 +78,11 @@ class SideBar: public KHBox
 
         ~SideBar() {}
 
+        int minimumSizeHint()
+        {
+            return 100;
+        }
+
         void deleteBrowsers()
         {
             DEBUG_BLOCK
