9c919aa57f64671adeda8b43f556826f5f46507b
Mark Kretschmann <kretschmann@kde.org>
2009-03-05 18:25:13 +0000

    Created simple C++ template files, both for .cpp and .h files.
    I've noticed that much of the time for creating new classes is spent on
    the overhead of simply creating the scaffolding, so I thought it's a
    good idea to add these.
    
    svn path=/trunk/extragear/multimedia/amarok/; revision=935591

---
+++ HACKING/templates/Template.h
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * Copyright (c) 2009 Foo Bar <foo@bar.com>                                   *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or              *
+ * modify it under the terms of the GNU General Public License as             *
+ * published by the Free Software Foundation; either version 2 of             *
+ * the License, or (at your option) any later version.                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.      *
+ ******************************************************************************/
+
+#ifndef AMAROK_TEMPLATE_H
+#define AMAROK_TEMPLATE_H
+
+#include <QObject>
+
+/**
+ * A class for
+ *
+ * @author Foo Bar <foo@bar.com>
+ **/
+class AMAROK_EXPORT Template : public QObject 
+{
+    Q_OBJECT
+
+public:
+    Template();
+    ~Template();
+
+public Q_SLOTS:
+
+protected:
+
+private Q_SLOTS:
+
+private:
+
+// Member variables:
+
+};
+
+#endif
+
