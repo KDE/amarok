5729714579f09f25df453c7f72a6c0dfd25be6f1
Ian Monroe <ian.monroe@gmail.com>
2009-03-07 06:37:41 +0000

    If cmake detects that the script generator is already installed, it does not build it. If the script generator isn't installed it works like before. As with all build system changes, some testing would be appreciated.
    At some point in the next couple of weeks Amarok will refuse to build
    without the QtScript Generator already installed. You can download the
    binding generator seperately at http://code.google.com/p/qtscriptgenerator/downloads/list
    This is what Amarok 2.1 will be doing. Users having trouble building the generator
    with cmake can use this now.
    
    177863 is closed for sure. :>
    
    CCMAIL: amarok@kde.org
    BUG: 177863
    
    svn path=/trunk/extragear/multimedia/amarok/; revision=936142

---
+++ cmake/modules/CheckQtScriptBindings.cmake
@@ -0,0 +1,32 @@
+## Ian Monroe <ian@monroe.nu> Copyright 2009 
+# released under public domain or:
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+
+message(STATUS "Checking if the QtScript Qt Bindings are installed.")
+set(LINK_LIBRARIES "-lQtCore -lkdeui -lkdecore -lQtScript")
+#set(LINK_LIBRARIES "${QT_QTCORE_LIBS} ${KDE4_KDEUI_LIBS} ${KDE4_KDECORE_LIBS} -${QT_QTSCRIPT_LIBS}")
+try_run(BINDINGS_RUN_RESULT BINDINGS_COMPILE_RESULT
+        ${PROJECT_BINARY_DIR}/CMakeTmp/generator
+        ${PROJECT_SOURCE_DIR}/cmake/modules/QtScriptBindingsTest.cpp 
+        CMAKE_FLAGS "-DCOMPILE_DEFINITIONS:STRING=${LINK_LIBRARIES}"
+        )
+if(BINDINGS_RUN_RESULT EQUAL 0)
+    message( STATUS "QtBindings found")
+    set(QTSCRIPT_QT_BINDINGS_FOUND TRUE)
+else(BINDINGS_RUN_RESULT EQUAL 0)
+    set(QTSCRIPT_QT_BINDINGS_FOUND FALSE)
+endif(BINDINGS_RUN_RESULT EQUAL 0)
+
---
+++ cmake/modules/QtScriptBindingsTest.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2009 Ian Monroe <ian@monroe.nu>
+ * released under public domain or:
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <KDE/KApplication>
+#include <KDE/KAboutData>
+#include <KDE/KCmdLineArgs>
+#include <QtScript/QScriptEngine>
+#include <QtCore/QStringList>
+
+#define FAIL 0xA
+
+int main(int argc, char **argv)
+{
+    KAboutData about( "findgeneratorplugins", "", ki18n("Generator Exists?"), "1.0", ki18n("Find if the QtScript Plugins Are Installed"), KAboutData::License_LGPL_V2 );
+    KCmdLineArgs::init( argc, argv, &about );
+    KApplication app;
+    
+    QStringList allowedBindings;
+    allowedBindings << "qt.core" << "qt.gui" << "qt.sql" << "qt.xml" << "qt.uitools" << "qt.network";
+    QScriptEngine engine;
+    foreach( QString binding, allowedBindings )
+    {
+        if( !engine.importExtension( binding ).isUndefined() )
+        { // undefined indiciates success
+            qDebug( binding.toLatin1() +  " not found" );
+            return FAIL;
+        }
+    }
+    return 0;
+}
