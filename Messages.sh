312c6f2ac9fa39af55c731a94e8e50e9667233ac
Albert Astals Cid <tsdgeos@terra.es>
2009-03-13 23:57:59 +0000

    follow code
    svn path=/trunk/extragear/multimedia/amarok/; revision=939157

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-$XGETTEXT_QT src/collectionscanner/CollectionScanner.cpp -o $podir/amarokcollectionscanner_qt.pot
+$XGETTEXT_QT utilities/collectionscanner/CollectionScanner.cpp -o $podir/amarokcollectionscanner_qt.pot
