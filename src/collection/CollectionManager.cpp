91dd9259f7eee6071f505206c5cda7fe874f0b53
Mark Kretschmann <kretschmann@kde.org>
2009-03-17 09:35:35 +0000

    Remove "--noincremental" argument from kbuildsycoca4 call, which doesn't seem to do anything useful, except making the call much slower.
    svn path=/trunk/extragear/multimedia/amarok/; revision=940325

--- src/collection/CollectionManager.cpp
+++ src/collection/CollectionManager.cpp
@@ -110,7 +110,7 @@ CollectionManager::init()
     //This code was in enginecontroller originally, it now ends up here.
     if( plugins.count() == 0 )
     {
-        KRun::runCommand( "kbuildsycoca4 --noincremental", 0 );
+        KRun::runCommand( "kbuildsycoca4", 0 );
 
         KMessageBox::error( 0, i18n(
                 "<p>Amarok could not find any collection plugins. "
