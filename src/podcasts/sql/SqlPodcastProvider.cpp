1cc91118cf9e089a475af884c05d68e07089dc5c
Jeff Mitchell <mitchell@kde.org>
2009-05-21 20:57:16 +0000

    Warnings--
    svn path=/trunk/extragear/multimedia/amarok/; revision=971175

--- src/podcasts/sql/SqlPodcastProvider.cpp
+++ src/podcasts/sql/SqlPodcastProvider.cpp
@@ -564,7 +564,6 @@ SqlPodcastProvider::autoUpdate()
 void
 SqlPodcastProvider::update( Meta::PodcastChannelPtr channel )
 {
-    bool result = false;
     m_updatingChannels++;
     PodcastReader * podcastReader = new PodcastReader( this );
 
