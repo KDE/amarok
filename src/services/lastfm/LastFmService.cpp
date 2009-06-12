b1d9a4d7a3a95eff6feb0811c8938ff7b24d49be
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2009-06-12 17:54:10 +0000

    add long descr. and image to last.fm service
    svn path=/trunk/extragear/multimedia/amarok/; revision=980938

--- src/services/lastfm/LastFmService.cpp
+++ src/services/lastfm/LastFmService.cpp
@@ -184,6 +184,11 @@ LastFmService::LastFmService( LastFmServiceFactory* parent, const QString &name,
     setShortDescription( i18n( "Last.fm: The social music revolution" ) );
     setIcon( KIcon( "view-services-lastfm-amarok" ) );
 
+    setLongDescription( i18n( "Last.fm is a popular online service that provides personal radio stations and music reccomendations. Based on your listening habbits it tailor makes a personal station for you and provides you with reccomendations for new music. It is also possible to play stations with music that is simmilar to a particular artits as well as listen to streams from people you have added as friends or that last.fm considders your musical \"neighbours\"" ) );
+
+    KIconLoader loader;
+    setImagePath( loader.iconPath( "view-services-lastfm-amarok", -128, true ) );
+
     if( !username.isEmpty() && !password.isEmpty() )
         init();
 
