27ee101c866f9b63bb57fab19bbd9d8f07f276bc
Leo Franchi <lfranchi@kde.org>
2009-06-12 20:00:23 +0000

    add stub engine, make sample applet show currently playing info
    svn path=/trunk/extragear/multimedia/amarok/; revision=981115

--- playground/src/context/applets/jssample/contents/code/main.js
+++ playground/src/context/applets/jssample/contents/code/main.js
@@ -1,7 +1,16 @@
 plasmoid.drawStandardBackground = true;
 
+plasmoid.dataUpdate = function(a, b)
+{
+    print( "DOING SOMETHING" );
+    label.text = "Playing " + b.current[ "xesam:title" ] + " from " + b.current[ "xesam:author" ] + " on " + b.current[ "xesam:album" ];
+}
+
+
 layout = new LinearLayout( plasmoid );
 label = new Label();
 layout.addItem( label );
 
 label.text = "This is a Javascript Applet";
+
+plasmoid.dataEngine("amarok-current").connectSource( "current", plasmoid );
