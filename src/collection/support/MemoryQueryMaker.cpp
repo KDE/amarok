7ddbe3d5c89b7935f11e18dc739e2b6a3b79171b
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2009-06-12 18:01:14 +0000

    Copyright year update
    svn path=/trunk/extragear/multimedia/amarok/; revision=980969

--- src/collection/support/MemoryQueryMaker.cpp
+++ src/collection/support/MemoryQueryMaker.cpp
@@ -1,6 +1,6 @@
 /*
    Copyright (C) 2007-2009 Maximilian Kossick <maximilian.kossick@googlemail.com>
-             (c) 2007  Nikolaj Hald Nielsen <nhnFreespirit@gmail.com> 
+             (c) 2007-2009  Nikolaj Hald Nielsen <nhnFreespirit@gmail.com> 
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
