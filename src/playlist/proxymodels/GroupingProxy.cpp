cfcee815e454bc2c310d90fafc79f473e54f40d8
Teo Mrnjavac <teo.mrnjavac@gmail.com>
2009-06-12 18:52:41 +0000

    Make compile.
    svn path=/trunk/extragear/multimedia/amarok/; revision=981046

--- src/playlist/proxymodels/GroupingProxy.cpp
+++ src/playlist/proxymodels/GroupingProxy.cpp
@@ -374,11 +374,11 @@ int Playlist::GroupingProxy::lengthOfGroup( int row ) const
     int totalLenght = 0;
     for ( int i = firstInGroup( row ); i <= lastInGroup( row ); i++ )
     {
-        Meta::TrackPtr track = m_model->trackAt( i );
+        Meta::TrackPtr track = m_belowModel->trackAt( i );
         if ( track )
             totalLenght += track->length();
         else
-            warning() << "Playlist::GroupingProxy::lengthOfGroup(): TrackPtr is 0!  i = " << i << ", rowCount = " << m_model->rowCount();
+            warning() << "Playlist::GroupingProxy::lengthOfGroup(): TrackPtr is 0!  i = " << i << ", rowCount = " << m_belowModel->rowCount();
     }
 
     return totalLenght;
