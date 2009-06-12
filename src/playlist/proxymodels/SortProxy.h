8305c3f9e75fcfebda3fc72b0f2002a7f1c8ae5b
Teo Mrnjavac <teo.mrnjavac@gmail.com>
2009-06-12 18:49:16 +0000

    Initial work on Playlist::SortProxy class, added ctor and instance handling.
    svn path=/trunk/extragear/multimedia/amarok/; revision=981017

---
+++ src/playlist/proxymodels/SortProxy.h
@@ -0,0 +1,77 @@
+/***************************************************************************
+ *   Copyright 2008 Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>         *
+ *             2009 Teo Mrnjavac <teo.mrnjavac@gmail.com>                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef AMAROK_PLAYLISTSORTPROXY_H
+#define AMAROK_PLAYLISTSORTPROXY_H
+
+#include <QSortFilterProxyModel>
+#include "FilterProxy.h"
+#include <playlist/PlaylistModel.h>
+
+namespace Playlist
+{
+/**
+ * A ProxyModel that implements multilevel sorting for the Playlist.
+ * This proxy should sit above the FilterProxy and below the GroupingProxy.
+ * @author Teo Mrnjavac <teo.mrnjavac@gmail.com>
+ */
+class SortProxy : public QSortFilterProxyModel
+{
+    Q_OBJECT
+public:
+    /**
+     * Returns the single instance of SortProxy.
+     * @return the class instance.
+     */
+    static SortProxy *instance();
+
+signals:
+
+private:
+    /**
+     * Constructor.
+     */
+    SortProxy();
+
+    /**
+     * Destructor.
+     */
+    ~SortProxy();
+
+    /**
+     * The instance of the Playlist::Model, and base of the stack of Models/Proxies.
+     */
+    Model *m_sourceModel;
+
+    /**
+     * The Proxy or Model that's right below this one in the stack of Models/Proxies.
+     */
+    QSortFilterProxyModel *m_belowModel;
+
+    /**
+     * Instance member.
+     */
+    static SortProxy *s_instance;
+};
+
+
+}   //namespace Playlist
+
+#endif  //AMAROK_PLAYLISTSORTPROXY_H
