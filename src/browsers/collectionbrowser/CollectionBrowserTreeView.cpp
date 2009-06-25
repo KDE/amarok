da447340c421fc0d581e0a4e659309a6a826ec66
Mark Kretschmann <kretschmann@kde.org>
2009-06-25 19:40:50 +0000

    Make compile: accidentally removed include.
    svn path=/trunk/extragear/multimedia/amarok/; revision=987300

--- src/browsers/collectionbrowser/CollectionBrowserTreeView.cpp
+++ src/browsers/collectionbrowser/CollectionBrowserTreeView.cpp
@@ -15,6 +15,8 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
+#include "CollectionBrowserTreeView.h"
+
 #include "Debug.h"
 
 CollectionBrowserTreeView::CollectionBrowserTreeView( QWidget *parent )
