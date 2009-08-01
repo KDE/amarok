7426be93ad7f0e46016d305c64c34c5f26ab633f
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2009-08-01 17:58:34 +0200

    Add a bookmark manager dialog that will eventually replace the context applet. The general idea is that most simple bookmark actions (like running it or creating a new one from the current view/state) will be integrated into the context where they are used, for instance, navigation bookmarks will be available from the breadcrumb in the browser dock, so the full manager will not be as frequrntly needed.

---
+++ src/amarokurls/BookmarkManager.cpp
@@ -0,0 +1,69 @@
+/****************************************************************************************
+ * Copyright (c) 2009 Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>                    *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Pulic License for more details.              *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+#include "BookmarkManager.h"
+
+#include "Debug.h"
+
+#include <KApplication>
+#include <KDialog>
+
+#include <QHBoxLayout>
+
+BookmarkManager * BookmarkManager::s_instance = 0;
+
+
+BookmarkManager * BookmarkManager::instance()
+{
+    if( s_instance == 0 )
+        s_instance = new BookmarkManager();
+
+    return s_instance;
+}
+
+BookmarkManager::BookmarkManager()
+    : QDialog()
+{
+    // Sets caption and icon correctly (needed e.g. for GNOME)
+    kapp->setTopWidget( this );
+    setWindowTitle( KDialog::makeStandardCaption( i18n("Bookmark Manager") ) );
+
+    QHBoxLayout *layout = new QHBoxLayout();
+    m_widget = new BookmarkManagerWidget( 0 );
+    layout->addWidget( m_widget );
+    setLayout( layout );
+
+}
+
+BookmarkManager::~BookmarkManager()
+{
+}
+
+void BookmarkManager::showOnce()
+{
+    DEBUG_BLOCK
+    instance()->activateWindow();
+    instance()->show();
+    instance()->raise();
+}
+
+namespace The {
+
+    BookmarkManager* bookmarkManager()
+    {
+        return BookmarkManager::instance();
+    }
+}
---
+++ src/amarokurls/BookmarkManager.h
@@ -0,0 +1,49 @@
+/****************************************************************************************
+ * Copyright (c) 2009 Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>                    *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Pulic License for more details.              *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+#ifndef BOOKMARKMANAGER_H
+#define BOOKMARKMANAGER_H
+
+#include "BookmarkManagerWidget.h"
+
+#include <qt4/QtGui/QDialog>
+
+class BookmarkManager;
+
+namespace The {
+    AMAROK_EXPORT BookmarkManager* bookmarkManager();
+}
+
+class BookmarkManager : public QDialog
+{
+    friend BookmarkManager* The::bookmarkManager();
+        
+public:
+
+    static BookmarkManager * instance();
+    ~BookmarkManager();
+
+     static void showOnce();
+    
+private:
+
+    BookmarkManager();
+
+    static BookmarkManager *s_instance;
+    BookmarkManagerWidget * m_widget;
+};
+
+#endif // BOOKMARKMANAGER_H
