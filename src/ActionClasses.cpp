3da90ebecff65762dfb353c2c3fadd5b642ba050
Frederik Gladhorn <gladhorn@kde.org>
2009-08-05 18:15:25 +0200

    add osd when enabling stop after current track

--- src/ActionClasses.cpp
+++ src/ActionClasses.cpp
@@ -34,6 +34,8 @@
 #include <KHelpMenu>
 #include <KLocale>
 #include <KToolBar>
+#include <Osd.h>
+#include <Osd.h>
 
 
 extern KAboutData aboutData;
@@ -502,9 +504,13 @@ StopPlayingAfterCurrentTrackAction::stopPlayingAfterCurrentTrack()
     {
         The::playlistActions()->setStopAfterMode( Playlist::StopAfterCurrent );
         The::playlistActions()->setTrackToBeLast( The::playlistModel()->activeId() );
+        Amarok::OSD::instance()->setImage( QImage( KIconLoader::global()->iconPath( "amarok", -KIconLoader::SizeHuge ) ) );
+        Amarok::OSD::instance()->OSDWidget::show( i18n( "Stop after current track: On" ) );       
     } else {
         The::playlistActions()->setStopAfterMode( Playlist::StopNever );
         The::playlistActions()->setTrackToBeLast( 0 );
+        Amarok::OSD::instance()->setImage( QImage( KIconLoader::global()->iconPath( "amarok", -KIconLoader::SizeHuge ) ) );
+        Amarok::OSD::instance()->OSDWidget::show( i18n( "Stop after current track: Off" ) );
     }
 }
 
