dbcee23192cae534b972090052c1bc59f5807e46
Nikolaj Hald Nielsen <nhnFreespirit@gmail.com>
2009-08-09 20:03:51 +0200

    Fix issue with the slider "knob" for the active track in the playlist being painted all the way to the left if no track was currently playing.

--- src/playlist/view/listview/PrettyItemDelegate.cpp
+++ src/playlist/view/listview/PrettyItemDelegate.cpp
@@ -520,9 +520,14 @@ void Playlist::PrettyItemDelegate::paintActiveTrackExtras( const QRect &rect, QP
 
     long trackLength = EngineController::instance()->trackLength() * 1000;
     long trackPos = EngineController::instance()->trackPositionMs();
-    qreal trackPercentage = ( (qreal) trackPos / (qreal) trackLength );
+    qreal trackPercentage = 0.0;
+
+    if ( trackLength > 0 )
+        trackPercentage = ( (qreal) trackPos / (qreal) trackLength );
 
     int sliderWidth = width - ( offset + MARGINH );
+
+    debug() << "offset: " << offset << ", sliderWidth: " << sliderWidth << ", height: " << height << ", trackPercentage: " << trackPercentage;
     The::svgHandler()->paintCustomSlider( painter, offset, y, sliderWidth, height, trackPercentage, false );
     
 }
