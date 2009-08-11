c488de248cd2b28778604b42dc0d6369d258c350
Bart Cerneels <bart.cerneels@kde.org>
2009-08-11 19:50:03 +0200

    Update copyright header. SVN_SILENT

--- src/podcasts/sql/SqlPodcastProvider.cpp
+++ src/podcasts/sql/SqlPodcastProvider.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************************
- * Copyright (c) 2007 Bart Cerneels <bart.cerneels@kde.org>                             *
+ * Copyright (c) 2007-2009 Bart Cerneels <bart.cerneels@kde.org>                        *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
  * the terms of the GNU General Public License as published by the Free Software        *
