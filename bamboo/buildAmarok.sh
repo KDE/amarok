a1507beaac0e9c9432244b10b51f6990563daa21
Seb Ruiz <ruiz@kde.org>
2009-08-22 15:24:59 +1000

    Bamboo stuff

--- bamboo/buildAmarok.sh
+++ bamboo/buildAmarok.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo "Configuring Amarok"
-#cmake ../checkout -DCMAKE_BUILD_TYPE=debugfull -DCMAKE_INSTALL_PREFIX=~/kde
+cmake ../checkout -DCMAKE_BUILD_TYPE=debugfull -DCMAKE_INSTALL_PREFIX=~/kde
 echo "Building Amarok"
-#make -j5
+make -j5
 echo "Installing Amarok"
-#make install
+make install
 echo "Running tests"
-#amarok --test --nofork
+amarok --test --nofork
 echo "Waiting for tests to finish..."
-#sleep 5s
+sleep 5s
+kquitapp amarok
 echo "Copying tests to Bamboo"
 ant -f /home/atlassian/buildAmarok/build.xml qtest2junit
+exit 0
