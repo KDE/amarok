4718c3cbf890564d64b7b6b2a425eaf5684c6c72
Lydia Pintscher <lydia@kde.org>
2009-09-01 10:54:16 +0200

    bumping plug-in version before tagging is a good idea (TM)

--- src/PluginManager.h
+++ src/PluginManager.h
@@ -29,7 +29,7 @@ class PluginManager
 {
     public:
         /** Bump this number whenever the plugin framework gets incompatible with older versions */
-        static const int FrameworkVersion = 46;
+        static const int FrameworkVersion = 47;
 
         /**
          * It will return a list of services that match your
