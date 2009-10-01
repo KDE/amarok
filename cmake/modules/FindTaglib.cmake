a277f8250f0463213cd0968d02e5cecbfb9526c3
Jeff Mitchell <mitchell@kde.org>
2009-10-01 13:05:10 -0400

    Update FindTaglib-Extras to use the same logic as FindTaglib, which may fix instances where libraries are found in different places.

--- cmake/modules/FindTaglib.cmake
+++ cmake/modules/FindTaglib.cmake
@@ -11,7 +11,7 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 if(NOT TAGLIB_MIN_VERSION)
-  set(TAGLIB_MIN_VERSION "1.4")
+  set(TAGLIB_MIN_VERSION "1.6")
 endif(NOT TAGLIB_MIN_VERSION)
 
 if(NOT WIN32)
