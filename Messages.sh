18406b0d7bfb886deaa05a3bb6c5a12e5bcdd7d8
Kevin Funk <krf@electrostorm.net>
2009-10-18 23:22:47 +0200

    Fix Messages.sh. Still not translatable, trickier than I thought... CCBUG: 189751

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
 
 $XGETTEXT_QT utilities/collectionscanner/CollectionScanner.cpp -o $podir/amarokcollectionscanner_qt.pot
-$EXTRACTATTR --attr=layout,name src/data/DefaultPlaylistLayouts.xml >> rc.cpp
--- src/Messages.sh
+++ src/Messages.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 $EXTRACTRC `find . -name "*.rc" -o -name "*.ui" -o -name "*.kcfg"` >> rc.cpp
+$EXTRACTATTR --attr=layout,name data/DefaultPlaylistLayouts.xml >> rc.cpp
 LIST=`find . -name \*.h -o -name \*.cpp`
 if test -n "$LIST"; then
 	$XGETTEXT $LIST -o $podir/amarok.pot
