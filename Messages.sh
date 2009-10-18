b73451ee00f51539e220bace52f415bd90039a4b
Kevin Funk <krf@electrostorm.net>
2009-10-18 22:39:43 +0200

    Make playlist layouts translatable BR: 189751

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
 $XGETTEXT_QT utilities/collectionscanner/CollectionScanner.cpp -o $podir/amarokcollectionscanner_qt.pot
+$EXTRACTATTR --attr=layout,name src/data/DefaultPlaylistLayouts.xml >> rc.cpp
