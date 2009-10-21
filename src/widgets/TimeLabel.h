5bf703585791145f6dcefce03630f3392b33da51
Leo Franchi <lfranchi@kde.org>
2009-10-21 11:01:18 -0400

    Revert "Remaining time of track could get cut off for very long tracks. Patch by"
    this patch doesn't actually work, as discussed on #amarok last night. Author is aware and re-working patch. Also, as discussed on the ML, naming is a bit obscure, so might as well fix that as well when you send the working version :)
    
    This reverts commit b46cd2b13696f964cb4e185324ace04ebee6fdfb.

--- src/widgets/TimeLabel.h
+++ src/widgets/TimeLabel.h
@@ -1,6 +1,5 @@
 /****************************************************************************************
  * Copyright (c) 2005 Max Howell <max.howell@methylblue.com>                            *
- * Copyright (c) 2009 Victor Wollesen <victor.w@pervices.com>                            *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
  * the terms of the GNU General Public License as published by the Free Software        *
@@ -71,23 +70,8 @@ public:
             QLabel::setText( text );
     }
 
-    void setNomWidth( int nomWidth )
-    {
-        if( nomWidth > 0 )
-        {
-            m_nomWidth = nomWidth;
-            QWidget::setFixedWidth( m_nomWidth );
-        }
-    }
-
-    int nomWidth() const
-    {
-        return m_nomWidth;
-    }
-
 private:
     bool m_showTime;
-    int  m_nomWidth;
 
 };
 
