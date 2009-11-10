7d69678ea7135de625a3ca462b6f5c316c77db83
Mark Kretschmann <kretschmann@kde.org>
2009-11-10 16:54:22 +0100

    Bump plugin versions.

--- src/PluginManager.h
+++ src/PluginManager.h
@@ -29,7 +29,7 @@ class PluginManager
 {
     public:
         /** Bump this number whenever the plugin framework gets incompatible with older versions */
-        static const int FrameworkVersion = 49;
+        static const int FrameworkVersion = 50;
 
         /**
          * It will return a list of services that match your
