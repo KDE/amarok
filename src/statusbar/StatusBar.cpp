6dfe821cacd47ea540edfbceb3940f3fde16ada6
Rick W. Chen <stuffcorpse@archlinux.us>
2009-11-15 12:09:39 +1300

    Show length of queued tracks in the status bar tooltip

--- src/statusbar/StatusBar.cpp
+++ src/statusbar/StatusBar.cpp
@@ -23,6 +23,7 @@
 #include "LongMessageWidget.h"
 #include "meta/MetaUtility.h"
 #include "meta/capabilities/SourceInfoCapability.h"
+#include "playlist/PlaylistItem.h"
 #include "playlist/PlaylistModelStack.h"
 
 #include "KJobProgressBar.h"
@@ -300,7 +301,37 @@ StatusBar::updateTotalPlaylistLength() //SLOT
     {
         m_playlistLengthLabel->setText( i18ncp( "%1 is number of tracks, %2 is time", "%1 track (%2)", "%1 tracks (%2)", trackCount, prettyTotalLength ) );
         m_playlistLengthLabel->show();
-        m_playlistLengthLabel->setToolTip( i18n( "Total playlist size: %1", prettyTotalSize ) );
+
+        quint64 queuedTotalLength( 0 );
+        quint64 queuedTotalSize( 0 );
+        int queuedCount( 0 );
+
+        for( int i = 0; i < trackCount; ++i )
+        {
+            if( The::playlist()->stateOfRow( i ) & Playlist::Item::Queued )
+            {
+                queuedTotalLength += The::playlist()->trackAt( i )->length();
+                queuedTotalSize += The::playlist()->trackAt( i )->filesize();
+                queuedCount++;
+            }
+        }
+
+        const QString prettyQueuedTotalLength = Meta::msToPrettyTime( queuedTotalLength );
+        const QString prettyQueuedTotalSize   = Meta::prettyFilesize( queuedTotalSize );
+
+        QString tooltipLabel;
+        if( queuedCount > 0 && queuedTotalLength > 0 )
+        {
+            tooltipLabel = i18n( "Total playlist size: %1", prettyTotalSize )       + '\n'
+                         + i18n( "Queue size: %1",          prettyQueuedTotalSize ) + '\n'
+                         + i18n( "Queue length: %1",        prettyQueuedTotalLength );
+        }
+        else
+        {
+            tooltipLabel = i18n( "Total playlist size: %1", prettyTotalSize );
+        }
+
+        m_playlistLengthLabel->setToolTip( tooltipLabel );
         m_separator->show();
     }
     else if( ( totalLength == 0 ) && ( trackCount > 0 ) )
