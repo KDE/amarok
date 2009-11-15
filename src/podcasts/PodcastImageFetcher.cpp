10db843682f654745b00e65aef2cdffa35dd0d35
Bart Cerneels <bart.cerneels@kde.org>
2009-11-15 15:54:32 +0100

    Remove channel from the todo queue faster.

--- src/podcasts/PodcastImageFetcher.cpp
+++ src/podcasts/PodcastImageFetcher.cpp
@@ -92,6 +92,8 @@ PodcastImageFetcher::run()
         KIO::FileCopyJob *job =
                 KIO::file_copy( channel->imageUrl(), cachedPath,
                                 -1, KIO::HideProgressInfo );
+        //remove channel from the todo list
+        m_channels.removeAll( channel );
         m_jobChannelMap.insert( job, channel );
         connect( job, SIGNAL( finished( KJob * ) ), SLOT( slotDownloadFinished( KJob * ) ) );
     }
@@ -109,13 +111,10 @@ PodcastImageFetcher::slotDownloadFinished( KJob *job )
     if( channel.isNull() )
         return;
 
-    m_channels.removeAll( channel );
     if( job->error() )
-    {
         error() << "downloading podcast image " << job->errorString();
-    }
-
-    channel->setImage( QPixmap( cachedImagePath( channel ).toLocalFile() ) );
+    else
+        channel->setImage( QPixmap( cachedImagePath( channel ).toLocalFile() ) );
 
     //call run again to start the next batch of transfers.
     run();
