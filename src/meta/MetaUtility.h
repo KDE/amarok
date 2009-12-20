6e46ba17c8ca7180ff3db1bc3afa0cbaddb4c380
Maximilian Kossick <maximilian.kossick@googlemail.com>
2009-12-20 13:01:03 +0100

    moved constants to MetaConstants.h to make them usable in mocks

--- src/meta/MetaUtility.h
+++ src/meta/MetaUtility.h
@@ -19,6 +19,7 @@
 
 #include "amarok_export.h"
 #include "Meta.h"
+#include "meta/MetaConstants.h"
 
 #include <QMap>
 #include <QString>
@@ -63,36 +64,6 @@ namespace Meta
 
     namespace Field
     {
-        //actual string values are not final yet
-        static const QString ALBUM          = "xesam:album";
-        static const QString ARTIST         = "xesam:author";
-        static const QString BITRATE        = "xesam:audioBitrate";
-        static const QString BPM            = "xesam:audioBPM";
-        static const QString CODEC          = "xesam:audioCodec";
-        static const QString COMMENT        = "xesam:comment";
-        static const QString COMPOSER       = "xesam:composer";
-        static const QString DISCNUMBER     = "xesam:discNumber";
-        static const QString FILESIZE       = "xesam:size";
-        static const QString GENRE          = "xesam:genre";
-        static const QString LENGTH         = "xesam:mediaDuration";
-        static const QString RATING         = "xesam:userRating";
-        static const QString SAMPLERATE     = "xesam:audioSampleRate";
-        static const QString TITLE          = "xesam:title";
-        static const QString TRACKNUMBER    = "xesam:trackNumber";
-        static const QString URL            = "xesam:url";
-        static const QString YEAR           = "xesam:contentCreated";
-        static const QString ALBUMGAIN      = "xesam:albumGain";
-        static const QString ALBUMPEAKGAIN  = "xesam:albumPeakGain";
-        static const QString TRACKGAIN      = "xesam:trackGain";
-        static const QString TRACKPEAKGAIN  = "xesam:trackPeakGain";
-
-        static const QString SCORE          = "xesam:autoRating";
-        static const QString PLAYCOUNT      = "xesam:useCount";
-        static const QString FIRST_PLAYED   = "xesam:firstUsed";
-        static const QString LAST_PLAYED    = "xesam:lastUsed";
-
-        static const QString UNIQUEID       = "xesam:id";
-
 
         //deprecated
         AMAROK_EXPORT QVariantMap mapFromTrack( const Meta::TrackPtr track );
