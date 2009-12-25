00adb925ab6b951776b53507eb9d3f77c9b8178e
Maximilian Kossick <maximilian.kossick@googlemail.com>
2009-12-25 15:48:21 +0100

    more mock classes

---
+++ tests/collection/sqlcollection/SqlMountPointManagerMock.h
@@ -0,0 +1,55 @@
+/****************************************************************************************
+ * Copyright (c) 2009 Maximilian Kossick <maximilian.kossick@googlemail.com>       *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+#ifndef SQLMOUNTPOINTMANAGERMOCK_H
+#define SQLMOUNTPOINTMANAGERMOCK_H
+
+#include "SqlCollection.h"
+
+#include <QString>
+
+//Note: this class will probably break horribly on win32
+
+class SqlMountPointManagerMock : public SqlMountPointManager
+{
+public:
+    int getIdForUrl( const KUrl &url )
+    {
+        return -1;
+    }
+
+    QString getAbsolutePath ( const int deviceId, const QString& relativePath ) const
+    {
+        return relativePath.right( relativePath.length() -1 );
+    }
+
+    QString getRelativePath( const int deviceId, const QString& absolutePath ) const
+    {
+        return '.' + absolutePath;
+    }
+
+    IdList getMountedDeviceIds() const
+    {
+        return IdList();
+    }
+
+    QStringList collectionFolders()
+    {
+        return QStringList();
+    }
+};
+
+#endif // SQLMOUNTPOINTMANAGERMOCK_H
