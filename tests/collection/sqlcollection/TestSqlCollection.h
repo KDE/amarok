9d1269454998399079854f42865d9eb62a0ca533
Maximilian Kossick <maximilian.kossick@googlemail.com>
2009-12-26 11:59:54 +0100

    test whether SqlCollection reacts to MPM signals correctly. This test was actually written before the implementation in MPM/SqlCollection :)

--- tests/collection/sqlcollection/TestSqlCollection.h
+++ tests/collection/sqlcollection/TestSqlCollection.h
@@ -14,52 +14,40 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
-#ifndef SQLMOUNTPOINTMANAGERMOCK_H
-#define SQLMOUNTPOINTMANAGERMOCK_H
+#ifndef TESTSQLCOLLECTION_H
+#define TESTSQLCOLLECTION_H
 
-#include "SqlCollection.h"
+#include <QtTest/QtTest>
 
-#include <QString>
+#include <KTempDir>
 
-//Note: this class will probably break horribly on win32
+class SqlCollection;
+class SqlMountPointManagerMock;
+class SqlStorage;
 
-class SqlMountPointManagerMock : public SqlMountPointManager
+class TestSqlCollection : public QObject
 {
+    Q_OBJECT
+
 public:
-    int getIdForUrl( const KUrl &url )
-    {
-        return -1;
-    }
+    TestSqlCollection();
 
-    QString getAbsolutePath ( const int deviceId, const QString& relativePath ) const
-    {
-        return relativePath.right( relativePath.length() -1 );
-    }
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
 
-    QString getRelativePath( const int deviceId, const QString& absolutePath ) const
-    {
-        return '.' + absolutePath;
-    }
+    void testDeviceAddedWithTracks();
+    void testDeviceAddedWithoutTracks();
+    void testDeviceRemovedWithTracks();
+    void testDeviceRemovedWithoutTracks();
 
-    IdList getMountedDeviceIds() const
-    {
-        return IdList();
-    }
-
-    QStringList collectionFolders()
-    {
-        return QStringList();
-    }
-
-    void emitDeviceAdded( int id )
-    {
-        emit deviceAdded( id );
-    }
-
-    void emitDeviceRemoved( int id )
-    {
-        emit deviceRemoved( id );
-    }
+private:
+    SqlCollection *m_collection;
+    SqlMountPointManagerMock *m_mpmMock;
+    SqlStorage *m_storage;
+    KTempDir *m_tmpDir;
 };
 
-#endif // SQLMOUNTPOINTMANAGERMOCK_H
+QTEST_MAIN( TestSqlCollection )
+
+#endif // TESTSQLCOLLECTION_H
