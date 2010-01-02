b8f750d7b5614b6a7588761f4a0811f4dc166786
Rick W. Chen <stuffcorpse@archlinux.us>
2010-01-03 02:18:22 +1300

    cover fetch queue: move CoverFetchPayload to where it belongs

--- src/covermanager/CoverFetchUnit.cpp
+++ src/covermanager/CoverFetchUnit.cpp
@@ -15,7 +15,11 @@
  ****************************************************************************************/
 
 #include "CoverFetchUnit.h"
-#include "CoverFetchQueue.h"
+#include "Debug.h"
+
+#include <QDomNodeList>
+#include <QRegExp>
+#include <QSet>
 
 /*
  * CoverFetchUnit
@@ -117,3 +121,186 @@ bool CoverFetchUnit::operator!=( const CoverFetchUnit &other ) const
 {
     return !( *this == other );
 }
+
+/*
+ * CoverFetchPayload
+ */
+
+CoverFetchPayload::CoverFetchPayload( const Meta::AlbumPtr album, CoverFetchPayload::Type type )
+    : m_album( album )
+    , m_method( album->hasAlbumArtist() ? QString( "album.getinfo" ) : QString( "album.search" ) )
+    , m_type( type )
+{
+}
+
+CoverFetchPayload::~CoverFetchPayload()
+{
+}
+
+CoverFetchPayload::Type
+CoverFetchPayload::type() const
+{
+    return m_type;
+}
+
+const KUrl::List &
+CoverFetchPayload::urls() const
+{
+    return m_urls;
+}
+
+bool
+CoverFetchPayload::isPrepared() const
+{
+    return !m_urls.isEmpty();
+}
+
+/*
+ * CoverFetchInfoPayload
+ */
+
+
+CoverFetchInfoPayload::CoverFetchInfoPayload( const Meta::AlbumPtr album )
+    : CoverFetchPayload( album, CoverFetchPayload::INFO )
+{
+    prepareUrls();
+}
+
+CoverFetchInfoPayload::~CoverFetchInfoPayload()
+{
+}
+
+void
+CoverFetchInfoPayload::prepareUrls()
+{
+    KUrl url;
+    url.setScheme( "http" );
+    url.setHost( "ws.audioscrobbler.com" );
+    url.setPath( "/2.0/" );
+    url.addQueryItem( "api_key", "402d3ca8e9bc9d3cf9b85e1202944ca5" );
+    url.addQueryItem( "album", album()->name() );
+
+    if( album()->hasAlbumArtist() )
+    {
+        url.addQueryItem( "artist", album()->albumArtist()->name() );
+    }
+    url.addQueryItem( "method", method() );
+
+    m_urls.append( url );
+}
+
+/*
+ * CoverFetchArtPayload
+ */
+
+CoverFetchArtPayload::CoverFetchArtPayload( const Meta::AlbumPtr album )
+    : CoverFetchPayload( album, CoverFetchPayload::ART )
+{
+}
+
+CoverFetchArtPayload::~CoverFetchArtPayload()
+{
+}
+
+void
+CoverFetchArtPayload::setXml( const QByteArray &xml )
+{
+    m_xml = QString::fromUtf8( xml );
+    prepareUrls();
+}
+
+void
+CoverFetchArtPayload::prepareUrls()
+{
+    QDomDocument doc;
+    if( !doc.setContent( m_xml ) )
+    {
+        debug() << "The xml obtained from Last.fm is invalid.";
+        return;
+    }
+
+    QString albumArtist;
+    QDomNodeList results;
+    QSet< QString > artistSet;
+
+    const QString &searchMethod = method();
+
+    if( searchMethod == "album.getinfo" )
+    {
+        results = doc.documentElement().childNodes();
+        albumArtist = normalize( album()->albumArtist()->name() );
+    }
+    else if( searchMethod == "album.search" )
+    {
+        results = doc.documentElement().namedItem( "results" ).namedItem( "albummatches" ).childNodes();
+
+        const Meta::TrackList tracks = album()->tracks();
+        QStringList artistNames( "Various Artists" );
+        foreach( const Meta::TrackPtr &track, tracks )
+        {
+            artistNames << track->artist()->name();
+        }
+        artistSet = normalize( artistNames ).toSet();
+    }
+    else return;
+
+    for( uint x = 0, len = results.length(); x < len; ++x )
+    {
+        const QDomNode albumNode = results.item( x );
+        const QString artist = normalize( albumNode.namedItem( "artist" ).toElement().text() );
+
+        if( searchMethod == "album.getinfo" && artist != albumArtist )
+            continue;
+        else if( searchMethod == "album.search" && !artistSet.contains( artist ) )
+            continue;
+
+        const QDomNodeList list = albumNode.childNodes();
+        for( int i = 0, count = list.count(); i < count; ++i )
+        {
+            const QDomNode &node = list.item( i );
+            if( node.nodeName() == "image" && node.hasAttributes() )
+            {
+                const QString imageSize = node.attributes().namedItem( "size" ).nodeValue();
+                if( node.isElement() && imageSize == coverSize( ExtraLarge ) )
+                {
+                    const KUrl url( node.toElement().text() );
+                    if( url.isValid() )
+                        m_urls.append( url );
+                }
+            }
+        }
+    }
+}
+
+QString
+CoverFetchArtPayload::coverSize( enum CoverSize size ) const
+{
+    QString str;
+    switch( size )
+    {
+    case Small:  str = "small";      break;
+    case Medium: str = "medium";     break;
+    case Large:  str = "large";      break;
+    default:     str = "extralarge"; break;
+    }
+    return str;
+}
+
+QString
+CoverFetchArtPayload::normalize( const QString &raw )
+{
+    const QRegExp spaceRegExp  = QRegExp( "\\s" );
+    return raw.toLower().remove( spaceRegExp ).normalized( QString::NormalizationForm_KC );
+}
+
+QStringList
+CoverFetchArtPayload::normalize( const QStringList &rawList )
+{
+    QStringList cooked;
+    foreach( const QString &raw, rawList )
+    {
+        cooked << normalize( raw );
+    }
+    return cooked;
+}
+
--- src/covermanager/CoverFetchUnit.h
+++ src/covermanager/CoverFetchUnit.h
@@ -56,4 +56,90 @@ private:
     const CoverFetchPayload *m_url;
 };
 
+/**
+ * An abstract class for preparing URLs suitable for fetching album covers from
+ * Last.fm.
+ */
+class CoverFetchPayload
+{
+public:
+    enum Type { INFO, ART };
+    CoverFetchPayload( const Meta::AlbumPtr album, enum Type type );
+    virtual ~CoverFetchPayload();
+
+    enum Type type() const;
+    const KUrl::List &urls() const;
+
+protected:
+    KUrl::List m_urls;
+
+    const QString &method() const { return m_method; }
+    Meta::AlbumPtr album()  const { return m_album; }
+
+    bool isPrepared() const;
+    virtual void prepareUrls() = 0;
+
+private:
+    Meta::AlbumPtr m_album;
+    const QString  m_method;
+    enum Type      m_type;
+
+    Q_DISABLE_COPY( CoverFetchPayload );
+};
+
+/**
+ * Prepares URL suitable for getting an album's info as an CoverFetchInfoPayload from Last.fm.
+ */
+class CoverFetchInfoPayload : public CoverFetchPayload
+{
+public:
+    explicit CoverFetchInfoPayload( const Meta::AlbumPtr album );
+    ~CoverFetchInfoPayload();
+
+protected:
+    void prepareUrls();
+
+private:
+    Q_DISABLE_COPY( CoverFetchInfoPayload );
+};
+
+/**
+ * Prepares URL suitable for getting an album's cover from Last.fm.
+ */
+class CoverFetchArtPayload : public CoverFetchPayload
+{
+public:
+    explicit CoverFetchArtPayload( const Meta::AlbumPtr album );
+    ~CoverFetchArtPayload();
+
+    void setXml( const QByteArray &xml );
+
+protected:
+    void prepareUrls();
+
+private:
+    QString m_xml;
+
+    /// Available album cover sizes in Last.fm's api
+    enum CoverSize
+    {
+        Small = 0,  //! 34px
+        Medium,     //! 64px
+        Large,      //! 128px
+        ExtraLarge  //! 300px
+    };
+
+    /// convert CoverSize enum to string
+    QString coverSize( enum CoverSize size ) const;
+
+    /// lower, remove whitespace, and do Unicode normalization on a QString
+    QString normalize( const QString &raw );
+
+    /// lower, remove whitespace, and do Unicode normalization on a QStringList
+    QStringList normalize( const QStringList &rawList );
+
+    Q_DISABLE_COPY( CoverFetchArtPayload );
+};
+
+
 #endif /* AMAROK_COVERFETCHUNIT_H */
