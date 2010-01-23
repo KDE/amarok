c7e6ca451a1a0adc0c707b20fbb85198383275d9
Mark Kretschmann <kretschmann@kde.org>
2010-01-23 08:30:46 +0100

    Move APP_VERSION to separate file Version.h.
    This fixes the issues with amarokcollectionscanner (which is Qt-only)
    pulling in KDE stuff via Amarok.h. Also seems more elegant to me in general.
    
    Will also adjust our release script accordingly.

---
+++ Version.h
@@ -0,0 +1,29 @@
+/****************************************************************************************
+ * Copyright (c) 2010 Mark Kretschmann <kretschmann@kde.org>                            *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+#ifndef AMAROK_VERSION_H
+#define AMAROK_VERSION_H
+
+
+/// Update this when necessary
+//#ifdef CURRENT_GIT_VERSION
+//#define APP_VERSION #CURRENT_GIT_VERSION
+//#else
+#define APP_VERSION "2.2-GIT"
+//#endif
+
+
+#endif  // End include guard
--- utilities/collectionscanner/CollectionScanner.cpp
+++ utilities/collectionscanner/CollectionScanner.cpp
@@ -21,7 +21,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
  ***************************************************************************/
 
-#include "Amarok.h"  // for APP_VERSION
+#include "Version.h"  // for APP_VERSION
 #include "CollectionScanner.h"
 #include "AFTUtility.h"
 
