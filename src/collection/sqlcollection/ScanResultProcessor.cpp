2e58e40975a0730ea1d2d39107654469c64a0bf0
Mark Kretschmann <kretschmann@kde.org>
2010-01-25 12:03:37 +0100

    Remove feature: Scanner looking for "folder.png".
    As it turned out, the whole thing was a misunderstanding, and the user
    meant something completely different. So my patch was useless.
    
    @jefferai: Could you please check my commit for correctness again?
    (I already screwed up two things today, that's enough;)

--- src/collection/sqlcollection/ScanResultProcessor.cpp
+++ src/collection/sqlcollection/ScanResultProcessor.cpp
@@ -183,7 +183,7 @@ ScanResultProcessor::findBestImagePath( const QList<QString> &paths )
     //DEBUG_BLOCK
     QStringList files;
 
-    int goodnessPriority = 4;
+    int goodnessPriority = 3;
     QString goodPath;
     foreach( const QString &path, paths )
     {
@@ -215,17 +215,6 @@ ScanResultProcessor::findBestImagePath( const QList<QString> &paths )
             if( goodnessPriority > 2 )
             {
                 goodnessPriority = 2;
-                goodPath = path;
-            }
-        }
-
-        //next: try "folder" (some applications apparently use this)
-        if( file.contains( "folder", Qt::CaseInsensitive ) ||
-                file.contains( i18nc( "(Front) Cover of an album", "folder" ), Qt::CaseInsensitive ) )
-        {
-            if( goodnessPriority > 3 )
-            {
-                goodnessPriority = 3;
                 goodPath = path;
             }
         }
