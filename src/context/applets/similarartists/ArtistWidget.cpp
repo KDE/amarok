32294d532c749b0d4b16219a63ae041e792bb66e
Jof <jclavel@clabert.info>
2010-01-27 23:49:45 +0100

    Improve the display of similar artists applet and add the topTrack info for each artist.

--- src/context/applets/similarartists/ArtistWidget.cpp
+++ src/context/applets/similarartists/ArtistWidget.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************************
- * Copyright (c) 2009 Joffrey Clavel <jclavel@clabert.info>                             *
+ * Copyright (c) 2009-2010 Joffrey Clavel <jclavel@clabert.info>                             *
  * Copyright (c) 2010 Alexandre Mendes <alex.mendes1988@gmail.com>                      *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
@@ -27,12 +27,15 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QDesktopServices>
+#include <QDomElement> // for parse the topTrack xml
 
 ArtistWidget::ArtistWidget(QWidget *parent) : QWidget(parent)
 {
 
+    // set a fixed size for all widget, for harmonize the similar artists applet display
     this->setMinimumHeight(105);
     this->setMaximumHeight(105);
+    
     m_layout=new QGridLayout(this);
 
     this->setAttribute(Qt::WA_TranslucentBackground, true); // The background og this widget is transparent
@@ -49,10 +52,15 @@ ArtistWidget::ArtistWidget(QWidget *parent) : QWidget(parent)
     m_genre->setAttribute( Qt::WA_TranslucentBackground, true); // The background of the QLabel is transparent
     m_genre->setAlignment(Qt::AlignCenter);
 
+    m_topTrack=new QLabel( this );
+    m_topTrack->setAttribute( Qt::WA_TranslucentBackground, true); // The background of the QLabel is transparent
+    m_topTrack->setAlignment(Qt::AlignCenter);
+
 
     m_layout->addWidget(m_image,0,0,2,1); // the image display is extended on two row
     m_layout->addWidget(m_name,0,1);
-    m_layout->addWidget(m_genre,1,1);
+    m_layout->addWidget(m_genre,0,2);
+    m_layout->addWidget(m_topTrack,1,1,1,2);
 
     // open the url of the similar artist when his name is clicked
     connect(m_name,SIGNAL(linkActivated(QString)),this,SLOT(openUrl(QString)));
@@ -65,7 +73,9 @@ ArtistWidget::~ArtistWidget()
      delete m_image;
      delete m_name;
      delete m_genre;
+     delete m_topTrack;
      delete m_imageJob;
+     delete m_topTrackJob;
 }
 
 
@@ -149,7 +159,72 @@ void
 ArtistWidget::setArtist( const QString &nom, const KUrl &url) {
     DEBUG_BLOCK
     m_name->setText("<a href='" + url.url() + "'>" + nom +"</a>");
+    m_topTrack->setText(i18n("TopTrack") + " : " + i18n("loading..."));
+
+    // ask for the top tracks of this artist
+    QUrl urlTmp;
+    urlTmp.setScheme( "http" );
+    urlTmp.setHost( "ws.audioscrobbler.com" );
+    urlTmp.setPath( "/2.0/" );
+    urlTmp.addQueryItem( "method", "artist.gettoptracks" );
+    urlTmp.addQueryItem( "api_key", "402d3ca8e9bc9d3cf9b85e1202944ca5" );
+    urlTmp.addQueryItem( "artist", nom.toLocal8Bit() );
+    
+    m_topTrackJob = KIO::storedGet( urlTmp, KIO::NoReload, KIO::HideProgressInfo );
+    connect( m_topTrackJob, SIGNAL(result( KJob* )), SLOT(setTopTrack( KJob* ) ));
 }
+
+
+void ArtistWidget::setTopTrack(KJob* job)
+{
+    QString xml;
+    
+    if( !m_topTrackJob ) return; //track changed while we were fetching
+
+    // It's the correct job but it errored out
+    if( job->error() != KJob::NoError && job == m_topTrackJob )
+    {
+        // probably we haven't access to internet
+        // we make nothing
+        return;
+    }
+
+    // not the right job, so let's ignore it
+    if( job != m_topTrackJob)
+        return;
+
+    if( job )
+    {
+        KIO::StoredTransferJob* const storedJob = static_cast<KIO::StoredTransferJob*>( job );
+        xml = QString::fromUtf8( storedJob->data().data(), storedJob->data().size() );
+    }
+    else
+    {
+        return;
+    }
+
+    QDomDocument doc;
+    doc.setContent( xml );
+
+    // we get only the first top track
+    const QDomNode events = doc.documentElement().namedItem( "toptracks" );
+
+    QDomNode n = events.firstChild();
+
+
+    QDomNode nameNode = n.namedItem( "name" );
+    QDomElement eName = nameNode.toElement();
+    QString name;
+    if( !eName.isNull() )
+    {
+        name = eName.text();
+    }
+    
+
+        m_topTrack->setText(i18n("TopTrack") + " : " + name);
+    m_topTrackJob=0;
+}
+
 
  /**
   * Change the match pourcentage of the artist
@@ -168,6 +243,7 @@ ArtistWidget::clear() {
     m_image->clear();
     m_name->clear();
     m_genre->clear();
+    m_topTrack->clear();
 }
 
 
