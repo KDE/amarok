beb2a632fe2574dccb377c2b99b0776674069a41
Casey Link <unnamedrambler@gmail.com>
2010-02-14 14:25:55 -0500

    Add copyright to files i've modified significantly recently

--- src/browsers/filebrowser/FileView.cpp
+++ src/browsers/filebrowser/FileView.cpp
@@ -1,5 +1,6 @@
 /****************************************************************************************
  * Copyright (c) 2010 Nikolaj Hald Nielsen <nhn@kde.org>                                *
+ * Copyright (c) 2010 Casey Link <unnamedrambler@gmail.com>                             *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
  * the terms of the GNU General Public License as published by the Free Software        *
