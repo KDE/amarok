416801bfe0066f3fd2ff4914ad665c4091564c17
Nikolaj Hald Nielsen <nhn@kde.org>
2010-02-18 09:14:31 +0100

    Fix bad logic causing "Multiple background tasks running" to still be shown in the status bar, even when all but one of the multiple running jobs had finished. Also remove a bit of redundat code.
    ChangeLog++

--- src/statusbar/CompoundProgressBar.cpp
+++ src/statusbar/CompoundProgressBar.cpp
@@ -140,16 +140,17 @@ void CompoundProgressBar::childBarFinished( ProgressBar *bar )
 
     if( m_progressMap.count() == 1 )
     {
+        //only one job still running, so no need to use the details widget any more. Also set the text to the description of
+        //the job instead of the "Multiple background tasks running" text.
         setDescription( m_progressMap.values().at( 0 )->descriptionLabel()->text() );
         cancelButton()->setToolTip( i18n( "Abort" ) );
+        hideDetails();
     }
-
-    if( m_progressMap.empty() )
+    else if( m_progressMap.empty() )
     {
         progressBar()->setValue( 0 );
         hideDetails();
         emit( allDone() );
-        m_progressDetailsWidget->hide();
         return;
     }
     else
@@ -159,8 +160,6 @@ void CompoundProgressBar::childBarFinished( ProgressBar *bar )
     }
 
     progressBar()->setValue( calcCompoundPercentage() );
-
-    handleDetailsButton();
 }
 
 int CompoundProgressBar::calcCompoundPercentage()
@@ -212,6 +211,9 @@ void CompoundProgressBar::toggleDetails()
 
 void CompoundProgressBar::handleDetailsButton()
 {
+    if( m_progressMap.count() > 1 )    
         m_showDetailsButton->show();
+    else
+        m_showDetailsButton->hide();
 }
 
