90b9f9a4ea0e4bb4aab875bea2ac51fa2d4b1924
manu <manu@ubuntu.(none)>
2010-03-01 19:08:46 +0100

    new file:   CMakeLists.txt 	new file:   CoverBlingApplet.cpp 	new file:   CoverBlingApplet.h 	new file:   amarok-context-applet-coverbling.desktop 	new file:   blingdefaultcover.png 	new file:   pictureflow.cpp 	new file:   pictureflow.h

---
+++ src/context/applets/coverbling/CoverBlingApplet.cpp
@@ -0,0 +1,160 @@
+/****************************************************************************************
+ * Copyright (c) 2009 Simon Esneault <simon.esneault@gmail.com>                         *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+#include "CoverBlingApplet.h"
+
+// Amarok
+#include "Amarok.h"
+#include "Debug.h"
+#include "context/ContextView.h"
+#include "context/widgets/TextScrollingWidget.h"
+#include "collection/Collection.h"
+#include "collection/CollectionManager.h"
+#include "context/widgets/RatingWidget.h"
+// KDE
+#include <KAction>
+#include <KColorScheme>
+#include <KConfigDialog>
+#include <KGlobalSettings>
+#include <Plasma/BusyWidget>
+#include <Plasma/IconWidget>
+#include <Plasma/Theme>
+
+// Qt
+#include <QGraphicsLinearLayout>
+#include <QGraphicsProxyWidget>
+#include <QGraphicsSimpleTextItem>
+#include <QGraphicsWidget>
+#include <QGraphicsProxyWidget>
+#include <QLabel>
+
+#define DEBUG_PREFIX "PotosApplet"
+
+QStringList findFiles(const QString& path = QString())
+{
+  QStringList files;
+
+  QDir dir = QDir::current();
+  if(!path.isEmpty())
+    dir = QDir(path);
+
+  dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+  QFileInfoList list = dir.entryInfoList();
+  for (int i = 0; i < list.size(); ++i) 
+  {
+    QFileInfo fileInfo = list.at(i);
+    files.append(dir.absoluteFilePath(fileInfo.fileName()));
+  }
+  return files;
+}
+
+CoverBlingApplet::CoverBlingApplet( QObject* parent, const QVariantList& args )
+    : Context::Applet( parent, args )
+{
+    DEBUG_BLOCK
+    setHasConfigurationInterface( false );
+}
+
+void 
+CoverBlingApplet::init()
+{
+    setBackgroundHints( Plasma::Applet::NoBackground );
+
+    m_vertical_size = 300;
+    m_horizontal_size = 500;
+
+    resize( m_horizontal_size,m_vertical_size);
+   
+    m_layout = new QGraphicsProxyWidget(this);
+    m_pictureflow = new PictureFlow();
+    m_layout->setWidget(m_pictureflow);
+    QSize slideSize(150,150);
+    m_pictureflow->setSlideSize(slideSize);
+    m_pictureflow->show();    
+    Amarok::Collection *coll = CollectionManager::instance()->primaryCollection();
+    QueryMaker *qm = coll->queryMaker();
+    qm->setAutoDelete( true );
+    qm->setQueryType( QueryMaker::Album );
+    qm->orderBy( Meta::valArtist );
+
+    connect( qm, SIGNAL( newResultReady( QString, Meta::AlbumList ) ),
+             this, SLOT( slotAlbumQueryResult( QString, Meta::AlbumList ) ) );
+    connect( qm, SIGNAL(queryDone()), this, SLOT( slideChanged(1)));
+    connect(m_pictureflow, SIGNAL(centerIndexChanged(int)),this,SLOT(slideChanged(int)));
+    connect(m_pictureflow,SIGNAL(doubleClicked(int)),this,SLOT(playAlbum(int)));
+    qm->run();
+    m_label = new QGraphicsSimpleTextItem(this);
+    QBrush brush = KColorScheme( QPalette::Active ).foreground( KColorScheme::NormalText );
+    m_label ->setBrush( brush );
+    QFont labelFont;
+    QFont bigFont( labelFont );
+    bigFont.setPointSize( bigFont.pointSize() +  2 );
+    m_label->setFont( labelFont );
+    m_label ->setPos(m_horizontal_size/2,m_vertical_size-50);
+    // pour le rating
+    m_ratingWidget = new RatingWidget( this );
+    m_ratingWidget->setSpacing( 2 );
+    //connect( m_ratingWidget, SIGNAL( ratingChanged( int ) ), SLOT( changeTrackRating( int ) ) );
+    m_ratingWidget->setPos(m_horizontal_size/2,m_vertical_size-30);
+    m_ratingWidget->setRating(0);
+}
+
+CoverBlingApplet::~CoverBlingApplet()
+{
+    DEBUG_BLOCK
+}
+void CoverBlingApplet::slotAlbumQueryResult( QString collectionId, Meta::AlbumList albums) //SLOT
+{
+    DEBUG_BLOCK
+    Q_UNUSED( collectionId );
+    foreach( Meta::AlbumPtr album, albums )
+        m_pictureflow->addAlbum(album);
+}
+void CoverBlingApplet::slideChanged(int islideindex)
+{
+	Meta::AlbumPtr album = m_pictureflow->getAlbum(islideindex);
+	if (album)
+	{
+		Meta::ArtistPtr artist = album->albumArtist();
+		QString label = album->prettyName();
+		if (artist) label+= " - " + artist->prettyName();
+		m_label->setText(label);
+		m_label->show();
+	}
+}
+void CoverBlingApplet::playAlbum(int islideindex)
+{
+}
+void CoverBlingApplet::FillFlow(PictureFlow* iPictureFlow)
+{
+  QStringList files = findFiles("/home/manu/.kde/share/apps/amarok/albumcovers/large");
+  QImage img;
+  for(int i = 0; i < (int)files.count(); i++)
+    if(img.load(files[i]))
+     iPictureFlow->addSlide(img);
+}
+void 
+CoverBlingApplet::paintInterface( QPainter *p, const QStyleOptionGraphicsItem *option, const QRect &contentsRect )
+{
+    Q_UNUSED( p );
+    Q_UNUSED( option );
+    Q_UNUSED( contentsRect );
+    p->setRenderHint( QPainter::Antialiasing );
+    // tint the whole applet
+    addGradientToAppletBackground( p );
+}
+#include "CoverBlingApplet.moc"
+
---
+++ src/context/applets/coverbling/CoverBlingApplet.h
@@ -0,0 +1,73 @@
+/****************************************************************************************
+ * Copyright (c) 2009 Simon Esneault <simon.esneault@gmail.com>                         *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+//Plasma applet for showing photos from flickr
+
+#ifndef COVERBLING_APPLET_H
+#define COVERBLING_APPLET_H
+
+#include "context/Applet.h"
+#include "context/DataEngine.h"
+#include "EngineObserver.h"
+
+#include "../../engines/photos/PhotosInfo.h"
+
+#include "pictureflow.h"
+#include <QHBoxLayout>
+
+class TextScrollingWidget;
+class KConfigDialog;
+class PhotosScrollWidget;
+class QGraphicsSimpleTextItem;
+class QGraphicsProxyWidget;
+class RatingWidget;
+
+namespace Plasma
+{
+    class IconWidget;
+}
+
+ /** CoverBlingApplet will display photos from the Internet, relative to the current playing song
+   */
+class CoverBlingApplet : public Context::Applet, public EngineObserver
+{
+        Q_OBJECT
+
+    public:
+        CoverBlingApplet( QObject* parent, const QVariantList& args );
+        ~CoverBlingApplet();
+
+        void    init();
+        void    paintInterface( QPainter *painter, const QStyleOptionGraphicsItem *option, const QRect &contentsRect );
+        
+    public slots:
+	void 	slotAlbumQueryResult( QString collectionId, Meta::AlbumList albums);
+	void	slideChanged(int islideindex);
+        void	playAlbum(int islideindex);
+    private:
+	PictureFlow * m_pictureflow;
+	void FillFlow(PictureFlow* iPictureFlow);
+	QGraphicsProxyWidget * m_layout;
+	RatingWidget* m_ratingWidget;
+	QGraphicsSimpleTextItem* m_label;
+	int m_horizontal_size;
+	int m_vertical_size;
+};
+
+Q_DECLARE_METATYPE ( QList < PhotosInfo * > )
+K_EXPORT_AMAROK_APPLET( coverbling, CoverBlingApplet )
+
+#endif /* COVERBLING_APPLET_H */
