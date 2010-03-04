7f6fe35fd7bfdee3ba6b3d4ffe9ab67cd72c5316
Nanno Langstraat <langstr@gmail.com>
2010-03-04 16:17:23 +0100

    Playlist: random sort: use item id instead of row number as key for the random hash.
    This keeps the random playlist sort order stable across playlist item insertion/removal.

--- src/playlist/proxymodels/SortAlgorithms.cpp
+++ src/playlist/proxymodels/SortAlgorithms.cpp
@@ -26,7 +26,7 @@ void
 multilevelLessThan::setSortScheme( const SortScheme & scheme )
 {
     m_scheme = scheme;
-    m_randomSalt = qrand();
+    m_randomSalt = qrand();    //! Do a different random sort order every time.
 }
 
 bool
@@ -41,8 +41,8 @@ multilevelLessThan::operator()( const QAbstractItemModel* sourceModel, int sourc
 
         if( currentCategory == -1 ) //random
         {
-            long randomSeqnumA = constantRandomSeqnumForRow( sourceModelRowA );
-            long randomSeqnumB = constantRandomSeqnumForRow( sourceModelRowB );
+            long randomSeqnumA = constantRandomSeqnumForRow( sourceModel, sourceModelRowA );
+            long randomSeqnumB = constantRandomSeqnumForRow( sourceModel, sourceModelRowB );
 
             if( randomSeqnumA < randomSeqnumB )
             {   decided = true;  verdict = true;   }
@@ -110,18 +110,61 @@ multilevelLessThan::operator()( const QAbstractItemModel* sourceModel, int sourc
     return verdict;
 }
 
+
+// If the 'qrand()' save+restore ever turns out to be a performance bottleneck: try
+// switching to 'jrand48()', which has no common random pool and therefore doesn't have
+// to be saved+restored.
+//
+// I chose qrand() because I'm not sure about 'jrand48()' portability.
+typedef uint randomSeedType;    // For multilevelLessThan::constantRandomSeqnumForRow() 'qsrand()'
+
 long
-multilevelLessThan::constantRandomSeqnumForRow(int sourceRow) const
+multilevelLessThan::constantRandomSeqnumForRow( const QAbstractItemModel* sourceModel, int sourceModelRow ) const
 {
-    // If the 'seed = qrand(); qsrand( seed )' save+restore ever turns out to be a
-    // performance bottleneck: try switching to 'jrand48()', which has no common
-    // random pool and therefore doesn't have to be saved+restored.
-    int seed = qrand();
+    randomSeedType randomSeedForItem;
+    unsigned char *randomSeedForItem_bytes = (unsigned char*)( &randomSeedForItem );
+    memset( randomSeedForItem_bytes, 0x00, sizeof(randomSeedType) );
 
-    qsrand( sourceRow ^ m_randomSalt );    // Ensure we get the same random number for a given row every time
-    long randomSeqnum = qrand();    // qrand() is int; long to allow switch to 'jrand48()'.
 
-    qsrand( seed );    // Restore non-predictability for the rest of Amarok
+    // Use the playlist item id as the fixed key for the random generator.
+    //   This has all the properties we need:
+    //     - unique
+    //     - stable
+    //
+    //   Note that we do *NOT* assume the playlist item ids to be random. That happens
+    //   to be the case in Amarok v2.3, but we work just as well if an item id is e.g.
+    //   a C pointer or a linear number.
+    //
+    QModelIndex index = sourceModel->index( sourceModelRow, 0 );
+    quint64 id = index.data( UniqueIdRole ).value<quint64>();
+
+    const unsigned char *key = (const unsigned char *)( &id );
+    unsigned int keyLen = sizeof(id);
+
+
+    // Don't make any assumptions about the structure of the item key: treat it as bytes.
+    for ( unsigned int i = 0; i < keyLen; i++ )
+        randomSeedForItem_bytes[ i % sizeof(randomSeedType) ] ^= key[ i ];
+
+
+    // Mix in our salt, to get a different sort order from run to run.
+    const unsigned char *salt = (const unsigned char *)( &m_randomSalt );
+    for ( unsigned int i = 0; i < sizeof(m_randomSalt); i++ )
+        randomSeedForItem_bytes[ i % sizeof(randomSeedType) ] ^= salt[ i ];
+
+
+    // Generate the fixed sequence number based on the fixed key:
+
+    //   1. Save current non-predictable randomness.
+    int globalSeed = qrand();
+
+    //   2. (Ab)use the random generator as a hash function.
+    qsrand( randomSeedForItem );    // Ensure we get the same random number for a given item every time
+    long randomSeqnum = qrand();    // qrand() actually returns 'int'; use 'long' to allow switch to 'jrand48()'.
+
+    //   3. Restore non-predictability for the rest of Amarok.
+    qsrand( globalSeed );
+
 
     return randomSeqnum;
 }
