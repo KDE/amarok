899064fd1cbc2b9b8c77966a523b79108d0e62b0
Lydia Pintscher <lydia@kde.org>
2010-03-10 23:45:35 +0100

    bump plug-in version for 2.3.0

--- src/PluginManager.h
+++ src/PluginManager.h
@@ -31,7 +31,7 @@ class PluginManager
 {
     public:
         /** Bump this number whenever the plugin framework gets incompatible with older versions */
-        static const int FrameworkVersion = 53;
+        static const int FrameworkVersion = 54;
 
         /**
          * It will return a list of services that match your
