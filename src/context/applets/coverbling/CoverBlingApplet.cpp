6344b2ad141b905ec5cf3505bd0f50f2dff9c802
Mark Kretschmann <kretschmann@kde.org>
2010-03-17 18:30:29 +0100

    Add tooltips for buttons in CoverBling applet.

--- src/context/applets/coverbling/CoverBlingApplet.cpp
+++ src/context/applets/coverbling/CoverBlingApplet.cpp
@@ -118,26 +118,32 @@ CoverBlingApplet::init()
     m_blingtofirst = new Plasma::IconWidget( this );
     m_blingtofirst->setIcon( KStandardDirs::locate( "data", "amarok/images/blingtofirst.png" ) );
     m_blingtofirst->setMaximumSize( 16.0, 16.0 );
+    m_blingtofirst->setToolTip( i18n( "Jump to First" ) );
 
     m_blingtolast = new Plasma::IconWidget( this );
     m_blingtolast->setIcon( KStandardDirs::locate( "data", "amarok/images/blingtolast.png" ) );
     m_blingtolast->setMaximumSize( 16.0, 16.0 );
+    m_blingtolast->setToolTip( i18n( "Jump to Last" ) );
 
     m_blingfastback = new Plasma::IconWidget( this );
     m_blingfastback->setIcon( KStandardDirs::locate( "data", "amarok/images/blingfastback.png" ) );
     m_blingfastback->setMaximumSize( 16.0, 16.0 );
+    m_blingfastback->setToolTip( i18n( "Backward" ) );
 
     m_blingfastforward = new Plasma::IconWidget( this );
     m_blingfastforward->setIcon( KStandardDirs::locate( "data", "amarok/images/blingfastforward.png" ) );
     m_blingfastforward->setMaximumSize( 16.0, 16.0 );
+    m_blingfastforward->setToolTip( i18n( "Forward" ) );
 
     m_fullscreen = new Plasma::IconWidget( this );
     m_fullscreen->setIcon( KStandardDirs::locate( "data", "amarok/images/blingfullscreen.png" ) );
     m_fullscreen->setMaximumSize( 16.0, 16.0 );
+    m_fullscreen->setToolTip( i18n( "Maximize/Minimize" ) );
 
     m_jumptoplaying = new Plasma::IconWidget( this );
     m_jumptoplaying->setIcon( KStandardDirs::locate( "data", "amarok/images/blingjumptoplaying.png" ) );
     m_jumptoplaying->setMaximumSize( 16.0, 16.0 );
+    m_jumptoplaying->setToolTip( i18n( "Junp to Current" ) );
 
     connect( m_blingtofirst, SIGNAL( clicked() ), this, SLOT( skipToFirst() ) );
     connect( m_blingtolast, SIGNAL( clicked() ), this, SLOT( skipToLast() ) );
