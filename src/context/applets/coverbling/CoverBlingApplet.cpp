2fbce230e14acf69c95caa6e6c2751b6be1cffbf
Mark Kretschmann <kretschmann@kde.org>
2010-03-17 09:21:13 +0100

    Fix copyright headers.

--- src/context/applets/coverbling/CoverBlingApplet.cpp
+++ src/context/applets/coverbling/CoverBlingApplet.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************************
- * Copyright (c) 2010 Emmanuel Wagner <manu.wagner@sfr.fr>                         *
+ * Copyright (c) 2010 Emmanuel Wagner <manu.wagner@sfr.fr>                              *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
  * the terms of the GNU General Public License as published by the Free Software        *
