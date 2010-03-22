e1107f8c81c7a385bf0b6ca5d2186a959cc1755f
Mark Kretschmann <kretschmann@kde.org>
2010-03-22 12:50:29 +0100

    Temporarily disable Gapless Playback, for finding a bug.
    According to user reports, Gapless doesn't really work correctly anyway,
    so I reckon that disabling it for now doesn't really hurt much.

--- src/EngineController.cpp
+++ src/EngineController.cpp
@@ -172,10 +172,14 @@ EngineController::initializePhonon()
     connect( m_controller, SIGNAL( titleChanged( int ) ), SLOT( slotTitleChanged( int ) ) );
 
 
+    // Gapless Playback is temporarily disabled. Reason: Trying to find a bug in this code that makes
+    // the EngineController stop after each track, with all Phonon backends except the xine backend.
+#if 0
     if( AmarokConfig::trackDelayLength() > -1 )
         m_media->setTransitionTime( AmarokConfig::trackDelayLength() ); // Also Handles gapless.
     else if( AmarokConfig::crossfadeLength() > 0 )  // TODO: Handle the possible options on when to crossfade.. the values are not documented anywhere however
         m_media->setTransitionTime( -AmarokConfig::crossfadeLength() );
+#endif
 }
 
 
