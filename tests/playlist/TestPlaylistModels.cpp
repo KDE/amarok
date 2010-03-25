2cd9be48f0e0c260ed2f5bfc0d636ad7ce858cfc
Nikolaj Hald Nielsen <nhn@kde.org>
2010-03-25 11:43:00 +0100

    Add test stub for excercising the playlist models.

---
+++ tests/playlist/TestPlaylistModels.cpp
@@ -0,0 +1,47 @@
+/****************************************************************************************
+ * Copyright (c) 2010 Nikolaj Hald Nielsen <nhn@kde.com>                                *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Pulic License for more details.              *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+#include "TestPlaylistModels.h"
+
+#include <KStandardDirs>
+
+#include <QtDebug>
+#include <QtTest/QTest>
+#include <qtest_kde.h>
+
+QTEST_KDEMAIN_CORE( TestPlaylistModels )
+
+
+TestPlaylistModels::TestPlaylistModels() 
+    : QObject()
+{
+}
+
+void TestPlaylistModels::initTestCase()
+{
+}
+
+void TestPlaylistModels::testSorting()
+{
+}
+
+void TestPlaylistModels::testFiltering()
+{
+}
+
+void TestPlaylistModels::testSearching()
+{
+}
