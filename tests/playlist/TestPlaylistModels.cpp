4ac314318d884fad4979de843c775ee4e3e590ea
Nikolaj Hald Nielsen <nhn@kde.org>
2010-03-26 16:17:48 +0100

    get a little more thorough in the sorting test

--- tests/playlist/TestPlaylistModels.cpp
+++ tests/playlist/TestPlaylistModels.cpp
@@ -55,40 +55,46 @@ void TestPlaylistModels::initTestCase()
     Meta::TrackList tracks;
     
     QVariantMap map1;
-    map1.insert( Meta::Field::TITLE,  QString( "Cool as honey" ) );
-    map1.insert( Meta::Field::ARTIST,  QString( "Bonzai Bees" ) );
-    map1.insert( Meta::Field::ALBUM,  QString( "The Hive" ) );
-    tracks << Meta::TrackPtr( new MetaMock( map1 ) );
+    map1.insert( Meta::Field::TITLE,  QString( "Cool as honey" ) );    
+    MetaMock * metaMock = new MetaMock( map1 );
+    metaMock->m_artist = new MockArtist( "Bonzai Bees" );
+    metaMock->m_album = new MockAlbum( "The Hive" );
+    tracks << Meta::TrackPtr( metaMock );
     
     QVariantMap map2;
     map2.insert( Meta::Field::TITLE,  QString( "xTreme buzzing sound" ) );
-    map2.insert( Meta::Field::ARTIST,  QString( "Bonzai Bees" ) );
-    map2.insert( Meta::Field::ALBUM,  QString( "The Hive" ) );
-    tracks << Meta::TrackPtr( new MetaMock( map2 ) );
+    metaMock = new MetaMock( map2 );
+    metaMock->m_artist = new MockArtist( "Bonzai Bees" );
+    metaMock->m_album = new MockAlbum( "The Hive" );
+    tracks << Meta::TrackPtr( metaMock );
     
     QVariantMap map3;
     map3.insert( Meta::Field::TITLE,  QString( "Alphabet soup" ) );
-    map3.insert( Meta::Field::ARTIST,  QString( "Bonzai Bees" ) );
-    map3.insert( Meta::Field::ALBUM,  QString( "The Hive" ) );
-    tracks << Meta::TrackPtr( new MetaMock( map3 ) );
+    metaMock = new MetaMock( map3 );
+    metaMock->m_artist = new MockArtist( "Bonzai Bees" );
+    metaMock->m_album = new MockAlbum( "The Hive" );
+    tracks << Meta::TrackPtr( metaMock );
     
     QVariantMap map4;
     map4.insert( Meta::Field::TITLE,  QString( "Zlick" ) );
-    map4.insert( Meta::Field::ARTIST,  QString( "Grumpy Grizzlies" ) );
-    map4.insert( Meta::Field::ALBUM,  QString( "Nice Long Nap" ) );
-    tracks << Meta::TrackPtr( new MetaMock( map4 ) );
+    metaMock = new MetaMock( map4 );
+    metaMock->m_artist = new MockArtist( "Grumpy Grizzlies" );
+    metaMock->m_album = new MockAlbum( "Nice Long Nap" );
+    tracks << Meta::TrackPtr( metaMock );
     
     QVariantMap map5;
     map5.insert( Meta::Field::TITLE,  QString( "23 hours is not enough" ) );
-    map5.insert( Meta::Field::ARTIST,  QString( "Grumpy Grizzlies" ) );
-    map4.insert( Meta::Field::ALBUM,  QString( "Nice Long Nap" ) );
-    tracks << Meta::TrackPtr( new MetaMock( map5 ) );
+    metaMock = new MetaMock( map5 );
+    metaMock->m_artist = new MockArtist( "Grumpy Grizzlies" );
+    metaMock->m_album = new MockAlbum( "Nice Long Nap" );
+    tracks << Meta::TrackPtr( metaMock );
     
     QVariantMap map6;
-    map6.insert( Meta::Field::TITLE,  QString( "1 song to rule them all" ) );
-    map6.insert( Meta::Field::ARTIST,  QString( "Bonzai Bees" ) );
-    map6.insert( Meta::Field::ALBUM,  QString( "Pretty Flowers" ) );
-    tracks << Meta::TrackPtr( new MetaMock( map6 ) );
+    map6.insert( Meta::Field::TITLE,  QString( "1 song to rule them all" ) );  
+    metaMock = new MetaMock( map6 );
+    metaMock->m_artist = new MockArtist( "Bonzai Bees" );
+    metaMock->m_album = new MockAlbum( "Pretty Flowers" );
+    tracks << Meta::TrackPtr( metaMock );
       
     InsertCmdList insertCmds;
     int row = 0;
@@ -113,11 +119,15 @@ void TestPlaylistModels::initTestCase()
 
 void TestPlaylistModels::testSorting()
 {
-  
+   
+    ModelStack::instance()->filterProxy()->clearSearchTerm();
+    
+    //simple sort by title
+    //******************************//
+    
     SortScheme scheme = SortScheme();
     scheme.addLevel( SortLevel( internalColumnNames.indexOf( "Title" ), Qt::AscendingOrder ) );
     ModelStack::instance()->sortProxy()->updateSortMap( scheme );
-    ModelStack::instance()->filterProxy()->clearSearchTerm();
     
     AbstractModel * topModel = ModelStack::instance()->top();
     
@@ -128,8 +138,46 @@ void TestPlaylistModels::testSorting()
     QCOMPARE( topModel->trackAt( 4 )->prettyName(), QString( "xTreme buzzing sound" ) );
     QCOMPARE( topModel->trackAt( 5 )->prettyName(), QString( "Zlick" ) );
     
-    //TODO: More advanced sorting scheme tests go here
-  
+    
+    //Sort by Artist - Album - Title
+    //******************************//
+    
+    SortScheme scheme2 = SortScheme();
+
+    scheme2.addLevel( SortLevel( internalColumnNames.indexOf( "Artist" ), Qt::AscendingOrder ) );
+    scheme2.addLevel( SortLevel( internalColumnNames.indexOf( "Album" ), Qt::AscendingOrder ) );
+    scheme2.addLevel( SortLevel( internalColumnNames.indexOf( "Title" ), Qt::AscendingOrder ) );
+    
+    QCOMPARE( scheme2.length(), 3 );
+    
+    ModelStack::instance()->sortProxy()->updateSortMap( scheme2 );
+    topModel->qaim()->revert();
+    
+    QCOMPARE( topModel->trackAt( 0 )->prettyName(), QString( "1 song to rule them all" ) );
+    QCOMPARE( topModel->trackAt( 1 )->prettyName(), QString( "Alphabet soup" ) );
+    QCOMPARE( topModel->trackAt( 2 )->prettyName(), QString( "Cool as honey" ) );
+    QCOMPARE( topModel->trackAt( 3 )->prettyName(), QString( "xTreme buzzing sound" ) );
+    QCOMPARE( topModel->trackAt( 4 )->prettyName(), QString( "23 hours is not enough" ) );
+    QCOMPARE( topModel->trackAt( 5 )->prettyName(), QString( "Zlick" ) );
+    
+    //reverse some stuff
+    //******************************//
+    
+    SortScheme scheme3 = SortScheme();
+
+    scheme3.addLevel( SortLevel( internalColumnNames.indexOf( "Artist" ), Qt::AscendingOrder ) );
+    scheme3.addLevel( SortLevel( internalColumnNames.indexOf( "Album" ), Qt::DescendingOrder ) );
+    scheme3.addLevel( SortLevel( internalColumnNames.indexOf( "Title" ), Qt::AscendingOrder ) );
+      
+    ModelStack::instance()->sortProxy()->updateSortMap( scheme3 );
+    topModel->qaim()->revert();
+    
+    QCOMPARE( topModel->trackAt( 0 )->prettyName(), QString( "Alphabet soup" ) );
+    QCOMPARE( topModel->trackAt( 1 )->prettyName(), QString( "Cool as honey" ) );
+    QCOMPARE( topModel->trackAt( 2 )->prettyName(), QString( "xTreme buzzing sound" ) );
+    QCOMPARE( topModel->trackAt( 3 )->prettyName(), QString( "1 song to rule them all" ) );
+    QCOMPARE( topModel->trackAt( 4 )->prettyName(), QString( "23 hours is not enough" ) );
+    QCOMPARE( topModel->trackAt( 5 )->prettyName(), QString( "Zlick" ) );
 }
 
 void TestPlaylistModels::testFiltering()
@@ -138,6 +186,7 @@ void TestPlaylistModels::testFiltering()
     //make sure sort mode is reset
     SortScheme scheme = SortScheme();
     ModelStack::instance()->sortProxy()->updateSortMap( scheme ); 
+    
     ModelStack::instance()->filterProxy()->showOnlyMatches( true );
     ModelStack::instance()->filterProxy()->find( "ou" ); 
     
