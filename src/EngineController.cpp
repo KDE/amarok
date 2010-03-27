ee9792e774e2d0fd968819dc2719922ba8cf1b2b
Mark Kretschmann <kretschmann@kde.org>
2010-03-27 19:43:19 +0100

    Make Amarok work correctly with Phonon-VLC.
    Before this patch, Amarok used to stop after each track with the VLC
    backend. The same thing happened with other backends too. Only with xine
    it mostly worked, but not always.
    
    Also, I've enabled Gapless Playback again.
    ~yay

--- src/EngineController.cpp
+++ src/EngineController.cpp
@@ -172,14 +172,10 @@ EngineController::initializePhonon()
     connect( m_controller, SIGNAL( titleChanged( int ) ), SLOT( slotTitleChanged( int ) ) );
 
 
-    // Gapless Playback is temporarily disabled. Reason: Trying to find a bug in this code that makes
-    // the EngineController stop after each track, with all Phonon backends except the xine backend.
-#if 0
     if( AmarokConfig::trackDelayLength() > -1 )
         m_media->setTransitionTime( AmarokConfig::trackDelayLength() ); // Also Handles gapless.
     else if( AmarokConfig::crossfadeLength() > 0 )  // TODO: Handle the possible options on when to crossfade.. the values are not documented anywhere however
         m_media->setTransitionTime( -AmarokConfig::crossfadeLength() );
-#endif
 }
 
 
@@ -724,10 +720,8 @@ EngineController::setNextTrack( Meta::TrackPtr track )
         m_nextTrack = track;
         m_nextUrl = track->playableUrl();
     }
-    else
-    {
-        play( track );
-    }
+
+    play( track );
 }
 
 Phonon::State
