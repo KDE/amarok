KDE/amarok

commit 1fafbc4bd3a27a40028f6a68614822d673a08012

Moved playlist restore code to Playlist::Actions.
This way everything is set up and all slots are triggered when the queue is updated.
The delegate draws the icons and the toolbar is notified.
Also using the proper interface to add tracks prevents specific code to increase playlist length and duration

src/playlist/PlaylistActions.cpp
@@ -26,6 +26,7 @@
 
 #include "core/support/Amarok.h"
 #include "core/support/Components.h"
+#include "core-impl/playlists/types/file/PlaylistFileSupport.h"
 #include "amarokconfig.h"
 #include "dbus/PlayerDBusHandler.h"
 #include "core/support/Debug.h"
@@ -76,6 +77,8 @@ Playlist::Actions::Actions()
 
     m_topmostModel = Playlist::ModelStack::instance()->top();
     playlistModeChanged(); // sets m_navigator.
+
+    restoreDefaultPlaylist();
 }
 
 Playlist::Actions::~Actions()
@@ -465,6 +468,47 @@ Playlist::Actions::repaintPlaylist()
     The::mainWindow()->playlistWidget()->currentView()->repaint();
 }
 
+void
+Playlist::Actions::restoreDefaultPlaylist()
+{
+    Playlists::PlaylistFilePtr playlist = Playlists::loadPlaylistFile( Playlist::ModelStack::instance()->bottom()->defaultPlaylistPath() );
+    if ( playlist )
+    {
+        Meta::TrackList tracks = playlist->tracks();
+
+        QMutableListIterator<Meta::TrackPtr> i( tracks );
+        while ( i.hasNext() )
+        {
+            i.next();
+            Meta::TrackPtr track = i.value();
+            if ( ! track )
+                i.remove();
+            else if( Playlists::canExpand( track ) )
+            {
+                Playlists::PlaylistPtr playlist = Playlists::expand( track );
+                //expand() can return 0 if the KIO job errors out
+                if( playlist )
+                {
+                    i.remove();
+                    Meta::TrackList newtracks = playlist->tracks();
+                    foreach( Meta::TrackPtr t, newtracks )
+                        if( t )
+                            i.insert( t );
+                }
+            }
+        }
+
+        The::playlistController()->insertTracks( 0, tracks );
+        Meta::TrackList queuedTracks = playlist->queue();
+        QList<int> rows;
+        foreach( Meta::TrackPtr track, queuedTracks )
+        {
+            rows << The::playlist()->firstRowForTrack( track ) ;
+        }
+        queue( rows );
+    }
+
+}
 
 namespace The
 {

src/playlist/PlaylistModel.cpp
@@ -67,55 +67,6 @@ Playlist::Model::Model( QObject *parent )
      */
     The::playlistManager();
 
-    Playlists::PlaylistFilePtr playlist = Playlists::loadPlaylistFile( defaultPlaylistPath() );
-    if ( playlist )
-    {
-        Meta::TrackList tracks = playlist->tracks();
-
-        QMutableListIterator<Meta::TrackPtr> i( tracks );
-        while ( i.hasNext() )
-        {
-            i.next();
-            Meta::TrackPtr track = i.value();
-            if ( ! track )
-                i.remove();
-            else if( Playlists::canExpand( track ) )
-            {
-                Playlists::PlaylistPtr playlist = Playlists::expand( track );
-                //expand() can return 0 if the KIO job errors out
-                if( playlist )
-                {
-                    i.remove();
-                    Meta::TrackList newtracks = playlist->tracks();
-                    foreach( Meta::TrackPtr t, newtracks )
-                        if( t )
-                            i.insert( t );
-                }
-            }
-        }
-
-        // Insert playlist items loaded from file.
-        // Don't emit any signals, because we're in the constructor.
-        foreach( Meta::TrackPtr track, tracks )
-        {
-            m_totalLength += track->length();
-            m_totalSize += track->filesize();
-            subscribeTo( track );
-            if ( track->album() )
-                subscribeTo( track->album() );
-
-            Item* i = new Item( track );
-            m_items.append( i );
-            m_itemIds.insert( i->id(), i );
-        }
-
-        Meta::TrackList queue = playlist->queue();
-        foreach( Meta::TrackPtr track, queue )
-        {
-            setRowQueued( firstRowForTrack( track ) );
-        }
-    }
-
    //Select previously saved track
    const int playingTrack = AmarokConfig::lastPlaying();
 

src/playlist/PlaylistActions.h
@@ -122,6 +122,7 @@ public slots:
     void repopulateDynamicPlaylist();
     void queue( QList<int> rows );
     void dequeue( QList<int> rows );
+    void restoreDefaultPlaylist();
 
     /**
     * Repaint the playlist.

commit 39e470690ac6e958f6b10fc9cca578eaa3811560

Playlist: fix small breakage introduced by commit 1fafbc4bd3a27a40028f6a68614822d673a08012

src/playlist/PlaylistActions.cpp
@@ -44,6 +44,7 @@
 #include "navigators/FavoredRandomTrackNavigator.h"
 #include "PlaylistModelStack.h"
 #include "playlist/PlaylistWidget.h"
+#include "playlistmanager/PlaylistManager.h"
 
 #include <typeinfo>
 
@@ -471,6 +472,13 @@ Playlist::Actions::repaintPlaylist()
 void
 Playlist::Actions::restoreDefaultPlaylist()
 {
+    DEBUG_BLOCK
+
+    // The PlaylistManager needs to be loaded or podcast episodes and other
+    // non-collection Tracks will not be loaded correctly.
+    The::playlistManager();
+
+
     Playlists::PlaylistFilePtr playlist = Playlists::loadPlaylistFile( Playlist::ModelStack::instance()->bottom()->defaultPlaylistPath() );
     if ( playlist )
     {
@@ -499,14 +507,18 @@ Playlist::Actions::restoreDefaultPlaylist()
         }
 
         The::playlistController()->insertTracks( 0, tracks );
+
         QList<int> queuedRows = playlist->queue();
         queue( playlist->queue() );
-    }
 
+        //Select previously playing track
+        const int lastPlayingRow = AmarokConfig::lastPlaying();
+        if( lastPlayingRow >= 0 )
+            Playlist::ModelStack::instance()->bottom()->setActiveRow( lastPlayingRow );
+    }
 }
 
 namespace The
 {
     AMAROK_EXPORT Playlist::Actions* playlistActions() { return Playlist::Actions::instance(); }
 }
-

src/playlist/PlaylistModel.cpp
@@ -40,7 +40,6 @@
 #include "PlaylistItem.h"
 #include "core-impl/playlists/types/file/PlaylistFileSupport.h"
 #include "UndoCommands.h"
-#include "playlistmanager/PlaylistManager.h"
 
 #include <KGlobal>
 #include <KUrl>
@@ -61,17 +60,6 @@ Playlist::Model::Model( QObject *parent )
         , m_setStateOfItem_batchMinRow( -1 )
 {
     DEBUG_BLOCK
-
-    /* The PlaylistManager needs to be loaded or podcast episodes and other
-     * non-collection Tracks will not be loaded correctly.
-     */
-    The::playlistManager();
-
-   //Select previously saved track
-   const int playingTrack = AmarokConfig::lastPlaying();
-
-   if ( playingTrack > -1 )
-       setActiveRow( playingTrack );
 }
 
 Playlist::Model::~Model()