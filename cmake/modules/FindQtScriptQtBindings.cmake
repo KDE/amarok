415f1d610a42d24685d9dcea60970c0beb880601
Mark Kretschmann <kretschmann@kde.org>
2010-04-17 13:20:27 +0200

    Remove deprecated analyzers/ folder.
    This stuff was from Amarok 1.x and was never used in 2.x.

--- cmake/modules/FindQtScriptQtBindings.cmake
+++ cmake/modules/FindQtScriptQtBindings.cmake
@@ -17,28 +17,4 @@
 
 include(CheckCXXSourceRuns)
 
-if(NOT WIN32)
-    file( READ "${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules/bindingstest/QtScriptBindingsTest.cpp" source )
-    message(STATUS "Checking if the QtScript Qt Bindings are installed.")
-
-
-    set(CMAKE_REQUIRED_DEFINTIONS ${QT_DEFINITIONS} ${KDE4_DEFINITIONS} )
-    set(CMAKE_REQUIRED_INCLUDES ${QT_QTCORE_INCLUDE_DIR} ${QT_QTSCRIPT_INCLUDE_DIR} ${KDE4_INCLUDES})
-    set(CMAKE_REQUIRED_LIBRARIES ${QT_QTSCRIPT_LIBRARY} ${QT_QTCORE_LIBRARY} ${QT_QTGUI_LIBRARY} -L${KDE4_LIB_DIR} -lkdecore -lkdeui)
-    message( STATUS "includes ${CMAKE_REQUIRED_INCLUDES} libraries ${CMAKE_REQUIRED_LIBRARIES}" )
-    CHECK_CXX_SOURCE_RUNS( "${source}" BINDINGS_RUN_RESULT)
-
-    if(BINDINGS_RUN_RESULT EQUAL 1)
-        message( STATUS "QtBindings found")
-        set(QTSCRIPTQTBINDINGS_FOUND TRUE)
-    else(BINDINGS_RUN_RESULT EQUAL 1)
-        message( STATUS "QtBindings not found. run `cd cmake/modules/bindingstest; mkdir build; cd build; cmake ..; make; ./bindingstest; echo $?` If it prints '0' then you're actually fine.")
-        set(QTSCRIPTQTBINDINGS_FOUND FALSE)
-    endif(BINDINGS_RUN_RESULT EQUAL 1)
-
-    set(CMAKE_REQUIRED_DEFINTIONS "" )
-    set(CMAKE_REQUIRED_INCLUDES "")
-    set(CMAKE_REQUIRED_LIBRARIES "")
-else(NOT WIN32)
     set(QTSCRIPTQTBINDINGS_FOUND TRUE)
-endif(NOT WIN32)
