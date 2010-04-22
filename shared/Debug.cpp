eb778afdad88aa8f82ac843974115ff6572cdd79
Jeff Mitchell <mitchell@kde.org>
2010-04-22 06:48:09 -0400

    Fix hang-on-exit.
    CCMAIL:amarok-devel@kde.org

--- shared/Debug.cpp
+++
@@ -1,4 +0,0 @@
-#include <QMutex> 
-#include "core/support/Debug.h"
-
-QMutex Debug::mutex;
---
+++ src/core/support/Debug.cpp
@@ -0,0 +1,5 @@
+#include <QMutex> 
+#include "core/support/Debug.h"
+#include "shared/amarok_export.h"
+
+AMAROK_EXPORT QMutex Debug::mutex;
