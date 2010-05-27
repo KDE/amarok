25111afcd401ce7df3f8715669c926d620a74971
Lydia Pintscher <lydia@kde.org>
2010-05-27 20:19:17 +0200

    bump plugin version for release

--- src/core/plugins/Plugin.h
+++ src/core/plugins/Plugin.h
@@ -33,7 +33,7 @@ class QWidget;
 namespace Plugins
 {
     /** Bump this number whenever the plugin framework gets incompatible with older versions */
-    static const int PluginFrameworkVersion = 55;
+    static const int PluginFrameworkVersion = 56;
 
     class PluginConfig;
 
