KDE/amarok

commit bc32723df802bc6306c98ee4b2c4eddca733cebc

Use Plaintext instead of html in TextScrollingWidget.
This fixes issue with some strings getting shown inccorectly in the current track applet becuase of special characters ( like < and > ) being present.
ChangeLog++

BUG: 222765

src/context/widgets/TextScrollingWidget.cpp
@@ -77,7 +77,7 @@ TextScrollingWidget::setScrollingText( const QString text, QRectF rect )
 void
 TextScrollingWidget::setText( const QString &text )
 {
-    setHtml( text );
+    setPlainText( text );
 }
 
 QString

commit 8faa6e1454524b604d4114a65b1f86d0e3ee01f3

Fix lyrics applet title and ampersands
Commit bc32723 introduced a regression. While the current track applet
will now correctly show special characters (which was broken by an even
earlier commit 3d3a77f), the lyrics applet now suffers from the same
issue. This fixes that by allowing a particular text format to be set
for the TextScrollingWidget, with plain text as the default.

CCBUG:220714
CCBUG:222765

src/context/applets/lyrics/LyricsApplet.cpp
@@ -78,6 +78,7 @@ void LyricsApplet::init()
     m_titleLabel = new TextScrollingWidget( this );
     QFont bigger = m_titleLabel->font();
     bigger.setPointSize( bigger.pointSize() + 2 );
+    m_titleLabel->setTextFormat( Qt::RichText );
     m_titleLabel->setFont( bigger );
     m_titleLabel->setZValue( m_titleLabel->zValue() + 100 );
     m_titleLabel->setText( i18n( "Lyrics" ) );

src/context/widgets/TextScrollingWidget.cpp
@@ -35,6 +35,7 @@
 TextScrollingWidget::TextScrollingWidget( QGraphicsItem* parent )
     : QGraphicsTextItem( parent )
     , m_fm( 0 )
+    , m_textFormat( Qt::PlainText )
     , m_delta( 0 )
     , m_currentDelta( 0. )
     , m_animfor( 0 )
@@ -53,7 +54,7 @@ TextScrollingWidget::setBrush( const QBrush &brush )
 }
 
 void
-TextScrollingWidget::setScrollingText( const QString text, QRectF rect )
+TextScrollingWidget::setScrollingText( const QString &text, const QRectF &rect )
 {
  //   DEBUG_BLOCK
     if ( !m_fm )
@@ -67,22 +68,60 @@ TextScrollingWidget::setScrollingText( const QString text, QRectF rect )
     Plasma::Animator::self()->stopCustomAnimation( m_animfor );
     m_animating = false ;
 
-    const QRect textRect = m_fm->boundingRect( m_text );
-    m_delta = textRect.width() + 5 > m_rect.width()
-            ? textRect.width() + 8 - m_rect.width() : 0;
-    setText( m_fm->elidedText ( m_text, Qt::ElideRight, (int)m_rect.width() ) );
+    const int textWidth = m_fm->width( m_text );
+    m_delta = textWidth + 5 > m_rect.width() ? textWidth + 8 - m_rect.width() : 0;
+    setText( m_fm->elidedText( m_text, Qt::ElideRight, (int)m_rect.width() ) );
 }
 
 void
 TextScrollingWidget::setText( const QString &text )
 {
-    setPlainText( text );
+    if( requirePlainText() )
+        setPlainText( text );
+    else
+        setHtml( text );
+}
+
+void
+TextScrollingWidget::setTextFormat( Qt::TextFormat fmt )
+{
+    m_textFormat = fmt;
 }
 
 QString
 TextScrollingWidget::text() const
 {
-    return toPlainText();
+    if( requirePlainText() )
+        return toPlainText();
+    else
+        return toHtml();
+}
+
+bool
+TextScrollingWidget::requirePlainText() const
+{
+    bool plain;
+    switch( m_textFormat )
+    {
+    case Qt::RichText:
+        plain = false;
+        break;
+
+    case Qt::AutoText:
+        plain = !Qt::mightBeRichText( m_text );
+        break;
+
+    case Qt::PlainText:
+    default:
+        plain = true;
+    }
+    return plain;
+}
+
+Qt::TextFormat
+TextScrollingWidget::textFormat() const
+{
+    return m_textFormat;
 }
 
 void
@@ -136,7 +175,7 @@ TextScrollingWidget::animationFinished( int id )
         else
         {
             m_animating = false;
-            setText( m_fm->elidedText ( m_text, Qt::ElideRight, (int)( m_rect.width() ) ) );
+            setText( m_fm->elidedText( m_text, Qt::ElideRight, (int)( m_rect.width() ) ) );
         }
     }
 }

src/context/widgets/TextScrollingWidget.h
@@ -51,11 +51,13 @@ class AMAROK_EXPORT TextScrollingWidget : public QGraphicsTextItem
         /**
         * Set the Text and more important the QRectF which will define the scrolling area
         */
-        void setScrollingText( const QString, QRectF );
+        void setScrollingText( const QString &text, const QRectF &rect );
 
         void setText( const QString &text );
+        void setTextFormat( Qt::TextFormat fmt );
 
         QString text() const;
+        Qt::TextFormat textFormat() const;
 
         bool isAnimating();
 
@@ -83,11 +85,14 @@ class AMAROK_EXPORT TextScrollingWidget : public QGraphicsTextItem
         QRectF            m_rect;           // box size
         QFontMetrics     *m_fm;             // font metrics which will cut the text.
         QString           m_text;           // full sentence
+        Qt::TextFormat    m_textFormat;     // text format
         int               m_delta;          // complete delta
         float             m_currentDelta;   // current delta
         int               m_animfor;        // anim id for
         int               m_animback;       // anim id back
         bool              m_animating;      // boolean !
+
+        bool requirePlainText() const;
 };
 
 #endif