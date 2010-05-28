4816a16185068500384dd30f78bbaa9abe622619
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-05-28 18:37:38 +0530

    Debug for understanding

--- src/core-impl/collections/upnpcollection/UpnpQueryMaker.cpp
+++ src/core-impl/collections/upnpcollection/UpnpQueryMaker.cpp
@@ -53,153 +53,183 @@ void UpnpQueryMaker::run()
 
 void UpnpQueryMaker::abortQuery()
 {
+DEBUG_BLOCK
 }
 
 int UpnpQueryMaker::resultCount() const
 {
+DEBUG_BLOCK
     return 42;
 }
 
 QueryMaker* UpnpQueryMaker::setQueryType( QueryType type )
 {
+DEBUG_BLOCK
     debug() << "Query type" << type;
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::setReturnResultAsDataPtrs( bool resultAsDataPtrs )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addReturnValue( qint64 value )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addReturnFunction( ReturnFunction function, qint64 value )
 {
+DEBUG_BLOCK
     debug() << "Return function with value" << value;
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::orderBy( qint64 value, bool descending )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::orderByRandom()
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::includeCollection( const QString &collectionId )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::excludeCollection( const QString &collectionId )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addMatch( const Meta::TrackPtr &track )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addMatch( const Meta::ArtistPtr &artist )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addMatch( const Meta::AlbumPtr &album )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addMatch( const Meta::ComposerPtr &composer )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addMatch( const Meta::GenrePtr &genre )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addMatch( const Meta::YearPtr &year )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addMatch( const Meta::DataPtr &data )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addMatch( const Meta::LabelPtr &label )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addFilter( qint64 value, const QString &filter, bool matchBegin, bool matchEnd )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::excludeFilter( qint64 value, const QString &filter, bool matchBegin, bool matchEnd )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::addNumberFilter( qint64 value, qint64 filter, NumberComparison compare )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::excludeNumberFilter( qint64 value, qint64 filter, NumberComparison compare )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::limitMaxResultSize( int size )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::setAlbumQueryMode( AlbumQueryMode mode )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::setLabelQueryMode( LabelQueryMode mode )
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::beginAnd()
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::beginOr()
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::endAndOr()
 {
+DEBUG_BLOCK
     return this;
 }
 
 QueryMaker* UpnpQueryMaker::setAutoDelete( bool autoDelete )
 {
+DEBUG_BLOCK
     debug() << "Auto delete";
     return this;
 }
 
 int UpnpQueryMaker::validFilterMask()
 {
+DEBUG_BLOCK
     return 42;
 }
 
