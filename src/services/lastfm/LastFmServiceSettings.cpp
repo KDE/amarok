KDE/amarok

commit fe8cdbf03b0c088058f752a425347e90c95aaca3

last.fm features that require authentication should be disabled by default

src/services/lastfm/LastFmServiceSettings.cpp
@@ -178,8 +178,8 @@ LastFmServiceSettings::defaults()
     m_config.reset();
 
     // By default this checkboxes is:
-    m_configDialog->kcfg_SubmitPlayedSongs->setChecked( true );
-    m_configDialog->kcfg_RetrieveSimilarArtists->setChecked( true );
+    m_configDialog->kcfg_SubmitPlayedSongs->setChecked( false );
+    m_configDialog->kcfg_RetrieveSimilarArtists->setChecked( false );
 }
 
 

src/services/lastfm/LastFmConfigWidget.ui
@@ -119,7 +119,7 @@
       <item row="0" column="0" >
        <widget class="QCheckBox" name="kcfg_SubmitPlayedSongs" >
         <property name="enabled" >
-         <bool>true</bool>
+         <bool>false</bool>
         </property>
         <property name="text" >
          <string>&amp;Submit tracks</string>
@@ -135,7 +135,7 @@
          <string>&amp;Retrieve similar artists</string>
         </property>
         <property name="checked" >
-         <bool>true</bool>
+         <bool>false</bool>
         </property>
        </widget>
       </item>

src/services/lastfm/LastFmServiceConfig.cpp
@@ -155,8 +155,8 @@ LastFmServiceConfig::reset()
     m_username = "";
     m_password = "";
     m_sessionKey = "";
-    m_scrobble = true;
-    m_fetchSimilar = true;
+    m_scrobble = false;
+    m_fetchSimilar = false;
 }
 
 

commit 8c46410d2ba60ed7715966810af47f1bd8aa0e90

Allow scrobbling option to be changed
fe8cdbf0 introduced a bug whereby the "Submit tracks" checkbox cannot
ever be enabled so that a user can change the setting.  Made it so that
the setting can be changed after the authentication is checked.  An even
better idea would be to check authentication immediately when the dialog
box opens.

CC: unnamedrambler@gmail.com

src/services/lastfm/LastFmServiceSettings.cpp
@@ -141,6 +141,7 @@ LastFmServiceSettings::onAuthenticated()
              {
                  m_configDialog->testLogin->setText( i18nc( "The operation completed as expected", "Success" ) );
                  m_configDialog->testLogin->setEnabled( false );
+                 m_configDialog->kcfg_SubmitPlayedSongs->setEnabled( true );
              }
              break;
 