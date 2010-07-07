4637772a864be01dc60979ce74c354e8c5a0815d
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-07-07 21:53:04 +0530

    Fixed bad license text

--- src/core-impl/collections/upnpcollection/UpnpQueryMaker.cpp
+++ src/core-impl/collections/upnpcollection/UpnpQueryMaker.cpp
@@ -1,28 +1,18 @@
 /****************************************************************************************
- UpnpQueryMaker::* Copyright (c) 2010 Nikhil Marathe <nsm.nikhil@gmail.com>                             *
+ * Copyright (c) 2010 Nikhil Marathe <nsm.nikhil@gmail.com>                             *
  *                                                                                      *
- * This program is free software
-{
-}
- you can redistribute it and/or modify it under        *
+ * This program is free software; you can redistribute it and/or modify it under        *
  * the terms of the GNU General Public License as published by the Free Software        *
- UpnpQueryMaker::* Foundation
-{
-}
- either version 2 of the License, or (at your option) any later           *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
  * version.                                                                             *
  *                                                                                      *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
- * WARRANTY
-{
-}
- without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
  *                                                                                      *
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
-
 #define DEBUG_PREFIX "UpnpQueryMaker"
 
 #include "UpnpQueryMaker.h"
