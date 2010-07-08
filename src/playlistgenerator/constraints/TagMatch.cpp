KDE/amarok

commit d8130906366ad1d6bd058159e589d3c774e121b3

APG: Fix inconsistent translation of constraint names
The constraint names should always be translated in the GUI, and never
translated in the XML file.  Sometimes they were getting translated in
the XML file, which caused problems for users when using localized
versions of Amarok.

And while I was playing around with translation of the constraint names,
I took the opportunity to tweak their appearance in the GUI.

If you're using a localized version of Amarok, please test that the
constraints in APG presets are always exported/imported/loaded/saved
correctly.  Thanks.

BUG: 242111

src/playlistgenerator/constraints/TagMatch.cpp
@@ -52,9 +52,10 @@ ConstraintTypes::TagMatch::createNew( ConstraintNode* p )
 ConstraintFactoryEntry*
 ConstraintTypes::TagMatch::registerMe()
 {
-    return new ConstraintFactoryEntry( i18n("TagMatch"),
-                    i18n("Make all tracks in the playlist match the specified characteristic"),
-                    &TagMatch::createFromXml, &TagMatch::createNew );
+    return new ConstraintFactoryEntry( "TagMatch",
+                                       i18n("Match Tags"),
+                                       i18n("Make all tracks in the playlist match the specified characteristic"),
+                                       &TagMatch::createFromXml, &TagMatch::createNew );
 }
 
 ConstraintTypes::TagMatch::TagMatch( QDomElement& xmlelem, ConstraintNode* p )
@@ -167,7 +168,7 @@ ConstraintTypes::TagMatch::toXml( QDomDocument& doc, QDomElement& elem ) const
 QString
 ConstraintTypes::TagMatch::getName() const
 {
-    QString v( i18n("Tag Match:%1 %2 %3 %4") );
+    QString v( i18n("Match tag:%1 %2 %3 %4") );
     v = v.arg( ( m_invert ? i18n(" not") : "" ), m_fieldsModel->pretty_name_of( m_field ), comparisonToString() );
     if ( m_field == "rating" ) {
         double r = m_value.toDouble() / 2.0;
@@ -532,7 +533,7 @@ ConstraintTypes::TagMatch::comparisonToString() const
 QString
 ConstraintTypes::TagMatch::valueToString() const
 {
-    if ( m_fieldsModel->type_of( m_field ) == FieldTypeDate )
+    if ( m_fieldsModel->type_of( m_field ) == FieldTypeDate ) {
         if ( m_comparison != Constraint::CompareDateWithin ) {
             return m_value.toDate().toString( Qt::ISODate );
         } else {
@@ -552,8 +553,11 @@ ConstraintTypes::TagMatch::valueToString() const
             }
             return QString("%1 %2").arg( m_value.value<DateRange>().first ).arg( unit );
         }
-    else
+    } else if ( m_fieldsModel->type_of( m_field ) == FieldTypeString ) {
+        return QString( i18nc("an arbitrary string surrounded by quotes", "\"%1\"") ).arg( m_value.toString() );
+    } else {
         return m_value.toString();
+    }
 }
 
 bool

src/playlistgenerator/ConstraintFactory.cpp
@@ -36,9 +36,13 @@
  * Constraint Factory Registry Entries    *
  ******************************************/
 
-ConstraintFactoryEntry::ConstraintFactoryEntry( const QString& name, const QString& desc,
-        Constraint*( *xmlf )( QDomElement&, ConstraintNode* ), Constraint*( *nf )( ConstraintNode* ) )
+ConstraintFactoryEntry::ConstraintFactoryEntry( const QString& name,
+                                                const QString& i18nN,
+                                                const QString& desc,
+                                                Constraint*( *xmlf )( QDomElement&, ConstraintNode* ),
+                                                Constraint*( *nf )( ConstraintNode* ) )
         : m_name( name )
+        , m_i18nName( i18nN )
         , m_description( desc )
         , m_createFromXmlFunc( xmlf )
         , m_createNewFunc( nf )
@@ -72,18 +76,22 @@ ConstraintFactory::ConstraintFactory()
     r = ConstraintTypes::TagMatch::registerMe();
     m_registryIds[0] = r;
     m_registryNames[r->m_name] = r;
+    m_registryUntranslateNames[r->m_i18nName] = r->m_name;
 
     r = ConstraintTypes::PlaylistLength::registerMe();
     m_registryIds[1] = r;
     m_registryNames[r->m_name] = r;
+    m_registryUntranslateNames[r->m_i18nName] = r->m_name;
 
     r = ConstraintTypes::PreventDuplicates::registerMe();
     m_registryIds[2] = r;
     m_registryNames[r->m_name] = r;
+    m_registryUntranslateNames[r->m_i18nName] = r->m_name;
 
     r = ConstraintTypes::Checkpoint::registerMe();
     m_registryIds[4] = r;
     m_registryNames[r->m_name] = r;
+    m_registryUntranslateNames[r->m_i18nName] = r->m_name;
 
     // ADD NEW CONSTRAINT TYPES HERE FOLLOWING SAME PATTERN (DON'T FORGET TO INCREMENT ID)
 }
@@ -147,6 +155,11 @@ const QStringList ConstraintFactory::names() const
     return m_registryNames.keys();
 }
 
+const QStringList ConstraintFactory::i18nNames() const
+{
+    return m_registryUntranslateNames.keys();
+}
+
 QList< QPair<int, QString> > ConstraintFactory::registeredConstraints() const
 {
     QList< QPair<int, QString> > d;
@@ -156,11 +169,7 @@ QList< QPair<int, QString> > ConstraintFactory::registeredConstraints() const
     return d;
 }
 
-int ConstraintFactory::getTypeId( const QString& name ) const
+const QString ConstraintFactory::untranslateName( const QString& trn ) const
 {
-    foreach( int i, m_registryIds.keys() ) {
-        if ( m_registryIds[i]->m_name == name )
-            return i;
-    }
-    return -1;
+    return m_registryUntranslateNames.value( trn );
 }

src/playlistgenerator/ConstraintFactory.h
@@ -27,13 +27,18 @@ class Constraint;
 class ConstraintNode;
 
 class ConstraintFactoryEntry {
-	public:
+    public:
         friend class ConstraintFactory;
-		ConstraintFactoryEntry(const QString&, const QString&, Constraint* (*xmlf)(QDomElement&, ConstraintNode*), Constraint* (*nf)(ConstraintNode*));
+        ConstraintFactoryEntry(const QString&,
+                               const QString&,
+                               const QString&,
+                               Constraint* (*xmlf)(QDomElement&, ConstraintNode*),
+                               Constraint* (*nf)(ConstraintNode*));
 
-	private:
-		const QString m_name;
-		const QString m_description;
+    private:
+        const QString m_name;
+        const QString m_i18nName;
+        const QString m_description;
         Constraint* (*m_createFromXmlFunc)(QDomElement&, ConstraintNode*);
         Constraint* (*m_createNewFunc)(ConstraintNode*);
 };
@@ -53,8 +58,9 @@ class ConstraintFactory {
         ConstraintNode* createGroup(ConstraintNode*, int row=INT_MAX) const;
 
         const QStringList names() const;
+        const QStringList i18nNames() const;
         QList< QPair<int, QString> > registeredConstraints() const;
-        int getTypeId(const QString&) const;
+        const QString untranslateName( const QString& ) const;
 
     private:
         ConstraintFactory();
@@ -62,6 +68,7 @@ class ConstraintFactory {
         static ConstraintFactory* s_self;
         QHash<int, ConstraintFactoryEntry*> m_registryIds;
         QHash<QString, ConstraintFactoryEntry*> m_registryNames;
+        QHash<QString, QString> m_registryUntranslateNames;
 };
 
 #endif // APG_CONSTRAINT_FACTORY

src/playlistgenerator/PresetEditDialog.cpp
@@ -61,10 +61,10 @@ APG::PresetEditDialog::PresetEditDialog( PresetPtr p )
 
     QAction* a;
     QMenu* m = new QMenu( i18n("Add new"), this );
-    a = m->addAction( QString( i18n("ConstraintGroup") ) );
+    a = m->addAction( QString( i18n("Constraint Group") ) );
     connect( a, SIGNAL( triggered( bool ) ), adderMapper, SLOT( map() ) );
-    adderMapper->setMapping( a, i18n("ConstraintGroup") );
-    foreach( const QString& name, ConstraintFactory::instance()->names() ) {
+    adderMapper->setMapping( a, i18n("Constraint Group") );
+    foreach( const QString& name, ConstraintFactory::instance()->i18nNames() ) {
         a = m->addAction( name );
         connect( a, SIGNAL( triggered( bool ) ), adderMapper, SLOT( map() ) );
         adderMapper->setMapping( a, name );
@@ -91,7 +91,7 @@ void
 APG::PresetEditDialog::addNode( const QString& name )
 {
     debug() << "Adding new" << name;
-    if ( name == i18n("ConstraintGroup") ) {
+    if ( name == i18n("Constraint Group") ) {
         m_controller->addGroup();
     } else {
         m_controller->addConstraint( name );

src/playlistgenerator/TreeModel.cpp
@@ -178,7 +178,7 @@ APG::TreeModel::insertGroup( const QModelIndex& thisIdx )
 }
 
 QModelIndex
-APG::TreeModel::insertConstraint( const QModelIndex& thisIdx, const QString& constraintType )
+APG::TreeModel::insertConstraint( const QModelIndex& thisIdx, const QString& constraintName )
 {
     int row = thisIdx.row();
     if ( thisIdx.isValid() ) {
@@ -186,6 +186,7 @@ APG::TreeModel::insertConstraint( const QModelIndex& thisIdx, const QString& con
         ConstraintNode* n = 0;
         if ( p->getNodeType() == Constraint::ConstraintGroupType ) {
             beginInsertRows( thisIdx, 0, 0 );
+            QString constraintType = ConstraintFactory::instance()->untranslateName( constraintName );
             n = ConstraintFactory::instance()->createConstraint( constraintType, p, 0 );
             endInsertRows();
             if ( n != 0 ) {
@@ -196,6 +197,7 @@ APG::TreeModel::insertConstraint( const QModelIndex& thisIdx, const QString& con
             p = p->parent();
             QModelIndex parentIdx = parent( thisIdx );
             beginInsertRows( parentIdx, row + 1, row + 1 );
+            QString constraintType = ConstraintFactory::instance()->untranslateName( constraintName );
             n = ConstraintFactory::instance()->createConstraint( constraintType, p, row + 1 );
             endInsertRows();
             if ( n != 0 ) {

src/playlistgenerator/constraints/Checkpoint.cpp
@@ -56,9 +56,10 @@ ConstraintTypes::Checkpoint::createNew( ConstraintNode* p )
 ConstraintFactoryEntry*
 ConstraintTypes::Checkpoint::registerMe()
 {
-    return new ConstraintFactoryEntry( i18n("Checkpoint"),
-                                i18n("Fixes a track, album, or artist to a certain position in the playlist"),
-                                &Checkpoint::createFromXml, &Checkpoint::createNew );
+    return new ConstraintFactoryEntry( "Checkpoint",
+                                       i18n("Checkpoint"),
+                                       i18n("Fixes a track, album, or artist to a certain position in the playlist"),
+                                       &Checkpoint::createFromXml, &Checkpoint::createNew );
 }
 
 ConstraintTypes::Checkpoint::Checkpoint( QDomElement& xmlelem, ConstraintNode* p )

src/playlistgenerator/constraints/PlaylistLength.cpp
@@ -53,9 +53,10 @@ ConstraintTypes::PlaylistLength::createNew( ConstraintNode* p )
 ConstraintFactoryEntry*
 ConstraintTypes::PlaylistLength::registerMe()
 {
-    return new ConstraintFactoryEntry( i18n("PlaylistLength"),
-                                i18n("Sets the preferred duration of the playlist"),
-                                &PlaylistLength::createFromXml, &PlaylistLength::createNew );
+    return new ConstraintFactoryEntry( "PlaylistLength",
+                                       i18n("Playlist Duration"),
+                                       i18n("Sets the preferred duration of the playlist"),
+                                       &PlaylistLength::createFromXml, &PlaylistLength::createNew );
 }
 
 ConstraintTypes::PlaylistLength::PlaylistLength( QDomElement& xmlelem, ConstraintNode* p )
@@ -108,7 +109,7 @@ ConstraintTypes::PlaylistLength::toXml( QDomDocument& doc, QDomElement& elem ) c
 QString
 ConstraintTypes::PlaylistLength::getName() const
 {
-    QString v( i18n("Playlist length %1 %2") );
+    QString v( i18n("Playlist duration %1 %2") );
     return v.arg( comparisonToString() ).arg( QTime().addMSecs( m_length ).toString( "H:mm:ss" ) );
 }
 

src/playlistgenerator/constraints/PlaylistLengthEditWidget.ui
@@ -7,7 +7,7 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>233</width>
+    <width>247</width>
     <height>113</height>
    </rect>
   </property>
@@ -21,7 +21,7 @@
    <item>
     <widget class="QGroupBox" name="groupBox">
      <property name="title">
-      <string>PlaylistLength Constraint Settings</string>
+      <string>Playlist Duration Constraint Settings</string>
      </property>
      <layout class="QGridLayout" name="gridLayout_2">
       <item row="0" column="0">

commit 124ed607360f2eaaa50bcd20254aec04b0da7f72

APG: fix over-quoting in TagMatch constraint
d8130906 introduced a bug whereby an extraneous pair of quotes would be
added to strings in the TagMatch constraint every time the XML file was
saved.  Oops.

src/playlistgenerator/constraints/TagMatch.cpp
@@ -176,7 +176,14 @@ ConstraintTypes::TagMatch::getName() const
     } else if ( m_field == "length" ) {
         return v.arg( QTime().addMSecs( m_value.toInt() ).toString( "H:mm:ss" ) );
     } else {
-        return v.arg( valueToString() );
+        if ( m_fieldsModel->type_of( m_field ) == FieldTypeString ) {
+            // put quotes around any strings (eg, track title or artist name) ...
+            QString s = QString( i18nc("an arbitrary string surrounded by quotes", "\"%1\"") ).arg( valueToString() );
+            return v.arg( s );
+        } else {
+            // ... but don't quote put quotes around anything else
+            return v.arg( valueToString() );
+        }
     }
 }
 
@@ -553,8 +560,6 @@ ConstraintTypes::TagMatch::valueToString() const
             }
             return QString("%1 %2").arg( m_value.value<DateRange>().first ).arg( unit );
         }
-    } else if ( m_fieldsModel->type_of( m_field ) == FieldTypeString ) {
-        return QString( i18nc("an arbitrary string surrounded by quotes", "\"%1\"") ).arg( m_value.toString() );
     } else {
         return m_value.toString();
     }