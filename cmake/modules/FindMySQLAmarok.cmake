8c9bc801c1ecd327bf221879858ec27024bb84d7
Jeff Mitchell <mitchell@kde.org>
2010-07-12 18:29:18 -0400

    Make the MySQL check far more robust. It now checks for the existince of mysql_config and uses it when possible. This gets rid of the need for hardcoded lib configs in various places.

--- cmake/modules/FindMySQLAmarok.cmake
+++ cmake/modules/FindMySQLAmarok.cmake
@@ -12,59 +12,77 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
+if(NOT WIN32)
+    find_program(MYSQLCONFIG_EXECUTABLE NAMES mysql_config PATHS ${BIN_INSTALL_DIR} ~/usr/bin /usr/local/bin)
+endif(NOT WIN32)
 
-find_path(MYSQL_INCLUDE_DIR mysql.h
-   ~/usr/include/mysql
-   /opt/local/include/mysql5/mysql
-   /opt/mysqle/include/mysql
-   /opt/mysql/mysql/include 
-   /usr/mysql/include/mysql
-   /usr/include/mysql
-   /usr/local/include/mysql
-   /opt/local/include/mysql
-   /opt/ports/include/mysql5/mysql
-)
+if(MYSQLCONFIG_EXECUTABLE)
 
-if(WIN32)
-    set(MYSQL_CLIENT_LIBRARY_NAME libmysql)
-else(WIN32)
-    set(MYSQL_CLIENT_LIBRARY_NAME mysqlclient)
-endif(WIN32)
+    exec_program(${MYSQLCONFIG_EXECUTABLE} ARGS --include RETURN_VALUE _return_VALUE OUTPUT_VARIABLE MYSQL_INCLUDE_DIR)
+    exec_program(${MYSQLCONFIG_EXECUTABLE} ARGS --cflags RETURN_VALUE _return_VALUE OUTPUT_VARIABLE MYSQL_CFLAGS)
+    exec_program(${MYSQLCONFIG_EXECUTABLE} ARGS --libs RETURN_VALUE _return_VALUE OUTPUT_VARIABLE MYSQL_LIBRARIES)
+    exec_program(${MYSQLCONFIG_EXECUTABLE} ARGS --libmysqld-libs RETURN_VALUE _return_VALUE OUTPUT_VARIABLE MYSQL_EMBEDDED_LIBRARIES)
 
-find_library(MYSQL_LIBRARIES NAMES ${MYSQL_CLIENT_LIBRARY_NAME}
-   PATHS
-   ~/usr/lib/mysql
-   /opt/mysql/mysql/lib 
-   /usr/mysql/lib/mysql
-   /opt/local/lib/mysql5/mysql
-   /opt/mysqle/lib/mysql
-   /usr/lib/mysql
-   /usr/lib64/mysql
-   /usr/lib64
-   /usr/local/lib/mysql
-   /opt/local/lib/mysql
-   /opt/ports/lib/mysql5/mysql
-)
+    if(MYSQL_EMBEDDED_LIBRARIES)
+        set( HAVE_MYSQL_EMBEDDED true )
+    endif(MYSQL_EMBEDDED_LIBRARIES)
 
-find_library(MYSQL_EMBEDDED_LIBRARIES NAMES mysqld_pic mysqld libmysqld
-   PATHS
-   ~/usr/lib/mysql
-   /opt/local/lib/mysql5/mysql
-   /opt/mysqle/lib/mysql
-   /usr/lib/mysql
-   /usr/lib64/mysql
-   /usr/local/lib/mysql
-   /opt/mysql/lib/mysql
-   /opt/local/lib/mysql
-   /opt/ports/lib/mysql5/mysql
-)
+else(MYSQLCONFIG_EXECUTABLE)
 
-macro_push_required_vars()
-set( CMAKE_REQUIRED_INCLUDES ${MYSQL_INCLUDE_DIR} )
-set( CMAKE_REQUIRED_LIBRARIES ${MYSQL_EMBEDDED_LIBRARIES} )
-include_directories( ${MYSQL_INCLUDE_DIR} )
-check_cxx_source_compiles( "#if (defined(_WIN32) || defined(_WIN64))\n#define __LCC__\n#endif\n#include <mysql.h>\nint main() { int i = MYSQL_OPT_USE_EMBEDDED_CONNECTION; }" HAVE_MYSQL_OPT_EMBEDDED_CONNECTION )
-macro_pop_required_vars()
+    find_path(MYSQL_INCLUDE_DIR mysql.h
+       ~/usr/include/mysql
+       /opt/local/include/mysql5/mysql
+       /opt/mysqle/include/mysql
+       /opt/mysql/mysql/include 
+       /usr/mysql/include/mysql
+       /usr/include/mysql
+       /usr/local/include/mysql
+       /opt/local/include/mysql
+       /opt/ports/include/mysql5/mysql
+    )
+
+    if(WIN32)
+        set(MYSQL_CLIENT_LIBRARY_NAME libmysql)
+    else(WIN32)
+        set(MYSQL_CLIENT_LIBRARY_NAME mysqlclient)
+    endif(WIN32)
+
+    find_library(MYSQL_LIBRARIES NAMES ${MYSQL_CLIENT_LIBRARY_NAME}
+      PATHS
+        ~/usr/lib/mysql
+        /opt/mysql/mysql/lib 
+        usr/mysql/lib/mysql
+        opt/local/lib/mysql5/mysql
+        opt/mysqle/lib/mysql
+        usr/lib/mysql
+        usr/lib64/mysql
+        usr/lib64
+        usr/local/lib/mysql
+        opt/local/lib/mysql
+        opt/ports/lib/mysql5/mysql
+    )
+
+    find_library(MYSQL_EMBEDDED_LIBRARIES NAMES mysqld_pic mysqld libmysqld
+      PATHS
+        ~/usr/lib/mysql
+        /opt/local/lib/mysql5/mysql
+        /opt/mysqle/lib/mysql
+        /usr/lib/mysql
+        /usr/lib64/mysql
+        /usr/local/lib/mysql
+        /opt/mysql/lib/mysql
+        /opt/local/lib/mysql
+        /opt/ports/lib/mysql5/mysql
+    )
+
+    macro_push_required_vars()
+    set( CMAKE_REQUIRED_INCLUDES ${MYSQL_INCLUDE_DIR} )
+    set( CMAKE_REQUIRED_LIBRARIES ${MYSQL_EMBEDDED_LIBRARIES} )
+    include_directories( ${MYSQL_INCLUDE_DIR} )
+    check_cxx_source_compiles( "#if (defined(_WIN32) || defined(_WIN64))\n#define __LCC__\n#endif\n#include <mysql.h>\nint main() { int i = MYSQL_OPT_USE_EMBEDDED_CONNECTION; }" HAVE_MYSQL_EMBEDDED )
+    macro_pop_required_vars()
+
+endif(MYSQLCONFIG_EXECUTABLE)
 
 if(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
    set(MYSQL_FOUND TRUE)
@@ -74,12 +92,12 @@ else(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
    message(STATUS "MySQL not found.")
 endif(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
 
-if(MYSQL_INCLUDE_DIR AND MYSQL_EMBEDDED_LIBRARIES AND HAVE_MYSQL_OPT_EMBEDDED_CONNECTION)
+if(MYSQL_INCLUDE_DIR AND MYSQL_EMBEDDED_LIBRARIES AND HAVE_MYSQL_EMBEDDED)
    set(MYSQL_EMBEDDED_FOUND TRUE)
    message(STATUS "Found MySQL Embedded: ${MYSQL_INCLUDE_DIR}, ${MYSQL_EMBEDDED_LIBRARIES}")
-else(MYSQL_INCLUDE_DIR AND MYSQL_EMBEDDED_LIBRARIES AND HAVE_MYSQL_OPT_EMBEDDED_CONNECTION)
+else(MYSQL_INCLUDE_DIR AND MYSQL_EMBEDDED_LIBRARIES AND HAVE_MYSQL_EMBEDDED)
    set(MYSQL_EMBEDDED_FOUND FALSE)
    message(STATUS "MySQL Embedded not found.")
-endif(MYSQL_INCLUDE_DIR AND MYSQL_EMBEDDED_LIBRARIES AND HAVE_MYSQL_OPT_EMBEDDED_CONNECTION)
+endif(MYSQL_INCLUDE_DIR AND MYSQL_EMBEDDED_LIBRARIES AND HAVE_MYSQL_EMBEDDED)
 
 mark_as_advanced(MYSQL_INCLUDE_DIR MYSQL_LIBRARIES MYSQL_EMBEDDED_LIBRARIES)
