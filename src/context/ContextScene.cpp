5cf4947428c2b8e60c0e2ad6822c996676c71603
Mark Kretschmann <kretschmann@kde.org>
2010-08-31 08:22:31 +0200

    Also use workaround for Plasma crash bug with KDE 4.5.1.
    Apparently the bug fix for Plasma came too late for 4.5.0 tagging, so we
    have to use our workaround for 4.5.1 as well.

--- src/context/ContextScene.cpp
+++ src/context/ContextScene.cpp
@@ -43,13 +43,16 @@ ContextScene::~ContextScene()
 
 void ContextScene::loadDefaultSetup()
 {
-    // WORKAROUND for bug in KDE 4.5.0:
+    // WORKAROUND for a bug in KDE 4.5.0 and 4.5.1:
     // Delete amarok-appletsrc config file (created by Plasma), because Plasma tries
     // to load all applets listed in there, which can lead to crashes due to applets
     // being loaded twice.
     // See: BUG 246756
-    if( KDE::versionMajor() == 4 && KDE::versionMinor() == 5 && KDE::versionRelease() == 0 )
+    if( ( KDE::versionMajor() == 4 && KDE::versionMinor() == 5 && KDE::versionRelease() == 0 ) ||
+        ( KDE::versionMajor() == 4 && KDE::versionMinor() == 5 && KDE::versionRelease() == 1 ) )
+    {
         QFile::remove( KStandardDirs::locateLocal( "config", "amarok-appletsrc", false ) );
+    }
 
     Plasma::Containment* c = addContainment( "amarok_containment_vertical" );
     c->setScreen( -1 );
