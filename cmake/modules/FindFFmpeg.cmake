e0046cbb13e8979b3cb2467969e0f8d4b9e9ef96
Leo Franchi <lfranchi@kde.org>
2010-10-03 16:08:45 -0400

    Tweak cmake code for finding libavformat and libavcodec. Thanks to James Duncan for the patch!

--- cmake/modules/FindFFmpeg.cmake
+++ cmake/modules/FindFFmpeg.cmake
@@ -1,59 +1,140 @@
-find_path(LIBAVCODEC_INCLUDE_DIR NAMES avcodec.h
-   HINTS
-   ~/usr/include
-   /opt/local/include
-   /usr/include
-   /usr/local/include
-   /opt/kde4/include
-   ${KDE4_INCLUDE_DIR}
-   PATH_SUFFIXES libavcodec
-)
+# - Find libavcodec and libavformat
+# Find the libavcodec and libavformat include directories
+# and libraries
+#
+# This module defines
+#   FFMPEG_FOUND            - true if all of the following
+#                            are located and set properly
+#   LIBAVCODEC_INCLUDE_DIR  - libavcodec include directory
+#   LIBAVCODEC_LIBRARY      - libavcodec library
+#   LIBAVFORMAT_INCLUDE_DIR - libavformat include directory
+#   LIBAVFORMAT_LIBRARY     - libavformat library
 
-find_library(LIBAVCODEC_LIBRARY NAMES avcodec
+find_package( PkgConfig )
+if( PKG_CONFIG_FOUND )
+    # If pkg-config finds libavcodec, this will set:
+    #   PC_LIBAVCODEC_FOUND (to TRUE)
+    #   PC_LIBAVCODEC_INCLUDEDIR
+    #   PC_LIBAVCODEC_INCLUDE_DIRS
+    #   PC_LIBAVCODEC_LIBDIR
+    #   PC_LIBAVCODEC_LIBRARY_DIRS
+    # These variables are then used as hints to find_path()
+    # and find_library()
+    pkg_search_module( PC_LIBAVCODEC libavcodec )
+
+    # Same as above, but searching for libavformat instead
+    pkg_search_module( PC_LIBAVFORMAT libavformat )
+endif( PKG_CONFIG_FOUND )
+
+###############
+## libavcodec #
+###############
+
+find_path( LIBAVCODEC_INCLUDE_DIR libavcodec/avcodec.h
+    HINTS
+        # Hints provided by pkg-config
+        ${PC_LIBAVCODEC_INCLUDEDIR}
+        ${PC_LIBAVCODEC_INCLUDEDIR}/*
+        ${PC_LIBAVCODEC_INCLUDE_DIRS}
     PATHS
-    ~/usr/lib
-   /opt/local/lib
-   /usr/lib
-   /usr/lib64
-   /usr/local/lib
-   /opt/kde4/lib
-   ${KDE4_LIB_DIR}
+        # Standard include directories
+        /usr/include/
+        ~/usr/include/
+        /opt/local/include/
+        /usr/local/include/
+        /opt/kde4/include/
+        ${KDE4_INCLUDE_DIR}/
+        # Search all subdirs of the above
+        /usr/include/*
+        ~/usr/include/*
+        /opt/local/include/*
+        /usr/local/include/*
+        /opt/kde4/include/*
+        ${KDE4_INCLUDE_DIR}/*
+    PATH_SUFFIXES
+        # Subdirectory hints
+        libavcodec
+        ffmpeg
+        ffmpeg/libavcodec
 )
 
-find_path(LIBAVFORMAT_INCLUDE_DIR NAMES avformat.h
-   HINTS
-   ~/usr/include
-   /opt/local/include
-   /usr/include
-   /usr/local/include
-   /opt/kde4/include
-   ${KDE4_INCLUDE_DIR}
-   PATH_SUFFIXES libavformat
-)
-
-find_library(LIBAVFORMAT_LIBRARY NAMES avformat
+find_library( LIBAVCODEC_LIBRARY avcodec
+    HINTS
+        # Hints provided by pkg-config
+        ${PC_LIBAVCODEC_LIBDIR}
+        ${PC_LIBAVCODEC_LIBRARY_DIRS}
     PATHS
-    ~/usr/lib
-   /opt/local/lib
-   /usr/lib
-   /usr/lib64
-   /usr/local/lib
-   /opt/kde4/lib
-   ${KDE4_LIB_DIR}
+        ~/usr/lib/
+        /opt/local/lib/
+        /usr/lib/
+        /usr/lib64/
+        /usr/local/lib/
+        /opt/kde4/lib/
+        ${KDE4_LIB_DIR}
 )
 
-if(LIBAVCODEC_INCLUDE_DIR AND LIBAVCODEC_LIBRARY AND
-   LIBAVFORMAT_INCLUDE_DIR AND LIBAVFORMAT_LIBRARY)
-   set(FFMPEG_FOUND TRUE)
-   message(STATUS "Found ffmpeg:")
-   message(STATUS "\tlibavcodec: ${LIBAVCODEC_INCLUDE_DIR}, ${LIBAVCODEC_LIBRARY}")
-   message(STATUS "\tlibavformat: ${LIBAVFORMAT_INCLUDE_DIR}, ${LIBAVFORMAT_LIBRARY}")
-else(LIBAVCODEC_INCLUDE_DIR AND LIBAVCODEC_LIBRARY AND LIBAVFORMAT_INCLUDE_DIR AND LIBAVFORMAT_LIBRARY)
-   set(FFMPEG_FOUND FALSE)
-   if (FFMPEG_FIND_REQUIRED)
-      message(FATAL_ERROR "Could NOT find required package ffmpeg")
-   endif(FFMPEG_FIND_REQUIRED)
-endif(LIBAVCODEC_INCLUDE_DIR AND LIBAVCODEC_LIBRARY AND
-      LIBAVFORMAT_INCLUDE_DIR AND LIBAVFORMAT_LIBRARY)
+################
+## libavformat #
+################
 
-mark_as_advanced(LIBAVCODEC_LIBRARY LIBAVFORMAT_LIBRARY)
+find_path( LIBAVFORMAT_INCLUDE_DIR libavformat/avformat.h
+    HINTS
+        # Hints provided by pkg-config
+        ${PC_LIBAVFORMAT_INCLUDEDIR}
+        ${PC_LIBAVFORMAT_INCLUDEDIR}/*
+        ${PC_LIBAVFORMAT_INCLUDE_DIRS}
+    PATHS
+        # Standard include directories
+        /usr/include/
+        ~/usr/include/
+        /opt/local/include/
+        /usr/local/include/
+        /opt/kde4/include/
+        ${KDE4_INCLUDE_DIR}/
+        # Search all subdirs of the above
+        /usr/include/*
+        ~/usr/include/*
+        /opt/local/include/*
+        /usr/local/include/*
+        /opt/kde4/include/*
+        ${KDE4_INCLUDE_DIR}/*
+    PATH_SUFFIXES
+        # Subdirectory hints
+        libavformat
+        ffmpeg
+        ffmpeg/libavformat
+)
+
+find_library( LIBAVFORMAT_LIBRARY avformat
+    HINTS
+        # Hints provided by pkg-config
+        ${PC_LIBAVFORMAT_LIBDIR}
+        ${PC_LIBAVFORMAT_LIBRARY_DIRS}
+    PATHS
+        ~/usr/lib/
+        /opt/local/lib/
+        /usr/lib/
+        /usr/lib64/
+        /usr/local/lib/
+        /opt/kde4/lib/
+        ${KDE4_LIB_DIR}
+)
+
+include( FindPackageHandleStandardArgs )
+# Sets FFMPEG_FOUND to true if all of the following are set:
+#   LIBAVCODEC_INCLUDE_DIR
+#   LIBAVCODEC_LIBRARY
+#   LIBAVFORMAT_INCLUDE_DIR
+#   LIBAVFORMAT_LIBRARY
+find_package_handle_standard_args( FFmpeg DEFAULT_MSG
+    LIBAVCODEC_INCLUDE_DIR
+    LIBAVCODEC_LIBRARY
+    LIBAVFORMAT_INCLUDE_DIR
+    LIBAVFORMAT_LIBRARY
+)
+if( FFMPEG_FOUND )
+    message( STATUS "\tlibavcodec: ${LIBAVCODEC_INCLUDE_DIR}, ${LIBAVCODEC_LIBRARY}" )
+    message( STATUS "\tlibavformat: ${LIBAVFORMAT_INCLUDE_DIR}, ${LIBAVFORMAT_LIBRARY}" )
+endif( FFMPEG_FOUND )
+
+mark_as_advanced( LIBAVCODEC_LIBRARY LIBAVFORMAT_LIBRARY )
