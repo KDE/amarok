KDE/amarok

commit ec210e0cf3119818a1d359885b77f6ef21a86cea

Use QT_USE_FAST_CONCATENATION and QT_USE_FAST_OPERATOR_PLUS for performance.
See http://doc.trolltech.com/4.6/qstring.html#more-efficient-string-construction.

I had to clean up lots of incorrect string concatenations. Please check for
correctness.

Revision 2:
Various fixes for fast-QString patch.

Revision 3:
Fix liblastfm build issues by removing definitions for the folder.

src/services/lastfm/LastFmService.cpp
@@ -251,7 +251,7 @@ LastFmService::init()
 
     debug() << "username:" << QString( QUrl::toPercentEncoding( lastfm::ws::Username ) );
 
-    QString authToken =  md5( ( m_userName + md5( password.toUtf8() ) ).toUtf8() );
+    const QString authToken = md5( m_userName.toUtf8() ) + md5( password.toUtf8() );
 
     // now authenticate w/ last.fm and get our session key if we don't have one
     if( sessionKey.isEmpty() )

src/services/lastfm/LastFmServiceSettings.cpp
@@ -112,7 +112,8 @@ LastFmServiceSettings::testLogin()
 
     debug() << "username:" << QString( QUrl::toPercentEncoding( lastfm::ws::Username ) );
 
-    QString authToken =  md5( ( m_configDialog->kcfg_ScrobblerUsername->text() + md5( m_configDialog->kcfg_ScrobblerPassword->text().toUtf8() ) ).toUtf8() );
+    const QString authToken = QString( md5( m_configDialog->kcfg_ScrobblerUsername->text().toUtf8() ) +
+                                       md5( m_configDialog->kcfg_ScrobblerPassword->text().toUtf8() ) );
 
     // now authenticate w/ last.fm and get our session key
     QMap<QString, QString> query;

src/CMakeLists.txt
@@ -1,3 +1,7 @@
+# Improves speed of string concatenation
+add_definitions(-DQT_USE_FAST_CONCATENATION)
+add_definitions(-DQT_USE_FAST_OPERATOR_PLUS)
+
 add_definitions(${TAGLIB_CFLAGS} ${TAGLIB-EXTRAS_CFLAGS})
 
 if(APPLE)

src/aboutdialog/OcsPersonItem.cpp
@@ -171,7 +171,10 @@ OcsPersonItem::fillOcsData( const AmarokAttica::Person &ocsPerson )
         m_avatar->setAlignment( Qt::AlignCenter );
     }
     if( !( ocsPerson.city().isEmpty() && ocsPerson.country().isEmpty() ) )
-        m_aboutText.append( "<br/>" + ( ocsPerson.city().isEmpty() ? "" : ( ocsPerson.city() + ", " ) ) + ocsPerson.country() );
+        if( ocsPerson.city().isEmpty() )
+            m_aboutText.append( QString( "<br/>" + ocsPerson.city() + ", " ) );
+        else
+            m_aboutText.append( QString( "<br/>" + ocsPerson.country() ) );
 
     if( m_status == Author )
     {

src/browsers/collectionbrowser/CollectionWidget.cpp
@@ -173,9 +173,9 @@ CollectionWidget::init()
     // Filter presets. UserRole is used to store the actual syntax.
     KComboBox *combo = d->searchWidget->comboBox();
     const KIcon icon = KStandardGuiItem::find().icon();
-    combo->addItem( icon, i18n("Added Today"), i18n("added") + QString(":<1d") );
-    combo->addItem( icon, i18n("Added This Week"), i18n("added") + QString(":<1w") );
-    combo->addItem( icon, i18n("Added This Month"), i18n("added") + QString(":<1m") );
+    combo->addItem( icon, i18n("Added Today"), i18n("added:<1d" ) );
+    combo->addItem( icon, i18n("Added This Week"), i18n("added:<1w") );
+    combo->addItem( icon, i18n("Added This Month"), i18n("added:<1m") );
     combo->insertSeparator( combo->count() );
 
     QMenu *filterMenu = new QMenu( 0 );

src/browsers/playlistbrowser/DynamicModel.cpp
@@ -226,7 +226,7 @@ PlaylistBrowserNS::DynamicModel::data ( const QModelIndex & i, int role ) const
         case Qt::DisplayRole:
         case Qt::EditRole:
             if( i.row() == m_activePlaylist && m_activeUnsaved )
-                return title + " (" + i18n("modified") + ')';
+                return QString( i18n( "%1 (modified) ", title ) );
             else
                 return title;
         default:

src/context/applets/similarartists/SimilarArtistsApplet.cpp
@@ -396,12 +396,12 @@ SimilarArtistsApplet::saveMaxArtists()
     m_maxArtists = m_temp_maxArtists;
 
     dataEngine( "amarok-similarArtists" )->query( QString( "similarArtists:maxArtists:" )
-            + m_maxArtists );
+            + QString::number( m_maxArtists ) );
 
     KConfigGroup config = Amarok::config( "SimilarArtists Applet" );
     config.writeEntry( "maxArtists", m_maxArtists );
     dataEngine( "amarok-similarArtists" )->query( QString( "similarArtists:maxArtists:" )
-            + m_maxArtists );
+            + QString::number( m_maxArtists ) );
 }
 
 void

src/context/applets/upcomingevents/UpcomingEventsApplet.cpp
@@ -383,12 +383,13 @@ UpcomingEventsApplet::saveAddressAsLink()
     DEBUG_BLOCK
 
     m_enabledLinks = m_temp_enabledLinks;
+    const QString enabledLinks = m_enabledLinks ? "true" : "false";
 
-    dataEngine( "amarok-upcomingEvents" )->query( QString( "upcomingEvents:enabledLinks:" ) + m_enabledLinks );
+    dataEngine( "amarok-upcomingEvents" )->query( QString( "upcomingEvents:enabledLinks:" ) + enabledLinks );
 
     KConfigGroup config = Amarok::config("UpcomingEvents Applet");
     config.writeEntry( "enabledLinks", m_enabledLinks );
-    dataEngine( "amarok-upcomingEvents" )->query( QString( "upcomingEvents:enabledLinks:" ) + m_enabledLinks );
+    dataEngine( "amarok-upcomingEvents" )->query( QString( "upcomingEvents:enabledLinks:" ) + enabledLinks );
 }
 
 /**

commit d01e51eee853596aa04e377253c6f3d83b6483c3

Fix last.fm auth failure introduced by ec210e0cf3119818a1d359885b77f6ef21a86cea
BUG: 253788
CCMAIL: kretschmann@kde.org

src/services/lastfm/LastFmService.cpp
@@ -251,7 +251,7 @@ LastFmService::init()
 
     debug() << "username:" << QString( QUrl::toPercentEncoding( lastfm::ws::Username ) );
 
-    const QString authToken = md5( m_userName.toUtf8() ) + md5( password.toUtf8() );
+    const QString authToken = md5( ( m_userName + md5( password.toUtf8() ) ).toUtf8() );
 
     // now authenticate w/ last.fm and get our session key if we don't have one
     if( sessionKey.isEmpty() )

src/services/lastfm/LastFmServiceSettings.cpp
@@ -14,6 +14,8 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
 
+#define DEBUG_PREFIX "LastFmServiceSettings"
+
 #include "LastFmServiceSettings.h"
 
 #include "core/support/Amarok.h"
@@ -112,8 +114,8 @@ LastFmServiceSettings::testLogin()
 
     debug() << "username:" << QString( QUrl::toPercentEncoding( lastfm::ws::Username ) );
 
-    const QString authToken = QString( md5( m_configDialog->kcfg_ScrobblerUsername->text().toUtf8() ) +
-                                       md5( m_configDialog->kcfg_ScrobblerPassword->text().toUtf8() ) );
+    const QString authToken = md5( ( m_configDialog->kcfg_ScrobblerUsername->text() +
+                                     md5( m_configDialog->kcfg_ScrobblerPassword->text().toUtf8() ) ).toUtf8() );
 
     // now authenticate w/ last.fm and get our session key
     QMap<QString, QString> query;