73fb64f3ce98034b266333f4c203fc0344f38122
Mark Kretschmann <kretschmann@kde.org>
2010-10-17 17:42:44 +0200

    Follow the KDE HIG with case sensitivity.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1158,7 +1158,7 @@ MainWindow::slotShowMenuBar()
         if (KMessageBox::warningContinueCancel(this,
             i18n("You have chosen to hide the menu bar.\n\nPlease remember that you can always use the shortcut \"%1\" to bring it back.")
                 .arg(m_showMenuBar->shortcut().toString()),
-            i18n("Hide menu"), KStandardGuiItem::cont(), KStandardGuiItem::cancel(), "showMenubar") != KMessageBox::Continue)
+            i18n("Hide Menu"), KStandardGuiItem::cont(), KStandardGuiItem::cancel(), "showMenubar") != KMessageBox::Continue)
         {
             //Cancel menu hiding. Revert menu item to checked state.
             m_showMenuBar->setChecked(true);
