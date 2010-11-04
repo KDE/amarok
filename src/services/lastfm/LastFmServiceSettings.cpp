cf4186944751ac57c148cefb40d76bc8f794d392
Leo Franchi <lfranchi@kde.org>
2010-11-04 18:51:11 -0400

    also fix auth token generation in settings dialog

--- src/services/lastfm/LastFmServiceSettings.cpp
+++ src/services/lastfm/LastFmServiceSettings.cpp
@@ -104,9 +104,9 @@ LastFmServiceSettings::testLogin()
         lastfm::setNetworkAccessManager( The::networkAccessManager() );
 
     debug() << "username:" << QString( QUrl::toPercentEncoding( lastfm::ws::Username ) );
-
-    const QString authToken = QString( md5( m_configDialog->kcfg_ScrobblerUsername->text().toUtf8() ) +
-                                       md5( m_configDialog->kcfg_ScrobblerPassword->text().toUtf8() ) );
+    
+    const QString authToken = md5( QString( "%1%2" ).arg( m_configDialog->kcfg_ScrobblerUsername->text() )
+                                                    .arg( md5( m_configDialog->kcfg_ScrobblerPassword->text().toUtf8() ) ).toUtf8() );
 
     // now authenticate w/ last.fm and get our session key
     QMap<QString, QString> query;
