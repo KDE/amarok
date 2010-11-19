KDE/amarok

commit e4ae348b5851e65ac50921c21035760bdf2a024f

Cumulative patch of Album Artist oriented staff. Adds QueryType for QueryMaker. Adds ability to group tracks by Album Artist in Collection Browser. Adds Album Artist field to TagDialog. Adds Album Artist token to TagGuesser and Organize Collection dialogs. Adds ability to set/update Album Artist on iPod.

src/core-impl/collections/mtpcollection/handler/capabilities/MtpWriteCapability.cpp
@@ -107,6 +107,11 @@ MtpWriteCapability::libSetArtist( Meta::MediaDeviceTrackPtr &track, const QStrin
     m_handler->libSetArtist( track, artist );
 }
 
+void
+MtpWriteCapability::libSetAlbumArtist( Meta::MediaDeviceTrackPtr &track, const QString &albumArtist )
+{
+    m_handler->ligSetAlbumArtist( track, albumArtist );
+}
 
 void
 MtpWriteCapability::libSetComposer( Meta::MediaDeviceTrackPtr &track, const QString& composer )

src/amarokurls/NavigationUrlGenerator.cpp
@@ -86,6 +86,9 @@ AmarokUrl NavigationUrlGenerator::CreateAmarokUrl()
             case CategoryId::Album:
                 sortMode += "album-";
                 break;
+            case CategoryId::AlbumArtist:
+                sortMode += "albumartist-";
+                break;
             case CategoryId::Composer:
                 sortMode += "composer-";
                 break;

src/amarokurls/NavigationUrlRunner.cpp
@@ -67,6 +67,8 @@ NavigationUrlRunner::run( AmarokUrl url )
                 levels.append( CategoryId::Artist );
             else if( levelString == "album" )
                 levels.append( CategoryId::Album );
+            else if( levelString == "albumartist" )
+                levels.append( CategoryId::AlbumArtist );
             else if( levelString == "composer" )
                 levels.append( CategoryId::Composer );
             else if( levelString == "year" )

src/browsers/CollectionTreeItemModelBase.cpp
@@ -437,7 +437,11 @@ CollectionTreeItemModelBase::mimeData(const QList<CollectionTreeItem*> & items) 
             while( tmpItem->isDataItem() )
             {
                 if( tmpItem->data() )
+                {
+                    if( levelCategory( tmpItem->level() - 1 ) == CategoryId::AlbumArtist )
+                        qm->setArtistQueryMode( Collections::QueryMaker::AlbumArtists );
                     qm->addMatch( tmpItem->data() );
+                }
                 else
                     qm->setAlbumQueryMode( Collections::QueryMaker::OnlyCompilations );
                 tmpItem = tmpItem->parent();
@@ -489,6 +493,9 @@ CollectionTreeItemModelBase::iconForLevel(int level) const
             icon = "media-optical-amarok";
             break;
         case CategoryId::Artist :
+            icon = "view-media-artist-amarok";
+            break;
+        case CategoryId::AlbumArtist :
             icon = "view-media-artist-amarok";
             break;
         case CategoryId::Composer :
@@ -544,13 +551,18 @@ void CollectionTreeItemModelBase::listForLevel(int level, Collections::QueryMake
                 case CategoryId::Album :
                     //restrict query to normal albums if the previous level
                     //was the artist category. in that case we handle compilations below
-                    if( level > 0 && m_levelType[level-1] == CategoryId::Artist && !parent->isVariousArtistItem() )
+                    if( level > 0 &&
+                        ( m_levelType[level-1] == CategoryId::Artist ||
+                          m_levelType[level-1] == CategoryId::AlbumArtist
+                        ) &&
+                        !parent->isVariousArtistItem() )
                     {
                         qm->setAlbumQueryMode( Collections::QueryMaker::OnlyNormalAlbums );
                     }
                     break;
 
                 case CategoryId::Artist :
+                case CategoryId::AlbumArtist:
                     //handle compilations only if the next level ist CategoryId::Album
                     if( nextLevel == Collections::QueryMaker::Album )
                     {
@@ -574,7 +586,11 @@ void CollectionTreeItemModelBase::listForLevel(int level, Collections::QueryMake
             if( tmpItem->isVariousArtistItem() )
                 qm->setAlbumQueryMode( Collections::QueryMaker::OnlyCompilations );
             else if( tmpItem->data() )
+            {
+                if( levelCategory( tmpItem->level() - 1 ) == CategoryId::AlbumArtist )
+                    qm->setArtistQueryMode( Collections::QueryMaker::AlbumArtists );
                 qm->addMatch( tmpItem->data() );
+            }
         }
         addFilters( qm );
         qm->setReturnResultAsDataPtrs( true );
@@ -601,6 +617,9 @@ CollectionTreeItemModelBase::mapCategoryToQueryType( int levelType ) const
         break;
     case CategoryId::Artist:
         type = Collections::QueryMaker::Artist;
+        break;
+    case CategoryId::AlbumArtist:
+        type = Collections::QueryMaker::AlbumArtist;
         break;
     case CategoryId::Composer:
         type = Collections::QueryMaker::Composer;
@@ -661,6 +680,8 @@ CollectionTreeItemModelBase::addFilters( Collections::QueryMaker * qm ) const
                     ADD_OR_EXCLUDE_FILTER( Meta::valAlbum, elem.text, false, false );
                 if( ( validFilters & Collections::QueryMaker::ArtistFilter ) )
                     ADD_OR_EXCLUDE_FILTER( Meta::valArtist, elem.text, false, false );
+                if( ( validFilters & Collections::QueryMaker::AlbumArtistFilter ) )
+                    ADD_OR_EXCLUDE_FILTER( Meta::valAlbumArtist, elem.text, false, false );
                 if( ( validFilters & Collections::QueryMaker::ComposerFilter ) )
                     ADD_OR_EXCLUDE_FILTER( Meta::valComposer, elem.text, false, false );
                 if( ( validFilters & Collections::QueryMaker::GenreFilter ) )
@@ -700,6 +721,11 @@ CollectionTreeItemModelBase::addFilters( Collections::QueryMaker * qm ) const
                 {
                     if ( ( validFilters & Collections::QueryMaker::ArtistFilter ) == 0 ) continue;
                     ADD_OR_EXCLUDE_FILTER( Meta::valArtist, elem.text, false, false );
+                }
+                else if ( lcField.compare( "albumartist", Qt::CaseInsensitive ) == 0 || lcField.compare( i18n( "albumartist" ), Qt::CaseInsensitive ) == 0 )
+                {
+                    if ( ( validFilters & Collections::QueryMaker::AlbumArtistFilter ) == 0 ) continue;
+                    ADD_OR_EXCLUDE_FILTER( Meta::valAlbumArtist, elem.text, false, false );
                 }
                 else if ( lcField.compare( "genre", Qt::CaseInsensitive ) == 0 || lcField.compare( i18n( "genre" ), Qt::CaseInsensitive ) == 0)
                 {
@@ -1164,6 +1190,7 @@ CollectionTreeItemModelBase::nameForLevel(int level) const
     {
         case CategoryId::Album      : return AmarokConfig::showYears() ? i18n( "Year - Album" ) : i18n( "Album" );
         case CategoryId::Artist     : return i18n( "Artist" );
+        case CategoryId::AlbumArtist: return i18n( "Album Artist" );
         case CategoryId::Composer   : return i18n( "Composer" );
         case CategoryId::Genre      : return i18n( "Genre" );
         case CategoryId::Year       : return i18n( "Year" );
@@ -1437,6 +1464,15 @@ void CollectionTreeItemModelBase::itemAboutToBeDeleted( CollectionTreeItem *item
         //Nuke it
         qm->deleteLater();
     }
+}
+
+int
+CollectionTreeItemModelBase::levelCategory( const int level ) const
+{
+    if( level >= 0 && level + levelModifier() < m_levelType.count() )
+        return m_levelType[ level + levelModifier() ];
+
+    return CategoryId::None;
 }
 
 #include "CollectionTreeItemModelBase.moc"

src/browsers/CollectionTreeItemModelBase.h
@@ -46,6 +46,7 @@ namespace CategoryId
     None = 0,
     Album,
     Artist,
+    AlbumArtist,
     Composer,
     Genre,
     Year,
@@ -87,6 +88,7 @@ class AMAROK_EXPORT CollectionTreeItemModelBase : public QAbstractItemModel
 
         virtual void setLevels( const QList<int> &levelType ) = 0;
         virtual QList<int> levels() const { return m_levelType; }
+        virtual int levelCategory( const int level ) const;
 
         virtual void addFilters( Collections::QueryMaker *qm ) const;
 

src/browsers/CollectionTreeView.cpp
@@ -1210,10 +1210,14 @@ CollectionTreeView::createMetaQueryFromItems( const QSet<CollectionTreeItem*> &i
         CollectionTreeItem *tmp = item;
         while( tmp->isDataItem() )
         {
-            if ( tmp->isVariousArtistItem() )
+            if( tmp->isVariousArtistItem() )
                 qm->setAlbumQueryMode( Collections::QueryMaker::OnlyCompilations );
-            else
+            else if( tmp->data() )
+            {
+                if( m_treeModel->levelCategory( tmp->data() - 1 ) == CategoryId::AlbumArtist )
+                    qm->setArtistQueryMode( Collections::QueryMaker::AlbumArtists );
                 qm->addMatch( tmp->data() );
+            }
             tmp = tmp->parent();
         }
         m_treeModel->addFilters( qm );

src/browsers/collectionbrowser/CollectionWidget.cpp
@@ -198,30 +198,35 @@ CollectionWidget::init()
 
     d->menuLevel[0] = filterMenu->addMenu( i18n( "First Level" ) );
 
-    QAction *firstArtistAction   = d->menuLevel[0]->addAction( i18n( "Artist" ) );
+    QAction *firstArtistAction      = d->menuLevel[0]->addAction( i18n( "Artist" ) );
     firstArtistAction->setData( CategoryId::Artist );
 
-    QAction *firstAlbumAction    = d->menuLevel[0]->addAction( i18n( "Album"  ) );
+    QAction *firstAlbumAction       = d->menuLevel[0]->addAction( i18n( "Album"  ) );
     firstAlbumAction->setData( CategoryId::Album );
 
-    QAction *firstGenreAction    = d->menuLevel[0]->addAction( i18n( "Genre"  ) );
+    QAction *firstAlbumArtistAction = d->menuLevel[0]->addAction( i18n( "Album Artist"  ) );
+    firstAlbumArtistAction->setData( CategoryId::AlbumArtist );
+
+    QAction *firstGenreAction       = d->menuLevel[0]->addAction( i18n( "Genre"  ) );
     firstGenreAction->setData( CategoryId::Genre );
 
-    QAction *firstComposerAction = d->menuLevel[0]->addAction( i18n( "Composer" ) );
+    QAction *firstComposerAction    = d->menuLevel[0]->addAction( i18n( "Composer" ) );
     firstComposerAction->setData( CategoryId::Composer );
 
-    QAction *firstLabelAction = d->menuLevel[0]->addAction( i18n( "Label" ) );
+    QAction *firstLabelAction       = d->menuLevel[0]->addAction( i18n( "Label" ) );
     firstLabelAction->setData( CategoryId::Label );
 
-    firstArtistAction->setCheckable  ( true );
-    firstAlbumAction->setCheckable   ( true );
-    firstGenreAction->setCheckable   ( true );
-    firstComposerAction->setCheckable( true );
-    firstLabelAction->setCheckable( true );
+    firstArtistAction->setCheckable     ( true );
+    firstAlbumAction->setCheckable      ( true );
+    firstAlbumArtistAction->setCheckable( true );
+    firstGenreAction->setCheckable      ( true );
+    firstComposerAction->setCheckable   ( true );
+    firstLabelAction->setCheckable      ( true );
 
     QActionGroup *firstGroup = new QActionGroup( this );
     firstGroup->addAction( firstArtistAction );
     firstGroup->addAction( firstAlbumAction );
+    firstGroup->addAction( firstAlbumArtistAction );
     firstGroup->addAction( firstGenreAction );
     firstGroup->addAction( firstComposerAction );
     firstGroup->addAction( firstLabelAction );
@@ -229,35 +234,40 @@ CollectionWidget::init()
     connect( d->menuLevel[0], SIGNAL( triggered( QAction *) ), SLOT( customFilter( QAction * ) ) );
 
     d->menuLevel[1] = filterMenu->addMenu( i18n( "Second Level" ) );
-    QAction *secondNullAction     = d->menuLevel[1]->addAction( i18n( "None" ) );
+    QAction *secondNullAction       = d->menuLevel[1]->addAction( i18n( "None" ) );
     secondNullAction->setData( CategoryId::None );
 
-    QAction *secondArtistAction   = d->menuLevel[1]->addAction( i18n( "Artist" ) );
+    QAction *secondArtistAction     = d->menuLevel[1]->addAction( i18n( "Artist" ) );
     secondArtistAction->setData( CategoryId::Artist );
 
-    QAction *secondAlbumAction    = d->menuLevel[1]->addAction( i18n( "Album"  ) );
+    QAction *secondAlbumAction      = d->menuLevel[1]->addAction( i18n( "Album"  ) );
     secondAlbumAction->setData( CategoryId::Album );
 
-    QAction *secondGenreAction    = d->menuLevel[1]->addAction( i18n( "Genre"  ) );
+    QAction *secondAlbumArtistAction= d->menuLevel[1]->addAction( i18n( "Album Artist"  ) );
+    secondAlbumArtistAction->setData( CategoryId::AlbumArtist );
+
+    QAction *secondGenreAction      = d->menuLevel[1]->addAction( i18n( "Genre"  ) );
     secondGenreAction->setData( CategoryId::Genre );
 
-    QAction *secondComposerAction = d->menuLevel[1]->addAction( i18n( "Composer" ) );
+    QAction *secondComposerAction   = d->menuLevel[1]->addAction( i18n( "Composer" ) );
     secondComposerAction->setData( CategoryId::Composer );
 
-    QAction *secondLabelAction = d->menuLevel[1]->addAction( i18n( "Label" ) );
+    QAction *secondLabelAction      = d->menuLevel[1]->addAction( i18n( "Label" ) );
     secondLabelAction->setData( CategoryId::Label );
 
-    secondNullAction->setCheckable    ( true );
-    secondArtistAction->setCheckable  ( true );
-    secondAlbumAction->setCheckable   ( true );
-    secondGenreAction->setCheckable   ( true );
-    secondComposerAction->setCheckable( true );
-    secondLabelAction->setCheckable( true );
+    secondNullAction->setCheckable       ( true );
+    secondArtistAction->setCheckable     ( true );
+    secondAlbumAction->setCheckable      ( true );
+    secondAlbumArtistAction->setCheckable( true );
+    secondGenreAction->setCheckable      ( true );
+    secondComposerAction->setCheckable   ( true );
+    secondLabelAction->setCheckable      ( true );
 
     QActionGroup *secondGroup = new QActionGroup( this );
     secondGroup->addAction( secondNullAction );
     secondGroup->addAction( secondArtistAction );
     secondGroup->addAction( secondAlbumAction );
+    secondGroup->addAction( secondAlbumArtistAction );
     secondGroup->addAction( secondGenreAction );
     secondGroup->addAction( secondComposerAction );
     secondGroup->addAction( secondLabelAction );
@@ -266,35 +276,40 @@ CollectionWidget::init()
     connect( d->menuLevel[1], SIGNAL( triggered( QAction *) ), SLOT( customFilter( QAction * ) ) );
 
     d->menuLevel[2] = filterMenu->addMenu( i18n( "Third Level" ) );
-    QAction *thirdNullAction     = d->menuLevel[2]->addAction( i18n( "None" ) );
+    QAction *thirdNullAction        = d->menuLevel[2]->addAction( i18n( "None" ) );
     thirdNullAction->setData( CategoryId::None );
 
-    QAction *thirdArtistAction   = d->menuLevel[2]->addAction( i18n( "Artist" ) );
+    QAction *thirdArtistAction      = d->menuLevel[2]->addAction( i18n( "Artist" ) );
     thirdArtistAction->setData( CategoryId::Artist );
 
-    QAction *thirdAlbumAction    = d->menuLevel[2]->addAction( i18n( "Album"  ) );
+    QAction *thirdAlbumAction       = d->menuLevel[2]->addAction( i18n( "Album"  ) );
     thirdAlbumAction->setData( CategoryId::Album );
 
-    QAction *thirdGenreAction    = d->menuLevel[2]->addAction( i18n( "Genre"  ) );
+    QAction *thirdAlbumArtistAction = d->menuLevel[2]->addAction( i18n( "Album Artist"  ) );
+    thirdAlbumArtistAction->setData( CategoryId::AlbumArtist );
+
+    QAction *thirdGenreAction       = d->menuLevel[2]->addAction( i18n( "Genre"  ) );
     thirdGenreAction->setData( CategoryId::Genre );
 
-    QAction *thirdComposerAction = d->menuLevel[2]->addAction( i18n( "Composer" ) );
+    QAction *thirdComposerAction    = d->menuLevel[2]->addAction( i18n( "Composer" ) );
     thirdComposerAction->setData( CategoryId::Composer );
 
-    QAction *thirdLabelAction = d->menuLevel[2]->addAction( i18n( "Label" ) );
+    QAction *thirdLabelAction       = d->menuLevel[2]->addAction( i18n( "Label" ) );
     thirdLabelAction->setData( CategoryId::Label );
 
-    thirdNullAction->setCheckable    ( true );
-    thirdArtistAction->setCheckable  ( true );
-    thirdAlbumAction->setCheckable   ( true );
-    thirdGenreAction->setCheckable   ( true );
-    thirdComposerAction->setCheckable( true );
-    thirdLabelAction->setCheckable( true );
+    thirdNullAction->setCheckable       ( true );
+    thirdArtistAction->setCheckable     ( true );
+    thirdAlbumAction->setCheckable      ( true );
+    thirdAlbumArtistAction->setCheckable( true );
+    thirdGenreAction->setCheckable      ( true );
+    thirdComposerAction->setCheckable   ( true );
+    thirdLabelAction->setCheckable      ( true );
 
     QActionGroup *thirdGroup = new QActionGroup( this );
     thirdGroup->addAction( thirdNullAction );
     thirdGroup->addAction( thirdArtistAction );
     thirdGroup->addAction( thirdAlbumAction );
+    thirdGroup->addAction( thirdAlbumArtistAction );
     thirdGroup->addAction( thirdGenreAction );
     thirdGroup->addAction( thirdComposerAction );
     thirdGroup->addAction( thirdLabelAction );
@@ -335,6 +350,9 @@ CollectionWidget::init()
             case CategoryId::Album:
                 firstAlbumAction->setChecked( true );
                 break;
+            case CategoryId::AlbumArtist:
+                firstAlbumArtistAction->setChecked( true );
+                break;
             case CategoryId::Genre:
                 firstGenreAction->setChecked( true );
                 break;
@@ -356,6 +374,9 @@ CollectionWidget::init()
                 break;
             case CategoryId::Album:
                 secondAlbumAction->setChecked( true );
+                break;
+            case CategoryId::AlbumArtist:
+                secondAlbumArtistAction->setChecked( true );
                 break;
             case CategoryId::Genre:
                 secondGenreAction->setChecked( true );
@@ -380,6 +401,9 @@ CollectionWidget::init()
                 break;
             case CategoryId::Album:
                 thirdAlbumAction->setChecked( true );
+                break;
+            case CategoryId::AlbumArtist:
+                thirdAlbumArtistAction->setChecked( true );
                 break;
             case CategoryId::Genre:
                 thirdGenreAction->setChecked( true );
@@ -416,8 +440,6 @@ CollectionWidget::init()
     QToolButton *tbutton = qobject_cast<QToolButton*>( d->searchWidget->toolBar()->widgetForAction( searchMenuAction ) );
     if( tbutton )
         tbutton->setPopupMode( QToolButton::InstantPopup );
-    
-
 }
 
 void

src/core-impl/collections/db/sql/SqlMeta.cpp
@@ -539,7 +539,17 @@ SqlTrack::setAlbumArtist( const QString &newAlbumArtist )
     if( m_album.isNull() )
         return;
 
-    m_cache.insert( Meta::valAlbumArtist, ArtistHelper::realTrackArtist( newAlbumArtist ) );
+    if( !newAlbumArtist.compare( "Various Artists", Qt::CaseInsensitive ) ||
+        !newAlbumArtist.compare( i18n( "Various Artists" ), Qt::CaseInsensitive ) )
+    {
+        m_cache.insert( Meta::valCompilation, true );
+    }
+    else
+    {
+        m_cache.insert( Meta::valAlbumArtist, ArtistHelper::realTrackArtist( newAlbumArtist ) );
+        m_cache.insert( Meta::valCompilation, false );
+    }
+
     if( !m_batchUpdate )
         commitMetaDataChanges();
 }
@@ -960,9 +970,6 @@ SqlTrack::commitMetaDataChanges()
                 newArtistName = QString();
             else if( oldAlbum && oldAlbum->hasAlbumArtist() )
                 newArtistName = oldAlbum->albumArtist()->name();
-
-            if( newArtistName.compare( "Various Artists", Qt::CaseInsensitive ) == 0 )
-                newArtistName.clear();
 
             m_album = m_collection->registry()->getAlbum( m_cache.contains( Meta::valAlbum)
                                                           ? m_cache.value( Meta::valAlbum ).toString()

commit eeeddae94ae381bda169ca0ad27045bf48698081

Fix typo introduced in e4ae348b.
ligSetAlbumArtist -> libSetAlbumArtist

src/core-impl/collections/mtpcollection/handler/capabilities/MtpWriteCapability.cpp
@@ -110,7 +110,7 @@ MtpWriteCapability::libSetArtist( Meta::MediaDeviceTrackPtr &track, const QStrin
 void
 MtpWriteCapability::libSetAlbumArtist( Meta::MediaDeviceTrackPtr &track, const QString &albumArtist )
 {
-    m_handler->ligSetAlbumArtist( track, albumArtist );
+    m_handler->libSetAlbumArtist( track, albumArtist );
 }
 
 void