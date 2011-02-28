0df0c8444c3c58cea48abdcdd6361f4548ff1670
Rick W. Chen <stuffcorpse@archlinux.us>
2011-02-28 21:57:09 +1300

    Revert "Revert "Make KNotification react on live stream song changes.""
    This reverts commit ab9f24236e5d413d69e876048d120b84f861663c.
    
    sorry that was a mistake.

--- src/dialogs/TagDialog.cpp
+++ src/dialogs/TagDialog.cpp
@@ -1370,6 +1370,7 @@ TagDialog::setControlsAccessability()
     ui->kLineEdit_title->setClearButtonShown( editable );
 
     enableOrDisable( kComboBox_artist );
+    enableOrDisable( kComboBox_albumArtist );
     enableOrDisable( kComboBox_composer );
     enableOrDisable( kComboBox_album );
     enableOrDisable( kComboBox_genre );
