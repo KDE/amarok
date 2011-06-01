b72b933d1816b0b07ef89b421980d5b130f822bc
Kevin Funk <krf@electrostorm.net>
2011-06-01 21:39:25 +0200

    Fix crash when accessing The::statusBar()
    Verify that The::statusBar() is not null, this might be the case during
    destruction phase.
    
    CCBUG: 274662

--- src/playlistgenerator/PresetModel.cpp
+++ src/playlistgenerator/PresetModel.cpp
@@ -209,10 +209,12 @@ APG::PresetModel::savePresetsToXml( const QString& filename, const QList<APG::Pr
         out.setCodec( "UTF-8" );
         xmldoc.save( out, 2, QDomNode::EncodingFromTextStream );
         if ( !filename.contains( "playlistgenerator.xml" ) ) {
-            The::statusBar()->longMessage( i18n("Preset exported to %1", filename), StatusBar::Information );
+            if( The::statusBar() )
+                The::statusBar()->longMessage( i18n("Preset exported to %1", filename), StatusBar::Information );
         }
     } else {
-        The::statusBar()->longMessage( i18n("Preset could not be exported to %1", filename), StatusBar::Sorry );
+        if( The::statusBar() )
+            The::statusBar()->longMessage( i18n("Preset could not be exported to %1", filename), StatusBar::Sorry );
         error() << "Can not write presets to " << filename;
     }
     qDeleteAll( nodes );
