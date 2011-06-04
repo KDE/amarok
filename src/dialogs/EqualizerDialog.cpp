KDE/amarok

commit 34163f83425fb824a48560f87c03d7fe29f6d0ee

Make preset names translatable
The EqualizerDialog class and its uses and really broken (duplicated
code, unnecessary copy operations, weird method names). Going to fix
this. WIP.

BUG: 263596
FIXED-IN: 2.4.2

src/dialogs/EqualizerDialog.cpp
@@ -25,6 +25,29 @@
 
 #include <KMessageBox>
 
+static int DEFAULT_PRESET_VALUES[][11] =
+{
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Manual
+    {0, 0, 0, 0, 0, 0, 0, -40, -40, -40, -50}, // Classical
+    {0, 0, 0, 20, 30, 30, 30, 20, 0, 0, 0}, // Club
+    {-10, 50, 35, 10, 0, 0, -30, -40, -40, 0, 0}, // Dance
+    {0, 29, 40, 23, 15, 0, 0, 0, 0, 0, 0}, // Full Bass
+    {-83, -50, -50, -50, -25, 15, 55, 80, 80, 80, 85}, // Full Treble
+    {-41, 35, 30, 0, -40, -25, 10, 45, 55, 60, 60}, // Full Bass + Treble
+    {-16, 25, 50, 25, -20, 0, -30, -40, -40, 0, 0}, // Laptop/Headphones
+    {-25, 50, 50, 30, 30, 0, -25, -25, -25, 0, 0}, // Large Hall
+    {0, -25, 0, 20, 25, 30, 30, 20, 15, 15, 10}, // Live
+    {0, 35, 35, 0, 0, 0, 0, 0, 0, 35, 35}, // Party
+    {-15, -10, 25, 35, 40, 25, -5, -15, -15, -10, -10}, // Pop
+    {0, 0, 0, -5, -30, 0, -35, -35, 0, 0, 0}, // Reggae
+    {-28, 40, 25, -30, -40, -20, 20, 45, 55, 55, 55}, // Rock
+    {-33, 25, 10, -5, -15, -5, 20, 45, 50, 55, 60}, // Soft
+    {-29, -15, -25, -25, -5, 20, 30, 45, 50, 55, 50}, // Ska
+    {0, 20, 20, 10, -5, -25, -30, -20, -5, 15, 45}, // Soft Rock
+    {-26, 40, 30, 0, -30, -25, 0, 40, 50, 50, 45}, // Techno
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} // Zero
+};
+
 
 EqualizerDialog * EqualizerDialog::s_instance = 0;
 
@@ -323,7 +346,7 @@ EqualizerDialog::eqCfgDeletePreset( QString & mPresetName )
       // Idea is to delete the preset only if it is user preset:
       // present on user list & absent on default list
       const int idUsr = AmarokConfig::equalizerPresetsNames().indexOf( mPresetName );
-      const int idDef = AmarokConfig::defEqualizerPresetsNames().indexOf( mPresetName );
+      const int idDef = eqDefaultPresetsList().indexOf( mPresetName );
 
       if( idUsr >= 0 && idDef < 0 )
       {
@@ -348,7 +371,7 @@ EqualizerDialog::eqCfgRestorePreset( QString mPresetName )
       // Idea is to delete the preset if it found on both
       // user list and default list - delete from the latter if so
       const int idUsr = AmarokConfig::equalizerPresetsNames().indexOf( mPresetName );
-      const int idDef = AmarokConfig::defEqualizerPresetsNames().indexOf( mPresetName );
+      const int idDef = eqDefaultPresetsList().indexOf( mPresetName );
 
       if( idDef >= 0 )
       {
@@ -396,13 +419,15 @@ EqualizerDialog::eqCfgGetPresetVal( QString mPresetName )
       // Idea is to return user preset with request name first
       // if not look into into default preset names
       const int idUsr = AmarokConfig::equalizerPresetsNames().indexOf( mPresetName );
-      const int idDef = AmarokConfig::defEqualizerPresetsNames().indexOf( mPresetName );
+      const int idDef = eqDefaultPresetsList().indexOf( mPresetName );
 
       QList<int> mPresetVal;
       if( idUsr >= 0 )
           mPresetVal = AmarokConfig::equalizerPresestValues().mid( idUsr * 11, 11 );
-      else if( idDef >= 0)
-          mPresetVal = AmarokConfig::defEqualizerPresestValues().mid( idDef * 11, 11 );
+      else if( idDef >= 0) {
+          for (int i = 0; i < 11; ++i)
+              mPresetVal << DEFAULT_PRESET_VALUES[idDef][i];
+      }
 
       return mPresetVal;
 }
@@ -415,7 +440,7 @@ EqualizerDialog::eqGlobalList()
     // first a default preset will comes
     // then user list is filtered to omit duplicates from default preset list
     QStringList mGlobalList;
-    mGlobalList += AmarokConfig::defEqualizerPresetsNames();
+    mGlobalList += EqualizerDialog::eqDefaultPresetsList();
     foreach( const QString &mUsrName, AmarokConfig::equalizerPresetsNames() )
     {
         if( mGlobalList.indexOf( mUsrName ) < 0 )
@@ -423,6 +448,61 @@ EqualizerDialog::eqGlobalList()
     }
     return mGlobalList;
 }
+
+QStringList
+EqualizerDialog::eqDefaultPresetsList()
+{
+    QStringList presets;
+    presets << "Manual"
+            << "Classical"
+            << "Club"
+            << "Dance"
+            << "Full Bass"
+            << "Full Treble"
+            << "Full Bass"
+            << "Treble"
+            << "Laptop/Headphones"
+            << "Large Hall"
+            << "Live"
+            << "Party"
+            << "Pop"
+            << "Reggae"
+            << "Rock"
+            << "Soft"
+            << "Ska"
+            << "Soft Rock"
+            << "Techno"
+            << "Zero";
+    return presets;
+}
+
+QStringList
+EqualizerDialog::eqDefaultTranslatedPresetsList()
+{
+    QStringList strings;
+    strings << i18n( "Manual" );
+    strings << i18n( "Classical" );
+    strings << i18n( "Club" );
+    strings << i18n( "Dance" );
+    strings << i18n( "Full Bass" );
+    strings << i18n( "Full Treble" );
+    strings << i18n( "Treble" );
+    strings << i18n( "Laptop/Headphones" );
+    strings << i18n( "Large Hall" );
+    strings << i18n( "Live" );
+    strings << i18n( "Party" );
+    strings << i18n( "Pop" );
+    strings << i18n( "Reggae" );
+    strings << i18n( "Rock" );
+    strings << i18n( "Soft" );
+    strings << i18n( "Ska" );
+    strings << i18n( "Soft Rock" );
+    strings << i18n( "Techno");
+    strings << i18n( "Zero" );
+    return strings;
+}
+
+
 
 namespace The {
 

src/ActionClasses.cpp
@@ -36,6 +36,7 @@
 #include <KLocale>
 #include <KToolBar>
 #include <Osd.h>
+#include <EqualizerDialog.h>
 
 
 extern OcsData ocsData;
@@ -389,7 +390,7 @@ EqualizerAction::eqGlobalList()
 {
     // Prepare a global list with duplicates removed
     QStringList mGlobalList;
-    mGlobalList += AmarokConfig::defEqualizerPresetsNames();
+    mGlobalList += EqualizerDialog::eqDefaultPresetsList();
     foreach( const QString &mUsrName, AmarokConfig::equalizerPresetsNames() )
     {
         if( mGlobalList.indexOf( mUsrName ) < 0 )
@@ -405,15 +406,7 @@ EqualizerAction::eqCfgGetPresetVal( int mPresetNo )
     if( mPresetNo > eqGlobalList().count() ||  mPresetNo < 0 )
         return mPresetVal;
     QString mPresetName = eqGlobalList().at(mPresetNo);
-    int idUsr = AmarokConfig::equalizerPresetsNames().indexOf( mPresetName );
-    int idDef = AmarokConfig::defEqualizerPresetsNames().indexOf( mPresetName );
-
-    if( idUsr >= 0 )
-        mPresetVal = AmarokConfig::equalizerPresestValues().mid( idUsr*11,11 );
-    else if( idDef >= 0)
-        mPresetVal = AmarokConfig::defEqualizerPresestValues().mid( idDef*11,11 );
-
-    return mPresetVal;
+    return EqualizerDialog::eqCfgGetPresetVal(mPresetName);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////

src/amarokconfig.kcfg
@@ -267,34 +267,6 @@
     <entry key="EqualizerPresestValues" type="IntList">
         <label>List with user presets values</label>
     </entry>
-    <entry key="DefEqualizerPresetsNames" type="StringList">
-        <label>List with user equalizer preset names</label>
-        <default>Manual,Classical,Club,Dance,Full Bass,Full Treble,Full Bass +  Treble,Laptop/Headphones,Large Hall,Live,Party,Pop,Reggae,Rock,Soft,Ska,Soft Rock,Techno,Zero</default>
-    </entry>
-    <entry key="DefEqualizerPresestValues" type="IntList">
-        <default>
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, <!-- Manual -->
-        0, 0, 0, 0, 0, 0, 0, -40, -40, -40, -50, <!-- Classical -->
-        0, 0, 0, 20, 30, 30, 30, 20, 0, 0, 0, <!-- Club -->
-        -10, 50, 35, 10, 0, 0, -30, -40, -40, 0, 0, <!-- Dance -->
-        0, 29, 40, 23, 15, 0, 0, 0, 0, 0, 0, <!-- Full Bass -->
-        -83, -50, -50, -50, -25, 15, 55, 80, 80, 80, 85, <!-- Full Treble -->
-        -41, 35, 30, 0, -40, -25, 10, 45, 55, 60, 60, <!-- Full Bass + Treble -->
-        -16, 25, 50, 25, -20, 0, -30, -40, -40, 0, 0, <!-- Laptop/Headphones -->
-        -25, 50, 50, 30, 30, 0, -25, -25, -25, 0, 0, <!-- Large Hall -->
-        0, -25, 0, 20, 25, 30, 30, 20, 15, 15, 10, <!-- Live -->
-        0, 35, 35, 0, 0, 0, 0, 0, 0, 35, 35, <!-- Party -->
-        -15, -10, 25, 35, 40, 25, -5, -15, -15, -10, -10, <!-- Pop -->
-        0, 0, 0, -5, -30, 0, -35, -35, 0, 0, 0, <!-- Reggae -->
-        -28, 40, 25, -30, -40, -20, 20, 45, 55, 55, 55, <!-- Rock -->
-        -33, 25, 10, -5, -15, -5, 20, 45, 50, 55, 60, <!-- Soft -->
-        -29, -15, -25, -25, -5, 20, 30, 45, 50, 55, 50, <!-- Ska -->
-        0, 20, 20, 10, -5, -25, -30, -20, -5, 15, 45, <!-- Soft Rock -->
-        -26, 40, 30, 0, -30, -25, 0, 40, 50, 50, 45, <!-- Techno -->
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 <!-- Zero -->
-        </default>
-        <label>List with user presets values</label>
-    </entry>
 
     <entry key="Replay Gain Mode" type="Enum">
         <label>The mode for adjusting playback volume.</label>

src/dialogs/EqualizerDialog.h
@@ -39,6 +39,10 @@ class EqualizerDialog : public KDialog, public Ui_EqualizerDialog
 
          static void showOnce();
 
+        static QStringList eqDefaultPresetsList();
+        static QStringList eqDefaultTranslatedPresetsList();
+        static QList<int> eqCfgGetPresetVal ( QString mPresetName );
+
     private Q_SLOTS:
         void eqUpdateUI( int index );
         void eqPresetChanged( int index );
@@ -64,7 +68,6 @@ class EqualizerDialog : public KDialog, public Ui_EqualizerDialog
         bool eqCfgRestorePreset( QString mPresetName );
         void eqCfgSetPresetVal( QString & mPresetName, QList<int> & mPresetValues);
         void eqRememberOriginalSettings();
-        QList<int> eqCfgGetPresetVal ( QString mPresetName );
         QStringList eqGlobalList();
 
         static EqualizerDialog *s_instance;

commit 117006267ef805700f997df6773776bf2612837a

Fix regression introduced by 34163f8

src/dialogs/EqualizerDialog.cpp
@@ -459,7 +459,6 @@ EqualizerDialog::eqDefaultPresetsList()
             << "Dance"
             << "Full Bass"
             << "Full Treble"
-            << "Full Bass"
             << "Treble"
             << "Laptop/Headphones"
             << "Large Hall"