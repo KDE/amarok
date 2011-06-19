ca200a0a7445c619c4ec061049bd67532f1801a1
Ralf Engels <ralf.engels@nokia.com>
2011-06-19 18:33:41 +0200

    Remove Amarok::ToolBar and VolumeWidget. Unused

--- src/browsers/BrowserCategory.cpp
+++ src/browsers/BrowserCategory.cpp
@@ -13,15 +13,13 @@
  * You should have received a copy of the GNU General Public License along with         *
  * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
  ****************************************************************************************/
- 
+
 #include "BrowserCategory.h"
 
 #include "BrowserBreadcrumbItem.h"
 #include "BrowserCategoryList.h"
 
 #include "core/support/Debug.h"
-
-#include "ToolBar.h"
 
 #include <QWidget>
 
