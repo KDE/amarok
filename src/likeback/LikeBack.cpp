0ce917dc5a70529f9657dc56ab12c446b2774fcf
Teo Mrnjavac <teo@kde.org>
2011-06-20 12:00:43 +0200

    Fixed wrong text in the Likeback information dialog.
    It used to say click on "Send" but there's no such button, because the
    "report bug" action starts the b.k.o assistant.
    BUG: 250220
    FIXED-IN: 2.4.2

--- src/likeback/LikeBack.cpp
+++ src/likeback/LikeBack.cpp
@@ -331,8 +331,8 @@ void LikeBack::showInformationMessage()
     {
         bugPhrase = i18nc( "Welcome dialog text, explanation for the bug button",
                            "If you experience an improper behavior in the application, just click on "
-                           "the bug icon in the top-right corner of the window, describe the behavior "
-                           "and click on 'Send'." );
+                           "the bug icon in the top-right corner of the window and follow the instructions"
+                           "to submit a bug report." );
     }
 
     // Construct the usage examples
