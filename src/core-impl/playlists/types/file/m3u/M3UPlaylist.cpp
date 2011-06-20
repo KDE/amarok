1acc2a4db4f8c076e57773d646bf4f268133daba
Bart Cerneels <bart.cerneels@kde.org>
2011-06-20 13:07:32 +0200

    Add artist in the extinf field of M3U.
    Rationale: the playlist file should contain as much info about it's tracks
    as placeholder when trackForUrl()'ing and missing metadata in files.
    
    Also removed a very strange foreach->for double loop.

--- src/core-impl/playlists/types/file/m3u/M3UPlaylist.cpp
+++ src/core-impl/playlists/types/file/m3u/M3UPlaylist.cpp
@@ -190,24 +190,19 @@ M3UPlaylist::save( const KUrl &location, bool relative )
     QList<int> lengths;
     foreach( Meta::TrackPtr track, m_tracks )
     {
-        if( track )
-        {
-            urls << track->playableUrl();
-            titles << track->name();
-            lengths << track->length();
-        }
-    }
+        Q_ASSERT(track);
 
-    for( int i = 0, n = urls.count(); i < n; ++i )
-    {
-        const KUrl &url = urls[i];
+        const KUrl &url = track->playableUrl();
+        int length = track->length() / 1000;
+        const QString &title = track->name();
+        const QString &artist = track->artist()->name();
 
-        if( !titles.isEmpty() && !lengths.isEmpty() )
+        if( !title.isEmpty() && !artist.isEmpty() && length )
         {
             stream << "#EXTINF:";
-            stream << QString::number( lengths[i] );
+            stream << QString::number( length );
             stream << ',';
-            stream << titles[i];
+            stream << artist << " - " << title;
             stream << '\n';
         }
         if( url.protocol() == "file" )
