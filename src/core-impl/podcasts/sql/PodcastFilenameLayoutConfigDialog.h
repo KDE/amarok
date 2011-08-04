81623683d6871bf6acabbae74060d80d01802bae
Sam Lade <sam@sentynel.com>
2011-08-04 19:04:09 +0100

    Add missing GPL notices, coding style++
    Patch by Sandeep Raghuraman
    
    REVIEW:102187

--- src/core-impl/podcasts/sql/PodcastFilenameLayoutConfigDialog.h
+++ src/core-impl/podcasts/sql/PodcastFilenameLayoutConfigDialog.h
@@ -1,3 +1,19 @@
+/****************************************************************************************
+ * Copyright (c) 2011 Sandeep Raghuraman <sandy.8925@gmail.com>                         *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
 #ifndef AMAROK_PODCASTFILENAMELAYOUTCONFIGDIALOG_H
 #define AMAROK_PODCASTFILENAMELAYOUTCONFIGDIALOG_H
 
@@ -15,7 +31,8 @@ class PodcastFilenameLayoutConfigDialog : public KDialog
 
     Q_OBJECT
 public:
-    explicit PodcastFilenameLayoutConfigDialog( Podcasts::SqlPodcastChannelPtr channel, QWidget* parent = 0 );
+    explicit PodcastFilenameLayoutConfigDialog( Podcasts::SqlPodcastChannelPtr channel, QWidget *parent = 0 );
+    //shows the filename configuration dialog
     bool configure();
 
 protected slots:
@@ -25,7 +42,7 @@ private:
     void init();
     Podcasts::SqlPodcastChannelPtr m_channel;
     Ui::PodcastFilenameLayoutConfigWidget *m_pflc;    
-    int choice;
+    int m_choice;
 };
 
 #endif
