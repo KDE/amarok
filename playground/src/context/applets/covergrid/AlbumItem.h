0da397249f895a5b036166d966ce03f2cf601243
Kevin Funk <krf@electrostorm.net>
2011-08-25 16:03:17 +0200

    Add covergrid applet to playground/
    Fixed some minor issues while applying the patch
    * Whitespace fixes
    * Added missing copyright headers to AlbumItem.(cpp|h)
    * Adapt API changes: Fix invalid signal/slot connection
    
    Courtesy of Emmanuel Wagner <manu.wagner@sfr.fr>
    
    REVIEW: 100841
    GUI:

---
+++ playground/src/context/applets/covergrid/AlbumItem.h
@@ -0,0 +1,44 @@
+/****************************************************************************************
+ * Copyright (c) 2011 Emmanuel Wagner <manu.wagner@sfr.fr>                              *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+#include "core/meta/Meta.h"
+
+#include <QLabel>
+
+class QMouseEvent;
+class QStyleOptionGraphicsItem;
+class QPaintEvent;
+class QEvent;
+
+class AlbumItem : public QLabel
+{
+    Q_OBJECT
+
+public:
+    AlbumItem( const QPixmap & pixmap, Meta::AlbumPtr album, QWidget * parent = 0, Qt::WindowFlags f = 0  );
+    Meta::AlbumPtr getAlbum();
+
+protected :
+    void mousePressEvent( QMouseEvent  * event );
+	void leaveEvent( QEvent * event);
+	void enterEvent( QEvent * event);
+	void mouseDoubleClickEvent( QMouseEvent * event );
+
+private:
+    Meta::AlbumPtr m_album;
+    int m_size;
+    QPixmap m_pixmap;
+};
