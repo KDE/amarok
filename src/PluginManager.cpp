07d95e29bf29bc0dc1673083c5d7448f5376d377
Mark Kretschmann <kretschmann@kde.org>
2011-11-13 15:05:44 +0100

    Bump plugin versions.

--- src/PluginManager.cpp
+++ src/PluginManager.cpp
@@ -34,7 +34,7 @@
 
 #include <cstdlib>
 
-const int Plugins::PluginManager::s_pluginFrameworkVersion = 66;
+const int Plugins::PluginManager::s_pluginFrameworkVersion = 67;
 Plugins::PluginManager* Plugins::PluginManager::s_instance = 0;
 
 Plugins::PluginManager*
