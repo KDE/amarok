fc48c864f34ec36f23117f003943e7513644e9ee
Andras Mantia <amantia@kde.org>
2011-11-23 22:31:41 +0200

    Send a real reply message and don't crash.
    REVIEW: 103213

--- src/dbus/DBusQueryHelper.cpp
+++ src/dbus/DBusQueryHelper.cpp
@@ -73,7 +73,7 @@ DBusQueryHelper::abortQuery()
     deleteLater();
     m_timeout = true;
 
-    QDBusMessage error = m_message.createError( QDBusError::InternalError, "Internal timeout" );
+    QDBusMessage error = m_message.createErrorReply( QDBusError::InternalError, "Internal timeout" );
     bool success = m_connection.send( error );
     if( !success )
         debug() << "sending async error failed";
