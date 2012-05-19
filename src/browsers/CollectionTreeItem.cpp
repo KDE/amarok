KDE/amarok

commit 09b764a8362c8d1909e74d7233507541bccf645e

Refactor CollectionTreeItem::addMatch() to handle more situations
In an attempt to make its usage less error-prone wrt Various Artists
, "No Labels" etc

src/browsers/CollectionTreeItem.cpp
@@ -279,17 +279,23 @@ CollectionTreeItem::queryMaker() const
 }
 
 void
-CollectionTreeItem::addMatch( Collections::QueryMaker *qm ) const
+CollectionTreeItem::addMatch( Collections::QueryMaker *qm, CategoryId::CatMenuId levelCategory ) const
 {
-    if( !m_data || m_type != Data )
-        return;
     if( !qm )
         return;
 
-    if( Meta::TrackPtr track = Meta::TrackPtr::dynamicCast( m_data ) )
+    if( isVariousArtistItem() )
+        qm->setAlbumQueryMode( Collections::QueryMaker::OnlyCompilations );
+    if( isNoLabelItem() )
+        qm->setLabelQueryMode( Collections::QueryMaker::OnlyWithoutLabels );
+    else if( Meta::TrackPtr track = Meta::TrackPtr::dynamicCast( m_data ) )
         qm->addMatch( track );
     else if( Meta::ArtistPtr artist = Meta::ArtistPtr::dynamicCast( m_data ) )
+    {
         qm->addMatch( artist );
+        if( levelCategory == CategoryId::AlbumArtist )
+            qm->setArtistQueryMode( Collections::QueryMaker::AlbumArtists );
+    }
     else if( Meta::AlbumPtr album = Meta::AlbumPtr::dynamicCast( m_data ) )
         qm->addMatch( album );
     else if( Meta::ComposerPtr composer = Meta::ComposerPtr::dynamicCast( m_data ) )

src/browsers/CollectionTreeItem.h
@@ -95,8 +95,16 @@ class CollectionTreeItem : public QObject
 
         Collections::QueryMaker* queryMaker() const;
 
-        /** Call addMatch for this objects data and it's query maker */
-        void addMatch( Collections::QueryMaker *qm ) const;
+        /**
+         * Call addMatch for this objects data and it's query maker. Handles VariousArtist
+         * item and NoLabel item, too.
+         *
+         * @param qm QueryMaker to add match to
+         * @param levelCategory category for level this item is in, one of the values from
+         *        CategoryId::CatMenuId enum. Used only for distinction between Artist and
+         *        AlbumArtist.
+         */
+        void addMatch( Collections::QueryMaker* qm, CategoryId::CatMenuId levelCategory ) const;
 
         bool operator<( const CollectionTreeItem& other ) const;
 

src/browsers/CollectionTreeItemModelBase.cpp
@@ -450,18 +450,8 @@ CollectionTreeItemModelBase::mimeData(const QList<CollectionTreeItem*> & items) 
         else
         {
             Collections::QueryMaker *qm = item->queryMaker();
-            CollectionTreeItem *tmpItem = item;
-            while( tmpItem->isDataItem() )
-            {
-                tmpItem->addMatch( qm );
-                if( levelCategory( tmpItem->level() - 1 ) == CategoryId::AlbumArtist )
-                    qm->setArtistQueryMode( Collections::QueryMaker::AlbumArtists );
-                tmpItem = tmpItem->parent();
-            }
-            if( item->isVariousArtistItem() )
-                qm->setAlbumQueryMode( Collections::QueryMaker::OnlyCompilations );
-            if( item->isNoLabelItem() )
-                qm->setLabelQueryMode( Collections::QueryMaker::OnlyWithoutLabels );
+            for( CollectionTreeItem *tmp = item; tmp; tmp = tmp->parent() )
+                tmp->addMatch( qm, levelCategory( tmp->level() - 1 ) );
             Collections::addTextualFilter( qm, m_currentFilter );
             queries.append( qm );
         }
@@ -589,18 +579,8 @@ void CollectionTreeItemModelBase::listForLevel(int level, Collections::QueryMake
             }
         }
 
-        for( CollectionTreeItem *tmpItem = parent; tmpItem->parent(); tmpItem = tmpItem->parent() )
-        {
-            // a various artist item means we are looking for compilations here
-            if( tmpItem->isVariousArtistItem() )
-                qm->setAlbumQueryMode( Collections::QueryMaker::OnlyCompilations );
-            else if( tmpItem->data() )
-            {
-                if( levelCategory( tmpItem->level() - 1 ) == CategoryId::AlbumArtist )
-                    qm->setArtistQueryMode( Collections::QueryMaker::AlbumArtists );
-                 tmpItem->addMatch( qm );
-            }
-        }
+        for( CollectionTreeItem *tmp = parent; tmp; tmp = tmp->parent() )
+            tmp->addMatch( qm, levelCategory( tmp->level() - 1 ) );
         Collections::addTextualFilter( qm, m_currentFilter );
         addQueryMaker( parent, qm );
         d->childQueries.insert( qm, parent );
@@ -1016,8 +996,8 @@ CollectionTreeItemModelBase::handleCompilations( CollectionTreeItem *parent ) co
     Collections::QueryMaker *qm = parent->queryMaker();
     qm->setAlbumQueryMode( Collections::QueryMaker::OnlyCompilations );
     qm->setQueryType( Collections::QueryMaker::Album );
-    for( CollectionTreeItem *tmpItem = parent; tmpItem->parent(); tmpItem = tmpItem->parent() )
-        tmpItem->addMatch( qm );
+    for( CollectionTreeItem *tmp = parent; tmp; tmp = tmp->parent() )
+        tmp->addMatch( qm, levelCategory( tmp->level() - 1 ) );
 
     Collections::addTextualFilter( qm, m_currentFilter );
     addQueryMaker( parent, qm );
@@ -1032,8 +1012,8 @@ CollectionTreeItemModelBase::handleTracksWithoutLabels( Collections::QueryMaker:
     Collections::QueryMaker *qm = parent->queryMaker();
     qm->setQueryType( queryType );
     qm->setLabelQueryMode( Collections::QueryMaker::OnlyWithoutLabels );
-    for( CollectionTreeItem *tmpItem = parent; tmpItem->parent(); tmpItem = tmpItem->parent() )
-        tmpItem->addMatch( qm );
+    for( CollectionTreeItem *tmp = parent; tmp; tmp = tmp->parent() )
+        tmp->addMatch( qm, levelCategory( tmp->level() - 1 ) );
 
     Collections::addTextualFilter( qm, m_currentFilter );
     addQueryMaker( parent, qm );

src/browsers/CollectionTreeView.cpp
@@ -1264,19 +1264,8 @@ CollectionTreeView::createMetaQueryFromItems( const QSet<CollectionTreeItem*> &i
     foreach( CollectionTreeItem *item, parents )
     {
         Collections::QueryMaker *qm = item->queryMaker();
-        CollectionTreeItem *tmp = item;
-        while( tmp->isDataItem() || tmp->isVariousArtistItem() )
-        {
-            if( tmp->isVariousArtistItem() )
-                qm->setAlbumQueryMode( Collections::QueryMaker::OnlyCompilations );
-            else if( tmp->data() )
-            {
-                if( m_treeModel->levelCategory( tmp->data() - 1 ) == CategoryId::AlbumArtist )
-                    qm->setArtistQueryMode( Collections::QueryMaker::AlbumArtists );
-                tmp->addMatch( qm );
-            }
-            tmp = tmp->parent();
-        }
+        for( CollectionTreeItem *tmp = item; tmp; tmp = tmp->parent() )
+            tmp->addMatch( qm, m_treeModel->levelCategory( tmp->level() - 1 ) );
         Collections::addTextualFilter( qm, m_treeModel->currentFilter() );
         queryMakers.append( qm );
     }

commit 93326c39f7c7f294de06eb50bce4a451c0f6b393

Fix collection browser hiding tracks when artist != album artist
I broke this in commit 09b764a8362c8d1909e74d7233507541bccf645e - sorry.
Only excuse for me is that QueryMaker's contract is really weird as
setArtistQueryMode() lasts only until next addMatch( Meta::Artist )
call, which is undocumented.

I would propose to make AlbumQueryMode an optional argument of the
addMatch( Meta::Artist ), because setSomething() methods really look
like the setting is permanent.

src/browsers/CollectionTreeItem.cpp
@@ -292,9 +292,10 @@ CollectionTreeItem::addMatch( Collections::QueryMaker *qm, CategoryId::CatMenuId
         qm->addMatch( track );
     else if( Meta::ArtistPtr artist = Meta::ArtistPtr::dynamicCast( m_data ) )
     {
-        qm->addMatch( artist );
         if( levelCategory == CategoryId::AlbumArtist )
+            // this needs to be BEFORE addMatch(), because addMatch() resets ArtistQueryMode
             qm->setArtistQueryMode( Collections::QueryMaker::AlbumArtists );
+        qm->addMatch( artist );
     }
     else if( Meta::AlbumPtr album = Meta::AlbumPtr::dynamicCast( m_data ) )
         qm->addMatch( album );

src/core/collections/QueryMaker.h
@@ -168,6 +168,9 @@ class AMAROK_CORE_EXPORT QueryMaker : public QObject
         /**
          * Set artist query mode. If this method is not called,
          * QueryMaker defaults to ArtistQueryMode::TrackArtist.
+         *
+         * WARNING: this gets reset to TrackArtist at the end of the following call to
+         * addMatch( Meta::Artist )
          */
         virtual QueryMaker* setArtistQueryMode( ArtistQueryMode mode );
 