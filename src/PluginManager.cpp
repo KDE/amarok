59d9046307a194906f0d3b906b6817e28a66aa8b
Bart Cerneels <bart.cerneels@kde.org>
2012-05-30 10:26:09 +0200

    Bump plugin version prior to 2.6-beta1 release.
    DIGEST: Amarok 2.6-beta1 tagged.

--- src/PluginManager.cpp
+++ src/PluginManager.cpp
@@ -34,7 +34,7 @@
 
 #include <cstdlib>
 
-const int Plugins::PluginManager::s_pluginFrameworkVersion = 67;
+const int Plugins::PluginManager::s_pluginFrameworkVersion = 68;
 Plugins::PluginManager* Plugins::PluginManager::s_instance = 0;
 
 Plugins::PluginManager*
