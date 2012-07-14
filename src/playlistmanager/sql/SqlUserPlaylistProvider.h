b46817d25a2ac77fe142943833e812d4937e8d2b
Matěj Laitl <matej@laitl.cz>
2012-07-14 11:25:53 +0200

    SqlUserPlaylistProvider: don't redefine signals
    Qt doesn't like it.

--- src/playlistmanager/sql/SqlUserPlaylistProvider.h
+++ src/playlistmanager/sql/SqlUserPlaylistProvider.h
@@ -60,11 +60,6 @@ class AMAROK_EXPORT SqlUserPlaylistProvider : public UserPlaylistProvider
 
         static Playlists::SqlPlaylistList toSqlPlaylists( Playlists::PlaylistList playlists );
 
-    signals:
-        void updated();
-        void playlistAdded( Playlists::PlaylistPtr playlist );
-        void playlistRemoved( Playlists::PlaylistPtr playlist );
-
     private slots:
         /**
          * Overriden only bacause of the m_debug flag
