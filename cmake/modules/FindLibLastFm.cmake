30786927942988f4a1b4ddea794ec80679006845
Matěj Laitl <matej@laitl.cz>
2012-07-20 14:58:47 +0200

    Update Last.fm info in CMakeLists, README, check its version in CMake
    Also tweak FindLibLastFm.cmake to parse the version and CMakeLists.txt
    to actually check the version.

--- cmake/modules/FindLibLastFm.cmake
+++ cmake/modules/FindLibLastFm.cmake
@@ -1,12 +1,13 @@
 # - Find LibLastFM
 # Find the liblastfm includes and the liblastfm libraries
 # This module defines
+# LIBLASTFM_FOUND, whether liblastfm was found. If it was, it further sets:
 # LIBLASTFM_INCLUDE_DIR, root lastfm include dir
 # LIBLASTFM_LIBRARY, the path to liblastfm
-# LIBLASTFM_FOUND, whether liblastfm was found
+# LIBLASTFM_VERSION, version of found liblastfm as a string, e.g "0.3"
 
 
-find_path(LIBLASTFM_INCLUDE_DIR NAMES lastfm
+find_path(LIBLASTFM_INCLUDE_DIR NAMES global.h
    HINTS
    ~/usr/include
    /opt/local/include
@@ -31,12 +32,23 @@ find_library( LIBLASTFM_LIBRARY NAMES lastfm
 
 if(LIBLASTFM_INCLUDE_DIR AND LIBLASTFM_LIBRARY)
    set(LIBLASTFM_FOUND TRUE)
-   message(STATUS "Found liblastfm: ${LIBLASTFM_INCLUDE_DIR}, ${LIBLASTFM_LIBRARY}")
 else(LIBLASTFM_INCLUDE_DIR AND LIBLASTFM_LIBRARY)
-   set(LIBLASTFM_FOUND FALSE)   
-   if (LIBLASTFM_FIND_REQUIRED)
-      message(FATAL_ERROR "Could NOT find required package LibLastFm")
-   endif(LIBLASTFM_FIND_REQUIRED)
+   set(LIBLASTFM_FOUND FALSE)
 endif(LIBLASTFM_INCLUDE_DIR AND LIBLASTFM_LIBRARY)
+
+if(LIBLASTFM_FOUND)
+   set(regex "#define LASTFM_VERSION_STRING \"(.*)\"")
+   file(STRINGS "${LIBLASTFM_INCLUDE_DIR}/global.h" LIBLASTFM_VERSION REGEX ${regex})
+   if(${LIBLASTFM_VERSION} MATCHES ${regex})
+      set(LIBLASTFM_VERSION ${CMAKE_MATCH_1})
+      message(STATUS "Found liblastfm: ${LIBLASTFM_INCLUDE_DIR}, ${LIBLASTFM_LIBRARY}, version ${LIBLASTFM_VERSION}")
+   else(${LIBLASTFM_VERSION} MATCHES ${regex})
+      message(WARNING "Found liblastfm: ${LIBLASTFM_INCLUDE_DIR} - but failed to parse version")
+      set(LIBLASTFM_FOUND FALSE)
+      unset(LIBLASTFM_INCLUDE_DIR)
+      unset(LIBLASTFM_LIBRARY)
+   endif(${LIBLASTFM_VERSION} MATCHES ${regex})
+   unset(regex)
+endif(LIBLASTFM_FOUND)
 
 mark_as_advanced(LIBLASTFM_INCLUDE_DIR LIBLASTFM_LIBRARY)
