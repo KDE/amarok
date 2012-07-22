242c202879cc2189236e7305f421733321360c14
Ryan Feng <odayfans@gmail.com>
2012-07-22 06:09:52 -0400

    Update TODOs

--- src/core-impl/collections/spotifycollection/support/Query.cpp
+++ src/core-impl/collections/spotifycollection/support/Query.cpp
@@ -1,3 +1,18 @@
+/****************************************************************************************
+ * Copyright (c) 2012 Ryan Feng <odayfans@gmail.com>                                    *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
 #include "Query.h"
 #include "core/support/Debug.h"
 
@@ -18,7 +33,7 @@ Query::Query( Collections::SpotifyCollection* collection, const QString& qid, co
 Query::~Query()
 {
     DEBUG_BLOCK
-    //DONE:TODO: Notify controller to remove current query from queue
+    //DONE:TODO: Notify controller to remove current query from map
     emit queryDone( qid() );
 }
 
