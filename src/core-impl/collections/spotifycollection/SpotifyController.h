2aa8dd71b08686b63f6b475bc4447ef35bcd48a4
Ryan Feng <odayfans@gmail.com>
2012-07-31 14:16:16 -0400

    Add SpotifyController.h
    SpotifyController.h contains function
    Spotify::Controller* The::Spotifycontroller(), it returns a pointer of
    the global object of Spotify::Controller

---
+++ src/core-impl/collections/spotifycollection/SpotifyController.h
@@ -0,0 +1,11 @@
+#ifndef SPOTIFY_CONTROLLER_H_
+#define SPOTIFY_CONTROLLER_H_
+
+#include "support/Controller.h"
+
+namespace The
+{
+AMAROK_EXPORT Spotify::Controller* SpotifyController();
+}
+
+#endif
