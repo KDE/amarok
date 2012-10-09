KDE/amarok

commit 82f08e626842024898c5ac1f56619e3aadba5605

Refactor the Collection config UI code, and move more stuff into the UI file.
Also, a resizing issue of the UI was fixed, as well as some other small issues.

The patch introduces one new issue, which is that the height of the config dialog
becomes too tall (about 650 pixels). However, work is underway from Matej that
will mitigate this problem.

src/widgets/CollectionSetupTreeView.cpp
@@ -1,0 +1,84 @@
+/****************************************************************************************
+ * Copyright (c) 2003 Scott Wheeler <wheeler@kde.org>                                   *
+ * Copyright (c) 2004 Max Howell <max.howell@methylblue.com>                            *
+ * Copyright (c) 2004-2008 Mark Kretschmann <kretschmann@kde.org>                       *
+ * Copyright (c) 2008 Seb Ruiz <ruiz@kde.org>                                           *
+ * Copyright (c) 2008 Sebastian Trueg <trueg@kde.org>                                   *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+#include "CollectionSetupTreeView.h"
+#include "dialogs/CollectionSetup.h"
+
+#include "core/support/Debug.h"
+#include "core/collections/Collection.h"
+#include "core-impl/collections/support/CollectionManager.h"
+
+#include <QAction>
+#include <QApplication>
+#include <QMenu>
+
+CollectionSetupTreeView::CollectionSetupTreeView( QWidget *parent )
+        : QTreeView( parent )
+{
+    DEBUG_BLOCK
+    m_rescanDirAction = new QAction( this );
+    connect( this, SIGNAL( pressed(const QModelIndex &) ), this, SLOT( slotPressed(const QModelIndex&) ) );
+    connect( m_rescanDirAction, SIGNAL( triggered() ), this, SLOT( slotRescanDirTriggered() ) );
+}
+
+CollectionSetupTreeView::~CollectionSetupTreeView()
+{
+    this->disconnect();
+    delete m_rescanDirAction;
+}
+
+void
+CollectionSetupTreeView::slotPressed( const QModelIndex &index )
+{
+    DEBUG_BLOCK
+    // --- show context menu on right mouse button
+    if( ( QApplication::mouseButtons() & Qt::RightButton ) && parent() )
+    {
+        m_currDir = qobject_cast<CollectionSetup*>(parent())->modelFilePath( index );
+        debug() << "Setting current dir to " << m_currDir;
+
+        // check if there is an sql collection covering the directory
+        bool covered = false;
+        QList<Collections::Collection*> queryableCollections = CollectionManager::instance()->queryableCollections();
+        foreach( Collections::Collection *collection, queryableCollections )
+        {
+            if( collection->isDirInCollection( m_currDir ) )
+                covered = true;
+        }
+
+        // it's covered, so we can show the rescan option
+        if( covered )
+        {
+            m_rescanDirAction->setText( i18n( "Rescan '%1'", m_currDir ) );
+            QMenu menu;
+            menu.addAction( m_rescanDirAction );
+            menu.exec( QCursor::pos() );
+        }
+    }
+}
+
+void
+CollectionSetupTreeView::slotRescanDirTriggered()
+{
+    DEBUG_BLOCK
+    CollectionManager::instance()->startIncrementalScan( m_currDir );
+}
+
+#include "CollectionSetupTreeView.moc"

src/CMakeLists.txt
@@ -694,6 +694,7 @@ set(amaroklib_LIB_SRCS
     widgets/AnimatedLabelStack.cpp
     widgets/BreadcrumbItemButton.cpp
     widgets/ClearSpinBox.cpp
+    widgets/CollectionSetupTreeView.cpp
     widgets/CoverLabel.cpp
     widgets/HintLineEdit.cpp
     widgets/kdatecombo.cpp

src/configdialog/dialogs/CollectionConfig.cpp
@@ -25,18 +25,16 @@
 CollectionConfig::CollectionConfig( QWidget* parent )
     : ConfigDialogBase( parent )
 {
-    setupUi( this );
-
-    m_collectionSetup = new CollectionSetup( collectionFoldersBox );
+    m_collectionSetup = new CollectionSetup( this );
     connect( m_collectionSetup, SIGNAL( changed() ), parent, SLOT( updateButtons() ) );
 
     QVBoxLayout *layout = new QVBoxLayout();
     layout->addWidget( m_collectionSetup );
-    collectionFoldersBox->setLayout( layout );
+    setLayout( layout );
 
     KConfigGroup transcodeGroup = Amarok::config( Collections::SQL_TRANSCODING_GROUP_NAME );
-    transcodingConfig->fillInChoices( Transcoding::Configuration::fromConfigGroup( transcodeGroup ) );
-    connect( transcodingConfig, SIGNAL(currentIndexChanged(int)), parent, SLOT(updateButtons()) );
+    m_collectionSetup->transcodingConfig()->fillInChoices( Transcoding::Configuration::fromConfigGroup( transcodeGroup ) );
+    connect( m_collectionSetup->transcodingConfig(), SIGNAL(currentIndexChanged(int)), parent, SLOT(updateButtons()) );
 }
 
 CollectionConfig::~CollectionConfig()
@@ -52,7 +50,7 @@ CollectionConfig::hasChanged()
 {
     DEBUG_BLOCK
 
-    return m_collectionSetup->hasChanged() || transcodingConfig->hasChanged();
+    return m_collectionSetup->hasChanged() || m_collectionSetup->transcodingConfig()->hasChanged();
 }
 
 bool
@@ -67,7 +65,7 @@ CollectionConfig::updateSettings()
     m_collectionSetup->writeConfig();
 
     KConfigGroup transcodeGroup = Amarok::config( Collections::SQL_TRANSCODING_GROUP_NAME );
-    transcodingConfig->currentChoice().saveToConfigGroup( transcodeGroup );
+    m_collectionSetup->transcodingConfig()->currentChoice().saveToConfigGroup( transcodeGroup );
 }
 
 #include "CollectionConfig.moc"

src/configdialog/dialogs/CollectionConfig.h
@@ -17,13 +17,12 @@
 #ifndef COLLECTIONCONFIG_H
 #define COLLECTIONCONFIG_H
 
-#include "ui_CollectionConfig.h"
 #include "configdialog/ConfigDialogBase.h"
 
 class CollectionSetup;
 
 
-class CollectionConfig : public ConfigDialogBase, public Ui_CollectionConfig
+class CollectionConfig : public ConfigDialogBase
 {
     Q_OBJECT
 

src/configdialog/dialogs/CollectionConfig.ui
@@ -6,42 +6,97 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>404</width>
-    <height>215</height>
+    <width>350</width>
+    <height>391</height>
    </rect>
+  </property>
+  <property name="sizePolicy">
+   <sizepolicy hsizetype="Minimum" vsizetype="Minimum">
+    <horstretch>0</horstretch>
+    <verstretch>0</verstretch>
+   </sizepolicy>
   </property>
   <property name="windowTitle">
    <string>Collection Setup</string>
   </property>
-  <layout class="QVBoxLayout" name="rootLayout">
+  <layout class="QVBoxLayout" name="verticalLayout">
    <item>
     <widget class="QGroupBox" name="collectionFoldersBox">
+     <property name="sizePolicy">
+      <sizepolicy hsizetype="MinimumExpanding" vsizetype="MinimumExpanding">
+       <horstretch>0</horstretch>
+       <verstretch>1</verstretch>
+      </sizepolicy>
+     </property>
      <property name="title">
       <string>Collection Folders</string>
      </property>
-    </widget>
-   </item>
-   <item>
-    <widget class="QGroupBox" name="importingBox">
-     <property name="title">
-      <string>Importing Music</string>
-     </property>
-     <layout class="QFormLayout" name="formLayout">
-      <item row="0" column="0">
-       <widget class="QLabel" name="transcodeLabel">
-        <property name="toolTip">
-         <string>Whether Amarok should transcode tracks when they are copied or moved to Local Collection.</string>
-        </property>
+     <layout class="QVBoxLayout" name="verticalLayout_2">
+      <item>
+       <widget class="QLabel" name="descriptionLabel">
         <property name="text">
-         <string>Transcode tracks:</string>
+         <string>These folders will be scanned for media to make up your collection. You can right-click on a folder to individually rescan it, if it was previously selected:</string>
+        </property>
+        <property name="alignment">
+         <set>Qt::AlignJustify|Qt::AlignVCenter</set>
+        </property>
+        <property name="wordWrap">
+         <bool>true</bool>
         </property>
        </widget>
       </item>
-      <item row="0" column="1">
-       <widget class="Transcoding::SelectConfigWidget" name="transcodingConfig">
-        <property name="toolTip">
-         <string>Whether Amarok should transcode tracks when they are copied or moved to Local Collection.</string>
+      <item>
+       <widget class="CollectionSetupTreeView" name="view">
+        <property name="sizePolicy">
+         <sizepolicy hsizetype="MinimumExpanding" vsizetype="MinimumExpanding">
+          <horstretch>0</horstretch>
+          <verstretch>1</verstretch>
+         </sizepolicy>
         </property>
+        <property name="headerHidden">
+         <bool>true</bool>
+        </property>
+       </widget>
+      </item>
+      <item>
+       <widget class="QWidget" name="buttonContainer" native="true"/>
+      </item>
+      <item>
+       <widget class="QWidget" name="checkboxContainer" native="true"/>
+      </item>
+      <item>
+       <widget class="QGroupBox" name="importingBox">
+        <property name="sizePolicy">
+         <sizepolicy hsizetype="Minimum" vsizetype="Minimum">
+          <horstretch>0</horstretch>
+          <verstretch>0</verstretch>
+         </sizepolicy>
+        </property>
+        <property name="title">
+         <string>Importing Music</string>
+        </property>
+        <layout class="QFormLayout" name="formLayout">
+         <property name="fieldGrowthPolicy">
+          <enum>QFormLayout::ExpandingFieldsGrow</enum>
+         </property>
+         <item row="0" column="0">
+          <widget class="QLabel" name="transcodeLabel">
+           <property name="toolTip">
+            <string>Whether Amarok should transcode tracks when they are copied or moved to Local Collection.</string>
+           </property>
+           <property name="text">
+            <string>Transcode tracks:</string>
+           </property>
+          </widget>
+         </item>
+         <item row="0" column="1">
+          <widget class="Transcoding::SelectConfigWidget" name="transcodingConfig">
+           <property name="toolTip">
+            <string>Whether Amarok should transcode tracks when they are copied or moved to Local Collection.</string>
+           </property>
+          </widget>
+         </item>
+        </layout>
        </widget>
       </item>
      </layout>
@@ -54,6 +109,11 @@
    <class>Transcoding::SelectConfigWidget</class>
    <extends>QComboBox</extends>
    <header>transcoding/TranscodingSelectConfigWidget.h</header>
+  </customwidget>
+  <customwidget>
+   <class>CollectionSetupTreeView</class>
+   <extends>QTreeView</extends>
+   <header>widgets/CollectionSetupTreeView.h</header>
   </customwidget>
  </customwidgets>
  <includes>

src/dialogs/CollectionSetup.cpp
@@ -38,109 +38,51 @@
 #include <QLabel>
 #include <QMenu>
 
-CollectionSetupTreeView::CollectionSetupTreeView( QWidget *parent )
-        : QTreeView( parent )
-{
-    DEBUG_BLOCK
-    m_rescanDirAction = new QAction( this );
-    connect( this, SIGNAL( pressed(const QModelIndex &) ), this, SLOT( slotPressed(const QModelIndex&) ) );
-    connect( m_rescanDirAction, SIGNAL( triggered() ), this, SLOT( slotRescanDirTriggered() ) );
-}
-
-CollectionSetupTreeView::~CollectionSetupTreeView()
-{
-    this->disconnect();
-    delete m_rescanDirAction;
-}
-
-void
-CollectionSetupTreeView::slotPressed( const QModelIndex &index )
-{
-    DEBUG_BLOCK
-    // --- show context menu on right mouse button
-    if( ( QApplication::mouseButtons() & Qt::RightButton ) && parent() )
-    {
-        m_currDir = qobject_cast<CollectionSetup*>(parent())->modelFilePath( index );
-        debug() << "Setting current dir to " << m_currDir;
-
-        // check if there is an sql collection covering the directory
-        bool covered = false;
-        QList<Collections::Collection*> queryableCollections = CollectionManager::instance()->queryableCollections();
-        foreach( Collections::Collection *collection, queryableCollections )
-        {
-            if( collection->isDirInCollection( m_currDir ) )
-                covered = true;
-        }
-
-        // it's covered, so we can show the rescan option
-        if( covered )
-        {
-            m_rescanDirAction->setText( i18n( "Rescan '%1'", m_currDir ) );
-            QMenu menu;
-            menu.addAction( m_rescanDirAction );
-            menu.exec( QCursor::pos() );
-        }
-    }
-}
-
-void
-CollectionSetupTreeView::slotRescanDirTriggered()
-{
-    DEBUG_BLOCK
-    CollectionManager::instance()->startIncrementalScan( m_currDir );
-}
-
 CollectionSetup* CollectionSetup::s_instance;
 
 
 CollectionSetup::CollectionSetup( QWidget *parent )
-        : KVBox( parent )
+        : QWidget( parent )
+        , Ui::CollectionConfig()
+        , m_ui( new Ui::CollectionConfig() )
 {
-    DEBUG_BLOCK
+    m_ui->setupUi(this);
 
     setSizePolicy( QSizePolicy::Expanding, QSizePolicy::Expanding );
 
     setObjectName( "CollectionSetup" );
     s_instance = this;
 
-    QLabel* descriptionLabel = new QLabel( i18n(
-        "These folders will be scanned for "
-        "media to make up your collection. You can "
-        "right-click on a folder to individually "
-        "rescan it, if it was previously selected:" ), this );
-    descriptionLabel->setAlignment( Qt::AlignJustify );
-    descriptionLabel->setWordWrap( true );
-
-    m_view  = new CollectionSetupTreeView( this );
-    m_view->setHeaderHidden( true );
-    m_view->setRootIsDecorated( true );
     if( KGlobalSettings::graphicEffectsLevel() != KGlobalSettings::NoEffects )
-        m_view->setAnimated( true );
-    m_view->setSizePolicy( QSizePolicy::Expanding, QSizePolicy::Expanding );
-    connect( m_view, SIGNAL( clicked( const QModelIndex & ) ), this, SIGNAL( changed() ) );
+        m_ui->view->setAnimated( true );
+    connect( m_ui->view, SIGNAL( clicked( const QModelIndex & ) ), this, SIGNAL( changed() ) );
 
-    KHBox* buttonBox = new KHBox( this );
-
-    KPushButton *rescan = new KPushButton( KIcon( "collection-rescan-amarok" ), i18n( "Full rescan" ), buttonBox );
+    KPushButton *rescan = new KPushButton( KIcon( "collection-rescan-amarok" ), i18n( "Full rescan" ), m_ui->buttonContainer );
     rescan->setToolTip( i18n( "Rescan your entire collection. This will <i>not</i> delete any statistics." ) );
     connect( rescan, SIGNAL( clicked() ), CollectionManager::instance(), SLOT( startFullScan() ) );
 
-    KPushButton *import = new KPushButton( KIcon( "tools-wizard" ), i18n( "Import" ), buttonBox );
+    KPushButton *import = new KPushButton( KIcon( "tools-wizard" ), i18n( "Import" ), m_ui->buttonContainer );
     import->setToolTip( i18n( "Import collection and/or statistics from older Amarok versions, the batch scanner or media players." ) );
     connect( import, SIGNAL( clicked() ), this, SLOT( importCollection() ) );
 
-    m_recursive = new QCheckBox( i18n("&Scan folders recursively (requires full rescan if newly checked)"), this );
-    m_monitor   = new QCheckBox( i18n("&Watch folders for changes"), this );
-    m_writeBack = new QCheckBox( i18n("Write metadata to file"), this );
-    m_writeBackStatistics = new QCheckBox( i18n("Write statistics to file"), this );
-    KHBox* writeBackCoverDimensionsBox = new KHBox( this );
+    QHBoxLayout *buttonLayout = new QHBoxLayout();
+    buttonLayout->addWidget( rescan );
+    buttonLayout->addWidget( import );
+    m_ui->buttonContainer->setLayout( buttonLayout );
+
+    m_recursive = new QCheckBox( i18n("&Scan folders recursively (requires full rescan if newly checked)"), m_ui->checkboxContainer );
+    m_monitor   = new QCheckBox( i18n("&Watch folders for changes"), m_ui->checkboxContainer );
+    m_writeBack = new QCheckBox( i18n("Write metadata to file"), m_ui->checkboxContainer );
+    m_writeBackStatistics = new QCheckBox( i18n("Write statistics to file"), m_ui->checkboxContainer );
+    KHBox* writeBackCoverDimensionsBox = new KHBox( m_ui->checkboxContainer );
     m_writeBackCover = new QCheckBox( i18n("Write covers to file, maximum size:"), writeBackCoverDimensionsBox );
     m_writeBackCoverDimensions = new KComboBox( writeBackCoverDimensionsBox );
+    m_writeBackCoverDimensions->setSizePolicy( QSizePolicy::Fixed, QSizePolicy::Minimum );
     m_writeBackCoverDimensions->addItem( i18nc("Maximum cover size option","Small (200 px)"), QVariant(200) );
     m_writeBackCoverDimensions->addItem( i18nc("Maximum cover size option","Medium (400 px)"), QVariant(400) );
     m_writeBackCoverDimensions->addItem( i18nc("Maximum cover size option","Large (800 px)"), QVariant(800) );
     m_writeBackCoverDimensions->addItem( i18nc("Maximum cover size option","Huge (1600 px)"), QVariant(1600) );
-    m_charset   = new QCheckBox( i18n("&Enable character set detection in ID3 tags"), this );
+    m_charset   = new QCheckBox( i18n("&Enable character set detection in ID3 tags"), m_ui->checkboxContainer );
     connect( m_recursive, SIGNAL( toggled( bool ) ), this, SIGNAL( changed() ) );
     connect( m_monitor  , SIGNAL( toggled( bool ) ), this, SIGNAL( changed() ) );
     connect( m_writeBack, SIGNAL( toggled( bool ) ), this, SIGNAL( changed() ) );
@@ -148,6 +90,15 @@ CollectionSetup::CollectionSetup( QWidget *parent )
     connect( m_writeBackCover, SIGNAL( toggled( bool ) ), this, SIGNAL( changed() ) );
     connect( m_writeBackCoverDimensions, SIGNAL( currentIndexChanged( int )), this, SIGNAL( changed() ) );
     connect( m_charset  , SIGNAL( toggled( bool ) ), this, SIGNAL( changed() ) );
+
+    QVBoxLayout *checkboxLayout = new QVBoxLayout();
+    checkboxLayout->addWidget( m_recursive );
+    checkboxLayout->addWidget( m_monitor );
+    checkboxLayout->addWidget( m_writeBack );
+    checkboxLayout->addWidget( m_writeBackStatistics );
+    checkboxLayout->addWidget( writeBackCoverDimensionsBox );
+    checkboxLayout->addWidget( m_charset );
+    m_ui->checkboxContainer->setLayout( checkboxLayout );
 
     m_recursive->setToolTip( i18n( "If selected, Amarok will read all subfolders." ) );
     m_monitor->setToolTip(   i18n( "If selected, the collection folders will be watched for changes.\nThe watcher will not notice changes behind symbolic links." ) );
@@ -174,11 +125,11 @@ CollectionSetup::CollectionSetup( QWidget *parent )
 
     // set the model _after_ constructing the checkboxes
     m_model = new CollectionFolder::Model();
-    m_view->setModel( m_model );
+    m_ui->view->setModel( m_model );
     #ifndef Q_OS_WIN
-    m_view->setRootIndex( m_model->setRootPath( QDir::rootPath() ) );
+    m_ui->view->setRootIndex( m_model->setRootPath( QDir::rootPath() ) );
     #else
-    m_view->setRootIndex( m_model->setRootPath( m_model->myComputer().toString() ) );
+    m_ui->view->setRootIndex( m_model->setRootPath( m_model->myComputer().toString() ) );
     #endif
 
     Collections::Collection *primaryCollection = CollectionManager::instance()->primaryCollection();
@@ -189,10 +140,8 @@ CollectionSetup::CollectionSetup( QWidget *parent )
     foreach( const QString &dir, dirs )
     {
         QModelIndex index = m_model->index( dir );
-        m_view->scrollTo( index, QAbstractItemView::EnsureVisible );
+        m_ui->view->scrollTo( index, QAbstractItemView::EnsureVisible );
     }
-
-    setSpacing( 6 );
 }
 
 bool

src/dialogs/CollectionSetup.h
@@ -20,7 +20,6 @@
 #ifndef AMAROK_COLLECTIONSETUP_H
 #define AMAROK_COLLECTIONSETUP_H
 
-#include <KVBox>      //baseclass
 #include <KComboBox>
 
 #include <QCheckBox>
@@ -29,30 +28,13 @@
 
 #include "core/support/Debug.h"
 
-class QTreeView;
+#include "ui_CollectionConfig.h"
 
+class TranscodingConfig;
 namespace CollectionFolder { class Model; }
 
-class CollectionSetupTreeView : public QTreeView
-{
-    Q_OBJECT
 
-    public:
-        CollectionSetupTreeView( QWidget* );
-        ~CollectionSetupTreeView();
-
-    protected slots:
-        /** Shows a context menu if the right mouse button is pressed over a directory. */
-        void slotPressed( const QModelIndex &index );
-        void slotRescanDirTriggered();
-
-    private:
-        QAction *m_rescanDirAction;
-        QString m_currDir;
-
-};
-
-class CollectionSetup : public KVBox
+class CollectionSetup : public QWidget, public Ui::CollectionConfig
 {
     Q_OBJECT
 
@@ -62,6 +44,7 @@ class CollectionSetup : public KVBox
         static CollectionSetup* instance() { return s_instance; }
 
         CollectionSetup( QWidget* );
+        virtual ~CollectionSetup() {}
 
         void writeConfig();
         bool hasChanged() const;
@@ -76,6 +59,7 @@ class CollectionSetup : public KVBox
         bool charset() const { return m_charset && m_charset->isChecked(); }
 
         const QString modelFilePath( const QModelIndex &index ) const;
+        Transcoding::SelectConfigWidget * transcodingConfig() const { return m_ui->transcodingConfig; }
 
     signals:
         void changed();
@@ -86,7 +70,7 @@ class CollectionSetup : public KVBox
     private:
         static CollectionSetup* s_instance;
 
-        CollectionSetupTreeView *m_view;
+        Ui::CollectionConfig *m_ui;
         CollectionFolder::Model *m_model;
         QStringList m_dirs;
         QCheckBox *m_recursive;

src/widgets/CollectionSetupTreeView.h
@@ -1,0 +1,25 @@
+#ifndef COLLECTIONSETUPTREEVIEW_H
+#define COLLECTIONSETUPTREEVIEW_H
+
+#include <QTreeView>
+
+class CollectionSetupTreeView : public QTreeView
+{
+    Q_OBJECT
+
+    public:
+        CollectionSetupTreeView( QWidget* );
+        ~CollectionSetupTreeView();
+
+    protected slots:
+        /** Shows a context menu if the right mouse button is pressed over a directory. */
+        void slotPressed( const QModelIndex &index );
+        void slotRescanDirTriggered();
+
+    private:
+        QAction *m_rescanDirAction;
+        QString m_currDir;
+
+};
+
+#endif // COLLECTIONSETUPTREEVIEW_H

commit 305d67913d82c32e9c15678dbe63621fcb6c93a7

Fix crash and a regression caused by commit 82f08e62684202
...and write code defensively that we only loose functionality but
don't crash in future.

Sometimes testing involves also right-clicking the touched widget,
Markey. ;)

CCMAIL: Mark Kretschmann <kretschmann@kde.org>

src/widgets/CollectionSetupTreeView.cpp
@@ -49,9 +49,11 @@ CollectionSetupTreeView::slotPressed( const QModelIndex &index )
 {
     DEBUG_BLOCK
     // --- show context menu on right mouse button
-    if( ( QApplication::mouseButtons() & Qt::RightButton ) && parent() )
+    QObject *grandParent = parent() ? parent()->parent() : 0;
+    CollectionSetup *collSetup = qobject_cast<CollectionSetup *>( grandParent );
+    if( ( QApplication::mouseButtons() & Qt::RightButton ) && collSetup )
     {
-        m_currDir = qobject_cast<CollectionSetup*>(parent())->modelFilePath( index );
+        m_currDir = collSetup->modelFilePath( index );
         debug() << "Setting current dir to " << m_currDir;
 
         // check if there is an sql collection covering the directory