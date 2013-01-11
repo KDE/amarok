KDE/amarok

commit 7f1f9f606ad1af5b3d1a07fd72898d78837cd551

DirectoryLoader: load tracks asyncly in a background thread
Also, load playlists even in subdirectories.

CHANGES:
 * Make dropping large directories into playlist more responsive by reading
   track metadata asynchronously in a background thread.

DIGEST: Amarok is now more responsive loading large directory
        structures into playlist

src/DirectoryLoader.cpp
@@ -19,14 +19,13 @@
 #include "DirectoryLoader.h"
 
 #include "core/support/Debug.h"
-#include "core-impl/collections/support/CollectionManager.h"
 #include "core-impl/playlists/types/file/PlaylistFileSupport.h"
+#include "core-impl/meta/file/File.h"
+#include "core-impl/meta/proxy/MetaProxy.h"
 #include "core/playlists/PlaylistFormat.h"
 #include "playlist/PlaylistController.h"
-#include "playlistmanager/PlaylistManager.h"
 
-#include <kio/job.h> // KIO::listRecursive
-
+#include <KIO/Job>
 
 DirectoryLoader::DirectoryLoader()
     : QObject( 0 )
@@ -43,7 +42,7 @@ DirectoryLoader::insertAtRow( int row )
 {
     m_row = row;
     m_localConnection = true;
-    connect( this, SIGNAL( finished( const Meta::TrackList& ) ), this, SLOT( doInsertAtRow() ) );
+    connect( this, SIGNAL(finished(Meta::TrackList)), SLOT(doInsertAtRow()) );
 }
 
 void
@@ -54,7 +53,7 @@ DirectoryLoader::doInsertAtRow()
 }
 
 void
-DirectoryLoader::init( const QList<QUrl>& qurls )
+DirectoryLoader::init( const QList<QUrl> &qurls )
 {
     QList<KUrl> kurls;
     foreach( const QUrl &qurl, qurls )
@@ -64,10 +63,9 @@ DirectoryLoader::init( const QList<QUrl>& qurls )
 }
 
 void
-DirectoryLoader::init( const QList<KUrl>& urls )
+DirectoryLoader::init( const QList<KUrl> &urls )
 {
     //drop from an external source or the file browser
-    debug() << "Drop from external source or file browser";
     QList<KIO::ListJob*> jobs;
 
     foreach( const KUrl &kurl, urls )
@@ -76,31 +74,13 @@ DirectoryLoader::init( const QList<KUrl>& urls )
         if( kitem.isDir() )
         {
             m_listOperations++;
-            KIO::ListJob* lister = KIO::listRecursive( kurl ); //kjob's delete themselves
-            connect( lister, SIGNAL( finished( KJob*) ),
-                     SLOT( listJobFinished( KJob*) ) );
-            connect( lister, SIGNAL( entries( KIO::Job*, const KIO::UDSEntryList& ) ),
-                     SLOT( directoryListResults( KIO::Job*, const KIO::UDSEntryList& ) ) );
+            KIO::ListJob* lister = KIO::listRecursive( kurl ); // KJobs delete themselves
+            connect( lister, SIGNAL(finished(KJob*)), SLOT(listJobFinished( KJob*)) );
+            connect( lister, SIGNAL(entries(KIO::Job*,KIO::UDSEntryList)),
+                             SLOT(directoryListResults(KIO::Job*,KIO::UDSEntryList)) );
         }
         else
-        {
-            if( Playlists::isPlaylist( kurl ) )
-            {
-                Playlists::PlaylistFilePtr playlist = Playlists::loadPlaylistFile( kurl );
-                if( playlist )
-                {
-                    playlist->triggerTrackLoad(); //playlist track loading is on demand.
-                    m_tracks << playlist->tracks();
-                }
-            }
-            else
-            {
-                Meta::TrackPtr track = CollectionManager::instance()->trackForUrl( kurl );
-                if( track )
-                    m_tracks << track;
-            }
-        }
-        //else TODO: notify user if can't decode, see also MyDirLister::matchesFilter       
+            appendFile( kurl );
     }
     if( m_listOperations == 0 )
     {
@@ -136,14 +116,9 @@ DirectoryLoader::finishUrlList()
     if( !m_expanded.isEmpty() )
     {
         qStableSort( m_expanded.begin(), m_expanded.end(), DirectoryLoader::directorySensitiveLessThan );
-        foreach( const KFileItem& item, m_expanded )
+        foreach( const KFileItem &item, m_expanded )
         {
-            if( !item.isDir() )
-            {
-                Meta::TrackPtr track = CollectionManager::instance()->trackForUrl( item.url() );
-                if( track )
-                    m_tracks.append( track );
-            }
+            appendFile( item.url() );
         }
         qStableSort( m_tracks.begin(), m_tracks.end(), Meta::Track::lessThan );
     }
@@ -152,27 +127,36 @@ DirectoryLoader::finishUrlList()
         deleteLater();
 }
 
-bool
-DirectoryLoader::directorySensitiveLessThan( const KFileItem& item1, const KFileItem& item2 )
+void
+DirectoryLoader::appendFile( const KUrl &url )
 {
-    QString dir1 =  item1.url().directory();
-    QString dir2 =  item2.url().directory();
-
-    debug() << "dir1: " << dir1;
-    debug() << "dir2: " << dir2;
-    if(dir1 == dir2)
+    if( Playlists::isPlaylist( url ) )
     {
-        debug() << "dir1==dir2";
-        return item1.url().url() < item2.url().url();
+        Playlists::PlaylistFilePtr playlist = Playlists::loadPlaylistFile( url );
+        if( playlist )
+        {
+            playlist->triggerTrackLoad(); // playlist track loading is on demand.
+            m_tracks << playlist->tracks();
+        }
     }
-    else if( dir1 < dir2 )
+    else if( MetaFile::Track::isTrack( url ) )
     {
-        debug() << "dir1<dir2";
-        return true;
-    } else {
-        debug() << "dir1>dir2";
-        return false;
+        MetaProxy::TrackPtr proxyTrack( new MetaProxy::Track( url ) );
+        proxyTrack->setName( url.fileName() ); // set temporary name
+        m_tracks << Meta::TrackPtr( proxyTrack.data() );
     }
 }
 
+bool
+DirectoryLoader::directorySensitiveLessThan( const KFileItem &item1, const KFileItem &item2 )
+{
+    QString dir1 = item1.url().directory();
+    QString dir2 = item2.url().directory();
 
+    if( dir1 == dir2 )
+        return item1.url().url() < item2.url().url();
+    else if( dir1 < dir2 )
+        return true;
+    else
+        return false;
+}

src/DirectoryLoader.h
@@ -29,25 +29,24 @@ class KJob;
 namespace KIO {
     class Job;
     class UDSEntry;
-    typedef QList< UDSEntry >  UDSEntryList;
-    typedef QList< KFileItem > KFileItemList;
+    typedef QList<UDSEntry>  UDSEntryList;
+    typedef QList<KFileItem> KFileItemList;
 }
-
-
 
 class DirectoryLoader : public QObject
 {
     Q_OBJECT
+
     public:
         DirectoryLoader();
         ~DirectoryLoader();
 
         void insertAtRow( int row ); // call before init to tell the loader the row to start inserting tracks
-        void init( const QList<KUrl>& urls ); //!< list all
-        void init( const QList<QUrl>& urls ); //!< convience
+        void init( const QList<KUrl> &urls ); //!< list all
+        void init( const QList<QUrl> &urls ); //!< convience
 
     signals:
-        void finished( const Meta::TrackList& );
+        void finished( const Meta::TrackList &tracks );
 
     private slots:
         void directoryListResults( KIO::Job *job, const KIO::UDSEntryList &list );
@@ -56,7 +55,14 @@ class DirectoryLoader : public QObject
 
     private:
         void finishUrlList();
-        static bool directorySensitiveLessThan( const KFileItem& item1, const KFileItem& item2 );
+
+        /**
+         * Probe the file @file, if it is a playlist or a track, add it to m_tracks
+         */
+        void appendFile( const KUrl &url );
+
+        static bool directorySensitiveLessThan( const KFileItem &item1, const KFileItem &item2 );
+
         /**
          * the number of directory list operations. this is used so that
          * the last directory operations knows its the last */

src/playlist/PlaylistController.cpp
@@ -236,12 +236,12 @@ Playlist::Controller::insertOptioned( Playlists::PlaylistList list, int options 
 }
 
 void
-Playlist::Controller::insertOptioned( QList<KUrl>& urls, int options )
+Playlist::Controller::insertOptioned( QList<KUrl> &urls, int options )
 {
-    DirectoryLoader* dl = new DirectoryLoader(); //dl handles memory management
+    DirectoryLoader *dl = new DirectoryLoader(); // dl handles memory management
     dl->setProperty( "options", QVariant( options ) );
-    connect( dl, SIGNAL( finished( const Meta::TrackList& ) ), this, SLOT( slotFinishDirectoryLoader( const Meta::TrackList& ) ) );
-
+    connect( dl, SIGNAL(finished(Meta::TrackList)),
+                 SLOT(slotDirectoryLoaderFinished(Meta::TrackList)) );
     dl->init( urls );
 }
 
@@ -285,21 +285,6 @@ Playlist::Controller::insertPlaylists( int topModelRow, Playlists::PlaylistList 
         tl += playlist->tracks();
     }
     insertTracks( topModelRow, tl );
-}
-
-void
-Playlist::Controller::insertUrls( int row, const QList<KUrl>& urls )
-{
-    Q_UNUSED( row );
-    DEBUG_BLOCK
-
-    // FIXME: figure out some way to have this insert at the appropriate row, rather than always at end
-    const int options = Append | DirectPlay;
-    DirectoryLoader* dl = new DirectoryLoader(); //dl handles memory management
-    dl->setProperty( "options", QVariant( options ) );
-    connect( dl, SIGNAL( finished( const Meta::TrackList& ) ), this, SLOT( slotFinishDirectoryLoader( const Meta::TrackList& ) ) );
-
-    dl->init( urls );
 }
 
 void
@@ -528,13 +513,10 @@ Playlist::Controller::clear()
  **************************************************/
 
 void
-Playlist::Controller::slotFinishDirectoryLoader( const Meta::TrackList& tracks )
+Playlist::Controller::slotDirectoryLoaderFinished( const Meta::TrackList &tracks )
 {
-    DEBUG_BLOCK
     if( !tracks.isEmpty() )
-    {
         insertOptioned( tracks, sender()->property( "options" ).toInt() );
-    }
 }
 
 int

src/playlist/PlaylistController.h
@@ -89,7 +89,7 @@ public slots:
     void insertOptioned( Meta::TrackList list, int options );
     void insertOptioned( Playlists::PlaylistPtr playlist, int options );
     void insertOptioned( Playlists::PlaylistList list, int options );
-    void insertOptioned( QList<KUrl>& urls, int options );
+    void insertOptioned( QList<KUrl> &urls, int options );
 
     /**
      * Handles the insertion of one or more tracks into the playlist on a specific row.
@@ -101,7 +101,6 @@ public slots:
     void insertTracks( int topModelRow, Meta::TrackList list );
     void insertPlaylist( int topModelRow, Playlists::PlaylistPtr playlist );
     void insertPlaylists( int topModelRow, Playlists::PlaylistList playlists );
-    void insertUrls( int topModelRow, const QList<KUrl>& urls );
 
     /**
      * Handles the removal of a single track from the playlist.
@@ -176,7 +175,7 @@ signals:
     void replacingPlaylist();
 
 private slots:
-    void slotFinishDirectoryLoader( const Meta::TrackList& );
+    void slotDirectoryLoaderFinished( const Meta::TrackList &tracks );
 
 private:
     Controller();

commit d8aa75f0f5ba607c7db14a5b1ddea6c17beeb966

DirectoryLoader, FileView: fix broken Copy/Move to collection
I accidentally broke this in commit 7f1f9f606ad1af5b3d1.

This doesn't go into ChangeLog, because the bug was introduced after
2.7 Beta was released. Thierry, please test that this commit fixes
your problem.

BUG: 313003
FIXED-IN: 2.7

src/DirectoryLoader.cpp
@@ -27,9 +27,10 @@
 
 #include <KIO/Job>
 
-DirectoryLoader::DirectoryLoader()
+DirectoryLoader::DirectoryLoader( LoadingMode loadingMode )
     : QObject( 0 )
         , m_listOperations( 0 )
+        , m_loadingMode( loadingMode )
         , m_localConnection( false )
         , m_row( 0 )
 {}
@@ -141,9 +142,21 @@ DirectoryLoader::appendFile( const KUrl &url )
     }
     else if( MetaFile::Track::isTrack( url ) )
     {
-        MetaProxy::TrackPtr proxyTrack( new MetaProxy::Track( url ) );
-        proxyTrack->setName( url.fileName() ); // set temporary name
-        m_tracks << Meta::TrackPtr( proxyTrack.data() );
+        Meta::TrackPtr track;
+        switch( m_loadingMode )
+        {
+            case AsyncLoading:
+            {
+                MetaProxy::TrackPtr proxyTrack( new MetaProxy::Track( url ) );
+                proxyTrack->setName( url.fileName() ); // set temporary name
+                track = proxyTrack.data();
+                break;
+            }
+            case BlockingLoading:
+                track = new MetaFile::Track( url );
+                break;
+        }
+        m_tracks << track;
     }
 }
 

src/DirectoryLoader.h
@@ -38,7 +38,18 @@ class DirectoryLoader : public QObject
     Q_OBJECT
 
     public:
-        DirectoryLoader();
+        /**
+         * BlockingLoading: the tracks are loaded synchronously in the main thread,
+         *                  but you have a guarantee that their metadata is valid
+         *                  from start
+         * AsyncLoading: the tracks and loaded using MetaProxy::Tracks, initial metadata
+         *               are just stubs and the real ones are fetched in the banground,
+         *               then you get metadataUpdated() */
+        enum LoadingMode {
+            BlockingLoading,
+            AsyncLoading
+        };
+        DirectoryLoader( LoadingMode loadingMode = AsyncLoading );
         ~DirectoryLoader();
 
         void insertAtRow( int row ); // call before init to tell the loader the row to start inserting tracks
@@ -67,6 +78,7 @@ class DirectoryLoader : public QObject
          * the number of directory list operations. this is used so that
          * the last directory operations knows its the last */
         int m_listOperations;
+        LoadingMode m_loadingMode;
         bool m_localConnection; //!< was insertAtRow called? otherwise finishUrlList should deleteLater
         int m_row; //!< for insertAtRow
         KIO::KFileItemList m_expanded;

src/browsers/filebrowser/FileView.cpp
@@ -287,7 +287,8 @@ FileView::slotPrepareMoveTracks()
     if( list.isEmpty() )
         return;
 
-    DirectoryLoader* dl = new DirectoryLoader();
+    // copy/move to collection is a modal dialog anyway, prevent bug 313003
+    DirectoryLoader* dl = new DirectoryLoader( DirectoryLoader::BlockingLoading ); // auto-deletes itself
     connect( dl, SIGNAL(finished(Meta::TrackList)), SLOT(slotMoveTracks(Meta::TrackList)) );
     dl->init( list.urlList() );
 }
@@ -308,7 +309,8 @@ FileView::slotPrepareCopyTracks()
     if( list.isEmpty() )
         return;
 
-    DirectoryLoader* dl = new DirectoryLoader();
+    // copy/move to collection is a modal dialog anyway, prevent bug 313003
+    DirectoryLoader* dl = new DirectoryLoader( DirectoryLoader::BlockingLoading ); // auto-deletes itself
     connect( dl, SIGNAL(finished(Meta::TrackList)), SLOT(slotCopyTracks(Meta::TrackList)) );
     dl->init( list.urlList() );
 }