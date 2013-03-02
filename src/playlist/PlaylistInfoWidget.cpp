3b72a007e4de3409cd4f1d710a98616a2e78d886
Ralf Engels <ralf-engels@gmx.de>
2013-03-02 10:20:00 +0100

    Speed up large playlists by not iterating over every item on ever change

--- src/playlist/PlaylistInfoWidget.cpp
+++ src/playlist/PlaylistInfoWidget.cpp
@@ -18,7 +18,12 @@
 
 #include "core/meta/support/MetaUtility.h"
 #include "core/support/Debug.h"
+#include "playlist/PlaylistActions.h"
 #include "playlist/PlaylistModelStack.h"
+
+#include "QEvent"
+#include "QHelpEvent"
+#include "QToolTip"
 
 PlaylistInfoWidget::PlaylistInfoWidget( QWidget *parent )
     : QLabel( parent )
@@ -46,8 +51,6 @@ PlaylistInfoWidget::~PlaylistInfoWidget()
 void
 PlaylistInfoWidget::updateTotalPlaylistLength() //SLOT
 {
-    DEBUG_BLOCK
-
     const quint64 totalLength = The::playlist()->totalLength();
     const int trackCount = The::playlist()->qaim()->rowCount();
 
@@ -57,46 +60,69 @@ PlaylistInfoWidget::updateTotalPlaylistLength() //SLOT
         setText( i18ncp( "%1 is number of tracks, %2 is time",
                          "%1 track (%2)", "%1 tracks (%2)",
                          trackCount, prettyTotalLength ) );
-        quint64 queuedTotalLength( 0 );
-        quint64 queuedTotalSize( 0 );
-        int queuedCount( 0 );
-
-        for( int i = 0; i < trackCount; ++i )
-        {
-            if( The::playlist()->queuePositionOfRow( i ) != 0 )
-            {
-                queuedTotalLength += The::playlist()->trackAt( i )->length();
-                queuedTotalSize += The::playlist()->trackAt( i )->filesize();
-                ++queuedCount;
-            }
-        }
-
-        const quint64 totalSize = The::playlist()->totalSize();
-        const QString prettyTotalSize = Meta::prettyFilesize( totalSize );
-        const QString prettyQueuedTotalLength = Meta::msToPrettyTime( queuedTotalLength );
-        const QString prettyQueuedTotalSize   = Meta::prettyFilesize( queuedTotalSize );
-
-        QString tooltipLabel;
-        if( queuedCount > 0 && queuedTotalLength > 0 )
-        {
-            tooltipLabel = i18n( "Total playlist size: %1", prettyTotalSize )       + '\n'
-                         + i18n( "Queue size: %1",          prettyQueuedTotalSize ) + '\n'
-                         + i18n( "Queue length: %1",        prettyQueuedTotalLength );
-        }
-        else
-        {
-            tooltipLabel = i18n( "Total playlist size: %1", prettyTotalSize );
-        }
-
-        setToolTip( tooltipLabel );
     }
     else if( ( totalLength == 0 ) && ( trackCount > 0 ) )
     {
         setText( i18ncp( "%1 is number of tracks", "%1 track", "%1 tracks", trackCount ) );
-        setToolTip( 0 );
     }
     else // Total Length will not be > 0 if trackCount is 0, so we can ignore it
     {
         setText( i18n( "No tracks" ) );
     }
 }
+
+bool
+PlaylistInfoWidget::event( QEvent *event )
+{
+    if( event->type() == QEvent::ToolTip ) {
+        QHelpEvent *helpEvent = static_cast<QHelpEvent *>(event);
+        const quint64 totalLength = The::playlist()->totalLength();
+        const int trackCount = The::playlist()->qaim()->rowCount();
+
+        if( totalLength == 0 || trackCount == 0 )
+        {
+            QToolTip::hideText();
+            event->ignore();
+        }
+        else
+        {
+            // - determine the queued tracks
+            quint64 queuedTotalLength( 0 );
+            quint64 queuedTotalSize( 0 );
+            int queuedCount( 0 );
+
+            QQueue<quint64> queue = The::playlistActions()->queue();
+            foreach( quint64 id, queue )
+            {
+                Meta::TrackPtr track = The::playlist()->trackForId( id );
+                queuedTotalLength += track->length();
+                queuedTotalSize += track->filesize();
+                ++queuedCount;
+            }
+
+            // - set the tooltip
+            const quint64 totalSize = The::playlist()->totalSize();
+            const QString prettyTotalSize = Meta::prettyFilesize( totalSize );
+            const QString prettyQueuedTotalLength = Meta::msToPrettyTime( queuedTotalLength );
+            const QString prettyQueuedTotalSize   = Meta::prettyFilesize( queuedTotalSize );
+
+            QString tooltipLabel;
+            if( queuedCount > 0 && queuedTotalLength > 0 )
+            {
+                tooltipLabel = i18n( "Total playlist size: %1", prettyTotalSize )       + '\n'
+                    + i18n( "Queue size: %1",          prettyQueuedTotalSize ) + '\n'
+                    + i18n( "Queue length: %1",        prettyQueuedTotalLength );
+            }
+            else
+            {
+                tooltipLabel = i18n( "Total playlist size: %1", prettyTotalSize );
+            }
+
+            QToolTip::showText( helpEvent->globalPos(), tooltipLabel );
+        }
+
+        return true;
+    }
+    return QWidget::event(event);
+}
+
--- src/playlist/PlaylistInfoWidget.h
+++ src/playlist/PlaylistInfoWidget.h
@@ -1,5 +1,6 @@
 /****************************************************************************************
  * Copyright (c) 2011 Kevin Funk <krf@electrostorm.net>                                 *
+ * Copyright (c) 2013 Ralf Engels <ralf-engels@gmx.de>                                  *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
  * the terms of the GNU General Public License as published by the Free Software        *
@@ -25,6 +26,9 @@ class PlaylistInfoWidget : public QLabel
 public:
     explicit PlaylistInfoWidget( QWidget* parent = 0 );
     virtual ~PlaylistInfoWidget();
+
+protected:
+    bool event( QEvent *event );
 
 private Q_SLOTS:
     void updateTotalPlaylistLength();
