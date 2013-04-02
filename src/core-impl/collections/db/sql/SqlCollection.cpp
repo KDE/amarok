KDE/amarok

commit 3344f1bade96252bc0efddaf4396053eb8925533

Fix some Krazy i18n errors
comply to kde4 standards, set external args inside the i18n call

src/core-impl/collections/db/sql/SqlCollection.cpp
@@ -284,8 +284,7 @@ SqlCollection::SqlCollection( SqlStorage* storage )
             KMessageBox::error( The::mainWindow(), //parent
                                 i18n( "The amarok database reported the following errors:\n"
                                       "%1\n"
-                                      "In most cases you will need to resolve these errors before Amarok will run properly." ).
-                                arg(storage->getLastErrors().join( "\n" ) ) );
+                                      "In most cases you will need to resolve these errors before Amarok will run properly.", storage->getLastErrors().join( "\n" ) );
         }
         else
         {

src/MainWindow.cpp
@@ -1209,8 +1209,7 @@ MainWindow::slotShowMenuBar()
     {
         //User have chosen to hide a menu. Lets warn him
         if (KMessageBox::warningContinueCancel(this,
-            i18n("You have chosen to hide the menu bar.\n\nPlease remember that you can always use the shortcut \"%1\" to bring it back.")
-                .arg(m_showMenuBar->shortcut().toString()),
+            i18n("You have chosen to hide the menu bar.\n\nPlease remember that you can always use the shortcut \"%1\" to bring it back.", m_showMenuBar->shortcut().toString() ),
             i18n("Hide Menu"), KStandardGuiItem::cont(), KStandardGuiItem::cancel(), "showMenubar") != KMessageBox::Continue)
         {
             //Cancel menu hiding. Revert menu item to checked state.

src/ScriptManager.cpp
@@ -301,8 +301,7 @@ void ScriptManager::handleException(const QScriptValue& value)
     if (!engine)
         return;
 
-    Amarok::Components::logger()->longMessage( i18n( "Script error reported by: %1\n%2" )
-            .arg( scriptNameForEngine( engine ), value.toString() ), Amarok::Logger::Error );
+    Amarok::Components::logger()->longMessage( i18n( "Script error reported by: %1\n%2", scriptNameForEngine( engine ), value.toString() ), Amarok::Logger::Error );
 }
 
 void

src/browsers/playlistbrowser/DynamicBiasWidgets.cpp
@@ -111,7 +111,7 @@ PlaylistBrowserNS::BiasDialog::factoriesChanged()
         m_biasSelection->setCurrentIndex( m_biasSelection->count() - 1 );
         m_descriptionLabel->setText( i18n( "This bias is a replacement for another bias\n"
                                          "which is currently not loaded or deactivated.\n"
-                                         "The original bias name was %1." ).arg( m_bias->name() ) );
+                                         "The original bias name was %1.", m_bias->name() );
     }
 
     connect( Dynamic::BiasFactory::instance(), SIGNAL( changed() ),

src/core-impl/collections/db/sql/SqlScanResultProcessor.cpp
@@ -84,7 +84,7 @@ SqlScanResultProcessor::displayMessages()
     QString text = i18n( "<ul><li>%1</li></ul>"
                          "In most cases this means that not all of your tracks were imported.<br>"
                          "See <a href='http://userbase.kde.org/Amarok/Manual/Various/TroubleshootingAndCommonProblems#Duplicate_Tracks'>"
-                         "Amarok Manual</a> for information about duplicate tracks." ).arg( errorList );
+                         "Amarok Manual</a> for information about duplicate tracks.", errorList );
     KMessageBox::error( The::mainWindow(), text, i18n( "Errors During Collection Scan" ),
                         KMessageBox::AllowLink );
 

src/services/lastfm/biases/WeeklyTopBias.cpp
@@ -118,9 +118,7 @@ QString
 Dynamic::WeeklyTopBias::toString() const
 {
     return i18nc("WeeklyTopBias bias representation",
-                 "Tracks from the Last.fm top lists from %1 to %2")
-        .arg( m_range.from.toString() )
-        .arg( m_range.to.toString() );
+                 "Tracks from the Last.fm top lists from %1 to %2", m_range.from.toString(), m_range.to.toString() );
 }
 
 QWidget*

commit 1ce94c4fba83b3d6279fc976b9fd3d9a2df5e775

Fix another build error introduced by commit 3344f1bade96252
This fixes the build for me.

src/core-impl/collections/db/sql/SqlCollection.cpp
@@ -281,10 +281,10 @@ SqlCollection::SqlCollection( SqlStorage* storage )
     {
         if( QApplication::type() != QApplication::Tty )
         {
-            KMessageBox::error( The::mainWindow(), //parent
-                                i18n( "The amarok database reported the following errors:\n"
-                                      "%1\n"
-                                      "In most cases you will need to resolve these errors before Amarok will run properly.", storage->getLastErrors().join( "\n" ) );
+            KMessageBox::error( The::mainWindow(), i18n( "The amarok database reported "
+                    "the following errors:\n%1\nIn most cases you will need to resolve "
+                    "these errors before Amarok will run properly.",
+                    storage->getLastErrors().join( "\n" ) ) );
         }
         else
         {