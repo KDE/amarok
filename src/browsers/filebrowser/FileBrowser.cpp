KDE/amarok

commit b2149a74c29734f206674c14d65f52c006437f56

FileView, FileBrowser: rework of keyboard, mouse & context menu behaviour
Also, some minor fixes to enable smooth keyboard navigation are done

CHANGES:
 * Harmonize keyboard, mouse and context menu behaviour of the Files pane
   so that it matches Local Music and Saved Playlists panes.

BUG: 307386
FIXED-IN: 2.7

src/browsers/filebrowser/FileBrowser.cpp
@@ -174,9 +174,9 @@ FileBrowser::FileBrowser( const char *name, QWidget *parent )
     : BrowserCategory( name, parent )
     , d( new FileBrowser::Private( this ) )
 {
-    setLongDescription( i18n( "The file browser lets you browse files anywhere on your system, " \
-                        "regardless of whether these files are part of your local collection. " \
-                        "You can then add these files to the playlist as well as perform basic "\
+    setLongDescription( i18n( "The file browser lets you browse files anywhere on your system, "
+                        "regardless of whether these files are part of your local collection. "
+                        "You can then add these files to the playlist as well as perform basic "
                         "file operations." )
                        );
     setImagePath( KStandardDirs::locate( "data", "amarok/images/hover_info_files.png" ) );
@@ -226,17 +226,11 @@ FileBrowser::initView()
         connect( action, SIGNAL(toggled(bool)), this, SLOT(toggleColumn(bool) ) );
     }
 
-    connect( d->fileView->header(), SIGNAL(geometriesChanged()), this, SLOT(slotSaveHeaderState()) );
-    connect( d->fileView, SIGNAL(activated( const QModelIndex & )),
-                          SLOT(itemActivated( const QModelIndex & )) );
-    if( !KGlobalSettings::singleClick() )
-    {
-        connect( d->fileView, SIGNAL(doubleClicked( const QModelIndex & )),
-                              SLOT(itemActivated( const QModelIndex & ))
-               );
-    }
-
-    connect( d->placesAction, SIGNAL(triggered( bool)), this, SLOT(showPlaces()) );
+    connect( d->fileView->header(), SIGNAL(geometriesChanged()),
+                                    SLOT(slotSaveHeaderState()) );
+    connect( d->fileView, SIGNAL(navigateToDirectory(QModelIndex)),
+                          SLOT(slotNavigateToDirectory(QModelIndex)) );
+    connect( d->placesAction, SIGNAL(triggered(bool)), SLOT(showPlaces()) );
 }
 
 FileBrowser::~FileBrowser()
@@ -275,7 +269,7 @@ FileBrowser::currentDir() const
 }
 
 void
-FileBrowser::itemActivated( const QModelIndex &index )
+FileBrowser::slotNavigateToDirectory( const QModelIndex &index )
 {
     if( d->showingPlaces )
     {
@@ -283,13 +277,6 @@ FileBrowser::itemActivated( const QModelIndex &index )
 
         if( !placesUrl.isEmpty() )
         {
-            d->fileView->setModel( d->mimeFilterProxyModel );
-            d->fileView->setSelectionMode( QAbstractItemView::ExtendedSelection );
-            d->fileView->setDragEnabled( true );
-            d->fileView->header()->setVisible( true );
-            d->restoreHeaderState();
-            d->showingPlaces = false;
-
             //needed to make the home folder url look nice. We cannot just strip all
             //protocol headers as that will break remote, trash, ...
             if( placesUrl.startsWith( "file://" ) )
@@ -306,35 +293,20 @@ FileBrowser::itemActivated( const QModelIndex &index )
                 d->placesModel->requestSetup( index );
             }
             else
-            {
-                d->fileView->setModel( d->mimeFilterProxyModel );
-                d->fileView->setSelectionMode( QAbstractItemView::ExtendedSelection );
-                d->fileView->header()->setVisible( true );
-                d->fileView->setDragEnabled( true );
-                d->restoreHeaderState();
-                d->showingPlaces = false;
-            }
+                warning() << __PRETTY_FUNCTION__ << "empty places url that doesn't need setup?";
         }
     }
     else
     {
         KFileItem file = index.data( KDirModel::FileItemRole ).value<KFileItem>();
-        KUrl filePath = file.url();
 
         if( file.isDir() )
         {
             d->backStack.push( d->currentPath );
-            setDir( filePath );
+            setDir( file.url() );
         }
         else
-        {
-            if( MetaFile::Track::isTrack( filePath ) )
-            {
-                QList<KUrl> urls;
-                urls << filePath;
-                The::playlistController()->insertOptioned( urls, Playlist::AppendAndPlay );
-            }
-        }
+            warning() << __PRETTY_FUNCTION__ << "called for non-directory";
     }
 }
 
@@ -345,12 +317,8 @@ FileBrowser::addItemActivated( const QString &callbackString )
     if( callbackString.isEmpty() )
         return;
 
-    d->kdirModel->dirLister()->openUrl( KUrl( callbackString ) );
     d->backStack.push( d->currentPath );
-    d->currentPath = KUrl(callbackString);
-    d->updateNavigateActions();
-    setupAddItems();
-    activate();
+    setDir( KUrl( callbackString ) );
 }
 
 void
@@ -423,11 +391,8 @@ FileBrowser::setupAddItems()
 void
 FileBrowser::reActivate()
 {
-    //go to root:
-    d->kdirModel->dirLister()->openUrl( KUrl( QDir::rootPath() ) );
-    d->currentPath = KUrl( QDir::rootPath() );
-    setupAddItems();
-    activate();
+    d->backStack.push( d->currentPath );
+    setDir( QDir::rootPath() );
 }
 
 QString
@@ -446,23 +411,26 @@ FileBrowser::setDir( const KUrl &dir )
         showPlaces();
     else
     {
-       //if we are currently showing "places" we need to remember to change the model
-       //back to the regular file model
-       if( d->showingPlaces )
-       {
-           d->fileView->setModel( d->mimeFilterProxyModel );
-           d->fileView->setSelectionMode( QAbstractItemView::ExtendedSelection );
-           d->fileView->setDragEnabled( true );
-           d->fileView->header()->setVisible( true );
-           d->restoreHeaderState(); // read config so the header state is restored
-           d->showingPlaces = false;
-       }
+        // if we are currently showing "places" we need to remember to change the model
+        // back to the regular file model
+        if( d->showingPlaces )
+        {
+            d->fileView->setModel( d->mimeFilterProxyModel );
+            d->fileView->setSelectionMode( QAbstractItemView::ExtendedSelection );
+            d->fileView->setDragEnabled( true );
+            d->fileView->header()->setVisible( true );
+            d->restoreHeaderState(); // read config so the header state is restored
+            d->showingPlaces = false;
+        }
 
-       d->kdirModel->dirLister()->openUrl( dir );
-       d->currentPath = dir;
-       d->updateNavigateActions();
-       setupAddItems();
-       activate();
+        d->kdirModel->dirLister()->openUrl( dir );
+        d->currentPath = dir;
+        d->updateNavigateActions();
+        setupAddItems();
+        activate();
+
+        // set the first item as current so that keyboard navigation works
+        new DelayedActivator( d->fileView );
     }
 }
 
@@ -505,6 +473,7 @@ FileBrowser::up()
 void
 FileBrowser::home()
 {
+    d->backStack.push( d->currentPath );
     setDir( KUrl( QDir::homePath() ) );
 }
 
@@ -515,21 +484,23 @@ FileBrowser::showPlaces()
     {
         d->placesModel = new FilePlacesModel( this );
         d->placesModel->setObjectName( "PLACESMODEL");
-        connect( d->placesModel, SIGNAL(setupDone( const QModelIndex &, bool )),
-                                 SLOT(setupDone( const QModelIndex &, bool )) );
+        connect( d->placesModel, SIGNAL(setupDone(QModelIndex,bool)),
+                                 SLOT(setupDone(QModelIndex,bool)) );
     }
 
     clearAdditionalItems();
 
     QStringList siblings;
     addAdditionalItem( new BrowserBreadcrumbItem( i18n( "Places" ), siblings, QDir::homePath(),
-                                                  this )
-                     );
+                                                  this ) );
     d->showingPlaces = true;
     d->fileView->setModel( d->placesModel );
     d->fileView->setSelectionMode( QAbstractItemView::SingleSelection );
     d->fileView->header()->setVisible( false );
     d->fileView->setDragEnabled( false );
+
+    // set the first item as current so that keyboard navigation works
+    new DelayedActivator( d->fileView );
 }
 
 void
@@ -541,21 +512,52 @@ FileBrowser::setupDone( const QModelIndex & index, bool success )
 
         if( !placesUrl.isEmpty() )
         {
-            d->fileView->setModel( d->mimeFilterProxyModel );
-            d->fileView->setSelectionMode( QAbstractItemView::ExtendedSelection );
-            d->fileView->header()->setVisible( true );
-            d->fileView->setDragEnabled( true );
-            d->restoreHeaderState();
-            d->showingPlaces = false;
-
-            //needed to make folder urls look nice. We cannot just strip all protocol headers
-            //as that will break remote, trash, ...
+            // needed to make folder urls look nice. We cannot just strip all protocol headers
+            // as that will break remote, trash, ...
             if( placesUrl.startsWith( "file://" ) )
                 placesUrl = placesUrl.replace( "file://", QString() );
 
+            d->backStack.push( d->currentPath );
             setDir( placesUrl );
         }
     }
+}
+
+DelayedActivator::DelayedActivator( QAbstractItemView *view )
+    : QObject( view )
+    , m_view( view )
+{
+    QAbstractItemModel *model = view->model();
+    if( !model )
+        deleteLater();
+
+    // short-cut for already-filled models
+    if( model->rowCount() > 0 )
+    {
+        slotRowsInserted( QModelIndex(), 0 );
+        return;
+    }
+
+    connect( model, SIGNAL(rowsInserted(QModelIndex,int,int)),
+                    SLOT(slotRowsInserted(QModelIndex,int)) );
+
+    connect( model, SIGNAL(destroyed(QObject*)), SLOT(deleteLater()) );
+    connect( model, SIGNAL(layoutChanged()), SLOT(deleteLater()) );
+    connect( model, SIGNAL(modelReset()), SLOT(deleteLater()) );
+}
+
+void
+DelayedActivator::slotRowsInserted( const QModelIndex &parent, int start )
+{
+    QAbstractItemModel *model = m_view->model();
+    if( model )
+    {
+        // prevent duplicate calls, deleteLater() may fire REAL later
+        disconnect( model, 0, this, 0 );
+        QModelIndex idx = model->index( start, 0, parent );
+        m_view->selectionModel()->setCurrentIndex( idx, QItemSelectionModel::NoUpdate );
+    }
+    deleteLater();
 }
 
 #include "FileBrowser.moc"

src/browsers/CollectionTreeView.cpp
@@ -434,6 +434,7 @@ CollectionTreeView::mouseReleaseEvent( QMouseEvent *event )
         event->accept();
         return;
     }
+
     Amarok::PrettyTreeView::mouseReleaseEvent( event );
 }
 

src/browsers/filebrowser/FileBrowser.h
@@ -21,6 +21,7 @@
 
 #include <KUrl>
 
+class QAbstractItemView;
 class QModelIndex;
 
 class FileBrowser : public BrowserCategory
@@ -47,7 +48,7 @@ public:
     QString currentDir() const;
 
 protected slots:
-    void itemActivated( const QModelIndex &index );
+    void slotNavigateToDirectory( const QModelIndex &index );
 
     void addItemActivated( const QString &callback );
 
@@ -101,6 +102,24 @@ private:
     Private *const d;
 
     Q_PRIVATE_SLOT( d, void slotSaveHeaderState() )
+};
+
+/**
+ * Helper class that calls setCurrentIndex on a model view as soon as the model
+ * adds a row and then it auto-deletes itself.
+ */
+class DelayedActivator : public QObject
+{
+    Q_OBJECT
+
+    public:
+        explicit DelayedActivator( QAbstractItemView *view );
+
+    private slots:
+        void slotRowsInserted( const QModelIndex &parent, int start );
+
+    private:
+        QAbstractItemView *m_view;
 };
 
 #endif // FILEBROWSERMKII_H

src/browsers/filebrowser/FileView.h
@@ -20,6 +20,7 @@
 
 #include "core/collections/Collection.h"
 #include "core/meta/Meta.h"
+#include "playlist/PlaylistController.h"
 #include "widgets/PrettyTreeView.h"
 
 #include <KFileItem>
@@ -61,8 +62,10 @@ class FileView : public Amarok::PrettyTreeView
 public:
     FileView( QWidget *parent );
 
-protected slots:
+signals:
+    void navigateToDirectory( const QModelIndex &index );
 
+protected slots:
     void slotAppendToPlaylist();
     void slotReplacePlaylist();
     void slotEditTracks();
@@ -72,19 +75,30 @@ protected slots:
     void slotCopyTracks( const Meta::TrackList &tracks );
     void slotDelete( Qt::MouseButtons buttons, Qt::KeyboardModifiers modifiers );
 
-
 protected:
-    QList<QAction *> actionsForIndices( const QModelIndexList &indices );
-    void addSelectionToPlaylist( bool replace );
-    
+    enum ActionType {
+        PlaylistAction = 1,
+        OrganizeAction = 2,
+        EditAction = 4,
+        AllActions = PlaylistAction | OrganizeAction | EditAction
+    };
+    QList<QAction *> actionsForIndices( const QModelIndexList &indices, ActionType type = AllActions );
+    void addSelectionToPlaylist( Playlist::AddOptions options );
+
+    /**
+     * Convenience.
+     */
+    void addIndexToPlaylist( const QModelIndex &idx, Playlist::AddOptions options );
+    void addIndicesToPlaylist( const QModelIndexList &indices, Playlist::AddOptions options );
+
     virtual void contextMenuEvent( QContextMenuEvent *e );
     virtual void mouseReleaseEvent( QMouseEvent *event );
     virtual void mouseDoubleClickEvent( QMouseEvent *event );
+
+    virtual void keyPressEvent( QKeyEvent *event );
+
     virtual void startDrag( Qt::DropActions supportedActions );
     KFileItemList selectedItems() const;
-
-private slots:
-    void slotEditTriggered();
 
 private:
     Meta::TrackList tracksForEdit() const;
@@ -92,17 +106,14 @@ private:
     QAction *m_appendAction;
     QAction *m_loadAction;
     QAction *m_editAction;
-    QAction *m_separator1;
+    QAction *m_moveToTrashAction;
     QAction *m_deleteAction;
 
     PopupDropper *m_pd;
     QMutex m_dragMutex;
     bool m_ongoingDrag;
-    bool m_moveActivated;
-    bool m_copyActivated;
-    CollectionAction *m_moveAction;
-    CollectionAction *m_copyAction;
-    QPersistentModelIndex m_lastSelectedIndex;
+    QWeakPointer<Collections::Collection> m_moveDestinationCollection;
+    QWeakPointer<Collections::Collection> m_copyDestinationCollection;
 };
 
 #endif // end include guard

commit 3c0af86dfd65a03e88e2021b1a5db7b40e6a539d

Fix potential null pointer dereference.
Error was introduced with b2149a7. Detected by clang-analyzer.

CCMAIL: matej@laitl.cz

src/browsers/filebrowser/FileBrowser.cpp
@@ -593,7 +593,10 @@ DelayedActivator::DelayedActivator( QAbstractItemView *view )
 {
     QAbstractItemModel *model = view->model();
     if( !model )
+    {
         deleteLater();
+        return;
+    }
 
     // short-cut for already-filled models
     if( model->rowCount() > 0 )