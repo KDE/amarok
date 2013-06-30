d2ce24b31fc400862d5e5bc24d1bc2586ea343b1
Konrad Zemek <konrad.zemek@gmail.com>
2013-06-30 19:50:03 +0200

    Extend capabilities of CollectionTestImpl
    Collections::CollectionTestImpl can now be a target of track insertion
    through Collections::CollectionLocationTestImpl, and implements
    trackForUrl( KUrl ) method used by Collections::CollectionManager.

--- tests/CollectionTestImpl.h
+++ tests/CollectionTestImpl.h
@@ -1,5 +1,6 @@
 /****************************************************************************************
- * Copyright (c) 2010 Maximilian Kossick <maximilian.kossick@googlemail.com>       *
+ * Copyright (c) 2010 Maximilian Kossick <maximilian.kossick@googlemail.com>            *
+ * Copyright (c) 2013 Konrad Zemek <konrad.zemek@gmail.com>                             *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
  * the terms of the GNU General Public License as published by the Free Software        *
@@ -18,37 +19,135 @@
 #define COLLECTIONTESTIMPL_H
 
 #include "core/collections/Collection.h"
+#include "core/collections/CollectionLocation.h"
 #include "core-impl/collections/support/MemoryCollection.h"
 #include "core-impl/collections/support/MemoryQueryMaker.h"
 
 #include <KIcon>
-
 #include <QSharedPointer>
-
-//simple Collections::Collection implementation based on MemoryCollection
-
-class CollectionLocationTestImpl;
 
 namespace Collections {
 
-class CollectionTestImpl : public Collections::Collection
+/**
+ * A simple Collections::Collection implementation based on MemoryCollection
+ */
+class CollectionTestImpl : public Collection
 {
 public:
     CollectionTestImpl( const QString &collectionId )
-        : Collections::Collection(), mc( new MemoryCollection() )
-    { this->id = collectionId; }
+        : Collection()
+        , id( collectionId )
+        , mc( new MemoryCollection() )
+    {
+    }
 
-    QueryMaker* queryMaker() { return new MemoryQueryMaker( mc.toWeakRef(), id ); }
+    QueryMaker* queryMaker()
+    {
+        return new MemoryQueryMaker( mc.toWeakRef(), id );
+    }
 
-    KIcon icon() const { return KIcon(); }
+    KIcon icon() const
+    {
+        return KIcon();
+    }
 
-    QString collectionId() const { return id; }
-    QString prettyName() const { return id; }
+    QString collectionId() const
+    {
+        return id;
+    }
+
+    QString prettyName() const
+    {
+        return id;
+    }
+
+    CollectionLocation *location()
+    {
+        return new CollectionLocationTestImpl( mc, this );
+    }
+
+    bool possiblyContainsTrack( const KUrl &url ) const
+    {
+        return findTrackForUrl( url );
+    }
+
+    Meta::TrackPtr trackForUrl( const KUrl &url )
+    {
+        return findTrackForUrl( url );
+    }
 
     QString id;
-
     QSharedPointer<MemoryCollection> mc;
 
+private:
+    Meta::TrackPtr findTrackForUrl( const KUrl &url ) const
+    {
+        QReadLocker( mc->mapLock() );
+
+        foreach( const Meta::TrackPtr track, mc->trackMap().values() )
+            if( track->playableUrl() == url )
+                return track;
+
+        return Meta::TrackPtr( 0 );
+    }
+
+    class CollectionLocationTestImpl : public CollectionLocation
+    {
+    public:
+        CollectionLocationTestImpl( QSharedPointer<MemoryCollection> mc,
+                                    Collection *parentCollection )
+            : CollectionLocation( parentCollection )
+            , m_mc( mc )
+        {
+        }
+
+        QString prettyLocation() const
+        {
+            return "/" + collection()->prettyName();
+        }
+
+        bool isWritable() const
+        {
+            return true;
+        }
+
+        void copyUrlsToCollection( const QMap<Meta::TrackPtr, KUrl> &sources,
+                                   const Transcoding::Configuration &configuration )
+        {
+            Q_UNUSED( configuration );
+
+            QWriteLocker( m_mc->mapLock() );
+
+            foreach( Meta::TrackPtr track, sources.keys() )
+            {
+                m_mc->addTrack( track );
+                transferSuccessful( track );
+            }
+
+            slotCopyOperationFinished();
+        }
+
+        bool insert( const Meta::TrackPtr &track, const QString &url )
+        {
+            Q_UNUSED( url );
+
+            QWriteLocker( m_mc->mapLock() );
+
+            if( m_mc->trackMap().contains( track->uidUrl() ) )
+                return false;
+
+            m_mc->addTrack( track );
+            return true;
+        }
+
+        QStringList actualLocation() const
+        {
+            return QStringList() << prettyLocation();
+        }
+
+    private:
+        QSharedPointer<MemoryCollection> m_mc;
+    };
 };
 
 } //namespace Collections
