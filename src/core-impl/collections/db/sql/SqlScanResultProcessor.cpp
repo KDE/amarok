KDE/amarok

commit ca88c8d303a7cda56f3890237d4a1adced736215

ScanResultProcessors: handle whole directories being removed in PartialUpdateScan
This gets a bit complicated, but hopefully the code is commented enough
that is it actually understandable by someone else than me.

BUGFIXES:
 * When you remove whole directories from the Local Collection and have
   Watch Folders for Changes enabled, the tracks now disappear from
   Collection Browser.

BUG: 311078
FIXED-IN: 2.8

src/core-impl/collections/db/sql/SqlScanResultProcessor.cpp
@@ -367,7 +367,16 @@ SqlScanResultProcessor::deleteDeletedDirectories()
 {
     SqlStorage *storage = m_collection->sqlStorage();
 
-    QList<DirectoryEntry> toCheck = mountedDirectories();
+    QList<DirectoryEntry> toCheck;
+    switch( m_type )
+    {
+        case GenericScanManager::FullScan:
+        case GenericScanManager::UpdateScan:
+            toCheck = mountedDirectories();
+            break;
+        case GenericScanManager::PartialUpdateScan:
+            toCheck = deletedDirectories();
+    }
 
     // -- check if the have been found during the scan
     foreach( const DirectoryEntry &e, toCheck )
@@ -408,11 +417,14 @@ SqlScanResultProcessor::deleteDeletedDirectories()
 }
 
 void
-SqlScanResultProcessor::deleteDeletedTracks( QSharedPointer<CollectionScanner::Directory> directory )
+SqlScanResultProcessor::deleteDeletedTracksAndSubdirs( QSharedPointer<CollectionScanner::Directory> directory )
 {
     Q_ASSERT( m_directoryIds.contains( directory.data() ) );
     int directoryId = m_directoryIds.value( directory.data() );
+    // only deletes tracks directly in this dir
     deleteDeletedTracks( directoryId );
+    // trigger deletion of deleted subdirectories in deleteDeletedDirectories():
+    m_scannedDirectoryIds.insert( directoryId );
 }
 
 void
@@ -548,11 +560,68 @@ SqlScanResultProcessor::mountedDirectories() const
         e.dirId = res.at( i++ ).toInt();
         e.deviceId = res.at( i++ ).toInt();
         e.dir = res.at( i++ );
-
         result << e;
     }
 
     return result;
+}
+
+QList<SqlScanResultProcessor::DirectoryEntry>
+SqlScanResultProcessor::deletedDirectories() const
+{
+    SqlStorage *storage = m_collection->sqlStorage();
+
+    QHash<int, DirectoryEntry> idToDirEntryMap; // for faster processing during filtering
+    foreach( int directoryId, m_scannedDirectoryIds )
+    {
+        QString query = QString( "SELECT deviceid, dir FROM directories WHERE id = %1" )
+                .arg( directoryId );
+        QStringList res = storage->query( query );
+        if( res.count() != 2 )
+        {
+            warning() << "unexpected query result" << res << "in deletedDirectories()";
+            continue;
+        }
+
+        int deviceId = res.at( 0 ).toInt();
+        QString dir = res.at( 1 );
+        // select all child directories
+        query = QString( "SELECT id, deviceid, dir FROM directories WHERE deviceid = %1 "
+                "AND dir LIKE '%2_%'" ).arg( deviceId ).arg( dir );
+        res = storage->query( query );
+        for( int i = 0; i < res.count(); )
+        {
+            DirectoryEntry e;
+            e.dirId = res.at( i++ ).toInt();
+            e.deviceId = res.at( i++ ).toInt();
+            e.dir = res.at( i++ );
+            idToDirEntryMap.insert( e.dirId, e );
+        }
+    }
+
+    // now we must fileter-out all found directories *and their children*, because the
+    // children are *not* in m_foundDirectories and deleteDeletedDirectories() would
+    // remove them errorneously
+    foreach( int foundDirectoryId, m_foundDirectories.values() )
+    {
+        if( idToDirEntryMap.contains( foundDirectoryId ) )
+        {
+            int existingDeviceId = idToDirEntryMap[ foundDirectoryId ].deviceId;
+            QString existingPath = idToDirEntryMap[ foundDirectoryId ].dir;
+            idToDirEntryMap.remove( foundDirectoryId );
+
+            // now remove all children of the existing directory
+            QMutableHashIterator<int, DirectoryEntry> it( idToDirEntryMap );
+            while( it.hasNext() )
+            {
+                const DirectoryEntry &e = it.next().value();
+                if( e.deviceId == existingDeviceId && e.dir.startsWith( existingPath ) )
+                    it.remove();
+            }
+        }
+    }
+
+    return idToDirEntryMap.values();
 }
 
 void

src/core-impl/collections/db/sql/SqlRegistry.cpp
@@ -93,7 +93,7 @@ SqlRegistry::getDirectory( const QString &path, uint mtime )
     // - create new entry
     if( res.isEmpty() )
     {
-        debug() << "SqlRegistry::getDirectory new Directory" << path;
+        debug() << "SqlRegistry::getDirectory(): new directory" << path;
         QString insert = QString( "INSERT INTO directories(deviceid,changedate,dir) "
                                   "VALUES (%1,%2,'%3');" )
                         .arg( QString::number( deviceId ), QString::number( mtime ),
@@ -111,8 +111,8 @@ SqlRegistry::getDirectory( const QString &path, uint mtime )
             QString update = QString( "UPDATE directories SET changedate = %1 "
                                       "WHERE id = %2;" )
                 .arg( QString::number( mtime ), res[0] );
-        debug() << "SqlRegistry::getDirectory update Directory"<<res[0]<<"from" << oldMtime << "to" << mtime;
-        debug() << update;
+            debug() << "SqlRegistry::getDirectory(): update directory" << path << "(id" <<
+                    res[0] << ") from" << oldMtime << "to" << mtime << "UNIX time";
             storage->query( update );
         }
     }

src/core-impl/collections/db/sql/SqlScanResultProcessor.h
@@ -52,7 +52,7 @@ class SqlScanResultProcessor : public AbstractScanResultProcessor
         /** Deletes all directories (and it's tracks) not contained in m_foundDirectories */
         virtual void deleteDeletedDirectories();
 
-        virtual void deleteDeletedTracks( QSharedPointer<CollectionScanner::Directory> directory );
+        virtual void deleteDeletedTracksAndSubdirs( QSharedPointer<CollectionScanner::Directory> directory );
 
         /** Removes all tracks contained in the directory dirId that are not contained in m_foundTracks. */
         virtual void deleteDeletedTracks( int directoryId );
@@ -106,13 +106,32 @@ class SqlScanResultProcessor : public AbstractScanResultProcessor
          */
         QList<DirectoryEntry> mountedDirectories() const;
 
-        Collections::SqlCollection* m_collection;
+        /**
+         * Get a list of directories that have been physically removed during the
+         * PartialUpdateScan.
+         */
+        QList<DirectoryEntry> deletedDirectories() const;
 
-        /** Contains all found directories with their absolute path and id */
+        Collections::SqlCollection *m_collection;
+
+        /**
+         * Contains all found directories with their absolute path and id
+         */
         QHash<QString, int> m_foundDirectories;
-        /** Contains all found tracks with the unique id and url id. QMultiHash only
-         *  because it implements contains( key, value ) */
+
+        /**
+         * Contains all found tracks with the unique id and url id. QMultiHash only
+         * because it implements contains( key, value )
+         */
         QMultiHash<QString, int> m_foundTracks;
+
+        /**
+         * In UpdateScan and PartialUpdateScan this set gets filled with directory ids
+         * that have been (non-recursively) fully scanned (not skipped). Direct child
+         * directories from the database that are not contained in m_foundDirectories can
+         * be considered deleted.
+         */
+        QSet<int> m_scannedDirectoryIds;
 
         // never dereference they key, it might be a stale pointer in corner cases
         QHash<CollectionScanner::Directory*, int> m_directoryIds;

src/scanner/AbstractScanResultProcessor.cpp
@@ -205,11 +205,10 @@ AbstractScanResultProcessor::scanSucceeded()
     // -- now check if some of the tracks are not longer used and also not moved to another directory
     foreach( QSharedPointer<CollectionScanner::Directory> dir, m_directories )
         if( !dir->isSkipped() )
-            deleteDeletedTracks( dir );
+            deleteDeletedTracksAndSubdirs( dir );
 
-    // -- delete all not-found directories
-    if( m_type != GenericScanManager::PartialUpdateScan )
-        deleteDeletedDirectories();
+    // -- delete all not-found directories (special handling for PartialUpdateScan):
+    deleteDeletedDirectories();
 
     cleanupMembers();
     emit endProgressOperation( this );

src/scanner/AbstractScanResultProcessor.h
@@ -94,10 +94,20 @@ class AMAROK_EXPORT AbstractScanResultProcessor : public QObject
         virtual void commitAlbum( CollectionScanner::Album *album ) = 0;
         virtual void commitTrack( CollectionScanner::Track *track, CollectionScanner::Album *srcAlbum ) = 0;
 
-        /** Deletes all directories (and it's tracks) not contained in m_foundDirectories */
+        /**
+         * Delete directories (and their tracks) that have been deleted.
+         *
+         * In FullScan nad UpdateScan mode, it should delete all database directories on
+         * mounted filesystems not encountered in the scan.
+         *
+         * In PartialUpdateScan mode, things are more complicated. It should only delete
+         * not-encountered (direct) subdirectories of directories passed to
+         * deleteDeletedTracksAndSubdirs(). However, if such directory is deleted, all its
+         * subdirectories should be removed too.
+         */
         virtual void deleteDeletedDirectories() = 0;
 
-        virtual void deleteDeletedTracks( QSharedPointer<CollectionScanner::Directory> directory ) = 0;
+        virtual void deleteDeletedTracksAndSubdirs( QSharedPointer<CollectionScanner::Directory> directory ) = 0;
 
         CollectionScanner::Album* sortTrack( CollectionScanner::Track *track,
                                              const QString &dirName = QString() );

commit d1d8a6fd2c81a17a9089e8bb81ddece0c5426904

SqlScanResultProcessor: fix stupid data-loss bug caused by my commit ca88c8d3
Commit ca88c8d3 introduced some extra logic and a member variable in
order to fix bug 311078. However, I forgot to add the variable into the
cleanupMembers() method, which resulted in state leaking from one
scanner to another.

What happened:
 1. User performed full rescan. For every directory in the collection,
    deleteDeletedTracksAndSubdirs() was called, which essentially
    inserted all existing directory ids to m_scannedDirectoryIds.
 2. All member variables were cleared except m_scannedDirectoryIds,
    which leaked into the next run.
 3. A file was changed, which triggered partial update scan run with
    only a couple of changed directories.
 4. Its deleteDeletedDirectories() method was called, which called
    deletedDirectories(), which used m_scannedDirectoryIds as a basis,
    removed just the newly scanned dirs from it and returned all the
    rest.
 5. deleteDeletedDirectories() deleted all but newly scanned
    directories.
 6. strohel feels ashamed.

The fix is to simply clear m_scannedDirectoryIds in cleanupMembers().

In addition to fixing bug 322603, this should also minimise changes of
reproducing bug 322474 (but not fixing the actual bug)

BUG: 322603
CCBUG: 322474
FIXED-IN: 2.8

src/core-impl/collections/db/sql/SqlScanResultProcessor.cpp
@@ -695,6 +695,7 @@ SqlScanResultProcessor::cleanupMembers()
 {
     m_foundDirectories.clear();
     m_foundTracks.clear();
+    m_scannedDirectoryIds.clear();
     m_directoryIds.clear();
     m_albumIds.clear();
 