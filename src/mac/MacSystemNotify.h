7c69a3fcd0e03e2c62fe481b28fe2800392c61ef
Daniel Meltzer <parallelgrapefruit@gmail.com>
2014-05-17 12:34:32 -0400

    Initial Work on a notification center backend for amarok.  Based on the quassel notification center code

---
+++ src/mac/MacSystemNotify.h
@@ -0,0 +1,48 @@
+/****************************************************************************************
+ * Copyright (c) 2014 Daniel Meltzer <parallelgrapefruit@gmail.com>                     *
+ * Based on Code from the Quassel OS X Backend.
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+ 
+#ifndef AMAROK_MACSYSTEMNOTIFY_H
+#define AMAROK_MACSYSTEMNOTIFY_H
+
+
+
+#include <QObject>
+#include <QString>
+
+#include "core/support/Debug.h"
+#include "core/meta/forward_declarations.h"
+#include "EngineController.h"
+    
+ // NOTE if not on mac, this whole file is useless, so not even going to try
+class OSXNotify : QObject
+{
+    Q_OBJECT
+
+    public:
+    OSXNotify( QString appName );
+
+    protected slots:
+        void show( Meta::TrackPtr );
+
+    private:
+        QString m_appName;
+        Meta::TrackPtr m_currentTrack;
+     
+};
+
+#endif
+ 
---
+++ src/mac/MacSystemNotify.mm
@@ -0,0 +1,94 @@
+/****************************************************************************************
+ * Copyright (c) 2014 Daniel Meltzer <parallelgrapefruit@gmail.com                      *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+#include "MacSystemNotify.h"
+
+#include "amarokconfig.h"
+#include "App.h"
+#include "core/support/Debug.h"
+#include "EngineController.h"
+#include "core/meta/Meta.h"
+#include "core/meta/support/MetaUtility.h" // for secToPrettyTime
+#include "SvgHandler.h"
+#include "TrayIcon.h"
+
+#import <Foundation/NSUserNotifiaction.h>
+#import <Foundation/CGImage>
+#import <Foundation/CGImageRef>
+
+namespace {
+    void SendNotifactionCenterMessage(NSString* title, NSString* subtitle, NSImage *image) {
+        NSUserNotifiactionCenter* center =
+            [NSUserNotificationCenter defaultUserNotifiactionCenter];
+        NSUserNotifaction *notifiaction =
+            [[NSUserNotifiaction alloc] init];
+
+        [notification setTitle: title];
+        [notification setSubtitle: subtitle];
+        [notification setContentImage: image];
+
+        [center deliverNotification: notification];
+
+        [notification release];
+    }
+}
+
+OSXNotify::OSXNotify(QString appName): QObject() :
+                m_appName( appName )
+{
+    EngineController *engine = The::engineController();
+
+    connect( engine, SIGNAL(trackChanged(Meta::TrackPtr)),
+             this, SLOT(show(Meta::TrackPtr)) );
+}
+
+void
+OSXNotify::show( Meta::TrackPtr track )
+{
+    DEBUG_BLOCK
+    QString text;
+    if( !track || track->playableUrl().isEmpty() )
+        text = i18n( "No track playing" );
+    else
+    {
+        text = track->prettyName();
+        if( track->artist() && !track->artist()->prettyName().isEmpty() )
+            text = track->artist()->prettyName() + " - " + text;
+        if( track->album() && !track->album()->prettyName().isEmpty() )
+            text += "\n (" + track->album()->prettyName() + ") ";
+        else
+            text += '\n';
+        if( track->length() > 0 )
+            text += Meta::msToPrettyTime( track->length() );
+    }
+
+    if( text.isEmpty() )
+        text =  track->playableUrl().fileName();
+
+    if( text.startsWith( "- " ) ) //When we only have a title tag, _something_ prepends a fucking hyphen. Remove that.
+        text = text.mid( 2 );
+
+    if( text.isEmpty() ) //still
+        text = i18n("No information available for this track");
+
+    QPixmap *image = The::coverCache()->getCover( track->album(), 100 );
+    CGImageRef cgImage = pixmap.toMacCGImageRef();
+    NSImage *nImage = [[NSImage alloc] initWithCGImage:cgImage size:NSZeroSize];
+    CFRelease(cgImage);
+    NSString *title = "Amarok";
+    NSString *subTitle = text.constData();
+    SendNotifactionCenterMessage( title, subTitle, nImage );
+}
