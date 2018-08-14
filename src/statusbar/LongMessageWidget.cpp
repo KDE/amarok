KDE/amarok

commit d78f666e58de146db7ff2d940a00b173583542f4

Port core application away from kdelibs4
Summary:
This patch ports the core app away from kdelibs4support to pure qt5/kf5. Kdelibs4support classes that don't have an equivalent in qt5 or kf5 like KSharedPtr or KHBox were replaced by new ones (AmarokSharedPtr and BoxWidget).
There are also a lot of small fixes and changes in this patch, which I unfortunately cannot break off into their own revisions, because I lost my git history to a faulty hard drive and my only backup was months old. Sorry for that.
I also temporarily deactivated the tests. They get ported and reactivated in a later patch to keep the scope of this at least somewhat manageable. Also in later patches are a new qml context area with the most important applets, some services and importers. It's all finished and ready for merge as soon as this patch lands.

Reviewers: #amarok, markey

Reviewed By: #amarok, markey

Subscribers: heikobecker, asturmlechner, stikonas, kpiwowarski, markey, spettini, #amarok

Tags: #amarok

Differential Revision: https://phabricator.kde.org/D10526

src/statusbar/LongMessageWidget.cpp
@@ -19,7 +19,7 @@
 
 #include "core/support/Debug.h"
 
-#include <KPushButton>
+#include <QPushButton>
 
 #include <QLabel>
 #include <QLayout>
@@ -47,10 +47,8 @@ LongMessageWidget::LongMessageWidget( QWidget *anchor, const QString &message,
     QPalette p = QToolTip::palette();
     setPalette( p );
 
-    KHBox *hbox = new KHBox( this );
-    layout()->addWidget( hbox );
-
-    hbox->setSpacing( 12 );
+    BoxWidget *hbox = new BoxWidget( false, this );
+    hbox->layout()->setSpacing( 12 );
 
     m_countdownFrame = new CountdownFrame( hbox );
     m_countdownFrame->setObjectName( "counterVisual" );
@@ -69,11 +67,9 @@ LongMessageWidget::LongMessageWidget( QWidget *anchor, const QString &message,
     alabel->setSizePolicy( QSizePolicy::MinimumExpanding, QSizePolicy::Preferred );
     alabel->setPalette( p );
 
-    hbox = new KHBox( this );
-    layout()->addWidget( hbox );
+    hbox = new BoxWidget( false, this );
 
-    KPushButton *button = new KPushButton( KStandardGuiItem::close(), hbox );
-    button->setObjectName( "closeButton" );
+    QPushButton *button = new QPushButton( QStringLiteral( "closeButton" ), hbox );
     connect( button, &QAbstractButton::clicked, this, &LongMessageWidget::close );
 
     reposition();

commit 648155d5d4ccc9ce39a1a44067e5564d7db28337

LongMessageWidget: fix Close button
Broken by commit d78f666e58de146db7ff2d940a00b173583542f4, which
replaced the KStandardGuiItem usage with a static (untranslatable!)
text...

src/statusbar/LongMessageWidget.cpp
@@ -19,6 +19,8 @@
 
 #include "core/support/Debug.h"
 
+#include <KStandardGuiItem>
+
 #include <QPushButton>
 
 #include <QLabel>
@@ -66,7 +68,8 @@ LongMessageWidget::LongMessageWidget( const QString &message )
 
     hbox = new BoxWidget( false, this );
 
-    QPushButton *button = new QPushButton( QStringLiteral( "closeButton" ), hbox );
+    QPushButton *button = new QPushButton( hbox );
+    KStandardGuiItem::apply( button, KStandardGuiItem::Close );
     connect( button, &QAbstractButton::clicked, this, &LongMessageWidget::close );
 
     reposition();