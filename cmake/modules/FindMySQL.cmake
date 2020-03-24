4337b3ef6e9ed30606fd06e15fd9ca350b1da01b
Andreas Sturmlechner <asturm@gentoo.org>
2020-03-24 23:33:21 +0100

    FindMySQL.cmake: Split out MySQL Embedded detection into FindMySQLe.cmake
    - MySQLe is pretty much dead now
    - If MySQLe is not found, we will not attempt to link MYSQLE_LIBRARIES anymore
    - Move ZLIB detection behind MySQLe conditional
    - WITH_MYSQL_EMBEDDED is now obsolete
    - Drop superfluous set_package_properties if defined in Find*.cmake module
    
    Thanks-to: Wolfgang Bauer <wbauer@tmo.at>
    
    Differential Revision: https://phabricator.kde.org/D28284

--- cmake/modules/FindMySQL.cmake
+++ cmake/modules/FindMySQL.cmake
@@ -1,11 +1,9 @@
-# - Try to find MySQL / MySQL Embedded library
+# - Try to find MySQL library
 # Find the MySQL includes and client library
 # This module defines
 #  MYSQL_INCLUDE_DIR, where to find mysql.h
 #  MYSQL_LIBRARIES, the libraries needed to use MySQL.
-#  MYSQL_EMBEDDED_LIBRARIES, the libraries needed to use MySQL Embedded.
 #  MySQL_FOUND, If false, do not try to use MySQL.
-#  MySQL_Embedded_FOUND, If false, do not try to use MySQL Embedded.
 
 # Copyright (c) 2006-2018, Jarosław Staniek <staniek@kde.org>
 #
@@ -30,22 +28,6 @@ if(MYSQLCONFIG_EXECUTABLE)
         OUTPUT_VARIABLE MYSQL_LIBRARIES
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-
-    execute_process(
-        COMMAND ${MYSQLCONFIG_EXECUTABLE} --libmysqld-libs
-        RESULT_VARIABLE MC_return_embedded
-        OUTPUT_VARIABLE MC_MYSQL_EMBEDDED_LIBRARIES
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-
-    if(NOT MC_MYSQL_EMBEDDED_LIBRARIES OR NOT "${MC_return_embedded}" STREQUAL "0")
-        # At least on OpenSUSE and FreeBSD --libmysql-libs doesn't exist, so we just use
-        # MYSQL_LIBRARIES for that. We'll see if that's enough when testing
-        # below.
-        set(MYSQL_EMBEDDED_LIBRARIES ${MYSQL_LIBRARIES})
-    else()
-        set(MYSQL_EMBEDDED_LIBRARIES ${MC_MYSQL_EMBEDDED_LIBRARIES})
-    endif()
 endif()
 
 # Try searching manually via find_path/find_library,  possibly with hints
@@ -65,7 +47,7 @@ find_path(MYSQL_INCLUDE_DIR mysql.h
     mysql
 )
 
-if(NOT MYSQL_LIBRARIES AND NOT MYSQL_EMBEDDED_LIBRARIES)
+if(NOT MYSQL_LIBRARIES)
     find_library(MYSQL_LIBRARIES NAMES mysqlclient
         PATHS
             $ENV{MYSQL_DIR}/libmysql_r/.libs
@@ -76,36 +58,10 @@ if(NOT MYSQL_LIBRARIES AND NOT MYSQL_EMBEDDED_LIBRARIES)
         PATH_SUFFIXES
             mysql
    )
-
-    find_library(MYSQL_EMBEDDED_LIBRARIES NAMES mysqld libmysqld
-        PATHS
-            $ENV{MYSQL_DIR}/libmysql_r/.libs
-            $ENV{MYSQL_DIR}/lib
-            $ENV{MYSQL_DIR}/lib/mysql
-            ${PC_MYSQL_LIBDIR}
-            ${PC_MYSQL_LIBRARY_DIRS}
-        PATH_SUFFIXES
-            mysql
-    )
 endif()
 
 if(PC_MYSQL_VERSION)
     set(MySQL_VERSION_STRING ${PC_MYSQL_VERSION})
-endif()
-
-if(MYSQL_EMBEDDED_LIBRARIES)
-    # libmysqld on FreeBSD apparently doesn't properly report what libraries
-    # it likes to link with, libmysqlclient does though.
-    #if(${CMAKE_HOST_SYSTEM_NAME} MATCHES "FreeBSD")
-    #    string(REGEX REPLACE "-lmysqlclient" "-lmysqld" _mysql_libs ${MYSQL_LIBRARIES})
-    #    string(STRIP ${_mysql_libs} _mysql_libs)
-    #    set(MYSQL_EMBEDDED_LIBRARIES ${_mysql_libs})
-    #endif()
-    cmake_push_check_state()
-    set(CMAKE_REQUIRED_INCLUDES ${MYSQL_INCLUDE_DIR})
-    set(CMAKE_REQUIRED_LIBRARIES ${MYSQL_EMBEDDED_LIBRARIES})
-    check_cxx_source_compiles( "#include <mysql.h>\nint main() { int i = MYSQL_OPT_USE_EMBEDDED_CONNECTION; }" HAVE_MYSQL_OPT_EMBEDDED_CONNECTION )
-    cmake_pop_check_state()
 endif()
 
 include(FindPackageHandleStandardArgs)
@@ -115,14 +71,9 @@ find_package_handle_standard_args(MySQL
     VERSION_VAR MySQL_VERSION_STRING
 )
 
-find_package_handle_standard_args(MySQL_Embedded
-    REQUIRED_VARS MYSQL_EMBEDDED_LIBRARIES MYSQL_INCLUDE_DIR HAVE_MYSQL_OPT_EMBEDDED_CONNECTION
-)
+mark_as_advanced(MYSQL_INCLUDE_DIR MYSQL_LIBRARIES)
 
-mark_as_advanced(MYSQL_INCLUDE_DIR MYSQL_LIBRARIES
-    MYSQL_EMBEDDED_LIBRARIES HAVE_MYSQL_OPT_EMBEDDED_CONNECTION
-)
 set_package_properties(MySQL PROPERTIES
     DESCRIPTION "MySQL Client Library (libmysqlclient)"
-    URL "http://www.mysql.com"
+    URL "https://www.mysql.com"
 )
