99b74f165328a82266976211c972aab7e00f544a
Robert Marshall <robert@capuchin.co.uk>
2020-06-30 15:00:21 +0100

    Fix compilation error for older Qt versions

--- src/scripting/scriptengine/AmarokCollectionViewScript.cpp
+++ src/scripting/scriptengine/AmarokCollectionViewScript.cpp
@@ -219,7 +219,7 @@ AmarokCollectionViewScript::setLevel( int level, int type )
     /* TODO - Use commented code once QT versions >= 5.12
     m_engine->throwError( QJSValue::TypeError, QStringLiteral("Invalid category!") );
     */
-    return m_engine->evaluate("throw new TypeError('Invalid category!')");
+    m_engine->evaluate("throw new TypeError('Invalid category!')");
 }
 
 void
