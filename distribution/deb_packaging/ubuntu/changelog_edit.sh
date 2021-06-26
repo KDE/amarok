14ad6697a8af2442d07e8f344ebb2ac5a8c4e6a9
Pedro de Carvalho Gomes <pedrogomes81@gmail.com>
2021-06-26 15:05:40 +0200

    Release of v2.9.71+1SNAPSHOT20210626150430+0200

--- distribution/deb_packaging/ubuntu/changelog_edit.sh
+++ distribution/deb_packaging/ubuntu/changelog_edit.sh
@@ -2,7 +2,7 @@
 DATENOW=$(date "+%s"); for i in 18.04_bionic 19.10_eoan 20.04_focal 20.10_groovy 21.04_hirsute; do version=$(echo $i|cut -d_ -f1); code=$(echo $i|cut -d_ -f2); cat << EOF > ${version}/new_cl_entry; cat ${version}/new_cl_entry ${version}/debian/changelog > ${version}/new_cl; mv ${version}/new_cl ${version}/debian/changelog; rm ${version}/new_cl*; done
 amarok (2:2.9.71+1SNAPSHOT$(date -d "@${DATENOW}" +"%Y%m%d%H%M%S%z")-0ubuntu1ppa1~ubuntu${version}.1) ${code}; urgency=medium
 
-  * Support Ubuntu 21.04 hirsute
+  * Fix stall when file not at playlist
 
  -- Pedro de Carvalho Gomes <pedrogomes81@gmail.com>  $(date -R -d "@${DATENOW}")
 
