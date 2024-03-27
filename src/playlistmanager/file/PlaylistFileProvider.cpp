49d45cd8470c99c7749bdf4b59ff1e54895f2e01
Tuomas Nurmi <tuomas@norsumanageri.org>
2024-03-27 21:01:43 +0200

    Fix a potential bug in saved playlist extensions.
    Uncovered by the tests that were previously disabled. Apparently
    QUrl::fromUserInput behaviour changed between Qt4 and Qt5. In Qt5, with
    default options it only returns a path if such local file exists, causing
    problems anywhere where a new file was being created. I went through the
    uses of the fromUserInput call, and it seems that this was the only place
    where it was used on possibly non-existant files.

--- src/playlistmanager/file/PlaylistFileProvider.cpp
+++ src/playlistmanager/file/PlaylistFileProvider.cpp
@@ -139,7 +139,7 @@ PlaylistFileProvider::save( const Meta::TrackList &tracks, const QString &name )
     filename.replace( QLatin1Char('/'), QLatin1Char('-') );
     filename.replace( QLatin1Char('\\'), QLatin1Char('-') );
 
-    Playlists::PlaylistFormat format = Playlists::getFormat( QUrl::fromUserInput(filename) );
+    Playlists::PlaylistFormat format = Playlists::getFormat( QUrl::fromUserInput( filename, QString(), QUrl::AssumeLocalFile ) );
     if( format == Playlists::Unknown ) // maybe the name just had a dot in it. We just add .xspf
     {
         format = Playlists::XSPF;
