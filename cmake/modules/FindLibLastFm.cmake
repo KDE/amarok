cec461773ff7f9a0ecc4660d60af9e1e71e7acd4
Tuomas Nurmi <tuomas@norsumanageri.org>
2025-02-21 20:53:32 +0200

    Bump version, drop Qt5/KF5 support in build system

--- cmake/modules/FindLibLastFm.cmake
+++ cmake/modules/FindLibLastFm.cmake
@@ -7,45 +7,24 @@
 # LIBLASTFM_VERSION, version of found liblastfm as a string, e.g "0.3"
 
 
-if( BUILD_WITH_QT6 )
-    find_path(LIBLASTFM_INCLUDE_DIR NAMES global.h
-    HINTS
-    ~/usr/include
-    /opt/local/include
-    /usr/include
-    /usr/local/include
-    PATH_SUFFIXES lastfm6
-    )
+find_path(LIBLASTFM_INCLUDE_DIR NAMES global.h
+HINTS
+~/usr/include
+/opt/local/include
+/usr/include
+/usr/local/include
+PATH_SUFFIXES lastfm6
+)
 
-    find_library( LIBLASTFM_LIBRARY NAMES lastfm6
-        PATHS
-        ~/usr/lib
-    /opt/local/lib
-    /usr/lib
-    /usr/lib64
-    /usr/local/lib
-    /usr/local/lib64
-    )
-else()
-    find_path(LIBLASTFM_INCLUDE_DIR NAMES global.h
-    HINTS
-    ~/usr/include
-    /opt/local/include
-    /usr/include
-    /usr/local/include
-    PATH_SUFFIXES lastfm5
-    )
-
-    find_library( LIBLASTFM_LIBRARY NAMES lastfm5
-        PATHS
-        ~/usr/lib
-    /opt/local/lib
-    /usr/lib
-    /usr/lib64
-    /usr/local/lib
-    /usr/local/lib64
-    )
-endif()
+find_library( LIBLASTFM_LIBRARY NAMES lastfm6
+    PATHS
+    ~/usr/lib
+/opt/local/lib
+/usr/lib
+/usr/lib64
+/usr/local/lib
+/usr/local/lib64
+)
 
 
 if(LIBLASTFM_INCLUDE_DIR AND LIBLASTFM_LIBRARY)
