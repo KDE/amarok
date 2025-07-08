2a28ed8206b295b65fe2b7a35f6375cc3bd806e9
Tuomas Nurmi <tuomas@norsumanageri.org>
2025-07-08 11:35:24 +0300

    Release preparations for 3.3.0

--- src/PluginManager.cpp
+++ src/PluginManager.cpp
@@ -41,7 +41,7 @@
  *
  *  This must match the desktop files.
  */
-const int Plugins::PluginManager::s_pluginFrameworkVersion = 84;
+const int Plugins::PluginManager::s_pluginFrameworkVersion = 85;
 Plugins::PluginManager* Plugins::PluginManager::s_instance = nullptr;
 
 Plugins::PluginManager*
