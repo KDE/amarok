b6943178c8b10a9f6c8bce7e236f6334d2a1c88b
Martin Bednár <martin@serafean.cz>
2026-01-01 18:47:59 +0100

    Use QExplicitlySharedDataPointer as base for AmarokSharedPointer
    AmarokSharedPointer essentially reimplemented part of
    ExplicitlySharedDataPointer, so use that as a base, and add
    functionality we need on top.

--- src/AmarokSharedPointer.h
+++ src/AmarokSharedPointer.h
@@ -20,84 +20,45 @@
 #ifndef AMAROKSHAREDPOINTER_H
 #define AMAROKSHAREDPOINTER_H
 
-#include <QObject>
-#include <QSharedData>
 #include <QDebug>
+#include <QExplicitlySharedDataPointer>
 
+
+// Simple wrapper to make QExplicitlySharedDataPointer more convenient
 template<class T>
-class AmarokSharedPointer
+class AmarokSharedPointer : public QExplicitlySharedDataPointer<T>
 {
 public:
-    inline AmarokSharedPointer() : d(nullptr) {}
-    inline explicit AmarokSharedPointer(T *t) : d(t) { if (d) d->ref.ref(); }
-    inline AmarokSharedPointer(const AmarokSharedPointer& other) : d(other.d) { if (d) d->ref.ref(); }
-    inline ~AmarokSharedPointer() { if (d && !d->ref.deref()) delete d; }
+    AmarokSharedPointer() = default;
+    ~AmarokSharedPointer() = default;
+    explicit AmarokSharedPointer(T *data) noexcept: QExplicitlySharedDataPointer<T>(data){};
+    AmarokSharedPointer(const AmarokSharedPointer &) = default;
+    AmarokSharedPointer(AmarokSharedPointer &&) = default;
 
-    AmarokSharedPointer& operator=(const AmarokSharedPointer& other)
-    {
-        if (d != other.d)
-        {
-            if (d && !d->ref.deref())
-                delete d;
-            d = other.d;
-            if (d)
-                d->ref.ref();
-        }
-        return *this;
-    }
-    AmarokSharedPointer& operator=(T *t)
-    {
-        if (d != t)
-        {
-            if (d && !d->ref.deref())
-                delete d;
-            d = t;
-            if (d)
-                d->ref.ref();
-        }
-        return *this;
-    }
-    inline bool operator==(const AmarokSharedPointer& other) const { return d == other.d; }
-    inline bool operator!=(const AmarokSharedPointer& other) const { return d != other.d; }
-    inline bool operator<(const AmarokSharedPointer& other) const { return std::less<const T*>()(d, other.d); }
-    inline const T& operator*() const { Q_ASSERT(d); return *d; }
-    inline T& operator*() { Q_ASSERT(d); return *d; }
-    inline const T* operator->() const { Q_ASSERT(d); return d; }
-    inline T* operator->() { Q_ASSERT(d); return d; }
-    explicit inline operator bool() const { return ( d != nullptr ); }
+    AmarokSharedPointer &operator=(const AmarokSharedPointer &) = default;
+    AmarokSharedPointer &operator=(AmarokSharedPointer &&) = default;
+    AmarokSharedPointer &operator=(T *t) noexcept { this->QExplicitlySharedDataPointer<T>::operator=(t); return *this;}
 
-    inline bool isNull() const { return d == nullptr; }
-    inline int count() const { return d ? d->ref.loadRelaxed() : 0; }
-    inline T* data() const { return d; }
-    inline void reset() { if (d && !d->ref.deref()) delete d; d = nullptr; }
-
+    explicit inline operator bool() const noexcept { return this->QExplicitlySharedDataPointer<T>::operator bool();}
+    inline bool isNull() const noexcept { return this->QExplicitlySharedDataPointer<T>::operator!(); }
+    inline int count() const noexcept { return this->constData() ? this->constData()->ref.loadRelaxed() : 0 ;}
     template <class U>
     static AmarokSharedPointer<T> staticCast(const AmarokSharedPointer<U>& o)
     {
-        return AmarokSharedPointer<T>(static_cast<T *>(o.data()));
+        return AmarokSharedPointer<T>(static_cast<T*>(o.data()));
     }
     template <class U>
     static AmarokSharedPointer<T> dynamicCast(const AmarokSharedPointer<U>& o)
     {
-        return AmarokSharedPointer<T>(dynamic_cast<T *>(o.data()));
+        return AmarokSharedPointer<T>(dynamic_cast<T*>(o.data()));
     }
-    template <class U>
-    static AmarokSharedPointer<T> qobjectCast(const AmarokSharedPointer<U>& o)
-    {
-        return AmarokSharedPointer<T>(qobject_cast<T *>(o.data()));
-    }
-
-
-
-private:
-    T *d;
 };
+
 template<typename T>
 QDebug operator<<(QDebug d, const AmarokSharedPointer<T> &ptr){
     d << (uintptr_t) ptr.data();
     return d;
 }
-template<class T>
-inline uint qHash( const AmarokSharedPointer<T> &p, uint seed ) { return qHash( p.data(), seed ); }
+
 
 #endif // AMAROKSHAREDPOINTER_H
