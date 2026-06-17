bbdd40eeccf0ebc1eb6adc6cac8707c45861d71a
Tuomas Nurmi <tuomas@norsumanageri.org>
2026-06-17 11:11:06 +0300

    Release preparations for 3.3.3

--- shared/Version.h
+++ shared/Version.h
@@ -18,6 +18,6 @@
 #define AMAROK_VERSION_H
 
 /// Update this when necessary
-#define AMAROK_VERSION "3.3.2-git"
+#define AMAROK_VERSION "3.3.3"
 
 #endif  // End include guard
--- src/PluginManager.cpp
+++ src/PluginManager.cpp
@@ -41,7 +41,7 @@
  *
  *  This must match the desktop files.
  */
-const int Plugins::PluginManager::s_pluginFrameworkVersion = 87;
+const int Plugins::PluginManager::s_pluginFrameworkVersion = 88;
 Plugins::PluginManager* Plugins::PluginManager::s_instance = nullptr;
 
 Plugins::PluginManager*
